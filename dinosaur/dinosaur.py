0762857ee2fd3727add67ea282659d43f5e36ebd
Andy Sigler <andrewsigler1@gmail.com>
2016-12-12 15:08:29 +0800

    setup platemap with list

--- dinosaur/dinosaur.py
+++ dinosaur/dinosaur.py
@@ -1,92 +1,59 @@
-# Dinosaur
+from opentrons import robot, containers, instruments
 
-from opentrons import Robot, containers, instruments
-from itertools import chain
-
-robot = Robot()
-
-p200rack = containers.load(
-    'tiprack-200ul',  
-    'B1',             
-    'tiprack'         
-)
-trough = containers.load(
-    'trough-12row',
-    'C1',
-    'trough'
-)
-plate = containers.load(
-    '96-PCR-flat',
-    'D1',
-    'plate'
-)
-trash = containers.load(
-    'point',
-    'D2',
-    'trash'
-)
+p200rack = containers.load('tiprack-200ul', 'B1')
+trough = containers.load('trough-12row', 'C1')
+plate = containers.load('96-PCR-flat', 'D1')
+trash = containers.load('point', 'D2')
 
 p200 = instruments.Pipette(
-    name="p200",
-    trash_container=trash,
-    tip_racks=[p200rack],
-    min_volume=20,
     axis="b",
-    channels=1
+    max_volume=200,
+    trash_container=trash,
+    tip_racks=[p200rack]
 )
 
-p200.set_max_volume(200)
+sources = {
+    1: {
+        'source': trough['A1'], # blue food coloring
+        'volume': 50
+    },
+    2: {
+        'source': trough['A2'], # green food coloring
+        'volume': 50
+    }
+}
+
+_ = None
+
+plate_image = [
+    _, _, _, _, _, _, 2, 1,  # 12
+    _, _, _, _, 2, 2, 1, _,  # 11
+    _, _, _, _, 2, 1, _, _,  # 10
+    _, _, 2, 2, 1, 1, 1, 1,  # 9
+    _, _, 2, 1, 1, 1, 1, 1,  # 8
+    2, 2, 1, 1, 1, 1, 1, _,  # 7
+    _, 2, 1, 1, 1, 1, 1, _,  # 6
+    2, 2, 1, 1, 1, 1, 1, _,  # 5
+    _, 2, 1, 1, 1, 1, 1, 1,  # 4
+    _, _, 2, 1, 1, 1, 1, 1,  # 3
+    _, _, _, 1, 1, _, _, _,  # 2
+    _, _, _, 1, 1, _, _, _   # 1
+]
+#   A  B  C  D  E  F  G  H
+
+def spread_sample(key):
+    
 
 
-# In[4]:
 
-volume = 200
-blue = trough['A1']
-green = trough['A2']
 
-# deposit to all blue wells
-p200.pick_up_tip(p200rack['A1'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['D1']).dispense(50, plate['E1']).dispense(50, plate['D2']).dispense(50, plate['E2'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['D3']).dispense(50, plate['E3']).dispense(50, plate['F3']).dispense(50, plate['G3'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['H3']).dispense(50, plate['C4']).dispense(50, plate['D4']).dispense(50, plate['E4'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['F4']).dispense(50, plate['G4']).dispense(50, plate['H4']).dispense(50, plate['C5'])
-p200.drop_tip(p200rack['A1'])
 
-p200.pick_up_tip(p200rack['B1'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['D5']).dispense(50, plate['E5']).dispense(50, plate['F5']).dispense(50, plate['G5'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['C6']).dispense(50, plate['D6']).dispense(50, plate['E6']).dispense(50, plate['F6'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['G6']).dispense(50, plate['C7']).dispense(50, plate['D7']).dispense(50, plate['E7'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['F7']).dispense(50, plate['G7']).dispense(50, plate['D8']).dispense(50, plate['E8'])
-p200.drop_tip(p200rack['B1'])
 
-p200.pick_up_tip(p200rack['C1'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['F8']).dispense(50, plate['G8']).dispense(50, plate['H8']).dispense(50, plate['E9'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['F9']).dispense(50, plate['G9']).dispense(50, plate['H9']).dispense(50, plate['F10'])
-p200.aspirate(volume, blue)
-p200.dispense(50, plate['G11']).dispense(50, plate['H12']).dispense(50, plate['F3']).dispense(50, plate['G3'])
-p200.drop_tip(p200rack['C1'])
-              
-# deposit to all green wells
-p200.pick_up_tip(p200rack['D1'])
-p200.aspirate(volume, green)
-p200.dispense(50, plate['C3']).dispense(50, plate['B4']).dispense(50, plate['A5']).dispense(50, plate['B5'])
-p200.aspirate(volume, green)
-p200.dispense(50, plate['B6']).dispense(50, plate['A7']).dispense(50, plate['B7']).dispense(50, plate['C8'])              
-p200.aspirate(volume, green)
-p200.dispense(50, plate['C9']).dispense(50, plate['D9']).dispense(50, plate['E10']).dispense(50, plate['E11'])              
-p200.aspirate(volume, green)
-p200.dispense(50, plate['F11']).dispense(50, plate['G12'])
-p200.drop_tip(p200rack['D1'])
+
+
+
+
+
 
 
 
