ae19bfcf56b1ec5c6b0a06b8da2ca1a8597a61d3
Andy Sigler <andrewsigler1@gmail.com>
2016-12-18 20:49:49 -0500

    smalls edits

--- Basics/Tips/tips.py
+++ Basics/Tips/tips.py
@@ -12,7 +12,7 @@ tip_rack_1 = containers.load('tiprack-200ul', 'B1')
 tip_rack_2 = containers.load('tiprack-200ul', 'B2')
 tip_rack_3 = containers.load('tiprack-200ul', 'B3')
 
-pipette = instruments.Pipette(axis='b')
+pipette = instruments.Pipette(axis='a')
 
 pipette.pick_up_tip(tip_rack_1['A1'])  # pick up <tip_rack_1:A1>
 pipette.drop_tip(tip_rack_1['A1'])     # drop tip <tip_rack_1:A1>
@@ -32,18 +32,18 @@ pipette.drop_tip()                     # drop tip at current position
     by attaching containers to a pipette
 """
 
-pipette_with_tips = instruments.Pipette(
-    axis='a',
+pipette_B = instruments.Pipette(
+    axis='b',
     tip_racks=[tip_rack_2, tip_rack_3],
     trash_container=trash
 )
 
-pipette_with_tips.pick_up_tip()  # picks up tip_rack_2:A1
-pipette_with_tips.return_tip()
-pipette_with_tips.pick_up_tip()  # picks up tip_rack_2:A2
-pipette_with_tips.drop_tip()     # automatically drops in trash
+pipette_B.pick_up_tip()  # picks up tip_rack_2:A1
+pipette_B.return_tip()
+pipette_B.pick_up_tip()  # picks up tip_rack_2:A2
+pipette_B.drop_tip()     # automatically drops in trash
 
 # use loop to pick up tips tip_rack_2:A3 through tip_rack_3:H12
 for i in range(94 + 96):
-    pipette_with_tips.pick_up_tip()
-    pipette_with_tips.return_tip()
+    pipette_B.pick_up_tip()
+    pipette_B.return_tip()
