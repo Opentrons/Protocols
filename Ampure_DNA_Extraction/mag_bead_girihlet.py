5f6259b95c4adeb217c7cc7b9081fdd1b68facd4
Andy Sigler <andrewsigler1@gmail.com>
2016-12-20 21:35:43 -0500

    starts refactors

--- Ampure_DNA_Extraction/mag_bead_girihlet.py
+++ Ampure_DNA_Extraction/mag_bead_girihlet.py
@@ -1,92 +1,23 @@
-
-# coding: utf-8
-
-# In[1]:
-
-# # This cell loads in the API
-# # and then prints out the serial ports
-
-
 from opentrons import robot, containers, instruments
-from itertools import chain
 import math
 
-# robot = Robot()
-# # robot.get_serial_ports_list()
-
-
-# In[ ]:
-
-#CONNECT TO ROBOT
-#either virtual or USB, then home
-
-#connect to OT-One over USB serial
-#robot.connect('/dev/tty.usbmodem1421')
-
-#connect to virtual smoothie
-#robot.connect()
-
-#robot.home()
-
-
-# In[2]:
-
-# robot.reset()
-##### CONTAINERS AND PIPETTE SET-UP
-
-#containers
-
-p10rack = containers.load(
-    'tiprack-10ul',  # container type from library
-    'A1',             # slot on deck
-    'p10rack'         # calibration reference for 1.2 compatibility
-)
-
-p200rack = containers.load(
-    'tiprack-200ul',  # container type from library
-    'A1',             # slot on deck
-    'p200rack'         # calibration reference for 1.2 compatibility
-)
-
-output_plate = containers.load(
-    '96-PCR-flat',
-    'B1',
-    'output_plate'
-)
-
-mag_plate = containers.load(
-    '96-PCR-tall',
-    'C1',
-    'mag_plate'
-)
-
-trough = containers.load(
-    'trough-12row',
-    'B1',
-    'trough'
-)
-
-trash = containers.load(
-    'point', 
-    'A2', 
-    'trash'
-)
-
-#pipette
+p10rack = containers.load('tiprack-10ul', 'A1')
+p200rack = containers.load('tiprack-200ul', 'A1')
+output_plate = containers.load('96-PCR-flat', 'B1')
+mag_plate = containers.load('96-PCR-tall', 'C1')
+trough = containers.load('trough-12row', 'B1')
+trash = containers.load('point', 'A2')
 
 p10 = instruments.Pipette(
-    name="p10",
-    min_volume=1,
     axis="b",
+    max_volume=10,
     trash_container=trash,
-    tip_racks=[p10rack],
-    channels=1
+    tip_racks=[p10rack]
 )
 
 p200 = instruments.Pipette(
-    name="p200",
-    min_volume=20,
     axis="a",
+    max_volume=200,
     trash_container=trash,
     tip_racks=[p200rack],
     channels=8
@@ -94,11 +25,6 @@ p200 = instruments.Pipette(
 
 mag_deck = instruments.Magbead()
 
-p10.set_max_volume(10)  # volume calibration, can be called whenever you want 
-p200.set_max_volume(200)
-
-
-# In[3]:
 
 # Variables set by user
 
@@ -145,85 +71,80 @@ final_volume = 20
 
 # Get number of samples to use in loop
 num_samples = len(samples)
-num_rows = math.ceil(num_samples/8)
+num_rows = math.ceil(num_samples / 8)
 
 
-# In[6]:
+def distribute_volumes(source, targets, volumes, pipette):
+    pipette.pick_up_tip()
+    for i, well in enumerate(targets):
+        if pipette.current_volume < volumes[i]:
+
+            # find the amount to aspirate next based on future volumes
+            aspirate_volume = volumes[i]
+            n = i + 1
+            while n >= len(volumes):
+                if pipette.current_volume + volumes[n] > pipette.max_volume:
+                    break
+                aspirate_volume += volumes[n]
+                n += 1
+
+            pipette.aspirate(aspirate_volume).delay(1).touch_tip()
+        pipette.dispense(volumes[i], well).touch_tip()
+    pipette.blow_out().drop_tip()
+
 
 # Add beads to samples based on DNA volume
-for i in range(num_samples):
-    mag_vol = ((samples[i])*8)/10  # calculate volume of mag bead to add based on sample volume
-    well = mag_plate[i]  # set location of where DNA is located
-    p10.pick_up_tip().aspirate(mag_vol, mag_beads_stock)  # aspirate correct volume from mag bead stock
-    p10.dispense(well.top()).blow_out() # dispense mag beads to top of tube, blow out
-    p10.mix(5, mag_vol, well.bottom())  # mix at bottom of well
-    p10.drop_tip() # drop tip in trash
-
-
-# In[7]:
+mag_vols = [(s * 8) / 10 for s in samples]
+distribute_volumes(mag_beads_stock, mag_plate[:len(samples)], mag_vols, p10)
 
 # Incubate at room temp and engage mag beads
 
-# home robot
 robot.home(enqueue=True)
+mag_deck.delay(mag_incubation_time).engage().delay(mag_deck_delay)
 
-# incubate at room temp for 15 minutes
-p10.delay(mag_incubation_time)
-
-# engage the magnet
-mag_deck.engage()
-p10.delay(mag_deck_delay)
-
-
-# In[10]:
-
-#Remove supernatent from samples and deposit in trash
-
-for i in range(num_samples):  # for all samples
-    well = (mag_plate[i].bottom(1))  # set aspirate position for just above the bottom of the plate
-    vol = (((samples[i])*8)/10) + samples[i]  # set volume to DNA + mag bead volume
-    
-    if vol > p10.max_volume:
-        volhalf = vol/2
-        p10.pick_up_tip().aspirate(volhalf, well, rate=0.5).blow_out(trash)
-        p10.aspirate(volhalf, well, rate=0.5).drop_tip()  # use a new tip for each well
-    else:
-        p10.pick_up_tip().aspirate(vol, well, rate=0.5).drop_tip()
+# Remove supernatent from samples and deposit in trash
 
 # pull up at a slower rate than usual in order to not disturbe magnets
-# This would be easier with a set volume - 
-# do they have any idea of how much supernatent they actually pick up per added mag bead volume?
+# This would be easier with a set volume
+# do they have any idea of how much supernatent they actually
+# pick up per added mag bead volume?
+
+for i in range(len(samples)):
+    well = mag_plate[i].bottom(1)
+    vol = (((samples[i]) * 8) / 10) + samples[i]
+    p10.pick_up_tip()
+    while vol > 0:
+        aspirate_volume = min(p10.max_volume, vol)
+        p10.aspirate(aspirate_volume, well, rate=0.5)
+        p10.blow_out(trash)
+        vol -= aspirate_volume
+    p10.drop_tip()
 
 
-# In[11]:
-
-# dispense ethanol based on wash volume
-
-new_volume = ethanol_volume/2 # divide ethanol volume in two to leave room for air
-air_volume = p200.max_volume - new_volume # air volume is difference between ethanol and max volume
-    
-wash_counter = 0 # set wash counter to 0
-
-
-while wash_counter < ethanol_washes: # loop through for as many washes as desired
-    
+for i in range(ethanol_washes):
     for i in range(num_rows):
-        well = mag_plate.rows[i].top() # deposit ethanol to top of mag bead plate
-        air = ethanol_stock.top(2) # pull air from right above ethanol stock to minimize loss
-        p200.pick_up_tip().aspirate(new_volume, ethanol_stock).aspirate(air_volume, air).dispense(well) # add ethanol to well
-        p200.aspirate(new_volume, ethanol_stock).aspirate(air_volume, air).dispense(well) # add ethanol to well
-        
-        p200.delay(ethanol_delay) # delay
-        
-        well = mag_plate[i].bottom(1) # pull ethanol from bottom of mag bead plate
-        air = mag_plate[i].top() # pull air from right above mag bead plate
-        p200.aspirate(new_volume, well).aspirate(air_volume, air).dispense(trash)
-        p200.aspirate(new_volume, well).aspirate(air_volume, air).drop_tip()     
-        
-    wash_counter += 1
+        well = mag_plate.rows[i].top()
+        p200.pick_up_tip()
 
+        p200.aspirate(ethanol_volume / 2, ethanol_stock)
+        p200.aspirate(ethanol_stock.top(2))
+        p200.dispense(well)
 
-# In[12]:
+        p200.aspirate(ethanol_volume / 2, ethanol_stock)
+        p200.aspirate(ethanol_stock.top(2))
+        p200.dispense(well)
+
+        p200.delay(ethanol_delay)
+
+        well = mag_plate[i].bottom(1)
+
+        p200.aspirate(ethanol_volume / 2, well)
+        p200.aspirate(mag_plate[i].top())
+        p200.dispense(trash)
+
+        p200.aspirate(ethanol_volume / 2, well)
+        p200.aspirate(mag_plate[i].top())
+        p200.drop_tip()
 
 # Disengage magnets, resuspend samples, delay 5 min, engage magents 5 min
 
@@ -242,16 +163,8 @@ mag_deck.engage()
 p200.delay(mag_deck_buffer_delay)
 
 
-# In[13]:
-
 # remove 20 uL from mag bead deck and send to 
 for i in range(num_rows):
     well = mag_plate.rows[i]
     output = output_plate.rows[i]
     p200.pick_up_tip().aspirate(final_volume, well).dispense(output).drop_tip()
-
-
-# In[ ]:
-
-
-
--- Antibody_Mapping_384/Tubes+to+384+Well+Plate+%28Notable%29.py
+++ Antibody_Mapping_384/Tubes+to+384+Well+Plate+%28Notable%29.py
@@ -1,55 +1,19 @@
+from opentrons import containers, instruments
 
-# coding: utf-8
-
-# In[ ]:
-
-#!pip install --upgrade opentrons
-
-
-# In[1]:
-
-from opentrons import robot, containers, instruments
-
-
-# In[2]:
-
-
-p200rack = containers.load(
-    'tiprack-200ul', 
-    'A1',
-    'p200rack')
-
-trash = containers.load(
-    'point',
-    'A2',
-    'trash'
-)
-tube_rack = containers.load(
-    'tube-rack-2ml',
-    'C2',
-    'tube_rack'
-)
-plate = containers.load(
-    '384-plate',
-    'C1',
-    'plate'
-)
+p200rack = containers.load('tiprack-200ul', 'A1')
+trash = containers.load('point', 'A2')
+tube_rack = containers.load('tube-rack-2ml', 'C2')
+plate = containers.load('384-plate', 'C1')
 
 p200 = instruments.Pipette(
-        axis="b",
-        max_volume=200,
-        min_volume=20,
-        tip_racks=[p200rack],
-        trash_container=trash,
-        channels=1,
-        name="p200"
+    axis="b",
+    max_volume=200,
+    tip_racks=[p200rack],
+    trash_container=trash
 )
-
-
-# In[3]:
 
 # dispense 40 uL from tube to plate, for 24 tubes
 for i in range(24):
-    p200.pick_up_tip().aspirate(40, tube_rack[i]).dispense(plate[i]).drop_tip()
-
-
+    p200.pick_up_tip()
+    p200.aspirate(40, tube_rack[i]).dispense(plate[i])
+    p200.drop_tip()
