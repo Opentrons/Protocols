7e68e8765346eeade69ba6443f4b63910023fab2
Dagney Cooke <dagneycooke@Dagneys-Air.home>
2016-12-30 15:20:26 -0500

    changed to six sample protocols and added loops, throwing a weird error

--- Heat_Shock_Transformation/heat_shock_transformation.py
+++ Heat_Shock_Transformation/heat_shock_transformation.py
@@ -67,6 +67,7 @@ p200 = instruments.Pipette(
     trash_container=trash,
     tip_racks=[tiprack200],
     min_volume=50,
+    max_volume=200,
     axis="b",
     channels=1
 )
@@ -76,61 +77,53 @@ p10 = instruments.Pipette(
     trash_container=trash,
     tip_racks=[tiprack10],
     min_volume=1,
+    max_volume=10,
     axis="a",
     channels=1
 )
 
-p200.set_max_volume(200)  # volume calibration, can be called whenever you want
-p200L.set_max_volume(200)
-
 
 # In[ ]:
 
-DNA_delay = 1800
+num_samples = 6
+
+DNA_delay = 18000
 heat_shock_delay = 60
 cold_delay = 300
 
 DNA_vol = 2
 cell_vol = 25
-total_vol = DNA_volume + Cell_vol
+total_vol = 27
 LB_vol = 200
 
 # two sample protocol
 
 # add DNA from tube B to tube A in cold deck
-p10.pick_up_tip()
-p10.aspirate(DNA_vol, cold_deck['B1']).dispense(cold_deck['A1']).touch_tip().mix(3, 10, cold_deck['A1'])
-p10.drop_tip()
 
-p10.pick_up_tip()
-p10.aspirate(DNA_vol, cold_deck['B2']).dispense(cold_deck['A2']).touch_tip().mix(3, 10, cold_deck['A2'])
-p10.drop_tip()
+for i in range(num_samples):
+    p10.pick_up_tip()
+    p10.aspirate(DNA_vol, cold_deck.cols[i][1]).dispense(cold_deck.cols[i][0]).touch_tip()
+    p10.mix(3, 10, cold_deck.cols[i][0])
+    p10.drop_tip()
 
 # delay after adding DNA
 p10.delay(DNA_delay)
 
 # move dna/cells from cold deck to heat deck and then back
-p200.pick_up_tip()
-p200.aspirate(total_vol, cold_deck['A1']).dispense(heat_deck['A1']).touch_tip()
-p200.delay(heat_shock_delay)
-p200.aspirate(total_vol, heat_deck['A1']).dispense(cold_deck['A1']).touch_tip()
-p200.drop_tip()
-
-p200.pick_up_tip()
-p200.aspirate(total_vol, cold_deck['A2']).dispense(heat_deck['A2']).touch_tip()
-p200.delay(heat_shock_delay)
-p200.aspirate(total_vol, heat_deck['A2']).dispense(cold_deck['A2']).touch_tip()
-p200.drop_tip()
+for i in range(num_samples):
+    p200.pick_up_tip()
+    p200.aspirate(total_vol, cold_deck.cols[i][0]).dispense(heat_deck.cols[i][0]).touch_tip()
+    p200.delay(heat_shock_delay)
+    p200.aspirate(total_vol, heat_deck.cols[i][0]).dispense(cold_deck.cols[i][0]).touch_tip()
+    p200.drop_tip()
 
 # delay after heat shock
 p200.delay(cold_delay)
 
 # add LB to dna/cells
-p200.pick_up_tip()
-p200.aspirate(LB_vol, tube_rack['A1']).dispense(cold_deck['A1'])
-p200.drop_tip()
 
-p200.pick_up_tip()
-p200.aspirate(LB_vol, tube_rack['A2']).dispense(cold_deck['A2'])
-p200.drop_tip()
+for i in range(num_samples):
+    p200.pick_up_tip()
+    p200.aspirate(LB_vol, tube_rack['A1']).dispense(cold_deck.cols[i][0])
+    p200.drop_tip()
 
