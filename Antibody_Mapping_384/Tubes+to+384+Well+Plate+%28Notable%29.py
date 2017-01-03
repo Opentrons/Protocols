e5ad12c5c155f3fca672d8a9986b2b45313fee6b
Dagney Cooke <dagneycooke@Dagneys-Air.fios-router.home>
2017-01-03 10:26:51 -0500

    updated tube rack to 80 well instead of standard 2 ml rack

--- Antibody_Mapping_384/Tubes+to+384+Well+Plate+%28Notable%29.py
+++ Antibody_Mapping_384/Tubes+to+384+Well+Plate+%28Notable%29.py
@@ -1,18 +1,4 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-#!pip install --upgrade opentrons
-
-
-# In[1]:
-
 from opentrons import robot, containers, instruments
-
-
-# In[2]:
-
 
 p200rack = containers.load(
     'tiprack-200ul', 
@@ -27,7 +13,7 @@ trash = containers.load(
 tube_rack = containers.load(
     'tube-rack-2ml',
     'C2',
-    'tube_rack'
+    'tube-rack-80well'
 )
 plate = containers.load(
     '384-plate',
@@ -45,11 +31,6 @@ p200 = instruments.Pipette(
         name="p200"
 )
 
-
-# In[3]:
-
 # dispense 40 uL from tube to plate, for 24 tubes
 for i in range(24):
     p200.pick_up_tip().aspirate(40, tube_rack[i]).dispense(plate[i]).drop_tip()
-
-
