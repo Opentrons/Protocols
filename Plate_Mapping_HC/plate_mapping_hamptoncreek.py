4f28b88000242accbac8bf078fc05aa921af2971
andySigler <andrewsigler1@gmail.com>
2017-01-09 13:36:08 -0500

    fixes arguments to transfer commands

--- Plate_Mapping_HC/plate_mapping_hamptoncreek.py
+++ Plate_Mapping_HC/plate_mapping_hamptoncreek.py
@@ -23,10 +23,12 @@ p200_multi = instruments.Pipette(
 
 dest_plates = [plate2, plate3, plate4, plate5, plate6, plate7]
 
-# map 90 uL to all even rows of all 6 destination plates
-even_rows = [row for dest in dest_plates for row in dest.rows[0::2]]
-p200_multi.transfer(90, plate1.rows[0::2], even_rows)
+# map 90 uL to all even rows (0, 2, 4, etc.) of all 6 destination plates
+for i in range(0, 12, 2):
+    target_rows = [d.rows[i] for d in dest_plates]
+    p200_multi.transfer(90, plate1.rows[i], target_rows)
 
-# map 45 uL to all odd rows of all 6 destination plates
-odd_rows = [row for dest in dest_plates for row in dest.rows[1::2]]
-p200_multi.transfer(45, plate1.rows[1::2], odd_rows)
+# map 45 uL to all odd rows (1, 3, 5, etc.) of all 6 destination plates
+for i in range(1, 12, 2):
+    target_rows = [d.rows[i] for d in dest_plates]
+    p200_multi.transfer(45, plate1.rows[i], target_rows)
