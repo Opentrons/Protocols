e6ee40a91b229e9d23f8241bf6ec9c8f17c9885a
Jenny Vu <Jenny@Jennys-MBP.fios-router.home>
2017-02-15 16:01:36 -0500

    updated deck and corrected code

--- LVVWD_Sample_Distribution/lvvwd_tube_distribution.py
+++ LVVWD_Sample_Distribution/lvvwd_tube_distribution.py
@@ -1,26 +1,50 @@
 from opentrons import robot, containers, instruments
 
-p1000rack = containers.load('tiprack-1000ul', 'A1', 'p1000rack')
 
-vialrack = containers.load('wheaton_vial_rack', 'D1', 'vialrack')
+p1000rack = containers.load(
+    'tiprack-1000ul',
+    'A1',
+    'p1000rack'
+)
 
-plate = containers.load('96-deep-well', 'B2', 'plate')
+vialrack = containers.load(
+    'wheaton_vial_rack',
+    'C1',
+    'vialrack'
+)
 
-trash = containers.load('point', 'B1', 'trash')
+plate = containers.load(
+    '96-deep-well',
+    'B1',
+    'plate'
+)
+
+trash = containers.load(
+    'point',
+    'B2',
+    'trash'
+)
 
 p1000 = instruments.Pipette(
-	name="p1000",
+    name="p1000",
     trash_container=trash,
     tip_racks=[p1000rack],
     min_volume=100,
     max_volume=1000,
     axis="b",
-    channels=1)
+    channels=1
+)
+
+
+columns_list = []
+for column in plate.cols:
+    columns_list.append(iter(column))
     
+
 # Distribute 48 samples to 96 well plate
 for i in range(48):
-	wellone = plate.cols[i][0]
-    welltwo = plate.cols[i][1]
-	p1000.pick_up_tip().aspirate(600, plate[i])
-	p1000.dispense(300, wellone).dispense(300, welltwo)
-	p1000.drop_tip()
+    p1000.pick_up_tip()
+    p1000.aspirate(600, vialrack[i])
+    p1000.dispense(300, next(columns_list[i % 8]))
+    p1000.dispense(300, next(columns_list[i % 8]))
+    p1000.drop_tip()
