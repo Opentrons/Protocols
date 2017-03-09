6a9e69605489636a3a59ad3a0af3d068e703ba7e
jerrywan4 <jw3650@nyu.edu>
2017-03-09 10:14:57 -0500

    Add files via upload

--- RTPCR/RTPCR.py
+++ RTPCR/RTPCR.py
@@ -3,7 +3,7 @@ from opentrons import robot, containers, instruments
 p50rack = containers.load('tiprack-200ul', 'A1', 'p50rack')
 tuberack = containers.load('tube-rack-.75ml', 'D2', 'tuberack')
 trash = containers.load('point', 'B2', 'trash')
-plate = containers.load('96-well-flat', 'C1', 'plate')
+plate = containers.load('96-flat', 'C1', 'plate')
 
 p50 = instruments.Pipette(
     name="p50",
@@ -24,5 +24,5 @@ for i in range(12):
 for i in range(8):
     p50.pick_up_tip()
     for well in plate.cols[i]:
-        p50.aspirate(15, tuberack[i + 12]).dispense(well)
+        p50.aspirate(15, tuberack[i + 12]).dispense(well)o
     p50.drop_tip()
