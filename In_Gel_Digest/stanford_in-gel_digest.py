4e6415883bc6928b9d903c494806ce4f444f6de5
andySigler <andrewsigler1@gmail.com>
2017-03-21 10:20:34 -0400

    removes for loop

--- In_Gel_Digest/stanford_in-gel_digest.py
+++ In_Gel_Digest/stanford_in-gel_digest.py
@@ -2,38 +2,37 @@
 # Mass Spectrometry / Opentrons
 from opentrons import containers, instruments
 
+# add two tube racks
+cold_deck = containers.load('tube-rack-2ml', 'A1')
+tube_rack = containers.load('tube-rack-2ml', 'C1')
 
-tip_rack = containers.load('tiprack-200ul', 'D1', 'Tip Rack')
-waste = containers.load('point', 'D2', 'Trash')
-tube_rack = containers.load('tube-rack-2ml', 'C1', 'Tube Rack')
-cold_deck = containers.load('tube-rack-2ml', 'A1', 'Cold Deck')
-
+# use p200, with a tiprack and trash
+tip_rack = containers.load('tiprack-200ul', 'D1')
+waste = containers.load('point', 'D2')
 p200 = instruments.Pipette(
     axis="b",
     max_volume=200,
     min_volume=20,
     trash_container=waste,
-    tip_racks=[tip_rack],
-    name="p200")
+    tip_racks=[tip_rack])
 
 cold_tubes = cold_deck.wells('A1', 'B1', 'C1', 'C1')
+target_tubes = tube_rack.wells('A1', length=20)
 
-volumes = [100, 150, 100, 100]
-delays = [30, 30, 5, 5]
-for i in range(4):
-    p200.distribute(
-        volumes[i],
-        cold_tubes[i],
-        tube_rack.wells('A1', length=20))
-    p200.delay(minutes=delays[i])
-    p200.transfer(
-        150,
-        tube_rack.wells('A1', length=20),
-        waste,
-        new_tip='always')
+p200.distribute(100, cold_tubes['A1'], target_tubes)
+p200.delay(minutes=30)
+p200.transfer(150, target_tubes, waste, new_tip='always')
 
-# Trypsin Digestion
-# (1 ug Trypsin in ProteaseMax and 50 mM Ammonium Bicarbonate)
-# Adding Protease (No Tip Change)
-p200.distribute(
-    25, cold_deck.wells('D1'), tube_rack.wells('A1', length=20))
+p200.distribute(150, cold_tubes['B1'], target_tubes)
+p200.delay(minutes=30)
+p200.transfer(150, target_tubes, waste, new_tip='always')
+
+p200.distribute(100, cold_tubes['C1'], target_tubes)
+p200.delay(minutes=5)
+p200.transfer(150, target_tubes, waste, new_tip='always')
+
+p200.distribute(100, cold_tubes['C1'], target_tubes)
+p200.delay(minutes=5)
+p200.transfer(150, target_tubes, waste, new_tip='always')
+
+p200.distribute(25, cold_deck.wells('D1'), target_tubes)
