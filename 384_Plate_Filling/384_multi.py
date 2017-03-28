02078c8e1b889ca9e0bbaafd59c792a73b582d5a
andySigler <andrewsigler1@gmail.com>
2017-03-28 13:31:38 -0400

    updates 384_multi with distribute command

--- 384_Plate_Filling/384_multi.py
+++ 384_Plate_Filling/384_multi.py
@@ -1,27 +1,27 @@
 from opentrons import robot, instruments, containers
 
-robot.head_speed(5500)
 
-tiprack = containers.load('tiprack-200ul', 'A1', 'p200rack')
 trough = containers.load('trough-12row', 'E1', 'trough')
 plate = containers.load('384-plate', 'C1', 'plate')
-trash = containers.load('point', 'B2', 'trash')
 
+tiprack = containers.load('tiprack-200ul', 'A1', 'p200rack')
+trash = containers.load('point', 'B2', 'trash')
 p200 = instruments.Pipette(
-	name = 'p200',
-	trash_container = trash,
-	tip_racks = [tiprack],
-	min_volume = 0.5,
-	max_volume = 10,
-	axis = 'a',
-	channels = 8
+    axis='a',
+    channels=8,
+    trash_container=trash,
+    tip_racks=[tiprack],
+    min_volume=0.5,
+    max_volume=10
 )
 
-p200.pick_up_tip()
-dispense_volume = 1
-for i in range(24):
-	if p200.current_volume < dispense_volume:
-		p200.aspirate(trough['A1'])
-	p200.dispense(dispense_volume, plate.rows[i][0])#.touch_tip(-5)
-	p200.dispense(dispense_volume, plate.rows[i][1])#.touch_tip(-5)
-p200.drop_tip()
+alternating_wells = []
+for row in plate.rows():
+    alternating_wells.append(row.wells('A', length=8, step=2))
+    alternating_wells.append(row.wells('B', length=8, step=2))
+
+p200.distribute(
+    1,
+    trough.wells('A1'),
+    alternating_wells
+)
