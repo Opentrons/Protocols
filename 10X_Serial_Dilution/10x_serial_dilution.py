629b4ef339152cef8fd9cfbe42d930eec50173a0
Andy Sigler <andrewsigler1@gmail.com>
2017-04-11 22:21:04 +0800

    Transfer command in 10x serial dilution (#61)
    * refactors to use .transfer() commands
    
    * adds comments

--- 10X_Serial_Dilution/10x_serial_dilution.py
+++ 10X_Serial_Dilution/10x_serial_dilution.py
@@ -1,55 +1,28 @@
-from opentrons import robot, containers, instruments
-from itertools import chain
+from opentrons import containers, instruments
 
-p200rack = containers.load(
-    'tiprack-200ul',  
-    'A1',            
-    'p200-rack'         
-)
-trough = containers.load(
-    'trough-12row',
-    'D2',
-    'trough'
-)
-plate = containers.load(
-    '96-PCR-flat',
-    'C1',
-    'plate'
-)
-trash = containers.load(
-    'point',
-    'B2',
-    'trash'
-)
-    
-p200 = instruments.Pipette(
-    name="p200", 
+
+# trough and 96-well plate
+trough = containers.load('trough-12row', 'D2')
+plate = containers.load('96-PCR-flat', 'C1')
+
+# 8-channel 200uL pipette, with a tiprack and trash
+m200rack = containers.load('tiprack-200ul', 'A1')
+trash = containers.load('point', 'B2')
+m200 = instruments.Pipette(
+    axis='a',
+    name='m200',
     trash_container=trash,
-    tip_racks=[p200rack],
-    min_volume=20, 
+    tip_racks=[m200rack],
     max_volume=200,
-    axis="a",
-    channels=8 
+    min_volume=20,
+    channels=8
 )
 
-sample_numbers = 3 # number of samples
-sample_location = plate.rows[0] # first row of plate
-diluent_location = trough['A1'] # what the samples are diluted with
+m200.distribute(180, trough['A1'], plate.rows('2', to='7'))
 
-dilution_number = 6 # how many dilutions
-dilution_factor = 10 # dilution factor 10X
-
-sample_volume = 20
-diluent_volume = 180
-
-p200.pick_up_tip()
-for i in range(1, dilution_number + 1):
-    if p200.current_volume < diluent_volume:
-        p200.aspirate(trough['A1'])
-    p200.dispense(plate.rows[i])
-p200.drop_tip()
-
-p200.pick_up_tip()
-for row in range(0, dilution_number):
-    p200.aspirate(sample_volume, plate.rows[row][0]).dispense(plate.rows[row + 1][0]).mix(3, sample_volume, plate.rows[row + 1][0])
-p200.drop_tip() 
+m200.transfer(
+    20,
+    plate.rows('1', to='6'),
+    plate.rows('2', to='7'),
+    mix_after=(3, 20)
+)
