0987627171f292c49bea86bb43d78600fb9623e2
andySigler <andrewsigler1@gmail.com>
2017-04-11 23:15:30 +0800

    refactors tubes to 384 with Transfer commands

--- Antibody_Mapping_384/Tubes_to_384_Well_Plate.py
+++ Antibody_Mapping_384/Tubes_to_384_Well_Plate.py
@@ -1,36 +1,24 @@
-from opentrons import robot, containers, instruments
+from opentrons import containers, instruments
 
-p200rack = containers.load(
-    'tiprack-200ul', 
-    'A1',
-    'p200rack')
 
-trash = containers.load(
-    'point',
-    'B2',
-    'trash'
-)
-tube_rack = containers.load(
-    'tube-rack-2ml',
-    'C2',
-    'tube-rack-80well'
-)
-plate = containers.load(
-    '384-plate',
-    'C1',
-    'plate'
-)
+# tube rack and 384-well plate
+tube_rack = containers.load('tube-rack-2ml', 'C2')
+plate = containers.load('384-plate', 'C1')
 
+# 1-channel 200uL pipette, with tip rack and trash
+p200rack = containers.load('tiprack-200ul', 'A1')
+trash = containers.load('point', 'A2')
 p200 = instruments.Pipette(
-        axis="b",
-        max_volume=200,
-        min_volume=20,
-        tip_racks=[p200rack],
-        trash_container=trash,
-        channels=1,
-        name="p200"
+    axis="b",
+    max_volume=200,
+    tip_racks=[p200rack],
+    trash_container=trash
 )
 
 # dispense 40 uL from tube to plate, for 24 tubes
-for i in range(24):
-    p200.pick_up_tip().aspirate(40, tube_rack[i]).dispense(plate[i]).drop_tip()
+p200.transfer(
+    40,
+    tube_rack.wells('A1', length=24),
+    plate.wells('A1', length=24),
+    new_tips='always'
+)
