b08dd14c104245389bec3db7e8e97facc99ea0e4
Andy Sigler <andrewsigler1@gmail.com>
2017-04-11 23:11:23 +0800

    Transfer command in 384-multi (#62)
    * refactors 384 to use transfer commands
    
    * using m200 name

--- 384_Plate_Filling/384_multi.py
+++ 384_Plate_Filling/384_multi.py
@@ -1,27 +1,29 @@
 from opentrons import robot, instruments, containers
 
-robot.head_speed(5500)
 
-tiprack = containers.load('tiprack-200ul', 'A1', 'p200rack')
+# trough and 384-well plate
 trough = containers.load('trough-12row', 'E1', 'trough')
 plate = containers.load('384-plate', 'C1', 'plate')
-trash = containers.load('point', 'B2', 'trash')
 
-p200 = instruments.Pipette(
-	name = 'p200',
-	trash_container = trash,
-	tip_racks = [tiprack],
-	min_volume = 0.5,
-	max_volume = 10,
-	axis = 'a',
-	channels = 8
+# 8-channel 10uL pipette, with tip rack and trash
+tiprack = containers.load('tiprack-200ul', 'A1', 'p200rack')
+trash = containers.load('point', 'B2', 'trash')
+m200 = instruments.Pipette(
+    axis='a',
+    trash_container=trash,
+    tip_racks=[tiprack],
+    max_volume=10,
+    min_volume=0.5,
+    channels=8,
 )
 
-p200.pick_up_tip()
-dispense_volume = 1
-for i in range(24):
-	if p200.current_volume < dispense_volume:
-		p200.aspirate(trough['A1'])
-	p200.dispense(dispense_volume, plate.rows[i][0])#.touch_tip(-5)
-	p200.dispense(dispense_volume, plate.rows[i][1])#.touch_tip(-5)
-p200.drop_tip()
+alternating_wells = []
+for row in plate.rows():
+    alternating_wells.append(row.wells('A', length=8, step=2))
+    alternating_wells.append(row.wells('B', length=8, step=2))
+
+m200.distribute(
+    1,
+    trough.wells('A1'),
+    alternating_wells
+)
