043cf2c1e52284ffb96e019b9319ff80d3b6602d
IanLondon <ianlondondu@gmail.com>
2017-07-18 18:38:30 -0400

    try wrapping a few in def run_protocol()

--- t4_dna_ligation/t4_dna_ligation.py
+++ t4_dna_ligation/t4_dna_ligation.py
@@ -1,39 +1,41 @@
 from opentrons import containers, instruments
 
-# add a 2ml tube rack
-tuberack = containers.load('tube-rack-2ml', 'C1')
 
-# add a p10 pipette, with tiprack and trash
-p10rack = containers.load('tiprack-200ul', 'A1')
-trash = containers.load('point', 'B2')
-p10 = instruments.Pipette(
-    axis='b',
-    max_volume=10,
-    trash_container=trash,
-    tip_racks=[p10rack]
-)
+def run_protocol(buffer_vol: float=1.0,
+                 vector_vol: float=1.5,
+                 insert_vol: float=2.0,
+                 ligase_vol: float=1.6):
+    # add a 2ml tube rack
+    tuberack = containers.load('tube-rack-2ml', 'C1')
 
-# single sample volumes
-buffer_vol = 1
-vector_vol = 1.5
-insert_vol = 2
-ligase_vol = 1.6
-water_vol = 20 - (buffer_vol + vector_vol + insert_vol + ligase_vol)
+    # add a p10 pipette, with tiprack and trash
+    p10rack = containers.load('tiprack-200ul', 'A1')
+    trash = containers.load('point', 'B2')
+    p10 = instruments.Pipette(
+        axis='b',
+        max_volume=10,
+        trash_container=trash,
+        tip_racks=[p10rack]
+    )
 
-if water_vol < 0:
-    raise RuntimeWarning('Volumes add up to more than 20uL')
+    # single sample volumes
+    mix_vol = buffer_vol + vector_vol + insert_vol + ligase_vol
+    water_vol = 20 - mix_vol
 
-p10.transfer(water_vol, tuberack.wells('D1'), tuberack.wells('B2'))
-p10.transfer(buffer_vol, tuberack.wells('A1'), tuberack.wells('B2'))
-p10.transfer(vector_vol, tuberack.wells('B1'), tuberack.wells('B2'))
-p10.transfer(insert_vol, tuberack.wells('C1'), tuberack.wells('B2'))
+    if water_vol < 0:
+        raise RuntimeWarning('Volumes add up to more than 20uL')
 
-# resuspend and add ligase
-p10.transfer(
-    ligase_vol,
-    tuberack.wells('A2'),
-    tuberack.wells('B2'),
-    mix_before=(3, 10),
-    mix_after=(3, 10),
-    touch_tip=True
-)
+    p10.transfer(water_vol, tuberack.wells('D1'), tuberack.wells('B2'))
+    p10.transfer(buffer_vol, tuberack.wells('A1'), tuberack.wells('B2'))
+    p10.transfer(vector_vol, tuberack.wells('B1'), tuberack.wells('B2'))
+    p10.transfer(insert_vol, tuberack.wells('C1'), tuberack.wells('B2'))
+
+    # resuspend and add ligase
+    p10.transfer(
+        ligase_vol,
+        tuberack.wells('A2'),
+        tuberack.wells('B2'),
+        mix_before=(3, mix_vol),
+        mix_after=(3, mix_vol),
+        touch_tip=True
+    )
