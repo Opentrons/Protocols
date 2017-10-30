65a1d27833dda75ffdbab05ce53a03759d308d87
IanLondon <ianlondondu@gmail.com>
2017-10-30 14:11:10 -0400

    no protolib branch from getBranch.py

--- scripts/getBranch.py
+++ scripts/getBranch.py
@@ -10,33 +10,5 @@ def getTravisBranch():
         return os.environ.get('TRAVIS_PULL_REQUEST_BRANCH')
 
 
-def getProtolibBranch():
-    # Map Protocols repo branch name to protocol-library protolib branch.
-    branchmapping = {
-        'master': 'master',
-        'develop': 'develop',
-        'experimental': 'experimental',  # create this
-        'remove-times': 'remove-times'  # remove this
-        # Add more here
-    }
-
-    travis_branch = getTravisBranch()
-    fallback = 'master'
-    return branchmapping.get(travis_branch, fallback)
-
-
 if __name__ == '__main__':
-    import sys
-
-    err = 'Invalid arg. Run this script like \
-        "python getBranch.py [protolib|upload]"'
-
-    if len(sys.argv) != 2:
-        raise ValueError(err)
-    if sys.argv[1] == 'protolib':
-        print(getProtolibBranch())
-    elif sys.argv[1] == 'upload':
-        # Upload using the branch name, no mapping!
-        print(getTravisBranch())
-    else:
-        raise ValueError(err)
+    print(getTravisBranch())
