13b0edcd3db37bdf3ffbe7d5777427de53495a97
Jerry Wan <jw3650@nyu.edu>
2018-02-23 14:09:45 -0500

    convert cell dilution to ot2

--- protocols/biofilm_setup/biofilm_setup.ot2.py
+++ protocols/biofilm_setup/biofilm_setup.ot2.py
@@ -2,8 +2,8 @@ from opentrons import containers, instruments
 
 vol = 100  # in ul, can change
 
-# tip rack for p200 pipette
-tip200_rack = containers.load('tiprack-200ul', '1')
+# tip rack for p300 pipette
+tip300_rack = containers.load('tiprack-200ul', '1')
 
 # 96 well plate
 plate = containers.load('96-PCR-flat', '2')
@@ -11,19 +11,18 @@ plate = containers.load('96-PCR-flat', '2')
 cells = containers.load('tube-rack-2ml', '3')
 
 # p100 (10 - 100 uL) (single)
-p200 = instruments.Pipette(
+p300 = instruments.P300_Single(
     mount='right',
-    max_volume=200,
-    min_volume=20,
-    channels=1,
-    tip_racks=[tip200_rack])
+    tip_racks=[tip300_rack])
+
+p300.pick_up_tip()
 
 # dilution steps
 for col in cells.cols():
-    p200.transfer(vol, col[3:1:-1], col[2:0:-1])
+    p300.transfer(vol, col[3:1:-1], col[2:0:-1])
 
 # biofilm setup steps
 for col in range(0, 6):
-    p200.transfer(vol, cells.cols(col)[3], plate.rows(col)[0:12:3])
-    p200.transfer(vol, cells.cols(col)[2], plate.rows(col)[1:12:3])
-    p200.transfer(vol, cells.cols(col)[1], plate.rows(col)[2:12:3])
+    p300.transfer(vol, cells.cols(col)[3], plate.rows(col)[0:12:3])
+    p300.transfer(vol, cells.cols(col)[2], plate.rows(col)[1:12:3])
+    p300.transfer(vol, cells.cols(col)[1], plate.rows(col)[2:12:3])
