5c44e8e8bfcdcb6ca65fc7f3e244d849110d5ad2
LauraOpentrons <laura@opentrons.com>
2018-07-02 09:45:43 -0400

    Add separate parsers for OT 1 OT2

--- protolib2/parseOT1.py
+++ protolib2/parseOT1.py
@@ -1,3 +1,12 @@
+from inspect import signature, Parameter
+# import json
+import time
+import shutil
+from opentrons import robot, containers
+from opentrons.instruments import Pipette as BasePipette
+from opentrons.instruments import Magbead as BaseMagbead
+from opentrons.util.environment import settings as opentrons_settings
+
 import sys
 import opentrons
 
@@ -7,3 +16,154 @@ print('Opentrons verson: ', opentrons.__version__)
 print('Parsing OT1. Files:')
 print(allProtocolFiles)
 print('*-' * 40)
+# HACK to get pipette type
+pipetteType = type(BasePipette(robot, 'a'))
+
+# monkeypatch containers.load with the load_container_spy fn,
+# use global all_containers to track containers per protocol
+global all_containers
+orig_containers_load = containers.load
+
+
+def load_container_spy(container_name, slot, label=None):
+    all_containers.append({
+        'type': container_name,
+        'slot': slot,
+        'name': label or container_name
+        })
+    return orig_containers_load(container_name, slot, label)
+
+
+containers.load = load_container_spy
+
+
+def parse(protocol_path):
+
+    if not protocol_path:
+        return {}
+    print('Parsing protocol: {}'.format(protocol_path))
+
+    # Calibration data overrides settings sometimes,
+    # making protocols interfere with each other and sometimes throw errors.
+    # This HACK works around issues with calibration.
+
+    try:
+        shutil.rmtree(
+            opentrons_settings.get('CALIBRATIONS_DIR', 'calibrations'))
+    except FileNotFoundError:
+        pass
+
+    # reset is needed to reset tip tracking, other states may also interfere
+    robot.reset()
+
+    global all_containers
+    # new protocol. start with no containers
+    all_containers = []
+
+    orig_time_sleep = time.sleep
+
+    def fake_sleep(*args, **kwargs):
+        # equivalent to pre-monkeypatched `time.sleep(0)`
+        orig_time_sleep(0)
+
+    def fake_delay(self, *args, **kwargs):
+        # allow chaining
+        fake_sleep(*args, **kwargs)
+        return self
+
+    # time.sleep
+    fake_time = time
+    fake_time.sleep = fake_sleep
+
+    # monkeypatch fake delay/sleep
+    PatchedPipette = BasePipette
+    PatchedPipette.delay = fake_delay
+
+    PatchedMagbead = BaseMagbead
+    PatchedMagbead.delay = fake_delay
+
+    # do the same opentrons/__init__.py trick to monkeypatch delays
+    class InstrumentsWrapper(object):
+        def __init__(self, robot):
+            self.robot = robot
+
+        def Pipette(self, *args, **kwargs):
+            return PatchedPipette(self.robot, *args, **kwargs)
+
+        def Magbead(self, *args, **kwargs):
+            return PatchedMagbead(self.robot, *args, **kwargs)
+
+    _globals = {
+        'robot': robot,
+        'opentrons.containers': containers,
+        'opentrons.instruments': InstrumentsWrapper(robot),
+        'time': fake_time
+    }
+
+    with open(protocol_path) as f:
+        protocol_text = f.read()
+
+    exec(
+        compile(protocol_text, protocol_path, 'exec'),
+        _globals
+    )
+
+    # Mini-parametric protocols have `def run_custom_protocol(...)` fn
+    # in global scope
+    protocol_function = _globals.get('run_custom_protocol')
+
+    if protocol_function is not None:
+        # call it with default args,
+        # in case there are any container.load commands.
+        # The default args will thus determine what the deck map looks like.
+        protocol_function()
+
+    return get_result_dict(
+        robot, protocol_function, all_containers)
+
+
+def get_result_dict(robot, protocol_function, all_containers):
+    return {
+        'instruments': get_instruments(robot),
+        'containers': all_containers,
+        'parameters': get_parameters(protocol_function)
+        if protocol_function else []
+    }
+
+
+def get_parameters(protocol_function):
+    """
+    Inspect the run_custom_protocol fn arg annotations to determine
+    what customizable fields should be
+    """
+    def get_annotation(annotation):
+        try:
+            # For normal annotations like float and int:
+            name = annotation.__name__
+            return {'type': name}
+        except AttributeError:
+            # annotation must implement get_json interface:
+            return annotation.get_json()
+
+    return [
+        {
+         'name': param.name,
+         'annotation': get_annotation(param.annotation)
+         if param.annotation != Parameter.empty else None,
+         'default': param.default
+         }
+        for param in signature(protocol_function).parameters.values()
+    ]
+
+
+def get_instruments(robot):
+    return [
+        {'name': instr.name,
+         'axis': axis.lower(),
+         'channels': getattr(instr, 'channels', None),
+         'type': instr.__class__.__name__.lower(),
+         'min_volume': getattr(instr, 'min_volume', None),
+         'max_volume': getattr(instr, 'max_volume', None)
+         }
+        for axis, instr in robot.get_instruments()
+    ]
---
+++ protolib2/tests/test_traverse.py
@@ -0,0 +1,252 @@
+import os
+import os.path
+
+from protolib import traverse
+
+data_path = os.path.join(os.path.dirname(__file__), 'data/library')
+
+
+def test_traverse():
+    res = [t
+           for t in traverse.scan(data_path)
+           if t['slug'] == 'protocol1']
+
+    del res[0]['containers']
+    del res[0]['instruments']
+
+    expected = [
+        {
+            'slug': 'protocol1',
+            'path': os.path.join(data_path, 'protocol1'),
+            'files': {
+                'OT 1 protocol': 'protocol.ot1.py',
+                'OT 2 protocol': None,
+                'description': 'README.md'
+            },
+            'detected-files': {
+                'OT 1 protocol': ['protocol.ot1.py'],
+                'OT 2 protocol': [],
+                'description': ['README.md']
+            },
+            'flags': {
+                'ignore': False,
+                'skip-tests': False,
+                'feature': True,
+                'embedded-app': False
+            },
+            'status': 'ok',
+            'errors': [],
+            'markdown': {},
+            'parameters': []
+        }
+    ]
+
+    assert res == expected
+
+
+def test_tree():
+    data = [
+        {
+            'categories': ['a'],
+            'subcategories': ['b']
+        },
+        {
+            'categories': ['a', 'c', 'd'],
+            'subcategories': ['e']
+        }
+    ]
+
+    data = [
+        {'categories': {'a': ['b']}},
+        {'categories': {'a': ['e']}},
+        {'categories': {'c': []}},
+        {'categories': {'d': ['e']}}
+    ]
+
+    assert traverse.tree(data) == {
+        'a': ['b', 'e'],
+        'c': [],
+        'd': ['e']
+    }
+
+
+def test_protocol_ignore():
+    res = [
+        p
+        for p in traverse.scan_for_protocols(data_path)
+        if p['flags']['ignore']
+    ]
+
+    expected = [
+        {
+            'slug': 'protocol3',
+            'path': os.path.join(data_path, 'protocol3'),
+            'detected-files': {
+                'OT 1 protocol': ['protocol.ot1.py'],
+                'OT 2 protocol': ['protocol.ot2.py'],
+                'description': ['README.md'],
+            },
+            'flags': {
+                'ignore': True,
+                'feature': False,
+                'skip-tests': False,
+                'embedded-app': False
+            },
+        }
+    ]
+    assert res == expected
+
+    found_protocols = [p for p in traverse.scan(data_path)]
+    slugs = [p['slug'] for p in found_protocols]
+    assert not any(['protocol3' in s for s in slugs])
+
+
+def test_protocol_feature_flag():
+    res = [
+        p
+        for p in traverse.scan_for_protocols(data_path)
+        if p['flags']['feature']
+    ]
+
+    expected = [
+        {
+            'slug': 'protocol1',
+            'path': os.path.join(data_path, 'protocol1'),
+            'detected-files': {
+                'OT 1 protocol': ['protocol.ot1.py'],
+                'OT 2 protocol': [],
+                'description': ['README.md'],
+            },
+            'flags': {
+                'ignore': False,
+                'feature': True,
+                'skip-tests': False,
+                'embedded-app': False
+            },
+        }
+    ]
+
+    assert res == expected
+
+    found_protocols = [p for p in traverse.scan(data_path)]
+    slugs = [p['slug'] for p in found_protocols]
+    assert any(['protocol1' in s for s in slugs])
+
+
+def test_embedded_app_flag():
+    res = [
+        p
+        for p in traverse.scan_for_protocols(data_path)
+        if p['flags'] and p['flags']['embedded-app']
+    ]
+
+    expected = [
+        {
+            'slug': 'protocolApp',
+            'path': os.path.join(data_path, 'protocolApp'),
+            'detected-files': {
+                'OT 1 protocol': [],
+                'OT 2 protocol': [],
+                'description': ['README.md'],
+            },
+            'flags': {
+                'ignore': False,
+                'feature': False,
+                'skip-tests': False,
+                'embedded-app': 'http://blah.aws.s3/something/app.html'
+            },
+        }
+    ]
+
+    assert res == expected
+
+
+def test_missing_py():
+    res = [
+        t for t in
+        traverse.scan(data_path)
+        if (t['status'] == 'error' and
+            t['errors'] == ['Found 0 protocol files required at least 1'])
+    ]
+
+    expected = [
+        {
+            'slug': 'category1/protocol1',
+            'path': os.path.join(data_path, 'category1/protocol1'),
+            'status': 'error',
+            'files': {
+                'description': 'README.md',
+                'OT 1 protocol': None,
+                'OT 2 protocol': None},
+            'detected-files': {
+                'description': ['README.md'],
+                'OT 1 protocol': [],
+                'OT 2 protocol': []},
+            'flags': {
+                'ignore': False,
+                'skip-tests': False,
+                'feature': False,
+                'embedded-app': False
+            },
+            'errors': ['Found 0 protocol files required at least 1'],
+            'markdown': {}
+        },
+        {
+            'slug': 'protocolApp',
+            'path': os.path.join(data_path, 'protocolApp'),
+            'flags': {
+                'ignore': False,
+                'feature': False,
+                'skip-tests': False,
+                'embedded-app': 'http://blah.aws.s3/something/app.html'
+            },
+            'files': {'description': 'README.md',
+                      'OT 1 protocol': None,
+                      'OT 2 protocol': None},
+            'detected-files': {
+                'description': ['README.md'],
+                'OT 1 protocol': [],
+                'OT 2 protocol': []},
+            'status': 'error',
+            'errors': ['Found 0 protocol files required at least 1'],
+            'markdown': {}
+        }
+    ]
+
+    assert res == expected
+
+
+def test_missing_md():
+    res = [
+        t for t in
+        traverse.scan(data_path)
+        if (t['slug'] == 'category1/protocol2')  # this is missing md fixture
+    ]
+    del res[0]['containers']
+    del res[0]['instruments']
+
+    expected = [
+        {
+            'slug': 'category1/protocol2',
+            'path': os.path.join(data_path, 'category1/protocol2'),
+            'status': 'error',
+            'files': {
+                'description': None,
+                'OT 1 protocol': 'protocol.ot1.py',
+                'OT 2 protocol': 'protocol.ot2.py'},
+            'detected-files': {
+                'description': [],
+                'OT 1 protocol': ['protocol.ot1.py'],
+                'OT 2 protocol': ['protocol.ot2.py']},
+            'flags': {
+                'ignore': False,
+                'skip-tests': False,
+                'feature': False,
+                'embedded-app': False
+            },
+            'errors': ['Found 0 description files required 1'],
+            'parameters': []
+        }
+    ]
+
+    assert res == expected
