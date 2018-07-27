5ac00b44295911cf8de5a66c557832aac60e43f3
ahiuchingau <ahiuchingau@gmail.com>
2018-07-27 11:08:51 -0400

    fixed linter errors

--- protocols/IPM_Medical_Group/mass_spec_sample_prep.ot2.py
+++ protocols/IPM_Medical_Group/mass_spec_sample_prep.ot2.py
@@ -1,21 +1,21 @@
-from opentrons import labware, instruments, robot
+from opentrons import labware, instruments
 
-# customization 
-sample_number = 48  # must be between 10 and 48
-incubation_time = 1 # in minutes
-centrifuge_time = 1 # in minutes
+# customization
+sample_number = 48   # must be between 10 and 48
+incubation_time = 1  # in minutes
+centrifuge_time = 1  # in minutes
 
 labware.create('custom-tuberack-7ml',
-				grid=(4, 6),
-				spacing=(20, 20),
-				diameter=10,
-				depth=72)
+               grid=(4, 6),
+               spacing=(20, 20),
+               diameter=10,
+               depth=72)
 
 labware.create('autosampler-rack-1ml',
-				grid=(6, 10),
-				spacing=(12.3, 11.5),
-				diameter=8.53,
-				depth=72)
+               grid=(6, 10),
+               spacing=(12.3, 11.5),
+               diameter=8.53,
+               depth=72)
 
 # labware setup
 MCT_rack1 = labware.load('tube-rack-2ml', 1)
@@ -50,8 +50,8 @@ p50 = instruments.P50_Single(
     tip_racks=[tiprack1_p50, tiprack2_p50])
 
 p300 = instruments.P300_Single(
-    tip_racks = [tiprack1_p300, tiprack2_p300],
-    mount = 'right')
+    tip_racks=[tiprack1_p300, tiprack2_p300],
+    mount='right')
 
 
 p50.pick_up_tip()
@@ -66,7 +66,8 @@ p50.pick_up_tip()
 p50.mix(3, 50, is_loc)
 p50.transfer(50, is_loc, MCT_tube_loc)
 
-p300.transfer(100, sample_loc, MCT_tube_loc, new_tip='always', mix_before=(3, 300))
+p300.transfer(100, sample_loc, MCT_tube_loc, new_tip='always',
+              mix_before=(3, 300))
 
 p300.delay(minutes=incubation_time)
 
