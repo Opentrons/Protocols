51581a1b6ecc98f9944e5cb9fa196ef255ed628e
ahiuchingau <ahiuchingau@gmail.com>
2018-07-30 10:19:32 -0400

    made changes

--- protocols/Newport_scientific/LCMS_bioassay_part2.ot2.py
+++ protocols/Newport_scientific/LCMS_bioassay_part2.ot2.py
@@ -1,24 +1,29 @@
 from opentrons import labware, instruments
+"""
+PART II: After incubation
+"""
 
 # customization
 sample_num = 96
 reagent_row_1 = 6
 
-incubation_plate = labware.load('96-PCR-tall', 1)
-incubation_plate2 = labware.load('96-PCR-tall', 2)
-pp_plate = labware.load('96-PCR-tall', 3)
-pp_plate2 = labware.load('96-PCR-tall', 5)
-b_gone_plate = labware.load('96-PCR-tall', 6)
-b_gone_plate2 = labware.load('96-PCR-tall', 9)
+# labware setup
+incubation_plate = labware.load('96-PCR-tall', '1')
+incubation_plate2 = labware.load('96-PCR-tall', '2')
+pp_plate = labware.load('96-PCR-tall', '3')
+pp_plate2 = labware.load('96-PCR-tall', '5')
+b_gone_plate = labware.load('96-PCR-tall', '6')
+b_gone_plate2 = labware.load('96-PCR-tall', '9')
 
-reagent_rack = labware.load('96-PCR-tall', 4)
-tiprack = labware.load('tiprack-200ul', 7)
-tiprack2 = labware.load('tiprack-200ul', 8)
-tiprack3 = labware.load('tiprack-200ul', 10)
+reagent_rack = labware.load('96-PCR-tall', '4')
+tiprack = labware.load('tiprack-200ul', '7')
+tiprack2 = labware.load('tiprack-200ul', '8')
+tiprack3 = labware.load('tiprack-200ul', '10')
 
 meoh_loc = reagent_rack.wells('A5')
 
-
+# Copy incubation plate to the PP plates and B-Gone plates. Locations to copy
+# from and destination locations are determined by the number of samples. 
 pp_dest = []
 b_gone_dest = []
 incub_source = []
@@ -29,7 +34,6 @@ if sample_num <= 72:
                     well for well in pp_plate.rows(1)[0:4]]
     b_gone_dest = [well for well in b_gone_plate.rows(0)] + [
                     well for well in b_gone_plate.rows(1)[0:4]]
-
 else:
     incub_source = [well for well in incubation_plate.rows(reagent_row_1)] + \
             [well for well in incubation_plate.rows(reagent_row_1 + 1)[0:4]]
@@ -58,20 +62,26 @@ for source_row, pp_row, b_gone_row in zip(incub_source_plate, pp_dest_plate,
             b_gone_dest.append(b_gone_well)
             well_count += 1
 
+
+# Pipette setup
 p300 = instruments.P300_Single(
     mount='left',
     tip_racks=[tiprack, tiprack2, tiprack3])
 
 
+# Transfer MeOH to PP plates
 p300.pick_up_tip()
 p300.transfer(300, meoh_loc, pp_dest, new_tip='never')
 p300.transfer(150, meoh_loc, pp_dest, new_tip='never')
 p300.drop_tip()
 
+# Transfer incubation plate to PP plates
 p300.transfer(150, incub_source, pp_dest, new_tip='always')
 
+# Transfer MeOH to B-Gone plates
 p300.pick_up_tip()
 p300.transfer(133, meoh_loc, b_gone_dest, new_tip='never')
 p300.drop_tip()
 
+# Transfer incubation plate to B-Gone plates
 p300.transfer(200, incub_source, b_gone_dest, new_tip='always')
