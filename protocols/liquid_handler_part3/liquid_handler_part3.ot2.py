33bdc352502548c07db6d2b10d8c495390a32382
ahiuchingau <ahiuchingau@gmail.com>
2018-08-24 15:37:27 -0400

    778-m-cell-culture-from-neha-gadhari-at-terramera-inc

---
+++ protocols/liquid_handler_part3/liquid_handler_part3.ot2.py
@@ -0,0 +1,26 @@
+from opentrons import labware, instruments
+
+plates = [
+    labware.load('96-flat', '1'),
+    labware.load('96-flat', '2'),
+    labware.load('96-flat', '3'),
+    labware.load('96-flat', '4'),
+    labware.load('96-flat', '5'),
+    labware.load('96-flat', '6'),
+    labware.load('96-flat', '7')
+    ]
+
+trough = labware.load('trough-12row', '10')
+
+tiprack1 = labware.load('tiprack-200ul', '8')
+
+m50 = instruments.P50_Multi(
+    mount='left',
+    tip_racks=[tiprack1])
+
+# transfer biological dye to columns 12-1 of all plates
+m50.pick_up_tip()
+for plate in plates:
+    m50.distribute(
+        20, trough.wells('A1'), plate.cols('12', to='1'), new_tip='never')
+m50.drop_tip()
