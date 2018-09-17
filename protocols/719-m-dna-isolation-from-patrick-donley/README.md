8754fd95ebbb63bd4ce1ac958ae2ce73728b9c28
ahiuchingau <ahiuchingau@gmail.com>
2018-09-17 18:54:12 -0400

    modify-719-m-dna-isolation-from-patrick-donley

--- protocols/719-m-dna-isolation-from-patrick-donley/README.md
+++ protocols/719-m-dna-isolation-from-patrick-donley/README.md
@@ -8,7 +8,7 @@
 	* DNA
 
 ## Description
-DNA isolation with magnetic nanoparticles in 96-well plate on a magnetic plate. Uses p10 multi and p300 multi channel pipettes. Process any number of columns in a single plate. Adjust height of aspiration and mixing so as to not disturb nanoparticles.
+DNA isolation with magnetic nanoparticles in 96-well plate on a magnetic plate. Uses p50 multi and p300 multi channel pipettes. Process any number of columns in a single plate. Adjust height of aspiration and mixing so as to not disturb nanoparticles.
 
 ### Robot
 * OT-2
@@ -19,25 +19,25 @@ DNA isolation with magnetic nanoparticles in 96-well plate on a magnetic plate. 
 
 ## Process
 1. Input number of columns of the 96-well plate you want to process.
-2. Input height (in mm) above the bottom of the well you want to aspirate.
+2. Input height (in mm) above the bottom of the well you want to aspirate. (default is 1 mm)
 3. Download your protocol.
 4. Upload your protocol into the [OT App](http://opentrons.com/ot-app).
 5. Set up your desk according to the deck map.
 6. Calibrate your trough, 96-well plate, and tipracks using OT App. For calibration tips, check out our [support articles](https://support.opentrons.com/ot-2/getting-started-software-setup/deck-calibration).
 7. Hit "run".
 8. Robot will transfer 261 uL of water from trough row 1 to all designated columns of the 96-well plate.
-9. Robot will transfer 30 uL of FBS from trough row 8 to all designated columns of the 96-well plate.
-10. Robot will transfer 9 uL of nanoparticles from trough row 9 to all designated columns of the 96-well plate.
+9. Robot will transfer 30 uL of FBS from source plate column 1 to all designated columns of the 96-well plate.
+10. Robot will transfer 9 uL of nanoparticles from source plate column 2 to all designated columns of the 96-well plate.
 11. Robot will mix contents of all designated columns.
 12. Robot will delay for 30 minutes.
 13. Robot will aspirate 250 uL from each well with the pipette at the designated height above the well and discard it into liquid trash container.
 14. Robot will add 250 uL of water back to each well, dispensing at top of the well. Tips will be returned to tip rack.
 15. Robot will delay for 15 minutes.
 16. Robot will aspirate 250 uL from each well with the pipette at the designated height above the well and discard it into liquid trash container. Tips will be the same as the ones in step 14, and will be discarded after this step.
-17. Robot will repeat steps 14-16 three additional times.
+17. Robot will repeat steps 14-16 two additional times.
 
 ### Additional Notes
-* Fill trough A1-A6 with water, A8 with FBS, and A9 with nanoparticles. Make sure to slightly overfill the water reservoirs.
+* Fill trough A1-A6 with water, source plate column 1 with FBS, and column 2 with nanoparticles. Make sure to slightly overfill the water reservoirs.
 * Liquid trash can be any [container](https://docs.opentrons.com/ot1/containers.html#point) with one position.
 
 ###### Internal
--- protocols/719-m-dna-isolation-from-patrick-donley/dna_isolation_duke.ot2.py
+++ protocols/719-m-dna-isolation-from-patrick-donley/dna_isolation_duke.ot2.py
@@ -1,33 +1,34 @@
-from opentrons import instruments, labware
+from opentrons import instruments, labware, modules
 
 # labware setup
-output_plate = labware.load('96-PCR-tall', '1')
+mag_deck = modules.load('magdeck', '1')
+mag_plate = labware.load('biorad-hardshell-96-PCR', '1', 'mag_plate',
+                         share=True)
 trough = labware.load('trough-12row', '2')
-tiprack_p10 = labware.load('tiprack-10ul', '4')
-tiprack_p300_1 = labware.load('tiprack-200ul', '3')
-tiprack_p300_2 = labware.load('tiprack-200ul', '5')
-tiprack_p300_3 = labware.load('tiprack-200ul', '6')
-tiprack_p300_4 = labware.load('tiprack-200ul', '7')
-tiprack_p300_5 = labware.load('tiprack-200ul', '8')
-tiprack_p300_6 = labware.load('tiprack-200ul', '9')
-liquid_trash = labware.load('point', '10')
+source_plate = labware.load('96-flat', '3', 'source_plate')
+tiprack_p50 = labware.load('tiprack-200ul', '4', 'tiprack50')
+tiprack_p300_1 = labware.load('tiprack-200ul', '5', 'tiprack300_1')
+tiprack_p300_2 = labware.load('tiprack-200ul', '6', 'tiprack300_2')
+tiprack_p300_3 = labware.load('tiprack-200ul', '7', 'tiprack300_3')
+tiprack_p300_4 = labware.load('tiprack-200ul', '8', 'tiprack300_4')
+tiprack_p300_5 = labware.load('tiprack-200ul', '9', 'tiprack300_5')
+tiprack_p300_6 = labware.load('tiprack-200ul', '11', 'tiprack300_6')
+liquid_trash = labware.load('point', '10', 'liquid_trash')
 
 # variables and reagents setup
-water = trough.well('A1')  # location of water in trough
-fbs = trough.well('A8')  # location of fbs in trough
-np = trough.well('A9')  # location of nanoparticles in trough
+water = trough.wells('A1')  # location of water in trough
+fbs = source_plate.cols('1')  # location of fbs in source plate
+np = source_plate.cols('2')  # location of nanoparticles in trough
 
 # instrument setup
-m10 = instruments.P10_Multi(
+m50 = instruments.P50_Multi(
     mount='left',
-    tip_racks=[tiprack_p10]
-)
+    tip_racks=[tiprack_p50])
 
 m300 = instruments.P300_Multi(
     mount='right',
     tip_racks=[tiprack_p300_1, tiprack_p300_2, tiprack_p300_3, tiprack_p300_4,
-               tiprack_p300_5, tiprack_p300_6]
-)
+               tiprack_p300_5, tiprack_p300_6])
 
 
 def check_trough_volume(water_vol):
@@ -42,14 +43,15 @@ def check_trough_volume(water_vol):
 
 
 def run_custom_protocol(
-    number_of_columns: int=12,
-    aspirate_height: int=5
+        number_of_columns: int=12,
+        aspirate_height: int=1
         ):
 
-    dest_loc = output_plate.cols[0:number_of_columns]
+    dest_loc = mag_plate.cols[0:number_of_columns]
 
     water_vol = 0
 
+    # Transfer water from trough to mag-plate
     m300.pick_up_tip()
     for col in dest_loc:
         water_vol += 261*8
@@ -57,30 +59,48 @@ def run_custom_protocol(
         m300.transfer(261, water, col, new_tip='never')
     m300.drop_tip()
 
-    m300.transfer(30, fbs, dest_loc, new_tip='once')
+    # Transfer FBS from column 1 of source plate to mag-plate
+    m50.pick_up_tip()
+    for loc in dest_loc:
+        m50.transfer(30, fbs, loc.top(), new_tip='never')
+    m50.drop_tip()
 
-    m10.transfer(9, np, dest_loc, new_tip='once')
+    # Transfer NP from column 2 of source plate ot mag-plate
+    m50.pick_up_tip()
+    for loc in dest_loc:
+        if m50.current_volume < 9:
+            m50.blow_out(np)
+            m50.aspirate(np)
+        m50.dispense(9, loc.top())
+    m50.drop_tip()
 
+    # Mix content in each well 3 times with 250 uL
     for index, col in enumerate(dest_loc):
         m300.pick_up_tip()
-        if index == 0:
-            tip_loc = m300.current_tip()
         m300.mix(3, 250, col)
         m300.return_tip()
 
-    m10.delay(minutes=30)
+    # Delay for 30 minutes
+    m50.delay(minutes=30)
 
-    m300.start_at_tip(tip_loc)
+    # Turn on magnetic module for 15 minutes
+    mag_deck.engage()
+    m50.delay(minutes=15)
+
+    # Discard solution from mag-plate
     for col in dest_loc:
         m300.transfer(
-            250,
+            270,
             col.bottom(aspirate_height),
             liquid_trash,
             new_tip='always')
 
+    # Turn off magnetic module
+    mag_deck.disengage()
+
     # Wash with water 4 times
     # Using the same tip to add water and to remove waste in each cycle
-    for cycle in range(4):
+    for cycle in range(3):
         # add 250 uL of water
         for index, col in enumerate(dest_loc):
             water_vol += 250*8
@@ -88,17 +108,19 @@ def run_custom_protocol(
             m300.pick_up_tip()
             if index == 0:
                 tip_loc = m300.current_tip()
-            m300.transfer(250, water, col, mix_after=(3, 300), new_tip='never')
-            m300.drop_tip()
+            m300.transfer(250, water, col, mix_after=(3, 250), new_tip='never')
+            m300.return_tip()
 
         # wait 15 minutes for NP to settle
+        mag_deck.engage()
         m300.delay(minutes=15)
 
         # remove 250 uL of water
         m300.start_at_tip(tip_loc)
         for col in dest_loc:
             m300.transfer(
-                250,
+                270,
                 col.bottom(aspirate_height),
                 liquid_trash,
                 new_tip='always')
+        mag_deck.disengage()
