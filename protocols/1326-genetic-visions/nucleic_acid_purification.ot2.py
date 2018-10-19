0587d22f6c1b0468c320e11614504bd383ce7f48
ahiuchingau <ahiuchingau@gmail.com>
2018-10-19 17:10:27 -0400

    1326-genetic-visions

---
+++ protocols/1326-genetic-visions/nucleic_acid_purification.ot2.py
@@ -0,0 +1,59 @@
+from opentrons import labware, instruments
+
+trough_name = 'beckman-trough-4row'
+if trough_name not in labware.list():
+    labware.create(
+        trough_name,
+        grid=(4, 1),
+        spacing=(24, 0),
+        depth=24.8,
+        diameter=2,
+        volume=40700)
+
+elution_plate = labware.load('96-flat', '1')
+etoh_plate = labware.load('96-deep-well', '2')
+buffer_1_plate = labware.load('96-deep-well', '3')
+buffer_2_plate = labware.load('96-deep-well', '6')
+trough = labware.load(trough_name, '5')
+
+tiprack = labware.load('opentrons-tiprack-300ul', '4')
+
+# reagent setup
+tris_hcl = trough.wells('A1')
+etoh = trough.wells('A2')
+wash_buffer_1 = trough.wells('A3')
+wash_buffer_2 = trough.wells('A4')
+
+m300 = instruments.P300_Multi(
+    mount='left',
+    tip_racks=[tiprack])
+
+tip_start_column = '4'
+
+# distribute 65 uL Tris-HCl to elution plate
+m300.pick_up_tip()
+for col in elution_plate.cols():
+    if m300.current_volume < 65:
+        m300.aspirate(tris_hcl)
+    m300.dispense(65, col.top())
+m300.drop_tip()
+
+# distribute 200 uL EtOH to EtOH plate
+m300.pick_up_tip()
+for col in etoh_plate.cols():
+    if m300.current_volume < 200:
+        m300.aspirate(etoh)
+    m300.dispense(200, col.top())
+m300.drop_tip()
+
+# distribute 500 uL of wash buffer to wash buffer 1 plate
+m300.pick_up_tip()
+for col in buffer_1_plate.cols():
+    m300.transfer(500, wash_buffer_1, col.top(), new_tip='never')
+m300.drop_tip()
+
+# distribute 500 uL of wash buffer to wash buffer 2 plate
+m300.pick_up_tip()
+for col in buffer_2_plate.cols():
+    m300.transfer(500, wash_buffer_2, col.top(), new_tip='never')
+m300.drop_tip()
