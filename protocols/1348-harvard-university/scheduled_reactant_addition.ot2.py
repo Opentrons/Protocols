3d27190d42f44b8db5270454b6e019c5a44ab53a
ahiuchingau <ahiuchingau@gmail.com>
2018-11-08 16:27:27 -0500

    switching out the P1000 since they will not be receiving it anytime soon

--- protocols/1348-harvard-university/scheduled_reactant_addition.ot2.py
+++ protocols/1348-harvard-university/scheduled_reactant_addition.ot2.py
@@ -14,17 +14,14 @@ if custom_6_well_plate not in labware.list():
 stock = labware.load(custom_6_well_plate, '1')
 reactor = labware.load(custom_6_well_plate, '2')
 
-tiprack_1000 = labware.load('tiprack-1000ul', '4')
-tiprack_300 = labware.load('opentrons-tiprack-300ul', '5')
+tipracks_300 = [labware.load('opentrons-tiprack-300ul', slot)
+                for slot in ['4', '5']]
 
 
 # pipette setup
-p1000 = instruments.P1000_Single(
-    mount='left',
-    tip_racks=[tiprack_1000])
 p300 = instruments.P300_Single(
     mount='right',
-    tip_racks=[tiprack_300])
+    tip_racks=tipracks_300)
 
 
 for index, reactor in enumerate(reactor.wells()[:3]):
@@ -34,23 +31,23 @@ for index, reactor in enumerate(reactor.wells()[:3]):
         time = 1
 
     # transfer stock 1 to reactor 1
-    p1000.transfer(400, stock.wells('A1'), reactor)
-    p1000.delay(minutes=time)
+    p300.transfer(400, stock.wells('A1'), reactor)
+    p300.delay(minutes=time)
 
     # repeat transfer stock 2 to reactor 1
     for cycle in range(5):
-        p1000.transfer(1000, stock.wells('A2'), reactor)
-        p1000.delay(minutes=time)
+        p300.transfer(1000, stock.wells('A2'), reactor)
+        p300.delay(minutes=time)
 
     # transfer stock 3 to reactor 1
     for cycle in range(5):
         p300.transfer(250, stock.wells('A3'), reactor)
-        p1000.delay(minutes=time)
+        p300.delay(minutes=time)
 
     # transfer stock 4 to reactor 1
     for cycle in range(5):
         p300.transfer(250, stock.wells('B1'), reactor)
-        p1000.delay(minutes=time)
+        p300.delay(minutes=time)
 
     # transfer stock 1 to reactor 1
-    p1000.transfer(400, stock.wells('A1'), reactor)
+    p300.transfer(400, stock.wells('A1'), reactor)
