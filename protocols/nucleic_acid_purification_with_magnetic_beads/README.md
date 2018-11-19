e8ef925a595e5082ff4f2996784acf219943bbd5
kristin <kristin@opentrons.com>
2018-11-19 13:23:32 -0500

    add nucleic acid purification to delivery branch

---
+++ protocols/nucleic_acid_purification_with_magnetic_beads/README.md
@@ -0,0 +1,80 @@
+# Nucleic Acid Purification with Magnetic Beads
+
+### Author
+[Opentrons](https://opentrons.com/)
+
+## Categories
+* Molecular Biology
+    * Nucleic Acid Purification
+
+## Description
+With this protocol, you can perform high-quality nucleic acid purifcations using magnetic beads and the [Opentrons Magnetic Module](https://shop.opentrons.com/products/magdeck?_ga=2.120183432.1039841802.1542049668-403439593.1535387376). This setup yields high quality PCR product and other nucleic acids without the use of centrifugation or vacuum separation. You can also use this for NGS library cleanup steps.
+
+While we have reagent recommendations in the **Materials Neeeded** section below, you can use any magnetic beads you prefer. For more detailed information on how to use this protocol, please see our [Technical Note](https://s3.amazonaws.com/opentrons-protocol-library-website/Technical+Notes/Nucleic+Acid+Purification+with+Magnetic+Module+OT2+Technical+Note.pdf).
+
+---
+
+---
+
+
+![Setup](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/Nucleic+Acid+Purification/Setup.png)
+
+Using the customization fields below, set up your protocol as follows:
+
+   * **Pipette:** Specify your pipette. We recommend using a p50 or p300 multi- or single-channel.
+   * **Pipette Mount:** Specify which mount (left or right) your pipette is on.
+   * **Sample number:** Customize the number of samples to run per protocol. A multiple of 8 is recommended when you are using a multichannel pipette.
+   * **Sample volume:** Specify the starting volume (in uL) of the input sample.
+   * **Bead Ratio:** Customize the ratio of beads for left or right side size-selection of fragments. *The default bead ratio is 1.8x the input sample volume.*
+   * **Elution Volume:** Specify the final volume (in uL) to elute the purified nucleic acid. *The Opentrons MagDeck supports elution volumes above 10 µL.*
+   * **Incubation Time:** Specify the amount of time (in minutes) that the bead solution and input sample interact.
+   * **Settling Time:** Specify the amount of time (in minutes) needed to pellet the beads. *Higher volumes may require a longer settling time.*
+   * **Drying Time:** Specify the drying time (in minutes) needed after wash steps.
+
+---
+
+---
+
+
+![Materials Needed](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/customizable-serial-dilution/materials.png)
+
+-- [Opentrons OT-2](http://opentrons.com/ot-2)
+
+-- [Opentrons Magnetic Module](https://shop.opentrons.com/products/magdeck?_ga=2.171718441.823190023.1542396855-403439593.1535387376)
+
+-- [Opentrons OT-2 Run App (Version 3.1.2 or later)](http://opentrons.com/ot-app)
+
+-- 200uL or 300 uL Tiprack ([Opentrons tips suggested](https://shop.opentrons.com/collections/opentrons-tips/products/opentrons-300ul-tips-racks-9-600-tips))
+
+-- [12-row automation-friendly trough](https://www.usascientific.com/12-channel-automation-reservoir.aspx)
+
+-- [BioRad HardShell 96-Well PCR Plate](http://www.bio-rad.com/en-us/sku/hsp9601-hard-shell-96-well-pcr-plates-low-profile-thin-wall-skirted-white-clear?ID=hsp9601)
+
+-- Magnetic Beads (We recommend using [Omega Biotek Mag-Bind TotalPure NGS](http://omegabiotek.com/store/product/mag-bind-totalpure-ngs/))
+
+-- 70% Ethanol 
+
+-- Elution Buffer (Typically  10 mM Tris pH 8.0, TE Buffer, or nuclease-free water)
+
+---
+
+---
+
+### Time Estimate
+* Varies.
+
+## Process
+1. Select all desired settings according to the "Setup" section above to create your cusotmized protocol.
+2. Download your customized OT-2 Serial Dilution protocol using the blue "Download" button.
+3. Upload your protocol file into the Opentrons Run App and follow the instructions there to set up your deck and proceed to run!
+4. Make sure to add reagents to your labware before placing it on the deck! You can see where to place your reagents below.
+
+![Labware setup](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/Nucleic+Acid+Purification/Nucleic+Acid+Purification+with+Magnetic+Beads+-+Reagent+Starting+Position+Image+(2).png)
+
+###### Internal
+Nucleic Acid Purification, v1
+
+### Additional Notes
+Please reference our [Technical Note](https://s3.amazonaws.com/opentrons-protocol-library-website/Technical+Notes/Nucleic+Acid+Purification+with+Magnetic+Module+OT2+Technical+Note.pdf) for more information about the expected output of this protocol, in addition to expanded sample data from the Opentrons lab. 
+
+We understand that there are limitations to the use of this protocol and we plan to make improvements soon! In the meantime, if you'd like to request a more complex purification workflow, please use our [Protocol Development Request Form](https://opentrons-protocol-dev.paperform.co/). You can also download the Python file from this page and modify it using our [API Documentation](https://docs.opentrons.com/). For additional questions about this protocol, please email protocols@opentrons.com.
---
+++ protocols/nucleic_acid_purification_with_magnetic_beads/dna_purification.ot2.py
@@ -0,0 +1,151 @@
+from opentrons import labware, instruments, modules
+from otcustomizers import StringSelection
+
+mag_deck = modules.load('magdeck', '1')
+mag_plate = labware.load('biorad-hardshell-96-PCR', '1', share=True)
+output_plate = labware.load('biorad-hardshell-96-PCR', '2')
+
+
+def run_custom_protocol(
+        pipette_type: StringSelection(
+            'p10_Single', 'p50_Single', 'p300_Single', 'p1000_Single',
+            'p10_Multi', 'p50_Multi', 'p300_Multi', 'p1000_Multi'
+            )='p300_Multi',
+        pipette_mount: StringSelection('left', 'right')='left',
+        sample_number: int=24,
+        sample_volume: float=20,
+        bead_ratio: float=1.8,
+        elution_buffer_volume: float=200,
+        incubation_time: float=1,
+        settling_time: float=1,
+        drying_time: float=5):
+
+    total_tips = sample_number*8
+    tiprack_num = total_tips//96 + (1 if total_tips % 96 > 0 else 0)
+    slots = ['3', '5', '6', '7', '8', '9', '10', '11'][:tiprack_num]
+    if pipette_type == 'p1000_Single':
+        tipracks = [labware.load('tiprack-1000ul', slot) for slot in slots]
+        pipette = instruments.P1000_Single(
+            mount=pipette_mount,
+            tip_racks=tipracks)
+    elif pipette_type == 'p300_Single':
+        tipracks = [labware.load('tiprack-200ul', slot) for slot in slots]
+        pipette = instruments.P300_Single(
+            mount=pipette_mount,
+            tip_racks=tipracks)
+    elif pipette_type == 'p50_Single':
+        tipracks = [labware.load('tiprack-200ul', slot) for slot in slots]
+        pipette = instruments.P50_Single(
+            mount=pipette_mount,
+            tip_racks=tipracks)
+    elif pipette_type == 'p10_Single':
+        tipracks = [labware.load('tiprack-10ul', slot) for slot in slots]
+        pipette = instruments.P10_Single(
+            mount=pipette_mount,
+            tip_racks=tipracks)
+    elif pipette_type == 'p10_Multi':
+        tipracks = [labware.load('tiprack-10ul', slot) for slot in slots]
+        pipette = instruments.P10_Multi(
+            mount=pipette_mount,
+            tip_racks=tipracks)
+    elif pipette_type == 'p50_Multi':
+        tipracks = [labware.load('tiprack-200ul', slot) for slot in slots]
+        pipette = instruments.P50_Multi(
+            mount=pipette_mount,
+            tip_racks=tipracks)
+    elif pipette_type == 'p300_Multi':
+        tipracks = [labware.load('tiprack-200ul', slot) for slot in slots]
+        pipette = instruments.P300_Multi(
+            mount=pipette_mount,
+            tip_racks=tipracks)
+
+    mode = pipette_type.split('_')[1]
+
+    if mode == 'Single':
+        if sample_number <= 5:
+            reagent_container = labware.load('tube-rack-2ml', '4')
+            liquid_waste = labware.load('trough-12row', '5').wells('A12')
+        else:
+            reagent_container = labware.load('trough-12row', '4')
+            liquid_waste = reagent_container.wells('A12')
+        samples = [well for well in mag_plate.wells()[:sample_number]]
+        output = [well for well in output_plate.wells()[:sample_number]]
+    else:
+        reagent_container = labware.load('trough-12row', '4')
+        liquid_waste = reagent_container.wells('A12')
+        col_num = sample_number // 8 + (1 if sample_number % 8 > 0 else 0)
+        samples = [col for col in mag_plate.cols()[:col_num]]
+        output = [col for col in output_plate.cols()[:col_num]]
+
+    # Define reagents and liquid waste
+    beads = reagent_container.wells(0)
+    ethanol = reagent_container.wells(1)
+    elution_buffer = reagent_container.wells(2)
+
+    # Define bead and mix volume
+    bead_volume = sample_volume*bead_ratio
+    if bead_volume/2 > pipette.max_volume:
+        mix_vol = pipette.max_volume
+    else:
+        mix_vol = bead_volume/2
+    total_vol = bead_volume + sample_volume + 5
+
+    # Mix beads and PCR samples
+    for target in samples:
+        pipette.pick_up_tip()
+        pipette.mix(5, mix_vol, beads)
+        pipette.transfer(bead_volume, beads, target, new_tip='never')
+        pipette.mix(10, mix_vol, target)
+        pipette.blow_out()
+        pipette.drop_tip()
+
+    # Incubate beads and PCR product at RT for 5 minutes
+    pipette.delay(minutes=incubation_time)
+
+    # Engagae MagDeck and incubate
+    mag_deck.engage()
+    pipette.delay(minutes=settling_time)
+
+    # Remove supernatant from magnetic beads
+    pipette.set_flow_rate(aspirate=25, dispense=150)
+    for target in samples:
+        pipette.transfer(total_vol, target, liquid_waste, blow_out=True)
+
+    # Wash beads twice with 70% ethanol
+    air_vol = pipette.max_volume * 0.1
+    for cycle in range(2):
+        for target in samples:
+            pipette.transfer(200, ethanol, target, air_gap=air_vol,
+                             new_tip='once')
+        pipette.delay(minutes=1)
+        for target in samples:
+            pipette.transfer(200, target, liquid_waste, air_gap=air_vol)
+
+    # Dry at RT
+    pipette.delay(minutes=drying_time)
+
+    # Disengage MagDeck
+    mag_deck.disengage()
+
+    # Mix beads with elution buffer
+    if elution_buffer_volume/2 > pipette.max_volume:
+        mix_vol = pipette.max_volume
+    else:
+        mix_vol = elution_buffer_volume/2
+    for target in samples:
+        pipette.pick_up_tip()
+        pipette.transfer(
+            elution_buffer_volume, elution_buffer, target, new_tip='never')
+        pipette.mix(20, mix_vol, target)
+        pipette.drop_tip()
+
+    # Incubate at RT for 3 minutes
+    pipette.delay(minutes=5)
+
+    # Engagae MagDeck for 1 minute and remain engaged for DNA elution
+    mag_deck.engage()
+    pipette.delay(minutes=settling_time)
+
+    # Transfer clean PCR product to a new well
+    for target, dest in zip(samples, output):
+        pipette.transfer(elution_buffer_volume, target, dest, blow_out=True)
