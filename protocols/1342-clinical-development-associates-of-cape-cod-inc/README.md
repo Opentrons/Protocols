0abf9c7717543b2279385075a0b204f455599acd
ahiuchingau <ahiuchingau@gmail.com>
2018-11-30 13:17:21 -0500

    1342-clinical-development-associates-of-cape-cod-inc

---
+++ protocols/1342-clinical-development-associates-of-cape-cod-inc/README.md
@@ -0,0 +1,49 @@
+# Fungitell Assay
+
+### Author
+[Opentrons](http://www.opentrons.com/)
+
+## Categories
+* Sample Prep
+    * Distribution
+
+## Description
+The Fungitell® assay is a protease zymogen-based colorimetric assay for quantitative detection of (1→3)-β-D-Glucan in serum, and it is a FDA-cleared in vitro diagnostic screening test for Invasive Fungal Infection.
+
+This protocol allows the robot to perform (1) Source Plate Preparation Sequence, (2) Destination Plate Preparation Sequence, and (3) Fungitell Addition. A P50 single-channel and P300 multi-channel pipette is required.
+
+
+### Robot
+* [OT-2](https://opentrons.com/ot-2)
+
+### Modules
+
+### Reagents
+
+## Process
+1. Download your protocol.
+2. Upload your protocol into the [OT App](https://opentrons.com/ot-app).
+3. Set up your deck according to the deck map.
+4. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support article](https://support.opentrons.com/ot-2/getting-started-software-setup/deck-calibration).
+5. Hit "Run".
+
+
+### Additional Notes
+![Layout](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/1342-clinical-development-associates-of-cape-cod-inc/layout.png)
+
+300 uL Tip Rack Setup
+* Remove A1, B1, G1 and H1 from the tip rack in slot 9
+---
+Reagent Setup
+* RGW: A1
+* APTS: B1
+* BG_STD: C1
+---
+Trough Setup
+* Fungitell: A1
+
+If you have any questions about this protocol, please contact protocols@opentrons.com.
+
+###### Internal
+g0Fpjf77
+1342
---
+++ protocols/1342-clinical-development-associates-of-cape-cod-inc/fungitell_protocol.ot2.py
@@ -0,0 +1,94 @@
+from opentrons import labware, instruments, robot
+
+# create custom labware
+tuberack_name = 'custom-pyrotube-rack'
+if tuberack_name not in labware.list():
+    labware.create(
+        tuberack_name,
+        grid=(6, 4),
+        spacing=(20, 20),
+        diameter=11.8,
+        depth=75)
+
+# labware setup
+SP = labware.load('96-flat', '4', 'Source Plate')
+destination = labware.load('96-flat', '5', 'Destination Plate')
+tuberack = labware.load(tuberack_name, '6')
+trough = labware.load('trough-12row', '2')
+
+# reagent setup
+RGW = tuberack.wells('A1')
+APTS = tuberack.wells('B1')
+BG_STD = tuberack.wells('C1')
+fungitell = trough.wells('A1')
+
+tipracks_50 = [labware.load('opentrons-tiprack-300ul', slot)
+               for slot in ['7', '8']]
+tipracks_300 = [labware.load('opentrons-tiprack-300ul', slot)
+                for slot in ['9', '10']]
+
+# instrument setup
+p50 = instruments.P50_Single(
+    mount='left',
+    tip_racks=tipracks_50)
+
+m300 = instruments.P300_Multi(
+    mount='right',
+    tip_racks=tipracks_300)
+
+# create standard curve
+STDs = [well for well in SP.wells('C2', 'D2', 'E2', 'F2')]
+
+
+# remove A, B, G, H from col 1 of 300 uL tiprack
+m300.transfer(100, RGW, SP.cols('2'))
+p50.pick_up_tip()
+p50.transfer(100, BG_STD, SP.wells('B2'), new_tip='never')
+p50.transfer(100, BG_STD, STDs[0], new_tip='never')
+p50.mix(5, 50, STDs[0])
+for source, dest in zip(STDs[0:3], STDs[1:]):
+    p50.transfer(100, source, dest, new_tip='never')
+    p50.mix(5, 50, dest)
+p50.drop_tip()
+
+
+# addition of standard curve concentrations in duplicate
+dests = [[destination[i], destination[i+1]] for i in range(0, 10, 2)]
+sources = SP.wells('B2', to='F2')
+
+for source, dest in zip(sources, dests):
+    p50.pick_up_tip()
+    p50.distribute(25, source, dest, new_tip='never')
+    p50.drop_tip()
+
+# addition of negative controls
+p50.distribute(25, RGW, [well for well in SP.wells('C2', 'D2')])
+
+# patient serum transfer
+dests = [[destination[i], destination[i+1]] for i in range(12, 30, 2)]
+dests += [[destination[i], destination[i+1]] for i in range(32, 55, 2)]
+
+sources = [well for col in SP.cols('5', to='7') for well in col[1:7]]
+sources += [well for well in SP.cols('8')[1:4]]
+
+p50.set_flow_rate(aspirate=10, dispense=15)
+
+for source, dest in zip(sources, dests):
+    p50.distribute(5, source, dest)
+
+p50.set_flow_rate(aspirate=25, dispense=50)
+# alkaline pre-treatment addition
+all_wells = [well for subdests in dests for well in subdests]
+p50.pick_up_tip()
+for well in all_wells:
+    if p50.current_volume <= 20:
+        p50.aspirate(APTS)
+    p50.dispense(20, well.top())
+p50.drop_tip()
+
+robot.pause("Off robot: Sequence Hold for Alkaline Pre-treatment of Serum \
+Samples and Fungitell Reconstitution.")
+
+# fungitell addition
+for col in destination.cols('1', to='7'):
+    m300.transfer(100, fungitell, col)
