cf1221dca91b21d9f97056cb074dc20031faa941
ahiuchingau <ahiuchingau@gmail.com>
2018-12-11 16:41:36 -0500

    1435-ncf-diagnostics-dna-technologies

---
+++ protocols/1435-ncf-diagnostics-dna-technologies/README.md
@@ -0,0 +1,36 @@
+# Tube Filling From 12-row Trough
+
+### Author
+[Opentrons](http://www.opentrons.com/)
+
+## Categories
+* Sample Prep
+    * Distribution
+
+## Description
+With this protocol, your robot can transfer solution from well A1 of the 12-column trough to up to 216 1.5 mL tubes. This protocol requires a P300 single-channel pipette.
+
+### Robot
+* [OT-2](https://opentrons.com/ot-2)
+
+### Modules
+
+### Reagents
+
+## Process
+1. Input the number of tubes you are planning to fill.
+2. Select the tube rack you are using.
+3. Input the desired volume to be transferred in each tube.
+4. Download your protocol.
+5. Upload your protocol into the [OT App](https://opentrons.com/ot-app).
+6. Set up your deck according to the deck map.
+7. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support article](https://support.opentrons.com/ot-2/getting-started-software-setup/deck-calibration).
+8. Hit "Run".
+
+
+### Additional Notes
+If you have any questions about this protocol, please contact protocols@opentrons.com.
+
+###### Internal
+skM6YHza
+1435
---
+++ protocols/1435-ncf-diagnostics-dna-technologies/tube_filling.ot2.py
@@ -0,0 +1,45 @@
+from opentrons import labware, instruments
+from otcustomizers import StringSelection
+import math
+
+metadata = {
+    'protocolName': 'Tube Filling',
+    'author': 'Alise <protocols@opentrons.com>',
+    'source': 'Protocol Library'}
+
+# labware setup
+trough = labware.load('trough-12row', '1')
+tiprack = labware.load('tiprack-200ul', '11')
+
+# instruments setup
+p300 = instruments.P300_Single(
+    mount='left',
+    tip_racks=[tiprack])
+
+
+def run_custom_protocol(
+    number_of_tubes: int=24,
+    tuberack: StringSelection(
+        'opentrons-tuberack-2ml-eppendorf',
+        'opentrons-tuberack-2ml-screwcap')='opentrons-tuberack-2ml-eppendorf',
+    transfer_volume: float=300
+        ):
+
+    if number_of_tubes > 216:
+        raise Exception("Number of tubes cannot exceed 216.")
+
+    number_of_racks = math.ceil(number_of_tubes / 24)
+    tuberacks = [labware.load(tuberack, str(slot))
+                 for slot in range(2, 11)][:number_of_racks]
+
+    tubes = [well for rack in tuberacks
+             for well in rack.wells()][:number_of_tubes]
+
+    p300.pick_up_tip()
+    for tube in tubes:
+        p300.transfer(
+            transfer_volume,
+            trough.wells('A1'),
+            tube,
+            new_tip='never')
+    p300.drop_tip()
