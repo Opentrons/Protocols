851bd4546710bb1123696281b4b25e38027354da
Laura-Danielle <laura@opentrons.com>
2019-01-04 10:23:07 -0500

    More fixin

--- protocols/1444-s-computing-research-unit-uct-part-3/assay_setup.ot2.py
+++ protocols/1444-s-computing-research-unit-uct-part-3/assay_setup.ot2.py
@@ -5,7 +5,7 @@ metadata = {
  'protocolName': 'Fluorescence Assay Setup',
  'author': 'Laura <protocols@opentrons.com>',
  'source': 'Custom Protocol Request'
- }
+}
 
 
 def run_custom_protocol(pipette_type: StringSelection(
@@ -13,7 +13,7 @@ def run_custom_protocol(pipette_type: StringSelection(
         mm_volume: int=10,
         donor_sub_volume: int=10,
         start: str='A',
-        end: str='A',
+        end: int=1,
         starting_well: int=0,
         ending_well: int=12,
         mm_loc: str='A1',
@@ -26,16 +26,26 @@ def run_custom_protocol(pipette_type: StringSelection(
         tuberack = labware.load('opentrons-tuberack-2ml-eppendorf', '1')
         source1 = tuberack.wells(mm_loc)
         source2 = tuberack.wells(donor_loc)
-        destination = experiment_plate.rows(
-            start, to=end)[starting_well:ending_well]
+
+        if end == 1:
+            destination = experiment_plate.rows(
+                start, length=end)[starting_well:ending_well]
+        else:
+            destination = [well
+                           for row in experiment_plate.rows(start, length=end)
+                           for well in row[starting_well:ending_well]]
+
         pipette = instruments.P10_Single(mount='right', tip_racks=[tiprack])
     else:
         tiprack = labware.load('opentrons-tiprack-300ul', '2')
         plate = labware.load('96-flat', '1')
-        source1 = plate.columns(mm_loc)
-        source2 = plate.columns(donor_loc)
-        destination = experiment_plate.columns(
-            start, to=end)[starting_well:ending_well]
+        source1 = plate.columns(mm_loc)[0]
+        source2 = plate.columns(donor_loc)[0]
+        if end == 1:
+            destination = experiment_plate.columns(start, length=end)[0]
+        else:
+            destination = [column[0] for column in experiment_plate.columns(
+                start, length=end)]
         pipette = instruments.P50_Multi(mount='left', tip_racks=[tiprack])
 
     pipette.pick_up_tip()
