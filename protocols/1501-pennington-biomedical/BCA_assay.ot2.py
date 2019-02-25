0e94b1bf2823ef9ab0ef49e5d58a0e98a423281d
ncdiehl11 <ndiehl@opentrons.com>
2019-02-25 11:21:39 -0500

    made code modifications according to review, added image in README

--- protocols/1501-pennington-biomedical/BCA_assay.ot2.py
+++ protocols/1501-pennington-biomedical/BCA_assay.ot2.py
@@ -1,4 +1,5 @@
 from opentrons import labware, instruments
+import math
 
 metadata = {
     'protocolName': 'BCA Assay',
@@ -7,67 +8,49 @@ metadata = {
 }
 
 # labware
-tube_rack_1 = labware.load('tube-rack-2ml', slot='3')
-tube_rack_2 = labware.load('tube-rack-2ml', slot='6')
-tip_rack_200 = [labware.load('tiprack-200ul', slot) for slot in ['7', '10']]
-trough = labware.load('trough-12row', slot='8')
-plate = labware.load('96-PCR-flat', slot='11')
+tube_rack_1 = labware.load('tube-rack-2ml', '3')
+tube_rack_2 = labware.load('tube-rack-2ml', '6')
+tip_rack_200_s = labware.load('tiprack-200ul', '7')
+tip_rack_200_m = labware.load('tiprack-200ul', '10')
+trough = labware.load('trough-12row', '8')
+plate = labware.load('96-PCR-flat', '11')
 
 # pipettes
 p300 = instruments.P300_Single(
     mount='right',
-    tip_racks=tip_rack_200
+    tip_racks=[tip_rack_200_s]
     )
 
 m300 = instruments.P300_Multi(
     mount='left',
-    tip_racks=tip_rack_200
+    tip_racks=[tip_rack_200_m]
     )
 
+# creates a list of 32 sets of wells in triplicate corresponding to a max
+# of 32 samples to be transferred
+wells = [r[(c*3):(c*3+3)] for c in range(4) for r in plate.rows()]
 
-"""
-create_tube_order outputs the well indices of a 24-well tube rack ordering
-across rows and then down columns
-"""
-
-
-def create_tube_order():
-    new_ind = []
-    for row in range(4):
-        for col in range(6):
-            new_ind.append(col*4 + row)
-    return new_ind
+# creates a list of 48 wells from both tube racks ordering across rows and
+# then down columns
+tubes_1 = [tube for row in tube_rack_1.rows() for tube in row]
+tubes_2 = [tube for row in tube_rack_2.rows() for tube in row]
+tubes_total = tubes_1 + tubes_2
 
 
 def run_custom_protocol(
-        sample_num: int = 32):
+        sample_num: int = 32,
+        sample_vol: int = 30,
+        solution_vol: int = 200
+        ):
 
-    # creates a list of 32 sets of wells in triplicate corresponding to a max
-    # of 32 samples to be transferred
-    well_order = []
-    for move in range(4):
-        for row in range(8):
-            triplicate_block = [row+move*24, row+move*24+8, row+move*24+16]
-            well_order.append(triplicate_block)
+    for i in range(sample_num):
+        p300.distribute(sample_vol, tubes_total[i], wells[i])
 
-    # creates a list of 48 wells from both tube racks ordering across rows and
-    # then down columns
-    tubes_1 = [tube for tube in tube_rack_1.wells(create_tube_order())]
-    tubes_2 = [tube for tube in tube_rack_2.wells(create_tube_order())]
-    tubes_temp = tubes_1 + tubes_2
-    tubes_total = tubes_temp[0:sample_num]
-
-    # distributes each sample to 96-well plate in triplicate for user-input
-    # number of samples
-    for sample, target in zip(tubes_total, well_order):
-        wells = [well for well in plate(target)]
-        p300.distribute(30, sample, wells)
-
-    # transfers solution from trough to corresponding column of samples
-    num_transfers = (int((sample_num-1)/8)+1)*3
+    # transfers solution 1 to all columns of samples
+    num_transfers = (math.ceil(sample_num/8))*3
     for col in range(num_transfers):
         m300.transfer(
-                200,
+                solution_vol,
                 trough.wells('A1'),
                 plate.rows['A'][col],
                 mix_after=(3, 75)
