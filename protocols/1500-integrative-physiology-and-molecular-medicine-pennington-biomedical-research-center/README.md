a7b960cec03867089b09123969e45fe868c8efb2
ncdiehl11 <ndiehl@opentrons.com>
2019-02-25 11:24:10 -0500

    made code modifications according to review, added image in README

--- protocols/1500-integrative-physiology-and-molecular-medicine-pennington-biomedical-research-center/README.md
+++ protocols/1500-integrative-physiology-and-molecular-medicine-pennington-biomedical-research-center/README.md
@@ -8,7 +8,7 @@
     * Assay
 
 ## Description
-With this protocol, your robot can perform a colorimetric acid assay for up to 32 samples in triplicate on a single 96-well PCR plate. Each set of triplicate samples are arranged next to each other horizontally (ex: A1, A2, A3). Subsequent samples in triplicate are filled down the column before moving 3 columns over (ex: sample 1- A1, A2, A3; sample 2- B1, B2, B3; ...; sample 8- H1, H2, H3; sample 9- A4, A5, A6, ...).
+With this protocol, your robot can perform a colorimetric acid assay for up to 32 samples in triplicate on a single 96-well PCR plate. Each set of triplicate samples are arranged next to each other horizontally (ex: A1, A2, A3). Subsequent samples in triplicate are filled down the column before moving 3 columns over (ex: sample 1- A1, A2, A3; sample 2- B1, B2, B3; ...; sample 8- H1, H2, H3; sample 9- A4, A5, A6, ...). [See additional notes below.]
 
 ---
 
@@ -32,14 +32,15 @@ You will need:
 1. p300-Single pipette distributes 175ul each of triplicates of sample from tube rack to 96 well plate, aligning triplicates of the same sample horizontally adjacent. The pipette tip is discarded.
 2. This process is repeated n times for n samples as input by the user. Subsequent samples fill down the column before starting a new set of samples 3 columns over. Tips are changed between each sample transfer.
 3. p300-Multi pipette 50ul of solution from the first column of the 12-row trough to the first column of the 96-well plate. The contents are mixed 3x, and the tips are discarded.
-4. This process is repeated across and plate until all sample triplicates have received solution from trough column 1.
+4. This process is repeated across the plate until all sample triplicates have received solution from trough column 1.
 5. Steps 3 and 4 are repeated with trough column 12 to complete the assay.
 
 ### Additional Notes
 
-![deck setup](https://s3-ap-southeast-2.amazonaws.com/paperform/u-4256/0/2019-02-06/v31345c/OT2%20Layout%20-%20BHB%20Protocol%20Development_Mey%20Feb%202019.jpg)
+![Sample transfer schematic](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/1501-pennington-biomedical/transfer_schematic.png)
 
 If you have any questions about this protocol, please contact protocols@opentrons.com.
 
 ###### Internal
+qT4pYTkP
 1500
--- protocols/1500-integrative-physiology-and-molecular-medicine-pennington-biomedical-research-center/colorimetric_assay.ot2.py
+++ protocols/1500-integrative-physiology-and-molecular-medicine-pennington-biomedical-research-center/colorimetric_assay.ot2.py
@@ -1,4 +1,5 @@
 from opentrons import labware, instruments
+import math
 
 metadata = {
     'protocolName': 'Colorimetric Assay',
@@ -7,72 +8,59 @@ metadata = {
 }
 
 # labware
-tube_rack_1 = labware.load('tube-rack-2ml', slot='3')
-tube_rack_2 = labware.load('tube-rack-2ml', slot='6')
-tips = [labware.load('tiprack-200ul', slot) for slot in ['7', '10']]
-trough = labware.load('trough-12row', slot='8')
-plate = labware.load('96-PCR-flat', slot='11')
+tube_rack_1 = labware.load('tube-rack-2ml', '3')
+tube_rack_2 = labware.load('tube-rack-2ml', '6')
+tip_rack_200_s = labware.load('tiprack-200ul', '7')
+tip_rack_200_m = labware.load('tiprack-200ul', '10')
+trough = labware.load('trough-12row', '8')
+plate = labware.load('96-PCR-flat', '11')
 
 # pipettes
 p300 = instruments.P300_Single(
-    mount='left',
-    tip_racks=tips
-)
+    mount='right',
+    tip_racks=[tip_rack_200_s]
+    )
 
 m300 = instruments.P300_Multi(
-    mount='right',
-    tip_racks=tips
-)
+    mount='left',
+    tip_racks=[tip_rack_200_m]
+    )
 
+# creates a list of 32 sets of wells in triplicate corresponding to a max
+# of 32 samples to be transferred
+wells = [r[(c*3):(c*3+3)] for c in range(4) for r in plate.rows()]
 
-def create_tube_order():
-    new_ind = []
-    for row in range(4):
-        for col in range(6):
-            new_ind.append(col*4 + row)
-    return new_ind
+# creates a list of 48 wells from both tube racks ordering across rows and
+# then down columns
+tubes_1 = [tube for row in tube_rack_1.rows() for tube in row]
+tubes_2 = [tube for row in tube_rack_2.rows() for tube in row]
+tubes_total = tubes_1 + tubes_2
 
 
 def run_custom_protocol(
-        sample_num: int = 32):
+        sample_num: int = 32,
+        sample_vol: int = 175,
+        solution_vol_1: int = 50,
+        solution_vol_2: int = 50
+        ):
 
-    # creates a list of 32 sets of wells in triplicate corresponding to a max
-    # of 32 samples to be transferred
-    well_order = []
-    for move in range(4):
-        for row in range(8):
-            triplicate_block = [row+move*24, row+move*24+8, row+move*24+16]
-            well_order.append(triplicate_block)
+    for i in range(sample_num):
+        p300.distribute(sample_vol, tubes_total[i], wells[i])
 
-    # creates a list of 48 wells from both tube racks ordering across rows and
-    # then down columns
-    tubes_1 = [tube for tube in tube_rack_1.wells(create_tube_order())]
-    tubes_2 = [tube for tube in tube_rack_2.wells(create_tube_order())]
-    tubes_temp = tubes_1 + tubes_2
-    tubes_total = tubes_temp[0:sample_num]
-
-    # distributes each sample to 96-well plate in triplicate for user-input
-    # number of samples
-    for sample, target in zip(tubes_total, well_order):
-        wells = [well for well in plate(target)]
-        p300.transfer(175, sample, wells)
-
-    # sets number of occupied columns of samples in plate
-    num_transfers = (int((sample_num-1)/8)+1)*3
-
-    # transfers solution 1 from trough to all columns of samples
+    # transfers solution 1 all columns of samples
+    num_transfers = (math.ceil(sample_num/8))*3
     for col in range(num_transfers):
         m300.transfer(
-                50,
+                solution_vol_1,
                 trough.wells('A1'),
                 plate.rows['A'][col],
                 mix_after=(3, 50)
                 )
 
-    # transfers solution 12 from trough to all columns of samples
+    # transfers solution 12 to all columns of samples
     for col in range(num_transfers):
         m300.transfer(
-                50,
+                solution_vol_2,
                 trough.wells('A12'),
                 plate.rows['A'][col],
                 mix_after=(3, 50)
