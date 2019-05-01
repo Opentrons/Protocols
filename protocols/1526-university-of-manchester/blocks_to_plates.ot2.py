6484e90045df78e46fda9bf9efe658a16be38bcc
ncdiehl11 <ndiehl@opentrons.com>
2019-05-01 11:35:43 -0400

    fix-1526-university-of-manchester

--- protocols/1526-university-of-manchester/blocks_to_plates.ot2.py
+++ protocols/1526-university-of-manchester/blocks_to_plates.ot2.py
@@ -1,4 +1,5 @@
 from opentrons import labware, instruments, robot
+from otcustomizers import StringSelection
 
 metadata = {
     'protocolName': 'Protein Crystallization Screen',
@@ -35,33 +36,47 @@ plates = [labware.load(plate_name, slot)
 
 block = labware.load(block_name, '7')
 
-tips300 = labware.load('opentrons-tiprack-300ul', '10')
+tips = labware.load('opentrons-tiprack-300ul', '10')
 
 # load pipette
-p300 = instruments.P300_Multi(
+m50 = instruments.P50_Multi(
     mount='right',
-    tip_racks=[tips300]
+    tip_racks=[tips]
+)
+m300 = instruments.P300_Multi(
+    mount='left',
+    tip_racks=[tips]
 )
 
 
-def perform_deck_transfer(vol):
-    for ind, source_col in enumerate(block.cols()):
-        p300.pick_up_tip(tips300.cols(ind))
-        for plate in plates:
-            dest_col = plate.cols(ind)
-            p300.transfer(vol,
-                          source_col,
-                          dest_col,
-                          new_tip='never')
-        p300.drop_tip()
+def run_custom_protocol(
+        number_of_full_decks: int = 2,
+        volume_of_transfer: float = 30.0,
+        pipette_type: StringSelection('P50-Multi', 'P300-Multi') = 'P50-Multi'
+        ):
+    def perform_deck_transfer(vol):
+        for ind, source_col in enumerate(block.cols()):
+            pipette.pick_up_tip(tips.cols(ind))
+            for plate in plates:
+                dest_col = plate.cols(ind)
+                pipette.transfer(vol,
+                                 source_col,
+                                 dest_col,
+                                 new_tip='never')
+            pipette.drop_tip()
 
+    # select pipette
+    if pipette_type == 'P50-Multi':
+        pipette = m50
+    else:
+        pipette = m300
 
-def run_custom_protocol(number_of_full_decks: int = 2,
-                        volume_of_transfer: float = 30.0,
-                        ):
     # perform transfers
     perform_deck_transfer(volume_of_transfer)
     for i in range(number_of_full_decks-1):
         robot.pause("Please replace plates in slots 1, 2, 3, 4, 5, 6, 8, 9, "
                     "and 11, and tip rack in slot 10 before resuming.")
         perform_deck_transfer(volume_of_transfer)
+
+
+run_custom_protocol()
