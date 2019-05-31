dc854df908546f7c9f9e2742b718322922d6960c
ncdiehl11 <ndiehl@opentrons.com>
2019-05-31 15:12:35 -0400

    made sample number customizable, fixed spots

--- protocols/1577/custom_chip_PCR_prep.ot2.py
+++ protocols/1577/custom_chip_PCR_prep.ot2.py
@@ -49,36 +49,74 @@ primer = tubes.wells('C1')
 dna_pol = tubes.wells('D1')
 mix_tube = tubes.wells('A2')
 
-# create master mix, enough for 4 extra samples
-p50.transfer(15.5*28, h2o, mix_tube.top(), blow_out=True)
-p50.transfer(5*28, red_buffer, mix_tube.top(), blow_out=True)
-p50.transfer(1*28, primer, mix_tube.top(), blow_out=True)
-p50.pick_up_tip()
-p50.transfer(0.25*28, dna_pol.bottom(), mix_tube, new_tip='never')
-p50.mix(10, 50, mix_tube)
-p50.drop_tip()
 
-# set up spots
-spots = [well for row in chip.rows() for well in row]
-for i, spot in enumerate(spots[8:16]):
-    offset = spot.from_center(r=0.8, theta=180, h=1.0)
-    new = (spot, offset)
-    spots[i] = new
+def run_custom_protocol(
+        number_of_samples: int = 24
+):
+    # check for too many samples
+    if number_of_samples > 24:
+        raise Exception("24 sample membranes maximum on chip.")
 
-# transfer sample to strips and immediately transfer water to membrane
-sample_strips = strips.wells()[0:24]
-for i, (spot, dest) in enumerate(zip(spot, sample_strips)):
-    source = spot if i >= 8 and i < 16 else spot.top()
-    p10.transfer(8, source, dest, blow_out=True)
-    p10.transfer(10, h2o, spot.top(1))
+    num_samples_for_mm = number_of_samples + 4
 
-# transfer master mix to new strip tubes
-mix_strips = strips.wells()[24:48]
-p50.transfer(21.75, mix_tube, mix_strips)
+    # create master mix, enough for 4 extra samples
+    p50.transfer(
+        15.5*num_samples_for_mm,
+        h2o,
+        mix_tube.top(),
+        blow_out=True
+        )
+    p50.transfer(
+        5*num_samples_for_mm,
+        red_buffer,
+        mix_tube.top(),
+        blow_out=True
+        )
+    if num_samples_for_mm > 10:
+        p50.transfer(
+            1*num_samples_for_mm,
+            primer,
+            mix_tube.top(),
+            blow_out=True
+            )
 
-# transfer sample to corresponding strip tube already containing master mix
-for source, dest in zip(sample_strips, mix_strips):
     p10.pick_up_tip()
-    p10.transfer(3, source, dest, new_tip='never')
-    p10.mix(10, 10, dest)
+    p10.transfer(
+        0.25*num_samples_for_mm,
+        dna_pol.bottom(),
+        mix_tube,
+        new_tip='never')
+    p10.mix(10, 10, mix_tube)
+    p10.blow_out(mix_tube.top())
     p10.drop_tip()
+
+    # set up spots
+    spots = [well for row in chip.rows() for well in row]
+    for ind in range(8, 16):
+        spot = spots[ind]
+        offset = spot.from_center(r=0.8, theta=180, h=1.0)
+        new = (spot, offset)
+        spots[ind] = new
+    spots = spots[0:number_of_samples]
+
+    # slow flow rate and transfer sample to strips and immediately transfer
+    # water to membrane
+    p10.set_flow_rate(aspirate=2, dispense=2)
+    sample_strips = strips.wells()[0:number_of_samples]
+    for i, (spot, dest) in enumerate(zip(spots, sample_strips)):
+        source = spot if i >= 8 and i < 16 else spot.top(1)
+        p10.transfer(8, source, dest, blow_out=True)
+        p10.transfer(10, h2o, source)
+
+    # transfer master mix to new strip tubes
+    mix_strips = strips.wells()[24:24+number_of_samples]
+    p50.transfer(21.75, mix_tube, mix_strips)
+
+    # reset flow rate to default and transfer sample to corresponding strip
+    # tube already containing master mix
+    p10.set_flow_rate(aspirate=5, dispense=10)
+    for source, dest in zip(sample_strips, mix_strips):
+        p10.pick_up_tip()
+        p10.transfer(3, source, dest, new_tip='never')
+        p10.mix(10, 10, dest)
+        p10.drop_tip()
