8ea529545be5010be0872b233858b7cc10902a76
ncdiehl11 <ndiehl@opentrons.com>
2019-06-04 17:35:53 -0400

    fix-1546

--- protocols/1546-cytena-gmbh-part1/DNA_normalization.ot2.py
+++ protocols/1546-cytena-gmbh-part1/DNA_normalization.ot2.py
@@ -33,54 +33,59 @@ if flat_plate_name not in labware.list():
 trough = labware.load('trough-12row', '1')
 PCR_plate = labware.load(PCR_plate_name, '2')
 flat_plate = labware.load(flat_plate_name, '3')
-tips10 = labware.load('tiprack-10ul', '4')
-tips300 = labware.load('opentrons-tiprack-300ul', '5')
+tips10 = labware.load('tiprack-10ul', '7')
+tips300 = labware.load('opentrons-tiprack-300ul', '8')
 
 # pipettes
-p10 = instruments.P10_Single(mount='right', tip_racks=[tips10])
-p300 = instruments.P300_Single(mount='left', tip_racks=[tips300])
+m10 = instruments.P10_Multi(mount='right', tip_racks=[tips10])
+m300 = instruments.P300_Multi(mount='left', tip_racks=[tips300])
 
 # reagent setup
 solution = trough.wells('A8')
 
 # transfer working solution to flat plate
-p300.distribute(75, solution, [well.top() for well in flat_plate.wells()])
+m300.distribute(75, solution, [well.top() for well in flat_plate.rows('A')])
 
 # transfer DNA from PCR plate to corresponding well of flat plate
 for source_col, dest_col in zip(PCR_plate.columns('2', to='12'),
                                 flat_plate.columns('2', to='12')):
-    for source, dest in zip(source_col, dest_col):
-        p10.pick_up_tip()
-        p10.transfer(3, source, dest, blow_out=True, new_tip='never')
-        p10.touch_tip(dest)
-        p10.drop_tip()
+    source = source_col[0]
+    dest = dest_col[0]
 
-for source, dest in zip(PCR_plate.wells('F1', length=3),
-                        flat_plate.wells('F1', length=3)):
-    p10.pick_up_tip()
-    p10.transfer(3,
+    m10.pick_up_tip()
+    m10.transfer(3, source, dest, blow_out=True, new_tip='never')
+    m10.touch_tip(dest)
+    m10.drop_tip()
+
+tip_sources = tips10.wells('H12', to='F12')
+for (tip, source, dest) in zip(tip_sources,
+                               PCR_plate.wells('F1', length=3),
+                               flat_plate.wells('F1', length=3)):
+    m10.pick_up_tip(tip)
+    m10.transfer(3,
                  source,
                  dest,
                  blow_out=True,
                  new_tip='never')
-    p10.touch_tip(dest)
-    p10.drop_tip()
+    m10.touch_tip(dest)
+    m10.drop_tip()
 
 robot.pause('Replace the 96-flat plate with a new empty 96-flat plate before '
             'resuming.')
 
-p300.pick_up_tip()
-for row in flat_plate.rows('A', length=5):
-    p300.transfer(75, solution, row[0:2], new_tip='never')
-p300.drop_tip()
+m300.pick_up_tip(tips300.wells('G12'))
+dests = [well.top() for well in flat_plate.rows('A')][0:5]
+m300.distribute(75, solution, dests, blow_out=True, new_tip='never')
+m300.drop_tip()
 
-for source, dest in zip(PCR_plate.wells('A2', length=5),
-                        flat_plate.wells('A2', length=5)):
-    p10.pick_up_tip()
-    p10.transfer(3,
+for (tip, source, dest) in zip(tips10.wells('E12', to='A12'),
+                               PCR_plate.wells('A2', length=5),
+                               flat_plate.wells('A2', length=5)):
+    m10.pick_up_tip(tip)
+    m10.transfer(3,
                  source,
                  dest,
                  blow_out=True,
                  new_tip='never')
-    p10.touch_tip(dest)
-    p10.drop_tip()
+    m10.touch_tip(dest)
+    m10.drop_tip()
