522723bf5ca36b462cb9311cd0ff687131660541
ncdiehl11 <ndiehl@opentrons.com>
2019-06-12 11:19:27 -0400

    1602

---
+++ protocols/1602-part1/README.md
@@ -0,0 +1,71 @@
+# NEBNext® Ultra™ II RNA Library Prep With MagDeck: Part 1/3 - mRNA Isolation, cDNA Synthesis
+
+### Author
+[Opentrons](http://www.opentrons.com/)
+
+## Categories
+* NGS Library Prep
+    * NEBNext Ultra II
+
+## Description
+Part 1 of 3: mRNA Isolution, cDNA Synthesis
+
+Links:
+* [Part 1: mRNA Isolution, cDNA Synthesis](./1602-part1)
+* [Part 2: End Prep of cDNA Library, Adaptor Ligation](./1602-part2)
+* [Part 3: PCR Enrichment](./1602-part3)
+
+With this protocol, your robot can perform the NEBNext® Ultra™ II RNA Library Prep as described by the [NEBNext® Ultra™ II RNA Library Prep Kit for Illumina® Instruction Manual](https://www.neb.com/-/media/catalog/datacards-or-manuals/manuale7770.pdf) on an Opentrons magnetic module. For reagent setup, please see 'Additional Notes' below.
+
+---
+
+You will need:
+* [Opentrons P10 multi-channel electronic pipette](https://shop.opentrons.com/collections/ot-2-pipettes/products/8-channel-electronic-pipette)
+* [Opentrons P300 multi-channel electronic pipette](https://shop.opentrons.com/collections/ot-2-pipettes/products/8-channel-electronic-pipette?variant=5984202489885)
+* [Opentrons 10ul pipette tips](https://shop.opentrons.com/collections/opentrons-tips/products/opentrons-10ul-tips)
+* [Opentrons 300ul pipette tips](https://shop.opentrons.com/collections/opentrons-tips/products/opentrons-300ul-tips)
+* [Bio-Rad Hard-Shell® 96-well PCR plates](http://www.bio-rad.com/en-us/sku/hss9601-hard-shell-96-well-pcr-plates-high-profile-semi-skirted-clear-clear?ID=HSS9601)
+* [12-channel reservoir for automation, USA Scientific # 1061-8150](https://www.usascientific.com/12-channel-automation-reservoir.aspx)
+
+### Robot
+* [OT-2](https://opentrons.com/ot-2)
+
+### Modules
+* [Opentrons Magnetic Module](https://shop.opentrons.com/collections/hardware-modules/products/magdeck)
+
+### Reagents
+* Please see Instruction Manual listed in 'Description' above.
+
+## Process
+1. Input which columns will be processed as a single list separated by commas (example: 1,2,3,4,5,6,7,8,9,10,11,12)
+2. Download your protocol.
+3. Upload your protocol into the [OT App](https://opentrons.com/ot-app).
+4. Set up your deck according to the deck map.
+5. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support article](https://support.opentrons.com/ot-2/getting-started-software-setup/deck-calibration).
+6. Hit "Run".
+
+### Additional Notes
+Reagent setup in trough 1 (slot 2):
+* Oligo Beads: A1
+* Wash Buffer: A2-A4
+* Tris Buffer: A5
+* Binding Buffer: A6
+* First Strand Master Mix: A7
+* Water: A8
+* First Strand Enzyme Mix: A9
+* Second Strand Reaction Buffer: A10
+* Second Strand Enzyme Mix: A11
+* Ethanol: A12
+
+Reagent setup in trough 2 (slot 3):
+* TE Buffer: A1
+
+---
+
+Review the reference guide before proceeding to confirm kit contents and make sure you have the required equipment and consumables.
+
+If you have any questions about this protocol, please contact protocols@opentrons.com.
+
+###### Internal
+YLrZZn8Z  
+1602
---
+++ protocols/1602-part2/adaptor_ligation.ot2.py
@@ -0,0 +1,182 @@
+from opentrons import labware, instruments, modules, robot
+
+metadata = {
+    'protocolName': 'End Prep of cDNA Library, Adaptor Ligation',
+    'author': 'Nick <protocols@opentrons.com>',
+    'source': 'Custom Protocol Request'
+}
+
+# labware and modules
+magdeck = modules.load('magdeck', '1')
+mag_plate = labware.load('biorad-hardshell-96-PCR', '1', share=True)
+trough = labware.load('trough-12row', '2')
+new_plate = labware.load('biorad-hardshell-96-PCR', '3', 'fresh plate')
+
+tipracks_m10 = [labware.load('tiprack-10ul', slot)
+                for slot in ['9', '10', '11']]
+tipracks_m300 = [labware.load('tiprack-200ul', slot)
+                 for slot in ['4', '5', '6', '7', '8']]
+
+# pipette setup
+m10 = instruments.P10_Multi(
+    mount='left',
+    tip_racks=tipracks_m10)
+
+m300 = instruments.P300_Multi(
+    mount='right',
+    tip_racks=tipracks_m300)
+
+# reagent setup
+end_prep_reaction_buffer = trough.wells('A1')
+end_prep_enzyme = trough.wells('A2')
+diluted_adapter = trough.wells('A3')
+ligation_enhancer = trough.wells('A4')
+ligation_mastermix = trough.wells('A5')
+USER_enzyme = trough.wells('A6')
+oligo_beads = trough.wells('A7')
+ethanol = trough.wells('A8')
+TE_buffer = trough.wells('A9')
+
+
+def run_custom_protocol(
+        sample_columns_separated_by_commas: str = '1,2,3,4,5,6,7,8,9,10,11,12'
+):
+
+    m300_tip_count = 0
+    m10_tip_count = 0
+
+    acceptable_cols = [str(col) for col in range(1, 13)]
+    cols = [col.strip()
+            for col in sample_columns_separated_by_commas.split(',')]
+    # check input columns
+    for col in cols:
+        if col not in acceptable_cols:
+            raise Exception('Invalid column input.')
+
+    mag_loc = [mag_plate.columns(col) for col in cols]
+    new_loc = [new_plate.columns(col) for col in cols]
+
+    def update_m300_tip_count(num):
+        nonlocal m300_tip_count
+        m300_tip_count += num
+        if m300_tip_count == 12 * len(tipracks_m300):
+            robot.pause("Your P300 tips have run out, please refill the tip "
+                        "racks in slot 4, 5, 6, 7, and 8. Resume after you "
+                        "have finished.")
+            m300.reset()
+            m300_tip_count = 0
+
+    def update_m10_tip_count(num):
+        nonlocal m10_tip_count
+        m10_tip_count += num
+        if m10_tip_count == 12 * len(tipracks_m10):
+            robot.pause("Your P10 tips have run out, please refill the tip "
+                        "racks in slot 9, 10, and 11. Resume after you have "
+                        "finished.")
+            m10.reset()
+            m10_tip_count = 0
+
+    # Transfer 7 uL NEBNext Ultra II End Prep Reaction Buffer to plate
+    m10.transfer(
+        7,
+        end_prep_reaction_buffer,
+        [loc[0].top() for loc in mag_loc]
+        )
+    update_m10_tip_count(1)
+
+    # Transfer 3 uL NEBNext Ultra II End Prep Enzyme to plate
+    for loc in mag_loc:
+        m10.pick_up_tip()
+        m10.transfer(3, end_prep_enzyme, loc, new_tip='never')
+        m10.mix(10, 10, loc)
+        m10.blow_out(loc)
+        m10.drop_tip()
+        update_m10_tip_count(1)
+
+    robot.pause("Place the plate in the thermal cycler. Place it back on the "
+                "magdeck when finished.")
+
+    # Transfer 2.5 uL Diluted Adapter to plate
+    m10.distribute(2.5, diluted_adapter, [loc[0].top() for loc in mag_loc])
+    update_m10_tip_count(1)
+
+    # Transfer 1 uL NEBNext Ligation Enhancer to plate
+    m10.distribute(1, ligation_enhancer, [loc[0].top() for loc in mag_loc])
+    update_m10_tip_count(1)
+
+    # Transfer 30 uL of NEBNext Ultra II Ligation Master Mix to plate
+    for loc in mag_loc:
+        m300.pick_up_tip()
+        m300.transfer(30, ligation_mastermix, loc, new_tip='never')
+        m300.mix(10, 80, loc)
+        m300.blow_out(loc)
+        m300.drop_tip()
+        update_m300_tip_count(1)
+
+    robot.pause("Place the plate in the thermal cycler. Place it back on the "
+                "magdeck when finished.")
+
+    # Transfer 3 uL USER Enzyme to plate
+    for loc in mag_loc:
+        m10.pick_up_tip()
+        m10.transfer(3, USER_enzyme, loc, new_tip='never')
+        m10.mix(20, 10, loc)
+        m10.blow_out(loc)
+        m10.drop_tip()
+        update_m10_tip_count(1)
+
+    robot.pause("Place the plate in the thermal cycler. Place it back on the "
+                "magdeck when finished.")
+
+    # Transfer 87 uL oligo beads to plate on the MagDeck
+    for loc in mag_loc:
+        m300.pick_up_tip()
+        m300.transfer(87, oligo_beads, loc, new_tip='never')
+        m300.mix(10, 50, loc)
+        m300.blow_out(loc)
+        m300.drop_tip()
+        update_m300_tip_count(1)
+
+    m300.delay(minutes=5)
+    robot._driver.run_flag.wait()
+    magdeck.engage()
+    robot.pause("Resume after solution becomes clear.")
+
+    # Discard 183 uL supernatant
+    for loc in mag_loc:
+        m300.transfer(183, loc, m300.trash_container.top())
+        update_m300_tip_count(1)
+
+    # Wash plate with 200 uL 80% Ethanol twice
+    for wash_cycle in range(2):
+        m300.transfer(200, ethanol, [loc[0].top() for loc in mag_loc])
+        update_m300_tip_count(1)
+        m300.delay(seconds=30)
+        for loc in mag_loc:
+            m300.transfer(200, loc, m300.trash_container.top())
+            update_m300_tip_count(1)
+
+    m300.delay(minutes=5)
+    robot._driver.run_flag.wait()
+    magdeck.disengage()
+
+    # Transfer 17 uL TE Buffer to plate
+    for loc in mag_loc:
+        m10.pick_up_tip()
+        m10.transfer(17, TE_buffer, loc, new_tip='never')
+        m10.mix(10, 10, loc)
+        m10.blow_out(loc)
+        m10.drop_tip()
+        update_m10_tip_count(1)
+
+    magdeck.engage()
+    robot.pause("Place a new clean plate in slot 3. Resume after solution"
+                "becomes clear.")
+
+    # Transfer supernatant to new plate in slot 3
+    for source, dest in zip(mag_loc, new_loc):
+        m300.pick_up_tip()
+        m300.transfer(30, source, dest, new_tip='never')
+        m300.blow_out(dest)
+        m300.drop_tip()
+        update_m300_tip_count(1)
