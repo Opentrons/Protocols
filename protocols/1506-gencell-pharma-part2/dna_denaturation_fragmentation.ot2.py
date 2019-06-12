a5ac064dd6f7701c96c3c0945048ab66f0ef2eed
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2019-06-12 15:10:04 -0400

    update all protocols

--- protocols/1506-gencell-pharma-part2/dna_denaturation_fragmentation.ot2.py
+++ protocols/1506-gencell-pharma-part2/dna_denaturation_fragmentation.ot2.py
@@ -1,4 +1,5 @@
 from opentrons import labware, instruments
+from otcustomizers import StringSelection
 import math
 
 metadata = {
@@ -7,45 +8,35 @@ metadata = {
     'source': 'Custom Protocol Request'}
 
 
-def run_custom_protocol(sample_num: int=16):
+def run_custom_protocol(
+        sample_num: StringSelection('4', '8', '12', '16')='4'
+        ):
+
+    sample_num = int(sample_num)
 
     # labware setup
-    labware.load('tempdeck', '10')
-    temp_rack = labware.load(
-        'opentrons-aluminum-block-2ml-eppendorf', '10', share=True)
-    samples_plate = labware.load('PCR-strip-tall', '1')
-    tiprack_10 = labware.load('tiprack-10ul', '6')
-    tiprack_300 = labware.load('opentrons-tiprack-300ul', '5')
+    eppendorf_rack = labware.load('opentrons-tuberack-2ml-screwcap', '4')
+    sample_plate = labware.load('PCR-strip-tall', '1')
+    tiprack_300 = labware.load('opentrons-tiprack-300ul', '9')
 
     # pipette setup
-    m10 = instruments.P10_Multi(
-        mount='left',
-        tip_racks=[tiprack_10])
-
-    m50 = instruments.P50_Multi(
+    p50 = instruments.P50_Single(
         mount='right',
         tip_racks=[tiprack_300])
 
     # reagent setup
-    randomprim = temp_rack.wells('A1')
-    samples = [well for well in samples_plate.wells('A1', length=sample_num)]
-    if sample_num <= 8:
-        sample_cols = [samples_plate.cols('1')]
-    else:
-        sample_cols = samples_plate.cols('1', length=math.ceil(sample_num/8))
+    randomprim = eppendorf_rack.wells('A1')
+
+    samples = [well
+               for well in sample_plate.wells('A1', length=sample_num*2)]
 
     # Transfer random primer
-    m50.pick_up_tip()
-    m50.mix(5, 30, randomprim)
+    p50.pick_up_tip()
+    p50.mix(10, 30, randomprim)
     for sample in samples:
-        if m50.current_volume <= 5:
-            m50.aspirate(randomprim)
-        m50.dispense(5, sample)
-    m50.drop_tip()
-
-    # mix samples
-    for col in sample_cols:
-        m10.pick_up_tip()
-        m10.mix(5, 10, col)
-        m10.blow_out(col[0].bottom(5))
-        m10.drop_tip()
+        if not p50.tip_attached:
+            p50.pick_up_tip()
+        p50.transfer(5, randomprim, sample, new_tip='never')
+        p50.mix(5, 10, sample)
+        p50.blow_out(sample)
+        p50.drop_tip()
