4a85e228dd7c4ece5f82f384994fef401ecc0466
ncdiehl11 <ndiehl@opentrons.com>
2019-07-01 15:40:43 -0400

    fix-1555-3

--- protocols/1555-absci/nucleic_acid_purification.ot2.py
+++ protocols/1555-absci/nucleic_acid_purification.ot2.py
@@ -36,9 +36,9 @@ if mag_plate_name not in labware.list():
                    volume=2000)
 
 # load labware
+pur4_plate = labware.load(plate_name, '1', 'plate for PUR4')
 trough = labware.load(trough_name, '2')
 bacteria_plate = labware.load(plate_name, '3', 'bacteria plate')
-pur4_plate = labware.load(plate_name, '1', 'plate for PUR4')
 tips300 = [labware.load('opentrons-tiprack-300ul', slot)
            for slot in ['5', '6', '7', '9', '10']]
 tips50 = labware.load('opentrons-tiprack-300ul', '11')
@@ -52,9 +52,9 @@ RE1_1 = trough.wells('A1')
 L2 = trough.wells('A2')
 N3 = trough.wells('A3')
 isopropanol = trough.wells('A4')
-EtOH = trough.wells('A5')
-RE1_2 = trough.wells('A6')
-waste = trough.wells('A12')
+EtOH = trough.wells('A5', length=3)
+TE = trough.wells('A8')
+waste = trough.wells('A9', length=4)
 
 PUR4 = pur4_plate.wells('A1')
 
@@ -68,21 +68,22 @@ def run_custom_protocol(
 
     num_columns = int(columns_to_process)
     if pipettes_type == 'single':
-        pipette50 = instruments.P50_Single(mount='right', tip_racks=tips50)
+        pipette50 = instruments.P50_Single(mount='right', tip_racks=[tips50])
         pipette300 = instruments.P300_Single(mount='left', tip_racks=tips300)
         bacteria_samples = bacteria_plate.wells('A1', length=8*num_columns)
         mag_samples = mag_plate.wells('A1', length=8*num_columns)
     else:
-        pipette50 = instruments.P50_Multi(mount='right', tip_racks=tips50)
+        pipette50 = instruments.P50_Multi(mount='right', tip_racks=[tips50])
         pipette300 = instruments.P300_Multi(mount='left', tip_racks=tips300)
-        bacteria_samples = bacteria_plate.rows['A'][0:num_columns]
-        mag_samples = mag_plate.rows['A'][0:num_columns]
+        bacteria_samples = bacteria_plate.rows('A')[0:num_columns]
+        mag_samples = mag_plate.rows('A')[0:num_columns]
 
     # distribute RE1 and mix
     for s in bacteria_samples:
         pipette300.pick_up_tip()
         pipette300.transfer(100, RE1_1, s, new_tip='never')
         pipette300.mix(20, 80, s)
+        pipette300.blow_out(s.top())
         pipette300.drop_tip()
 
     # distribute L2 and mix
@@ -90,6 +91,7 @@ def run_custom_protocol(
         pipette300.pick_up_tip()
         pipette300.transfer(100, L2, s, new_tip='never')
         pipette300.mix(2, 190, s)
+        pipette300.blow_out(s.top())
         pipette300.drop_tip()
 
     # incubate for 3 minutes at room temperature
@@ -106,35 +108,68 @@ def run_custom_protocol(
     dests = mag_plate.wells('A1', length=8*num_columns)
     pipette50.pick_up_tip(tips50.wells('H12'))
     pipette50.mix(5, 50, PUR4)
-    pipette50.distribute(10, PUR4, [d.top() for d in dests], new_tip='never')
+    pipette50.blow_out(PUR4.top())
+    pipette50.distribute(
+        10,
+        PUR4,
+        [d.bottom() for d in dests],
+        new_tip='never'
+    )
     pipette50.drop_tip()
 
     # transfer liquid from bacteria plate to magnetic plate
     for bac, mag in zip(bacteria_samples, mag_samples):
-        pipette300.transfer(240, bac, mag.top())
+        pipette300.transfer(100, bac, mag.top())
         pipette300.pick_up_tip()
-        pipette300.transfer(165, isopropanol, mag, new_tip='never')
-        pipette300.mix(10, 300, mag)
+        pipette300.transfer(80, isopropanol, mag, new_tip='never')
+        pipette300.mix(10, 150, mag)
+        pipette300.blow_out(mag.top())
         pipette300.drop_tip()
 
     # incubate on engaged magnet for 8 minutes
     magdeck.engage(height=18)
     pipette300.delay(minutes=8)
 
+    robot.pause('Please replace 300ul tipracks in slots 5-10 before resuming.')
+    pipette300.reset()
+
     # transfer out supernatant
-    pipette300.transfer(300, mag_samples, waste, new_tip='always')
+    for m in mag_samples:
+        pipette300.pick_up_tip()
+        pipette300.transfer(
+            300,
+            m.bottom(2.5),
+            waste[0],
+            new_tip='never'
+        )
+        pipette300.drop_tip()
 
     # perform 3 EtOH washes
-    for _ in range(3):
-        pipette300.distribute(200, EtOH, [s.top() for s in mag_samples])
+    for e, w in zip(EtOH, waste[1:4]):
+        pipette300.pick_up_tip()
+        pipette300.distribute(
+            200,
+            e,
+            [s.top() for s in mag_samples],
+            new_tip='never'
+        )
         pipette300.delay(seconds=30)
-        pipette300.transfer(300, mag_samples, waste, new_tip='always')
+        for m in mag_samples:
+            if not pipette300.tip_attached:
+                pipette300.pick_up_tip()
+            pipette300.transfer(
+                300,
+                m.bottom(2.5),
+                w.top(),
+                new_tip='never'
+            )
+            pipette300.drop_tip()
 
     # allow to dry for 10 minutes
     pipette300.delay(minutes=10)
 
-    # distribute RE1 to top of each well to not contaminate tips
-    pipette300.distribute(40, RE1_2, [s.top() for s in mag_samples])
+    # distribute TE to top of each well to not contaminate tips
+    pipette300.distribute(40, TE, [s.top() for s in mag_samples])
 
     magdeck.disengage()
 
