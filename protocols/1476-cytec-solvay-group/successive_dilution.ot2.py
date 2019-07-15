c07706d2e38f6bd076c893407ee7c5bea3a680f4
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2019-07-15 17:28:40 -0400

    fix-1476

--- protocols/1476-cytec-solvay-group/successive_dilution.ot2.py
+++ protocols/1476-cytec-solvay-group/successive_dilution.ot2.py
@@ -1,4 +1,5 @@
 from opentrons import labware, instruments
+from otcustomizers import StringSelection
 
 metadata = {
     'protocolName': 'Successive Dilution',
@@ -15,40 +16,37 @@ if trough_A_name not in labware.list():
         diameter=76,
         depth=64)
 
-tube_b_name = 'wheaton-serum-bottle-30-ml'
-if tube_b_name not in labware.list():
-    labware.create(
-        tube_b_name,
-        grid=(4, 2),
-        spacing=(29.1, 34.5),
-        diameter=25,
-        depth=92)
-
-# labware setup
-a_rack = labware.load(trough_A_name, '7')
-b_racks = [labware.load(tube_b_name, slot)
-           for slot in ['3', '6']]
-c_racks = [labware.load('opentrons-tuberack-15ml', slot)
-           for slot in ['1', '2', '4', '5']]
-tipracks = [labware.load('tiprack-1000ul', slot)
-            for slot in ['8', '9', '10']]
-
-# instruments setup
-p1000 = instruments.P1000_Single(
-    mount='left',
-    tip_racks=tipracks)
-
-# reagent setup
-solution_a = a_rack.wells('A1')
-solution_b = [well for rack in b_racks for well in rack]
-solution_c = [well for rack in c_racks for well in rack][:48]
-
 
 def run_custom_protocol(
         num_of_samples: int=2,
         num_of_dilutions: int=3,
         solution_A_volume: float=9000,
-        dilution_volume: float=1000):
+        dilution_volume: float=1000,
+        solution_B_container: StringSelection(
+            'opentrons-tuberack-15ml',
+            'opentrons-tuberack-2ml-eppendorf')='opentrons-tuberack-15ml',
+        dilution_tube_container: StringSelection(
+            'opentrons-tuberack-15ml',
+            'opentrons-tuberack-2ml-eppendorf')='opentrons-tuberack-15ml'):
+
+    # labware setup
+    a_rack = labware.load(trough_A_name, '7')
+    b_racks = [labware.load(solution_B_container, slot)
+               for slot in ['3', '6']]
+    c_racks = [labware.load(dilution_tube_container, slot)
+               for slot in ['1', '2', '4', '5']]
+    tipracks = [labware.load('tiprack-1000ul', slot)
+                for slot in ['8', '9', '10']]
+
+    # instruments setup
+    p1000 = instruments.P1000_Single(
+        mount='left',
+        tip_racks=tipracks)
+
+    # reagent setup
+    solution_a = a_rack.wells('A1')
+    solution_b = [well for rack in b_racks for well in rack]
+    solution_c = [well for rack in c_racks for well in rack][:48]
 
     # transfer solution A to C
     p1000.pick_up_tip()
