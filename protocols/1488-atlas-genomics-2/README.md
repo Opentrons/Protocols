9d73fdea048f8196efa5d7b451a5921a494051d9
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2019-07-26 11:21:27 -0400

    1488-atlas-genomics-2

---
+++ protocols/1488-atlas-genomics-2/README.md
@@ -0,0 +1,71 @@
+# Serial Dilution with CSV Input
+
+### Author
+[Opentrons](http://www.opentrons.com/)
+
+## Categories
+* Basic Pipetting
+    * Serial Dilution
+
+## Description
+With this protocol, your robot can perform serial dilution on up to 40 samples. The number of dilutions and each buffer and sample volume are defined by a CSV input. See Additional Notes for more details on the required CSV layout. After the serial dilutions are completed, the robot will transfer each final dilution sample in duplicate to a new assay plate (bDNA plate) containing a specific amount of buffer.
+
+---
+
+You will need:
+* P10 Multi-channel Pipette
+* P300 Multi-channel Pipette
+* 96-well Plates
+* 12-row Troughs
+* 10 uL Tip Rack
+* Opentrons 300 uL Tip Rack
+
+### Robot
+* [OT-2](https://opentrons.com/ot-2)
+
+### Modules
+
+### Reagents
+
+## Process
+1. Input the number of samples you are processing.
+2. Upload your dilution CSV.
+3. Input the starting column to fill samples in the bDNA plate.
+4. Input the volume of buffer and samples to be transferred to the bDNA plate.
+5. Select your output plate
+6. Download your protocol.
+7. Upload your protocol into the [OT App](https://opentrons.com/ot-app).
+8. Set up your deck according to the deck map.
+9. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support article](https://support.opentrons.com/ot-2/getting-started-software-setup/deck-calibration).
+10. Hit "Run".
+11. Robot will distribute buffer from 12-row trough in slot 7 to the dilution plate(s).
+12. Robot will serially dilute samples down the plate(s).
+13. Robot will distribute buffer from 12-row trough in slot 9 to the bDNA plate.
+14. Robot will transfer the last dilutions to the bDNA plate in duplicate.
+
+
+### Additional Notes
+Sample Plate(s) Setup:
+
+![sample plate](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/1488-atlas-genomics/sample_layout.png)
+
+* Sample plates are in slot 1-5, you will only need all 5 plates if you have 40 samples and more than 5 dilutions (the max you can do is 11 dilutions per sample)
+* Use a new plate if the next group of samples and their dilutions cannot fit on the rest of the plate
+
+---
+
+CSV Layout:
+
+![csv_layout](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/1488-atlas-genomics/csv_layout.png)
+
+* This creates Example 1 layout from above
+* Keep the headers
+* Each row represents each dilution
+
+---
+
+If you have any questions about this protocol, please contact protocols@opentrons.com.
+
+###### Internal
+TB9isL2R
+1488
---
+++ protocols/1488-atlas-genomics-2/cherrypicking_serial_dilution_bdna.ot2.py
@@ -0,0 +1,214 @@
+from opentrons import labware, instruments
+from otcustomizers import FileInput, StringSelection
+import math
+
+metadata = {
+    'protocolName': 'Serial Dilution',
+    'author': 'Alise <protocols@opentrons.com>',
+    'source': 'Custom Protocol Request'
+    }
+
+# tiprack-custom-200ul
+tiprack_200_name = 'tiprack-custom-200ul'
+if tiprack_200_name not in labware.list():
+    labware.create(
+        tiprack_200_name,
+        grid=(12, 8),
+        spacing=(9, 9),
+        diameter=8,
+        depth=52,
+        volume=200)
+
+# tiprack-custom-10ul
+tiprack_10_name = 'tiprack-custom-10ul'
+if tiprack_10_name not in labware.list():
+    labware.create(
+        tiprack_10_name,
+        grid=(12, 8),
+        spacing=(9, 9),
+        diameter=4.2,
+        depth=44,
+        volume=10)
+
+# bDNA plate
+bDNA_plate_name = 'bDNA plate'
+if bDNA_plate_name not in labware.list():
+    labware.create(
+        bDNA_plate_name,
+        grid=(12, 8),
+        spacing=(9, 9),
+        diameter=7,
+        depth=11,
+        volume=300)
+
+# trough_300ul
+trough_name = 'trough_300ul'
+if trough_name not in labware.list():
+    labware.create(
+        trough_name,
+        grid=(1, 1),
+        spacing=(0, 0),
+        diameter=0,
+        depth=41,
+        volume=300000)
+
+
+# plate_200ul_noskirt
+plate_name = 'plate_200ul_noskirt'
+if plate_name not in labware.list():
+    labware.create(
+        plate_name,
+        grid=(12, 8),
+        spacing=(9, 9),
+        diameter=6,
+        depth=21,
+        volume=200)
+
+# deep well plate
+deep_plate_name = 'deep_well_plate'
+if deep_plate_name not in labware.list():
+    labware.create(
+        deep_plate_name,
+        grid=(12, 8),
+        spacing=(9, 9),
+        diameter=7,
+        depth=22,
+        volume=500)
+
+# labware setup
+sample_plates = [labware.load(plate_name, str(slot))
+                 for slot in range(1, 6)]
+trough = labware.load(trough_name, '7')
+trough_2 = labware.load(deep_plate_name, '9')
+tiprack_10 = labware.load(tiprack_10_name, '8')
+tiprack_300 = labware.load(tiprack_200_name, '11')
+
+# instruments
+m10 = instruments.P10_Multi(
+    mount='left',
+    tip_racks=[tiprack_10])
+m300 = instruments.P300_Multi(
+    mount='right',
+    tip_racks=[tiprack_300])
+
+
+dilution_csv_example = """
+Buffer Volume,Sample Volume
+45,5
+45,5
+45,5
+90,10
+"""
+
+
+def transform_volumes(volume):
+    if volume <= 200:
+        return [volume]
+    else:
+        if volume % 200 >= 30 and volume % 200 <= 200:
+            return [200] * int(volume // 200) + [volume % 200]
+        else:
+            volume_list = [200] * int(volume // 200) + [volume % 200]
+            new_vol = (volume_list.pop(-2) + volume_list.pop(-1)) / 2
+            [volume_list.append(new_vol) for _ in range(2)]
+            return volume_list
+
+
+def csv_to_list(csv_string):
+    info_list = [cell for line in csv_string.splitlines() if line
+                 for cell in [line.split(',')]]
+    buffer_vols = []
+    sample_vols = []
+    for line in info_list[1:]:
+        buffer_vols.append(float(line[0]))
+        sample_vols.append(float(line[1]))
+    return buffer_vols, sample_vols
+
+
+def run_custom_protocol(
+        number_of_samples: int=40,
+        dilution_csv: FileInput=dilution_csv_example,
+        starting_column: str='3',
+        bDNA_buffer_volume: float=300,
+        bDNA_sample_volume: float=40,
+        output_plate_type: StringSelection(
+            'plate_200ul_noskirt', 'bDNA plate')='bDNA plate'):
+
+    output_plate = labware.load(output_plate_type, '6')
+
+    buffer_vols, sample_vols = csv_to_list(dilution_csv)
+
+    sample_col_num = math.ceil(number_of_samples / 8)
+    dil_cols = len(buffer_vols) + 1
+    groups_per_plate = 12 // dil_cols
+
+    # define dilution groups
+    dil_groups = [plate.cols(index * dil_cols, length=dil_cols)
+                  for plate in sample_plates
+                  for index in range(groups_per_plate)][:sample_col_num]
+
+    # reagent setup
+    diluent = trough.wells('A1')
+    diluent_volume_tracker = diluent.max_volume()
+
+    # distribute diluent
+    buffer_volumes = buffer_vols * sample_col_num
+    buffer_loc = [col for group in dil_groups for col in group[1:]]
+    m300.pick_up_tip()
+    for vol, col in zip(buffer_volumes, buffer_loc):
+        for new_vol in transform_volumes(vol):
+            if m300.current_volume <= new_vol:
+                if diluent_volume_tracker < new_vol * 8:
+                    diluent = next(diluent)
+                    diluent_volume_tracker = diluent.max_volume()
+                m300.blow_out(diluent.top())
+                m300.aspirate(200, diluent)
+                diluent_volume_tracker -= new_vol * 8
+            m300.dispense(new_vol, col)
+    m300.drop_tip()
+
+    # serial dilute samples in groups
+    for group in dil_groups:
+        m10.pick_up_tip()
+        for vol, source, dest in zip(sample_vols, group[:-1], group[1:]):
+            m10.set_flow_rate(aspirate=5, dispense=10)
+            m10.transfer(vol, source, dest, new_tip='never')
+            m10.set_flow_rate(aspirate=10, dispense=20)
+            m10.mix(5, 10, dest)
+        m10.drop_tip()
+
+    m10.set_flow_rate(aspirate=5, dispense=10)
+
+    # reagent setup
+    diluent_s = trough_2.wells('A1')
+    diluent_s_volume_tracker = diluent_s.max_volume()
+
+    # transfer dilent to bDNA plate
+    m300.pick_up_tip()
+    m300.aspirate(200, diluent_s)
+    for col in output_plate.cols():
+        for new_vol in transform_volumes(bDNA_buffer_volume):
+            if m300.current_volume <= bDNA_buffer_volume:
+                if diluent_s_volume_tracker < bDNA_buffer_volume * 8:
+                    diluent_s = next(diluent_s)
+                    diluent_s_volume_tracker = diluent_s.max_volume()
+                m300.blow_out(diluent_s.top())
+                m300.aspirate(200, diluent_s)
+                diluent_s_volume_tracker -= bDNA_buffer_volume * 8
+            m300.dispense(new_vol, col)
+    m300.dispense(diluent_s.top())
+    m300.drop_tip()
+
+    # transfer sample to bDNA plate
+    bDNA_dests = [col for col in output_plate.cols(starting_column, to='12')]
+    if len(bDNA_dests) % 2 == 1:
+        bDNA_dests.pop(-1)
+    bDNA_sources = [group[-1] for group in dil_groups]
+    bDNA_groups = [bDNA_dests[i*2:i*2+2] for i in range(len(dil_groups))]
+    if bDNA_sample_volume > 10:
+        pipette = m300
+    else:
+        pipette = m10
+    for source, dests in zip(bDNA_sources, bDNA_groups):
+        for dest in dests:
+            pipette.transfer(bDNA_sample_volume, source, dest)
