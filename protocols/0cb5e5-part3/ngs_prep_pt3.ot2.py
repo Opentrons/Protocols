765627426de977af57a79b5617a7094f79b25c78
ncdiehl11 <ndiehl@opentrons.com>
2019-08-12 18:03:59 -0400

    0cb5e5

---
+++ protocols/0cb5e5-part3/ngs_prep_pt3.ot2.py
@@ -0,0 +1,141 @@
+from opentrons import labware, instruments, modules, robot
+from otcustomizers import StringSelection
+import math
+
+metadata = {
+    'protocolName': 'NGS Library Prep Part 3/4: M5 PCR Amplification',
+    'author': 'Nick <protocols@opentrons.com>',
+    'source': 'Custom Protocol Request'
+}
+
+# create custom plate
+plate_name = 'Abgene-MIDI-96-800ul'
+if plate_name not in labware.list():
+    labware.create(
+        plate_name,
+        grid=(12, 8),
+        spacing=(9, 9),
+        diameter=7,
+        depth=27,
+        volume=800
+    )
+
+# load modules and labware
+magdeck = modules.load('magdeck', '1')
+magplate = labware.load(plate_name, '1', share=True)
+trough = labware.load('usascientific_12_reservoir_22ml', '2')
+tempdeck = modules.load('tempdeck', '4')
+plate_TM = labware.load(
+    'opentrons_96_aluminumblock_biorad_wellplate_200ul', '4', share=True)
+tempdeck.set_temperature(25)
+tempdeck.wait_for_temp()
+reagent_plate = labware.load(
+    'opentrons_96_aluminumblock_biorad_wellplate_200ul', '5', 'reagent plate')
+plate_2 = labware.load(
+    'opentrons_96_aluminumblock_biorad_wellplate_200ul', '7')
+tips10 = [labware.load('opentrons_96_tiprack_10ul', slot)
+          for slot in ['3', '6', '8']]
+tips50 = [labware.load('opentrons_96_tiprack_300ul', str(slot))
+          for slot in range(9, 12)]
+
+
+def run_custom_protocol(
+        number_of_samples: int = 96,
+        p10_multi_mount: StringSelection('right', 'left') = 'right',
+        p50_multi_mount: StringSelection('left', 'right') = 'left',
+        dual_index: StringSelection('yes', 'no') = 'yes',
+        reagent_starting_column: int = 1
+):
+    # checks
+    if number_of_samples > 96 or number_of_samples < 1:
+        raise Exception('Invalid number of samples')
+    if p10_multi_mount == p50_multi_mount:
+        raise Exception('Invalid pipette mount combination')
+    if reagent_starting_column > 7:
+        raise Exception('Invlaid reagent starting column')
+
+    # pipettes
+    m10 = instruments.P10_Multi(mount=p10_multi_mount, tip_racks=tips10)
+    m50 = instruments.P50_Multi(mount=p50_multi_mount, tip_racks=tips50)
+
+    # reagent setup
+    [fs2e1, fs1, rs, ss1, ss2e2, pcre3] = [
+        reagent_plate.rows('A')[ind]
+        for ind in range(reagent_starting_column-1, reagent_starting_column+5)
+    ]
+    [pb, eb, ps] = [trough.wells(ind) for ind in range(3)]
+    etoh = [chan for chan in trough.wells('A4', length=4)]
+    etoh_waste = [chan for chan in trough.wells('A8', length=4)]
+
+    # sample setup
+    num_cols = math.ceil(number_of_samples/8)
+    samples_TM = plate_TM.rows('A')[:num_cols]
+    samples_2 = plate_2.rows('A')[:num_cols]
+    samples_mag = magplate.rows('A')[:num_cols]
+
+    # tip check function
+    tip10_max = len(tips10)*12
+    tip50_max = len(tips50)*12
+    tip10_count = 0
+    tip50_count = 0
+
+    def tip_check(pipette):
+        nonlocal tip10_count
+        nonlocal tip50_count
+        if pipette == 'p50':
+            tip50_count += 1
+            if tip50_count > tip50_max:
+                m50.reset()
+                tip50_count = 1
+                robot.pause('Please replace 50ul tipracks before resuming.')
+        else:
+            tip10_count += 1
+            if tip10_count > tip10_max:
+                m10.reset()
+                tip10_count = 1
+                robot.pause('Please replace 10ul tipracks before resuming.')
+
+    def etoh_wash(inds):
+        for wash in inds:
+            tip_check('p50')
+            m50.pick_up_tip()
+            m50.transfer(
+                120,
+                etoh[wash],
+                [s.top() for s in samples_mag],
+                new_tip='never'
+            )
+            for s in samples_mag:
+                if not m50.tip_attached:
+                    tip_check('p50')
+                    m50.pick_up_tip()
+                m50.transfer(120, s, etoh_waste[wash], new_tip='never')
+                m50.drop_tip()
+
+    """M5 PCR Amplification"""
+    m10.home()
+    robot.pause("Place the i7 index plate on TM")
+
+    for s, index in zip(samples_2, samples_TM):
+        tip_check('p10')
+        m10.pick_up_tip()
+        m10.transfer(8, pcre3, s, new_tip='never')
+        m10.transfer(5, index, s, new_tip='never')
+        m10.mix(10, 9, s)
+        m10.blow_out(s.bottom(5))
+        m10.drop_tip()
+
+    if dual_index == 'yes':
+        robot.pause("Replace i7 index plate with i5 index plate on TM2")
+        for s, index in zip(samples_2, samples_TM):
+            tip_check('p10')
+            m10.pick_up_tip()
+            m10.transfer(5, s, index, new_tip='never')
+            m10.mix(10, 9, s)
+            m10.blow_out(s.bottom(5))
+            m10.drop_tip()
+
+    robot.comment('Seal the plate and thermocycle; place in slot 7 when \
+finished. Replace the the MIDI plate on the magdeck with a fresh plate. \
+Replace the index plate on TM with a fresh PCR plate. Proceed to Part 4/4: M6 \
+PCR Cleanup')
---
+++ protocols/0cb5e5-part4/ngs_prep_pt4.ot2.py
@@ -0,0 +1,176 @@
+from opentrons import labware, instruments, modules, robot
+from otcustomizers import StringSelection
+import math
+
+metadata = {
+    'protocolName': 'NGS Library Prep Part 4/4: M6 PCR Cleanup',
+    'author': 'Nick <protocols@opentrons.com>',
+    'source': 'Custom Protocol Request'
+}
+
+# create custom plate
+plate_name = 'Abgene-MIDI-96-800ul'
+if plate_name not in labware.list():
+    labware.create(
+        plate_name,
+        grid=(12, 8),
+        spacing=(9, 9),
+        diameter=7,
+        depth=27,
+        volume=800
+    )
+
+# load modules and labware
+magdeck = modules.load('magdeck', '1')
+magplate = labware.load(plate_name, '1', share=True)
+trough = labware.load('usascientific_12_reservoir_22ml', '2')
+tempdeck = modules.load('tempdeck', '4')
+plate_TM = labware.load(
+    'opentrons_96_aluminumblock_biorad_wellplate_200ul', '4', share=True)
+tempdeck.set_temperature(25)
+tempdeck.wait_for_temp()
+reagent_plate = labware.load(
+    'opentrons_96_aluminumblock_biorad_wellplate_200ul', '5', 'reagent plate')
+plate_2 = labware.load(
+    'opentrons_96_aluminumblock_biorad_wellplate_200ul', '7')
+tips10 = [labware.load('opentrons_96_tiprack_10ul', slot)
+          for slot in ['3', '6', '8']]
+tips50 = [labware.load('opentrons_96_tiprack_300ul', str(slot))
+          for slot in range(9, 12)]
+
+
+def run_custom_protocol(
+        number_of_samples: int = 96,
+        p10_multi_mount: StringSelection('right', 'left') = 'right',
+        p50_multi_mount: StringSelection('left', 'right') = 'left',
+        dual_index: StringSelection('yes', 'no') = 'yes',
+        reagent_starting_column: int = 1
+):
+    # checks
+    if number_of_samples > 96 or number_of_samples < 1:
+        raise Exception('Invalid number of samples')
+    if p10_multi_mount == p50_multi_mount:
+        raise Exception('Invalid pipette mount combination')
+    if reagent_starting_column > 7:
+        raise Exception('Invlaid reagent starting column')
+
+    # pipettes
+    m10 = instruments.P10_Multi(mount=p10_multi_mount, tip_racks=tips10)
+    m50 = instruments.P50_Multi(mount=p50_multi_mount, tip_racks=tips50)
+
+    # reagent setup
+    [fs2e1, fs1, rs, ss1, ss2e2, pcre3] = [
+        reagent_plate.rows('A')[ind]
+        for ind in range(reagent_starting_column-1, reagent_starting_column+5)
+    ]
+    [pb, eb, ps] = [trough.wells(ind) for ind in range(3)]
+    etoh = [chan for chan in trough.wells('A4', length=4)]
+    etoh_waste = [chan for chan in trough.wells('A8', length=4)]
+    liquid_waste = trough.wells('A12')
+
+    # sample setup
+    num_cols = math.ceil(number_of_samples/8)
+    samples_TM = plate_TM.rows('A')[:num_cols]
+    samples_2 = plate_2.rows('A')[:num_cols]
+    samples_mag = magplate.rows('A')[:num_cols]
+
+    # tip check function
+    tip10_max = len(tips10)*12
+    tip50_max = len(tips50)*12
+    tip10_count = 0
+    tip50_count = 0
+
+    def tip_check(pipette):
+        nonlocal tip10_count
+        nonlocal tip50_count
+        if pipette == 'p50':
+            tip50_count += 1
+            if tip50_count > tip50_max:
+                m50.reset()
+                tip50_count = 1
+                robot.pause('Please replace 50ul tipracks before resuming.')
+        else:
+            tip10_count += 1
+            if tip10_count > tip10_max:
+                m10.reset()
+                tip10_count = 1
+                robot.pause('Please replace 10ul tipracks before resuming.')
+
+    def etoh_wash(inds):
+        for wash in inds:
+            tip_check('p50')
+            m50.pick_up_tip()
+            m50.transfer(
+                120,
+                etoh[wash],
+                [s.top() for s in samples_mag],
+                new_tip='never'
+            )
+            for s in samples_mag:
+                if not m50.tip_attached:
+                    tip_check('p50')
+                    m50.pick_up_tip()
+                m50.transfer(120, s, etoh_waste[wash], new_tip='never')
+                m50.drop_tip()
+
+    """M6 PCR Cleanup"""
+    vol = 35 if dual_index == 'yes' else 30
+    for source, dest in zip(samples_2, samples_mag):
+        tip_check('p50')
+        m50.pick_up_tip()
+        m50.transfer(vol, source, dest, new_tip='never')
+        m50.mix(15, 45, dest)
+        m50.blow_out(dest.top())
+        m50.drop_tip()
+
+    m50.delay(minutes=5)
+    robot._driver.run_flag.wait()
+    magdeck.engage(height=18)
+    m50.delay(minutes=3)
+
+    sup_vol = 70 if dual_index == 'yes' else 60
+    for s in samples_mag:
+        tip_check('p50')
+        m50.transfer(sup_vol, s, liquid_waste)
+
+    magdeck.disengage()
+
+    for reagent, mix_vol in zip([eb, ps], [25, 45]):
+        for s in samples_mag:
+            tip_check('p50')
+            m50.pick_up_tip()
+            m50.transfer(30, reagent, s, new_tip='never')
+            m50.mix(15, mix_vol, s)
+            m50.drop_tip()
+
+    m50.delay(minutes=5)
+    robot._driver.run_flag.wait()
+    magdeck.engage(height=18)
+    m50.delay(minutes=3)
+
+    for s in samples_mag:
+        tip_check('p50')
+        m50.transfer(60, s, liquid_waste)
+
+    etoh_wash([2, 3])
+
+    m50.delay(minutes=5)
+    magdeck.disengage()
+
+    for s in samples_mag:
+        tip_check('p50')
+        m50.pick_up_tip()
+        m50.transfer(20, eb, s, new_tip='never')
+        m50.mix(15, 15, s)
+        m50.drop_tip()
+
+    m50.delay(minutes=2)
+    robot._driver.run_flag.wait()
+    magdeck.engage(height=18)
+    m50.delay(minutes=3)
+
+    for source, dest in zip(samples_mag, samples_TM):
+        tip_check('p50')
+        m50.transfer(17, source, dest, blow_out=True)
+
+    magdeck.disengage()
