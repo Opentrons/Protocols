16c11713d653f92c8219f1a301765128cd945442
ncdiehl11 <ndiehl@opentrons.com>
2019-08-27 17:04:29 -0400

    update flow rates and mix locations, remove p50 in post tag cleanup

--- protocols/nextera-flex-library-prep-post-tag-cleanup/nextera_flex_post_tag_cleanup.ot2.py
+++ protocols/nextera-flex-library-prep-post-tag-cleanup/nextera_flex_post_tag_cleanup.ot2.py
@@ -15,23 +15,17 @@ mag_plate = labware.load(
     'biorad_96_wellplate_200ul_pcr', '1', 'tagmentation rxn plate', share=True)
 res12 = labware.load(
     'usascientific_12_reservoir_22ml', '3', 'reagent reservoir')
-tips50 = labware.load('opentrons_96_tiprack_300ul', '4')
 
 twb = [chan for chan in res12.wells()[:2]]
 liquid_waste = res12.wells(11).top()
 
 
 def run_custom_protocol(
-        p50_type: StringSelection('single', 'multi') = 'single',
         p300_type: StringSelection('single', 'multi') = 'single',
-        p50_mount: StringSelection('left', 'right') = 'left',
         p300_mount: StringSelection('right', 'left') = 'right',
         number_of_samples_to_process: int = 24
 ):
     # check:
-    if p50_mount == p300_mount:
-        raise Exception('Input different mounts for P50 and P300 multi-channel \
-pipettes')
     if number_of_samples_to_process > 96 or number_of_samples_to_process < 1:
         raise Exception('Invalid number of samples to process (must be between \
 1 and 96).')
@@ -43,47 +37,22 @@ pipettes')
         labware.load('opentrons_96_tiprack_300ul', slot) for slot in slots300]
 
     # pipettes
-    if p50_type == 'multi':
-        pip50 = instruments.P50_Multi(mount=p50_mount, tip_racks=[tips50])
-        samples50 = mag_plate.rows('A')[:num_cols]
-        tsb = labware.load(
-            'opentrons_96_aluminumblock_generic_pcr_strip_200ul',
-            '2',
-            'strips for TSB').wells('A1')
-    else:
-        pip50 = instruments.P50_Single(mount=p50_mount, tip_racks=[tips50])
-        samples50 = mag_plate.wells()[:number_of_samples_to_process]
-        tsb = labware.load(
-            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
-            '2',
-            'tuberack for TSB').wells('A1')
     if p300_type == 'multi':
         pip300 = instruments.P300_Multi(mount=p300_mount, tip_racks=tips300)
         samples300 = mag_plate.rows('A')[:num_cols]
     else:
         pip300 = instruments.P300_Single(mount=p300_mount, tip_racks=tips300)
         samples300 = mag_plate.wells()[:number_of_samples_to_process]
+    pip300.set_flow_rate(aspirate=75, dispense=90)
 
-    # add TSB and slowly mix
-    for s in samples50:
-        pip50.pick_up_tip()
-        pip50.transfer(10, tsb, s, new_tip='never')
-        pip50.set_flow_rate(aspirate=10, dispense=20)
-        pip50.mix(10, 30, s)
-        pip50.blow_out(s.top(-2))
-        pip50.drop_tip()
-
-    robot.pause('Seal the plate with Microseal B, place on the preprogrammed \
-thermal cycler, and run the PTC program.')
-
-    robot._driver.run_flag.wait()
     magdeck.engage(height=18)
-    pip50.delay(minutes=3)
+    robot.comment('Incubating beads on magnet for 3 minutes.')
+    pip300.delay(minutes=3)
 
     # remove and discard supernatant
     for s in samples300:
         pip300.pick_up_tip()
-        pip300.transfer(65, s, liquid_waste, new_tip='never')
+        pip300.transfer(65, s.bottom(), liquid_waste, new_tip='never')
         pip300.blow_out()
         pip300.drop_tip()
 
@@ -94,25 +63,28 @@ thermal cycler, and run the PTC program.')
         magdeck.disengage()
 
         # resuspend beads in TWB
-        for s in samples300:
+        for i, s in enumerate(samples300):
             ind = (count*len(twb))//total_twb
             count += 1
 
-            pip300.set_flow_rate(aspirate=25, dispense=30)
-            disp_loc = (s, s.from_center(r=0.9, h=-0.8, theta=0))
+            side = i % 2 if p300_type == 'multi' else math.floor(i/8) % 2
+            angle = 0 if side == 0 else math.pi
+            disp_loc = (s, s.from_center(r=0.95, h=-0.6, theta=angle))
             pip300.pick_up_tip()
             pip300.transfer(100, twb[ind], disp_loc, new_tip='never')
-            pip300.mix(10, 80, s)
+            pip300.mix(10, 80, disp_loc)
             pip300.drop_tip()
 
         magdeck.engage(height=18)
 
         if wash < 2:
+            robot.comment('Incubating beads on magnet for 3 minutes')
             pip300.delay(minutes=3)
             # remove and discard supernatant
             for s in samples300:
                 pip300.pick_up_tip()
-                pip300.transfer(110, s, liquid_waste, new_tip='never')
+                pip300.transfer(
+                    120, s.bottom(0.5), liquid_waste, new_tip='never')
                 pip300.blow_out()
                 pip300.drop_tip()
 
