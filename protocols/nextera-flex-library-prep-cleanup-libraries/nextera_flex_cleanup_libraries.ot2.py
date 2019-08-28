65177dfbfd8f350354aafa84e2f606dbbc49973d
ncdiehl11 <ndiehl@opentrons.com>
2019-08-28 11:25:01 -0400

    consolidates cleanup steps using only PCR plates

--- protocols/nextera-flex-library-prep-cleanup-libraries/nextera_flex_cleanup_libraries.ot2.py
+++ protocols/nextera-flex-library-prep-cleanup-libraries/nextera_flex_cleanup_libraries.ot2.py
@@ -4,35 +4,22 @@ import math
 
 metadata = {
     'protocolName': 'Nextera DNA Flex NGS Library Prep: Cleanup Libraries \
-Part 2/2',
+Part 1/2',
     'author': 'Nick <protocols@opentrons.com>',
     'source': 'Custom Protocol Request'
 }
 
-# create custom labware
-deep_name = 'thermofisherscientific_96_deepwellplate_800ul'
-if deep_name not in labware.list():
-    labware.create(
-        deep_name,
-        grid=(12, 8),
-        spacing=(9, 9),
-        diameter=7,
-        depth=27,
-        volume=800
-    )
-
 # load labware and modules
 magdeck = modules.load('magdeck', '1')
 mag_plate = labware.load(
-    deep_name, '1', 'PCR rxn plate', share=True)
-deep_plate = labware.load(deep_name, '2', 'new midi plate')
+    'biorad_96_wellplate_200ul_pcr', '1', 'PCR rxn plate', share=True)
+new_plate = labware.load(
+    'biorad_96_wellplate_200ul_pcr', '2', 'new PCR plate')
 res12 = labware.load(
     'usascientific_12_reservoir_22ml', '3', 'reagent reservoir')
-elution_plate = labware.load(
-    'biorad_96_wellplate_200ul_pcr', '4', 'final elution plate')
-slots50 = [str(slot) for slot in range(5, 9)]
+slots50 = [str(slot) for slot in range(4, 8)]
 tips50 = [labware.load('opentrons_96_tiprack_300ul', slot) for slot in slots50]
-slots300 = [str(slot) for slot in range(9, 12)]
+slots300 = [str(slot) for slot in range(8, 12)]
 tips300 = [
     labware.load('opentrons_96_tiprack_300ul', slot) for slot in slots300]
 
@@ -49,9 +36,7 @@ def run_custom_protocol(
         p300_type: StringSelection('single', 'multi') = 'single',
         p50_mount: StringSelection('left', 'right') = 'left',
         p300_mount: StringSelection('right', 'left') = 'right',
-        DNA_type: StringSelection(
-            'standard DNA', 'small PCR amplicon') = 'standard DNA',
-        number_of_samples_to_process: int = 24
+        number_of_samples_to_process: int = 96
 ):
     # check:
     if p50_mount == p300_mount:
@@ -66,26 +51,24 @@ pipettes')
 
     if p50_type == 'multi':
         pip50 = instruments.P50_Multi(mount=p50_mount, tip_racks=tips50)
-        [mag_samples50, deep_samples50, elution_samples] = [
-            plate.rows('A')[:num_cols]
-            for plate in [mag_plate, deep_plate, elution_plate]
-        ]
+        [mag_samples50, new_samples50] = [
+            plate.rows('A')[:num_cols] for plate in [mag_plate, new_plate]]
     else:
         pip50 = instruments.P50_Single(mount=p50_mount, tip_racks=tips50)
-        [mag_samples50, deep_samples50, elution_samples] = [
+        [mag_samples50, new_samples50] = [
             plate.wells()[:number_of_samples_to_process]
-            for plate in [mag_plate, deep_plate, elution_plate]
+            for plate in [mag_plate, new_plate]
         ]
     if p300_type == 'multi':
         pip300 = instruments.P300_Multi(mount=p300_mount, tip_racks=tips300)
-        [mag_samples300, deep_samples300] = [
-            plate.rows('A')[:num_cols] for plate in [mag_plate, deep_plate]]
+        [mag_samples300, new_samples300] = [
+            plate.rows('A')[:num_cols] for plate in [mag_plate, new_plate]]
 
     else:
         pip300 = instruments.P300_Single(mount=p300_mount, tip_racks=tips300)
-        [mag_samples300, deep_samples300] = [
+        [mag_samples300, new_samples300] = [
             plate.wells()[:number_of_samples_to_process]
-            for plate in [mag_plate, deep_plate]
+            for plate in [mag_plate, new_plate]
         ]
 
     def slot_parse(slots):
@@ -126,43 +109,68 @@ pipettes')
             pip300.pick_up_tip()
             tip300_count += 1
 
-    if DNA_type == 'standard DNA':
-        if magdeck.status == 'disengaged':
-            magdeck.engage(height=18)
+    if magdeck.status == 'disengaged':
+        magdeck.engage(height=18)
+    robot.comment('Incubating on magnet for 5 minutes.')
+    pip50.delay(minutes=5)
+
+    # transfer supernatant from mag plate to new plate
+    for source, dest in zip(mag_samples50, new_samples50):
         pick_up('pip50')
-        pip50.mix(20, 40, spb)
-        pip50.blow_out(spb.top())
-        for s in deep_samples50:
-            pip50.transfer(15, spb, s.top(), new_tip='never')
-            pip50.blow_out(s.top())
+        pip50.transfer(45, source.bottom(0.5), dest, new_tip='never')
+        pip50.blow_out()
         pip50.drop_tip()
-        pip300.delay(minutes=4)
 
-        # transfer supernatant to corresponding well of new deepwell plate
-        for source, dest in zip(mag_samples300, deep_samples300):
-            pick_up('pip300')
-            pip300.transfer(125, source, dest, new_tip='never')
-            pip300.mix(10, 100, dest)
-            pip300.blow_out(dest.top())
-            pip300.drop_tip()
-        magdeck.disengage()
+    robot.pause('Vortex beads and add to channel 1 of the 12-channel reservoir \
+in slot 3.')
 
-        robot.pause('Incubate for 5 minutes at room temperature before placing \
-the second midi plate from slot 2 on the magnetic module in slot 1. Discard \
-the original plate occupying the magnetic module.')
-        robot._driver.run_flag.wait()
+    for s in new_samples300:
+        pick_up('pip300')
+        pip300.transfer(40, nuc_free_water, s.top(), new_tip='never')
+        pip300.blow_out(s.top())
+        pip300.transfer(45, spb, s, new_tip='never')
+        pip300.mix(10, 100, s)
+        pip300.blow_out(s.top())
+        pip300.drop_tip()
 
-    else:
-        pip300.home()
-        pip300.delay(minutes=5)
+    robot.pause('Seal PCR plate in slot 2 and incubate at room \
+temperature for 5 minutes. Then discard the original plate on the magnetic \
+stand and place the plate from slot 2 on the engaged magnetic deck. Place a \
+new PCR plate in slot 2, and resume.')
+
+    pick_up('pip50')
+    pip50.mix(20, 40, spb)
+    pip50.blow_out(spb.top())
+    for s in new_samples50:
+        pip50.transfer(15, spb, s.top(), new_tip='never')
+        pip50.blow_out(s.top())
+    pip50.drop_tip()
+    robot.comment('Incubating beads on magnet for 3 more minutes.')
+    pip300.delay(minutes=3)
+
+    # transfer supernatant to corresponding well of new PCR plate
+    for source, dest in zip(mag_samples300, new_samples300):
+        pick_up('pip300')
+        pip300.transfer(125, source.bottom(0.5), dest, new_tip='never')
+        pip300.mix(10, 100, dest)
+        pip300.blow_out(dest.top())
+        pip300.drop_tip()
+    magdeck.disengage()
+
+    robot.pause('Incubate for 5 minutes at room temperature before placing \
+the PCR plate from slot 2 on the magnetic module in slot 1. Discard the \
+original plate occupying the magnetic module. Place another fresh PCR plate \
+on slot 2 for the final elution.')
     robot._driver.run_flag.wait()
+
     magdeck.engage(height=18)
+    robot.comment('Incubating beads on magnet for 5 minutes.')
     pip300.delay(minutes=5)
 
     # remove supernatant
     for s in mag_samples300:
         pick_up('pip300')
-        pip300.transfer(150, s, liquid_waste[2], new_tip='never')
+        pip300.transfer(150, s.bottom(0.5), liquid_waste[2], new_tip='never')
         pip300.drop_tip()
 
     # 2x EtOH wash
@@ -175,37 +183,45 @@ the original plate occupying the magnetic module.')
         for s in mag_samples300:
             if not pip300.tip_attached:
                 pick_up('pip300')
-            pip300.transfer(220, s, liquid_waste[wash], new_tip='never')
+            pip300.transfer(
+                220, s.bottom(0.5), liquid_waste[wash], new_tip='never')
             pip300.drop_tip()
 
     # remove residual supernatant
     for s in mag_samples50:
         pick_up('pip50')
-        pip50.transfer(20, s, liquid_waste[2], new_tip='never')
+        pip50.transfer(20, s.bottom(0.5), liquid_waste[2], new_tip='never')
         pip50.drop_tip()
 
     # airdry for 5 minutes
+    robot.comment('Airdrying for 5 minutes.')
     pip50.delay(minutes=5)
     robot._driver.run_flag.wait()
     magdeck.disengage()
 
     # add RSB
-    for s in mag_samples50:
+    for i, s in enumerate(mag_samples50):
+        side = i % 2 if p50_type == 'multi' else math.floor(i/8) % 2
+        angle = 0 if side == 0 else math.pi
+        print(angle)
+        disp_loc = (s, s.from_center(r=0.95, h=-0.6, theta=angle))
+
         pick_up('pip50')
-        pip50.transfer(32, rsb, s, new_tip='never')
-        pip50.mix(5, 20, s)
-        pip50.blow_out(s.bottom(5))
+        pip50.transfer(32, rsb, disp_loc, new_tip='never')
+        pip50.mix(10, 20, disp_loc)
+        pip50.blow_out(s.top())
         pip50.drop_tip()
 
+    robot.comment('Incubating off then on magnet (2 mins each)')
     pip50.delay(minutes=2)
     robot._driver.run_flag.wait()
     magdeck.engage(height=18)
     pip50.delay(minutes=2)
 
     # transfer elution to new plate
-    for source, dest in zip(mag_samples50, elution_samples):
+    for source, dest in zip(mag_samples50, new_samples50):
         pick_up('pip50')
-        pip50.transfer(30, source, dest, new_tip='never')
+        pip50.transfer(30, source.bottom(0.5), dest, new_tip='never')
         pip50.blow_out()
         pip50.drop_tip()
 
