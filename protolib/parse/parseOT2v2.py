b0744b19808623dc34a7b13c4b83feab84298ddf
IanLondon <ianlondondu@gmail.com>
2019-09-12 17:48:53 -0400

    make parsed py protocol mimic existing PL data format

--- protolib/parse/parseOT2v2.py
+++ protolib/parse/parseOT2v2.py
@@ -5,6 +5,31 @@ from opentrons.protocol_api.execute import run_protocol
 from opentrons.protocols.parse import parse as parse_protocol
 
 
+def filter_none(arr):
+    return [i for i in arr if i is not None]
+
+
+def parse_labware(slot, labware):
+    if labware is None:
+        return None
+
+    # Thermocycler Geometry objects etc have no 'name'
+    # TODO IMMEDIATELY better way to distingush non-labware
+    # in `loaded_labwares`?
+    try:
+        labware_type = labware.name
+    except AttributeError:
+        return None
+
+    # TODO: Ian 2019-09-12 Labware should remember its label,
+    # use that for `name` instead of Labware.__str__
+    return {
+        'slot': str(slot),
+        'type': labware_type,  # load name or something
+        'name': str(labware),  # display name
+        'share': False}
+
+
 def parse(protocol_path):
     if not protocol_path:
         print('No protocol path... something weird happened!')
@@ -22,23 +47,24 @@ def parse(protocol_path):
     context.home()
     run_protocol(protocol, simulate=True, context=context)
 
-    pipettes = {mount: pipette.name for mount,
-                pipette in context.loaded_instruments.items()}
+    instruments = [{'mount': mount, 'name': pipette.name} for mount,
+                   pipette in context.loaded_instruments.items()]
 
-    # TODO: Ian 2019-09-12 Labware should remember its label,
-    # use that here instead of Labware.__str__
-    labware = {slot: str(labware)
-               for slot, labware
-               in context.loaded_labwares.items()
-               if labware is not None}
+    labware = filter_none([parse_labware(slot, labware)
+                           for slot, labware
+                           in context.loaded_labwares.items()])
 
     # TODO IMMEDIATELY: need `parameters` from parse_protocol fn
     parameters = {}
 
+    # TODO IMMEDIATELY
+    modules = []
+
     return {
-        "pipettes": pipettes,
+        "instruments": instruments,
         "labware": labware,
         "parameters": parameters,
+        "modules": modules,
         "metadata": protocol.metadata}
 
 
@@ -48,6 +74,5 @@ if __name__ == '__main__':
     print('OT2 APIv2: parsing {} to {}'.format(sourceFilePath, destFilePath))
 
     result = parse(sourceFilePath)
-    print('result', result)
     with open(destFilePath, 'w') as f:
         json.dump(result, f)
