e7220dccad10a26fd51079e7df42893ed550a627
Chazman Childers <protocols@opentrons.com>
2019-09-13 12:20:02 -0400

    optimized according to comments

--- protocols/41968f/Magbead-purification.ot2.py
+++ protocols/41968f/Magbead-purification.ot2.py
@@ -1,5 +1,6 @@
 from opentrons import labware, instruments, robot, modules
 from otcustomizers import StringSelection
+import math
 
 metadata = {
     'protocolName': 'Magbead Based Peptide Purification',
@@ -9,7 +10,7 @@ metadata = {
 
 # create and load labware
 magdeck = modules.load('magdeck', '4')
-mag_offset = 14.94  # based on definitions found in labware
+mag_height = 14.94  # based on definitions found in labware
 
 deep_name = 'whatman_96_deepwellplate_2000ul'
 if deep_name not in labware.list():
@@ -60,6 +61,9 @@ tips_reuse_a = labware.load('opentrons_96_tiprack_300ul', '8', 'Tiprack')
 tips_reuse_b = labware.load('opentrons_96_tiprack_300ul', '9', 'Tiprack')
 tips_reuse_c = labware.load('opentrons_96_tiprack_300ul', '10', 'Tiprack')
 
+tips_reuse = [
+    tips_reuse_a.rows('A'), tips_reuse_b.rows('A'), tips_reuse_c.rows('A')]
+
 
 def run_custom_protocol(
         p300_mount: StringSelection('right', 'left') = 'right',
@@ -67,14 +71,14 @@ def run_custom_protocol(
         mag_time: int = 10
         ):
     # checks
-    if 150 < aspirate_rate < 5:
+    if 150 < aspirate_rate or aspirate_rate < 5:
         raise Exception('The aspirate_rate should be between 5 and 150 ul/s')
 
     # create pipette
 
     pip300 = instruments.P300_Multi(mount=p300_mount)
     [deep300, samples300, final300] = [
-        plate.rows('A')[:12] for plate in [
+        plate.rows('A') for plate in [
             deep_plate, sample_plate, dest_plate
         ]
     ]
@@ -83,9 +87,8 @@ def run_custom_protocol(
         magdeck.disengage()
 
     # add 300ul of sample to deep plate and replace tips in tiprack
-    for source, dest in zip(samples300, deep300):
-        well_name = source.get_name()
-        pip300.pick_up_tip(tips_reuse_a.wells(well_name))
+    for source, dest, tips in zip(samples300, deep300, tips_reuse[0]):
+        pip300.pick_up_tip(tips)
         pip300.aspirate(300, source)
         pip300.dispense(300, dest)
         pip300.blow_out(dest.top())
@@ -97,13 +100,12 @@ def run_custom_protocol(
     in the OT app')
 
     # collect supernatant and discard tips
-    magdeck.engage(height=mag_offset)
+    magdeck.engage(height=mag_height)
     pip300.delay(seconds=mag_time)
     pip300.set_flow_rate(aspirate=aspirate_rate, dispense=300)
 
-    for source in deep300:
-        well_name = source.get_name()
-        pip300.pick_up_tip(tips_reuse_a.wells(well_name))
+    for source, tips in zip(deep300, tips_reuse[0]):
+        pip300.pick_up_tip(tips)
         pip300.aspirate(165, source)
         pip300.dispense(165, liq_waste(0))
         pip300.blow_out(liq_waste(0).top())
@@ -115,6 +117,7 @@ def run_custom_protocol(
     magdeck.disengage()
 
     # wash steps (3x)
+    mix_r = 0
     for x in range(3):
         pip300.set_flow_rate(aspirate=150, dispense=300)
         pip300.pick_up_tip(tips_1x.wells('A1'))
@@ -122,18 +125,23 @@ def run_custom_protocol(
             pip300.transfer(300, reagents(x), dest.top(), new_tip='never')
             pip300.blow_out(dest.top())
         pip300.return_tip()
-        for source in deep300:
-            well_name = source.get_name()
-            pip300.pick_up_tip(tips_reuse_b.wells(well_name))
-            pip300.mix(5, 290, source)
+        for source, tips in zip(deep300, tips_reuse[1]):
+            pip300.pick_up_tip(tips)
+            src_mxr = (source, source.from_center(
+                h=-0.7, r=0.8, theta=math.pi*mix_r))
+            mix_r = (mix_r + 1) % 2
+            pip300.mix(5, 290, src_mxr)
             pip300.blow_out(source.top())
-            magdeck.engage(height=mag_offset)
+            magdeck.engage(height=mag_height)
             pip300.delay(seconds=mag_time)
             pip300.set_flow_rate(aspirate=aspirate_rate, dispense=300)
             pip300.aspirate(300, source)
             pip300.dispense(300, liq_waste(x+1))
             magdeck.disengage()
-            pip300.return_tip()
+            if x == 2:
+                pip300.drop_tip()
+            else:
+                pip300.return_tip()
 
     # elution step
     for x in range(3):
@@ -144,23 +152,33 @@ def run_custom_protocol(
             pip300.blow_out(dest.top())
         pip300.return_tip()
         for y in range(2):
-            for source in deep300:
-                well_name = source.get_name()
-                pip300.pick_up_tip(tips_reuse_c.wells(well_name))
-                pip300.mix(5, 30, source)
+            for source, tips in zip(deep300, tips_reuse[2]):
+                pip300.pick_up_tip(tips)
+                src_mxr = (source, source.from_center(
+                    h=-0.7, r=0.8, theta=math.pi*mix_r))
+                mix_r = (mix_r + 1) % 2
+                pip300.mix(5, 30, src_mxr)
                 pip300.blow_out(source.top())
                 pip300.return_tip()
             pip300.delay(minutes=2.5)
-        magdeck.engage(height=mag_offset)
+            robot._driver.run_flag.wait()
+        magdeck.engage(height=mag_height)
         pip300.delay(seconds=mag_time)
         pip300.set_flow_rate(aspirate=aspirate_rate, dispense=300)
-        for source, dest in zip(deep300, final300):
-            well_name = source.get_name()
-            pip300.pick_up_tip(tips_reuse_c.wells(well_name))
+        for source, dest, tips in zip(deep300, final300, tips_reuse[2]):
+            pip300.pick_up_tip(tips)
             pip300.transfer(30, source, dest, new_tip='never')
             pip300.blow_out(dest.top())
-            pip300.return_tip()
+            if x == 2:
+                pip300.drop_tip()
+            else:
+                pip300.return_tip()
         magdeck.disengage()
 
     robot.comment("Congratulations. The protocol is now complete. Please \
     remove samples from the deck and store properly.")
+
+
+run_custom_protocol()
+for c in robot.commands():
+    print(c)
