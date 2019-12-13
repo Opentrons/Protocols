8ac0da6727a44962275e98f4c6ad4db9f202d451
IanLondon <ianlondondu@gmail.com>
2019-12-13 12:56:37 -0500

    dont try to check if protocols are parsed

--- checkChanges.sh
+++ checkChanges.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # exit with error status if `make parse-ot2` has work to do
-if ! make parse-ot2 --question; then
-    echo '***** MISSING UPDATES TO PROTOBUILDS (.py protocol files): *****';
-    make parse-ot2 --dry-run;
-    exit 1;
-fi
+# NOTE: this doesn't work b/c make uses timestamps and git does not preserve them.
+# if ! make parse-ot2 --question; then
+#     echo '***** MISSING UPDATES TO PROTOBUILDS (.py protocol files): *****';
+#     make parse-ot2 --dry-run;
+#     exit 1;
+# fi
 
 # exit with error status if errors/readmes have uncommited updates
-make parse-errors parse-README > /dev/null;
+echo 'parsing errors & readmes...'
+make parse-errors parse-README;
 if [[ $(git ls-files --modified protoBuilds) ]]; then
     echo '***** MISSING UPDATES TO PROTOBUILDS (metadata/readme files): *****';
     git ls-files --modified protoBuilds;
