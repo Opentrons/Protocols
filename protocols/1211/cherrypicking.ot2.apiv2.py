0c1fa7d64915e8afa30c52351201efd7615c6f90
ncdiehl11 <ndiehl@opentrons.com>
2020-07-06 20:07:57 -0400

    1211 initial full protocol package

--- protocols/1211/cherrypicking.ot2.apiv2.py
+++ protocols/1211/cherrypicking.ot2.apiv2.py
@@ -8,12 +8,12 @@ metadata = {
 
 def run(ctx):
 
-    pipette_type, pipette_mount, transfer_csv = get_values(  # noq: F821
+    pipette_type, pipette_mount, transfer_csv = get_values(  # noqa: F821
         "pipette_type", "pipette_mount", "transfer_csv")
 
     tiprack_map = {
-        'p10_single_gen1': 'opentrons_96_tiprack_10ul',
-        'p50_single_gen1': 'opentrons_96_tiprack_300ul',
+        'p10_single': 'opentrons_96_tiprack_10ul',
+        'p50_single': 'opentrons_96_tiprack_300ul',
         'p300_single_gen1': 'opentrons_96_tiprack_300ul',
         'p1000_single_gen1': 'opentrons_96_tiprack_1000ul',
         'p20_single_gen2': 'opentrons_96_tiprack_20ul',
@@ -23,9 +23,9 @@ def run(ctx):
 
     # load labware
     transfer_info = [[val.strip().lower() for val in line.split(',')]
-                     for line in  transfer_csv.splitlines()]
+                     for line in transfer_csv.splitlines()][1:]
     for line in transfer_info:
-        s_slot, s_lw, d_slot, d_lw = line[:2] + line[3:4]
+        s_lw, s_slot, d_lw, d_slot = line[:2] + line[3:5]
         for slot, lw in zip([s_slot, d_slot], [s_lw, d_lw]):
             if not int(slot) in ctx.loaded_labwares:
                 ctx.load_labware(lw.lower(), slot)
@@ -35,13 +35,13 @@ def run(ctx):
     tipracks = []
     for slot in range(1, 13):
         if slot not in ctx.loaded_labwares:
-            tiprack.append(ctx.load_labware(tiprack_type, str(slot)))
+            tipracks.append(ctx.load_labware(tiprack_type, str(slot)))
 
     # load pipette
     pip = ctx.load_instrument(pipette_type, pipette_mount, tip_racks=tipracks)
 
     tip_count = 0
-    tip_max = len(tip_racks*96)
+    tip_max = len(tipracks*96)
 
     def pick_up():
         nonlocal tip_count
@@ -53,9 +53,10 @@ def run(ctx):
         tip_count += 1
 
     for line in transfer_info:
-        s_lw, _, s_well, _, d_slot, d_well, vol = line[:7]
-        source = ctx.loaded_labwares[int(s_slot)].wells_by_name(s_well.upper())
-        dest = ctx.loaded_labwares[int(d_slot)].wells_by_name(d_slot.upper())
+        _, s_slot, s_well, _, d_slot, d_well, vol = line[:7]
+        source = ctx.loaded_labwares[
+            int(s_slot)].wells_by_name()[s_well.upper()]
+        dest = ctx.loaded_labwares[int(d_slot)].wells_by_name()[d_well.upper()]
         pick_up()
-        pip.transfer(vol, source, dest, new_tip='never')
+        pip.transfer(float(vol), source, dest, new_tip='never')
         pip.drop_tip()
