af95d473147b327936826a582454175aafab7267
Keoni Gandall <koeng101@gmail.com>
2020-10-19 14:13:05 -0700

    Added README.md + fields.json to 1980cd

--- protocols/1980cd/1980cd.py
+++ protocols/1980cd/1980cd.py
@@ -5,14 +5,14 @@ def get_values(step, sample_plates):
 
 def run(ctx):
 
-    step, sample_plates = get_values('step', 'sample_plates')
+    step, test_plates = get_values('step', 'test_plates')
 
     if step == 1:
         tip_rack = [ctx.load_labware('opentrons_96_filtertiprack_200ul', '1')]
         p300m = ctx.load_instrument('p300_multi_gen2', "right", tip_racks=tip_rack)
         
-        trough = ctx.load_labware('nest_1_reservoir_195ml','2').wells()[0]
-        plates = [ctx.load_labware('nest_96_wellplate_200ul_flat', str(i)) for i in range(3,12)[:sample_plates]]
+        trough = ctx.load_labware('nest_1_reservoir_195ml','2', label='Blocking solution').wells()[0]
+        plates = [ctx.load_labware('nest_96_wellplate_200ul_flat', str(i), label='Plate {}'.format(i)) for i in range(3,12)[:test_plates]]
         for plate in plates:
             p300m.pick_up_tip()
             [p300m.transfer(150, trough, col, new_tip='never') for col in plate.rows()[0]]
@@ -22,13 +22,13 @@ def run(ctx):
         tip_rack = [ctx.load_labware('opentrons_96_filtertiprack_200ul', '1')]
         p300m = ctx.load_instrument('p300_multi_gen2', "right", tip_racks=tip_rack)
         
-        reagents = ctx.load_labware('nest_12_reservoir_15ml', '2')
+        reagents = ctx.load_labware('nest_12_reservoir_15ml', '2', label='Controls')
         control_1 = reagents.wells_by_name()["A1"]
         control_2 = reagents.wells_by_name()["A2"]
         
-        sample_plate = ctx.load_labware('nest_96_wellplate_200ul_flat', '3')
-        test_plate_1 = ctx.load_labware('nest_96_wellplate_200ul_flat', '5')
-        test_plate_2 = ctx.load_labware('nest_96_wellplate_200ul_flat', '6')
+        sample_plate = ctx.load_labware('nest_96_wellplate_200ul_flat', '3', label='Sample plate')
+        test_plate_1 = ctx.load_labware('nest_96_wellplate_200ul_flat', '5', label='First test plate')
+        test_plate_2 = ctx.load_labware('nest_96_wellplate_200ul_flat', '6', label='Second test plate')
 
         for control, well in [(control_1,"A1"), (control_2,"A2")]:
             p300m.pick_up_tip()
@@ -47,10 +47,10 @@ def run(ctx):
         tip_racks = [ctx.load_labware('opentrons_96_filtertiprack_200ul', x) for x in ['1','4']]
         p300m = ctx.load_instrument('p300_multi_gen2', "right", tip_racks=tip_racks)
         
-        test_plate_1 = ctx.load_labware('nest_96_wellplate_200ul_flat', '5')
-        test_plate_2 = ctx.load_labware('nest_96_wellplate_200ul_flat', '6')
+        test_plate_1 = ctx.load_labware('nest_96_wellplate_200ul_flat', '5', label='First test plate')
+        test_plate_2 = ctx.load_labware('nest_96_wellplate_200ul_flat', '6', label='Second test plate')
 
-        reagents = ctx.load_labware('nest_12_reservoir_15ml', '2')
+        reagents = ctx.load_labware('nest_12_reservoir_15ml', '2', label='Detection Antibody')
         detection_antibody = reagents.wells_by_name()["A3"]
     
         for plate in [test_plate_1,test_plate_2]:
@@ -61,10 +61,10 @@ def run(ctx):
         tip_racks = [ctx.load_labware('opentrons_96_filtertiprack_200ul', x) for x in ['1','4']]
         p300m = ctx.load_instrument('p300_multi_gen2', "right", tip_racks=tip_racks)
 
-        test_plate_1 = ctx.load_labware('nest_96_wellplate_200ul_flat', '5')
-        test_plate_2 = ctx.load_labware('nest_96_wellplate_200ul_flat', '6')
+        test_plate_1 = ctx.load_labware('nest_96_wellplate_200ul_flat', '5', label='First test plate')
+        test_plate_2 = ctx.load_labware('nest_96_wellplate_200ul_flat', '6', label='Second test plate')
 
-        trough = ctx.load_labware('nest_1_reservoir_195ml','2').wells()[0]
+        trough = ctx.load_labware('nest_1_reservoir_195ml','2', label='Read buffer').wells()[0]
 
         for plate in [test_plate_1,test_plate_2]:
             for col in plate.rows()[0]:
