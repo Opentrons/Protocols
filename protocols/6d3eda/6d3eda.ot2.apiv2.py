5869ba28d1c3ec0e3dd27fc9ee1567a074f6d9ca
Keoni Gandall <koeng101@gmail.com>
2020-10-26 01:23:47 -0700

    Successful make all

---
+++ protocols/6d3eda/6d3eda.ot2.apiv2.py
@@ -0,0 +1,22 @@
+metadata = {"apiLevel": "2.5"}
+
+
+def run(ctx):
+    # Add tip tracking to this protocol
+    count, volume, input_labware, output_labware = get_values(  # noqa: F821
+            'sample_count', 'volume', 'input_labware', 'output_labware')
+    sample_plates = [ctx.load_labware(input_labware, '2')]
+    target_plates = [ctx.load_labware(output_labware, '5')]
+    tip_racks = [ctx.load_labware('opentrons_96_filtertiprack_200ul', '1')]
+    if count > 24:
+        sample_plates.append(ctx.load_labware(input_labware, '3'))
+        target_plates.append(ctx.load_labware(output_labware, '6'))
+
+    # Variable pipettes?
+    p300s = ctx.load_instrument(
+            'p300_single_gen2', "right", tip_racks=tip_racks)
+
+    for i, sample_plate in enumerate(sample_plates):
+        for well_num, well in enumerate(sample_plate.wells()[:count-(i*24)]):
+            p300s.transfer(volume, well, target_plates[i].wells()[well_num],
+                           blow_out=True, new_tip='always')
