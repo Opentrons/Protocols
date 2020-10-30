f6db9c01cda89eb8239a59eb8c3b7bd1f942d5da
Nick Diehl <ndiehl@opentrons.com>
2020-10-29 20:26:14 -0400

    raise height of aliquot dispense to 10mm

--- protocols/3b3f1c/STANDARD_MP_332_312_V4.ot2.apiv2.py
+++ protocols/3b3f1c/STANDARD_MP_332_312_V4.ot2.apiv2.py
@@ -264,7 +264,7 @@ LLOQC,2,C5,1,20,3,600,100,2900,98.5,2,B3,2,C5,2872,2,C5,,
         for a in aliquots:
             p1000.flow_rate.aspirate = 150
             p1000.flow_rate.dispense = 320
-            p1000.transfer(160, tubes_dict[std].height_dec(160), a.bottom(0.5),
+            p1000.transfer(160, tubes_dict[std].height_dec(160), a.bottom(10),
                            new_tip='never')
             p1000.blow_out(a.top(-6))
         p1000.drop_tip()
