6caf9fc8be4d141ed1aa487f1d336bc26d64680d
Sakib Hossain <sakibh97@gmail.com>
2020-11-20 13:26:54 -0500

    Updated flake8 linting, added list comprehensions, added mastermix_vol field

--- protocols/4175de/4175de.ot2.apiv2.py
+++ protocols/4175de/4175de.ot2.apiv2.py
@@ -1,6 +1,3 @@
-from opentrons import protocol_api
-import math
-
 metadata = {
     'protocolName': 'Cherrypicking PCR/qPCR prep',
     'author': 'Sakib <sakib.hossain@opentrons.com>',
@@ -8,20 +5,22 @@ metadata = {
     'apiLevel': '2.7'
 }
 
+
 def run(protocol):
-    
-    [transfer_csv, total_samples, p20_mount, pcr_labware] = get_values(  # noqa: F821
-    "transfer_csv", "total_samples", "p20_mount", "pcr_labware")
+    [transfer_csv, total_samples, p20_mount, pcr_labware,
+        mastermix_vol] = get_values(  # noqa: F821
+        "transfer_csv", "total_samples", "p20_mount", "pcr_labware",
+        "mastermix_vol")
 
     # Load Tip Racks
-    tiprack1 = protocol.load_labware('opentrons_96_filtertiprack_20ul', 1) 
-    tiprack2 = protocol.load_labware('opentrons_96_filtertiprack_20ul', 4)
-    tiprack3 = protocol.load_labware('opentrons_96_filtertiprack_20ul', 7)
+    tipracks = [protocol.load_labware('opentrons_96_filtertiprack_20ul', slot)
+                for slot in ['1', '4', '7']]
 
     # Load Module
     temp_mod = protocol.load_module('temperature module gen2', 3)
     pcr_plate = temp_mod.load_labware(pcr_labware)
-    mastermix = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap', 2)
+    mastermix = protocol.load_labware(
+        'opentrons_24_tuberack_generic_2ml_screwcap', 2)['A6']
 
     # Load labware via CSV
     transfer_info = [[val.strip().lower() for val in line.split(',')]
@@ -33,9 +32,14 @@ def run(protocol):
             if not int(slot) in protocol.loaded_labwares:
                 protocol.load_labware(lw.lower(), slot)
 
+    def parse_well(well):
+        letter = well[0]
+        number = well[1:]
+        return letter.upper() + str(int(number))
+
     # Load Pipette
-    pipette_type = 'p20_single_gen2'
-    pipette = protocol.load_instrument(pipette_type, p20_mount, tip_racks=[tiprack1, tiprack2, tiprack3])
+    p20 = protocol.load_instrument('p20_single_gen2', p20_mount,
+                                   tip_racks=tipracks)
 
     # Activate temperature module and set temperature to 4 Celsius
     # Pauses protocol until desired temperature is reached
@@ -43,22 +47,22 @@ def run(protocol):
     temp_mod.set_temperature(4)
 
     # Transfer Master Mix to Variable # of wells on PCR Plate sequentially
-    protocol.comment("Transferring master mix to the PCR plate on the Temperature Deck")
-    mastermix_wells = []
-    for i in range(math.ceil(int(total_samples)/12)):
-        for well in pcr_plate.rows()[i]:
-            mastermix_wells.append(well)
-    for well in range(int(total_samples)):
-        pipette.transfer(11.5, mastermix.wells_by_name()['A6'], mastermix_wells[well], new_tip='once')
+    protocol.comment("Transferring master mix...")
+    mastermix_wells = [well for row in pcr_plate.rows()
+                       for well in row][:int(total_samples)]
 
-    def parse_well(well):
-        letter = well[0]
-        number = well[1:]
-        return letter.upper() + str(int(number))
+    p20.pick_up_tip()
+    for well in mastermix_wells:
+        p20.transfer(float(mastermix_vol), mastermix, well,
+                     new_tip='never')
+    p20.drop_tip()
 
     # Transfer DNA from source wells to PCR Plate
+    protocol.comment("Transferring DNA samples from source to destination")
     for line in transfer_info:
         _, s_slot, s_well, h, _, d_slot, d_well, vol = line[:8]
-        source = protocol.loaded_labwares[int(s_slot)].wells_by_name()[parse_well(s_well)].bottom(float(h))
-        dest = protocol.loaded_labwares[int(d_slot)].wells_by_name()[parse_well(d_well)]
-        pipette.transfer(float(vol), source, dest, new_tip='always')
+        source = protocol.loaded_labwares[int(s_slot)].wells_by_name()[
+            parse_well(s_well)].bottom(float(h))
+        dest = protocol.loaded_labwares[int(d_slot)].wells_by_name()[
+            parse_well(d_well)]
+        p20.transfer(float(vol), source, dest, new_tip='always')
