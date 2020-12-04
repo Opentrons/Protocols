245c78481b69b4648deb6d3754fb0fb180a65320
Sakib Hossain <sakibh97@gmail.com>
2020-12-03 23:56:45 -0500

    Protocol package for 5191b6

--- protocols/5191b6/5191b6.ot2.apiv2.py
+++ protocols/5191b6/5191b6.ot2.apiv2.py
@@ -1,10 +1,5 @@
-# def get_values(*names):
-#     import json
-#     _all_values = json.loads("""{"vial_map":""}""")
-#     return [_all_values[n] for n in names]
-
 metadata = {
-    'protocolName': 'CSV Input Reagent Dispensing',
+    'protocolName': 'High-Throughput Synthesis',
     'author': 'Sakib <sakib.hossain@opentrons.com>',
     'description': 'Custom Protocol Request',
     'apiLevel': '2.7'
@@ -13,34 +8,27 @@ metadata = {
 
 def run(protocol):
 
-    # [p1000_tips, p300_tips, p1000_mount, p300_mount, reservoir_type, vial_map, htp_csv] = get_values(  # noqa: F821
-    #     "total_samples", "p20_mount", "p1000_mount")
-
-    vial_map = """Well Number,Labware,Slot,Well Position
-1,opentrons_24_tuberack_nest_2ml_screwcap,4,A1
-2,opentrons_24_tuberack_nest_2ml_screwcap,4,A2
-3,opentrons_24_tuberack_nest_2ml_screwcap,4,A3"""
-
-    htp_csv = """*Note numbering starts from top left to top right,Reservoir 1,Reservoir 2,Reservoir 3,Reservoir 4,Reservoir 5,Reservoir 6,Reservoir 7,Reservoir 8,Reservoir 9,Reservoir 10,Reservoir 11,Reservoir 12,
-Well Number,Metal Salt Stock Soln 1 (1 M) (uL),Metal Salt Stock Soln 2 (1 M) (uL),Metal Salt Stock Soln 3 (1 M) (uL),Metal Salt Stock Soln 4 (1 M) (uL),Organic Linker Stock Soln 1 (1 M) (uL),Organic Linker Stock Soln 2 (1 M) (uL),Organic Linker Stock Soln 3 (1 M) (uL),Organic Linker Stock Soln 4 (1M) (uL),Extra Solvent 1 (uL),Extra Solvent 2 (uL),Extra Solvent 3 (uL),Extra Solvent 4 (uL),Total Volume (uL)
-51,270,325,325,,150,,,,71,107,,,1248
-52,270,375,375,,150,,,,78,117,,,1365
-53,270,425,425,,150,,,,,,,,1270"""
-
+    [p1000_tips, p300_tips, p1000_mount, p300_mount, reservoir_type,
+     vial_map, htp_csv] = get_values(  # noqa: F821
+        "p1000_tips", "p300_tips", "p1000_mount", "p300_mount",
+        "reservoir_type", "vial_map", "htp_csv")
 
     # Load Tip Racks
-    p1000_tiprack = protocol.load_labware('opentrons_96_filtertiprack_1000ul', 1)
-    p300_tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', 2)
+    p1000_tiprack = protocol.load_labware(p1000_tips, 1)
+    p300_tiprack = protocol.load_labware(p300_tips, 2)
 
     # Load Pipettes
-    p1000 = protocol.load_instrument('p1000_single_gen2', 'left', tip_racks=[p1000_tiprack])
-    p300 = protocol.load_instrument('p300_single_gen2', 'right', tip_racks=[p300_tiprack])
+    p1000 = protocol.load_instrument('p1000_single_gen2',
+                                     p1000_mount, tip_racks=[p1000_tiprack])
+    p300 = protocol.load_instrument('p300_single_gen2',
+                                    p300_mount, tip_racks=[p300_tiprack])
 
-    reservoir = protocol.load_labware('nest_12_reservoir_15ml', 3)
+    reservoir = protocol.load_labware(reservoir_type, 3)
 
     # Load Reaction Vessel Labware
-    vial_info = [[val.strip() for val in line.split(',')] for line in vial_map.splitlines()
-                    if line.split(',')[0].strip()][1:]
+    vial_info = [[val.strip() for val in line.split(',')]
+                 for line in vial_map.splitlines()
+                 if line.split(',')[0].strip()][1:]
 
     for line in vial_info:
         s_lw, s_slot = line[1:3]
@@ -52,49 +40,41 @@ Well Number,Metal Salt Stock Soln 1 (1 M) (uL),Metal Salt Stock Soln 2 (1 M) (uL
     sample_tubes = []
     for line in vial_info:
         lw, slot, well = line[1:4]
-        sample_tubes.append(protocol.loaded_labwares[int(slot)].wells_by_name()[well])
-
+        sample_tubes.append(protocol.loaded_labwares[
+                            int(slot)].wells_by_name()[well])
 
     # Parse High-Throughput CSV
-    htp_info = [[val.strip().lower() for val in line.split(',')] for line in htp_csv.splitlines()
-                    if line.split(',')[0].strip()][2:]
+    htp_info = [[val.strip().lower() for val in line.split(',')]
+                for line in htp_csv.splitlines()
+                if line.split(',')[0].strip()][2:]
 
     # Get all dispense volumes in each row
     dispense_vols = [htp_info[i][1:13] for i in range(len(htp_info))]
 
-    # Group dispense volumes per column (Each list contains volumes per reservoir)
-    dispense_vols_serialized = [[vol[i] for vol in dispense_vols] for i in range(len(dispense_vols[0]))]
+    # Group dispense volumes per column
+    # (Each list contains volumes per reservoir)
+    dispense_vols_serialized = [[vol[i] for vol in
+                                dispense_vols] for i in range(12)]
 
-    # Dispenses volumes from each reservoir to corresponding wells
-    # Note: empty columns result in tips being wasted. Fix?
-    for res in range(len(dispense_vols_serialized)):
+    # Creates data structure with correct reservoir,
+    # wells, and corresponding volumes
+    raw_samples_data = {i: {well: vol for well, vol in
+                        enumerate(volumes) if vol != ''}
+                        for i, volumes in enumerate(dispense_vols_serialized)}
+
+    # Remove columns with empty values
+    samples_data = {i: v for i, v in raw_samples_data.items() if v}
+
+    for res in samples_data:
         p300.pick_up_tip()
         p1000.pick_up_tip()
-        for vial, vol in zip(sample_tubes, dispense_vols_serialized[res]):
-            if vol:
-                if int(vol) < 300:
-                    # print(f'P300: {vol} uL from {reservoir.wells()[res]} in {vial}')
-                    p300.transfer(int(vol), reservoir.wells()[res], vial, new_tip='never')
-                else:
-                    # print(f'P1000: {vol} uL from {reservoir.wells()[res]} in {vial}')
-                    p1000.transfer(int(vol), reservoir.wells()[res], vial, new_tip='never')
+        for i, vol in enumerate(samples_data[res]):
+            well, volume = i, int(samples_data[res][i])
+            if volume < 300:
+                p300.transfer(volume, reservoir.wells()[res],
+                              sample_tubes[well], new_tip='never')
             else:
-                continue
+                p1000.transfer(volume, reservoir.wells()[res],
+                               sample_tubes[well], new_tip='never')
         p300.drop_tip()
         p1000.drop_tip()
-
-
-
-
-# Pipette mounts
-# Use drop down input to get tip rack names (Step 1)
-# CSV for uploading custom reaction vessel racks with vials or well plates (Step 2)
-# CSV for stock solution reservoirs (Step 3)
-# Import CSV for High-Throughput Synthesis (Step 4)
-
-# Example: 
-
-# Choose pipette and pickup tip (if statement with volume)
-# Reservoir 1: 270 uL in A1
-# Reservoir 1: 27 uL in A2
-# etc...
---
+++ protocols/5191b6/README.md
@@ -0,0 +1,58 @@
+# High-Throughput Synthesis
+
+### Author
+[Opentrons](https://opentrons.com/)
+
+## Categories
+* Sample Prep
+	* Cherrypicking
+
+
+## Description
+This protocol performs a custom cherrypicked high-throughput synthesis. It requires a CSV input for the mapping of the wells and vials on the tube racks. It also requires a CSV for high-throughput synthesis which contains the dispending volumes from each reservoir.
+
+---
+
+![Materials Needed](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/001-General+Headings/materials.png)
+
+* [P1000 single-channel GEN2 electronic pipette](https://shop.opentrons.com/collections/ot-2-pipettes/products/single-channel-electronic-pipette)
+* [P300 single-channel GEN2 electronic pipette](https://shop.opentrons.com/collections/ot-2-pipettes/products/single-channel-electronic-pipette)
+* [Opentrons Tube Racks](https://shop.opentrons.com/collections/racks-and-adapters/products/tube-rack-set-1)
+
+---
+![Setup](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/001-General+Headings/Setup.png)
+
+Deck Setup
+* P1000 Tips (Slot 1)
+* P300 Tips (Slot 2)
+* NEST 12-well 15 mL reservoir (Slot 3)
+* Opentrons Tube Racks (Use Vial Map CSV)
+
+Example of the Vial Map CSV:
+```
+Well Number,Labware,Slot,Well Position
+1,opentrons_24_tuberack_nest_2ml_screwcap,4,A1
+2,opentrons_24_tuberack_nest_2ml_screwcap,4,A2
+3,opentrons_24_tuberack_nest_2ml_screwcap,4,A3
+...
+96,opentrons_24_tuberack_nest_2ml_screwcap,7,D6
+```
+
+The Vial Map CSV can be used to place custom labware and different tube racks from the labware library. It also allows mapping of the well positions of each vial on a tube rack corresponding to the well number in the high-throughput synthesis protocol.
+
+### Robot
+* [OT-2](https://opentrons.com/ot-2)
+
+## Process
+1. Upload Vial Map CSV and High-Throughput Synthesis Protocol CSVs. Choose pipette tips, pipette mounts, and reservoir.
+2. Download your protocol.
+3. Upload your protocol into the [OT App](https://opentrons.com/ot-app).
+4. Set up your deck according to the deck map.
+5. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support articles](https://support.opentrons.com/en/collections/1559720-guide-for-getting-started-with-the-ot-2).
+6. Hit "Run".
+
+### Additional Notes
+If you have any questions about this protocol, please contact the Protocol Development Team by filling out the [Troubleshooting Survey](https://protocol-troubleshooting.paperform.co/).
+
+###### Internal
+5191b6
