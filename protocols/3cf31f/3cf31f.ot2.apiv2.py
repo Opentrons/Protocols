1ac2062fb714955cf85ea3e764ca276647050c83
Sakib Hossain <sakibh97@gmail.com>
2020-12-21 15:00:21 -0500

    Protocol resources updated for 3cf31f

--- protocols/3cf31f/3cf31f.ot2.apiv2.py
+++ protocols/3cf31f/3cf31f.ot2.apiv2.py
@@ -10,22 +10,22 @@ metadata = {
 
 def run(ctx):
 
-    p50_mount = 'left'
-    p300_mount = 'right'
+    [p50_mount, p300_mount] = get_values(  # noqa: F821
+        "p50_mount", "p300_mount")
 
     # Load labware/tiprack/modules
     tiprack = ctx.load_labware('opentrons_96_tiprack_300ul', '11',
-                                'Tiprack 50/300ul')
+                               'Tiprack 50/300ul')
 
     cryovials = ctx.load_labware('custom_nanosep_tube_container', '3')
     centtubes = ctx.load_labware('custom_centrifuge_tube_container', '2')
     ultratubes = ctx.load_labware('custom_nanosep_tube_container', '1')
 
     tempdeck = ctx.load_module('tempdeck', '10')
-    temprack = tempdeck.load_labware('opentrons_24_aluminumblock_nest_1.5ml_snapcap')
+    temprack = tempdeck.load_labware(
+                'opentrons_24_aluminumblock_nest_1.5ml_snapcap')
 
     # Load pipettes
-    p50 = ctx.load_instrument('p50_single', p50_mount, tip_racks=[tiprack])
     p300 = ctx.load_instrument('p300_single', p300_mount, tip_racks=[tiprack])
 
     def pick_up(pip):
@@ -61,7 +61,7 @@ def run(ctx):
     for dest in centtubes48:
         pick_up(p300)
         p300.transfer(40, temprack.wells()[0], dest,
-                        new_tip='never')
+                      new_tip='never')
         p300.mix(3, 35, dest)
         p300.blow_out(dest.top())
         p300.drop_tip()
@@ -72,11 +72,11 @@ def run(ctx):
         m = (k//10) + 1
         pick_up(p300)
         p300.transfer(110, temprack.wells(m), cent,
-                        new_tip='never')
+                      new_tip='never')
         p300.mix(3, 150, cent)
         p300.blow_out(cent.top())
         p300.transfer(200, cent, ultra,
-                        new_tip='never')
+                      new_tip='never')
         p300.blow_out(ultra.top())
         p300.drop_tip()
 
