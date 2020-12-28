ea42d1024c000670fd0ce748c081751bc18e283e
Sakib Hossain <sakibh97@gmail.com>
2020-12-28 14:39:07 -0500

    Updated with flake8 linting

--- protocols/6faa1e/6faa1e.ot2.apiv2.py
+++ protocols/6faa1e/6faa1e.ot2.apiv2.py
@@ -9,15 +9,17 @@ metadata = {
 def run(ctx):
 
     [m20_mount] = get_values(  # noqa: F821
-    "m20_mount")
+        "m20_mount")
 
     # Load labware
     tiprack = ctx.load_labware('opentrons_96_tiprack_20ul', 10)
-    source_plate_1 = ctx.load_labware('nest_12_reservoir_15ml_icetray', 11, 'Source Plate #1')
-    
+    source_plate_1 = ctx.load_labware('nest_12_reservoir_15ml_icetray', 11,
+                                      'Source Plate #1')
+
     for slot in range(1, 9):
         if int(slot) not in ctx.loaded_labwares:
-            ctx.load_labware('thermofast_96well_semiskirted_eppendorf_cooler', slot, f'Destination Plate #{slot}')
+            ctx.load_labware('thermofast_96well_semiskirted_eppendorf_cooler',
+                             slot, f'Destination Plate #{slot}')
 
     # Load pipette
     m20 = ctx.load_instrument('p20_multi_gen2', m20_mount, tip_racks=[tiprack])
@@ -26,8 +28,8 @@ def run(ctx):
     mm_1 = source_plate_1['A1']
     mm_2 = source_plate_1['A2']
 
-    sample_set_1 = [ctx.loaded_labwares[i].rows()[0] for i in range(1,5)]
-    sample_set_2 = [ctx.loaded_labwares[i].rows()[0] for i in range(5,9)]
+    sample_set_1 = [ctx.loaded_labwares[i].rows()[0] for i in range(1, 5)]
+    sample_set_2 = [ctx.loaded_labwares[i].rows()[0] for i in range(5, 9)]
 
     m20.pick_up_tip()
     for source, dest in zip([mm_1, mm_2], [sample_set_1, sample_set_2]):
