6e8b92fa7f671687ed8fa1a10c0253ba2e548da4
Sakib Hossain <sakibh97@gmail.com>
2020-12-28 15:08:51 -0500

    Updated labware definitions, fixed linting, added README, added fields.json

--- protocols/470d8c/470d8c.ot2.apiv2.py
+++ protocols/470d8c/470d8c.ot2.apiv2.py
@@ -8,20 +8,29 @@ metadata = {
 
 def run(ctx):
 
+    [m300_mount, m20_mount] = get_values(  # noqa: F821
+        "m300_mount", "m20_mount")
+
     # Load labware
-    tipracks_20ul = [ctx.load_labware('opentrons_96_tiprack_20ul', slot) for slot in [7, 10, 11]]
-    
+    tipracks_20ul = [ctx.load_labware('opentrons_96_tiprack_20ul', slot) for
+                     slot in [7, 10, 11]]
     tiprack_300ul = [ctx.load_labware('opentrons_96_tiprack_300ul', 4)]
-
-    source_plate = ctx.load_labware('nest_12_reservoir_15ml_icetray', 1, 'Source Plate #1') # On top of ice tray
-
-    temp_plates = [ctx.load_labware('thermofast_pcr_plate_96well_semiskirted', slot, f'Template Plate #{i}') for i, slot in enumerate([8, 5, 2], 1)]
-
-    dest_plates = [ctx.load_labware('thermofast_pcr_plate_96well_semiskirted', slot, f'Destination Plate #{i}') for i, slot in enumerate([9, 6, 3], 1)]
+    source_plate = ctx.load_labware('nest_12_reservoir_15ml_icetray', 1,
+                                    'Source Plate #1')
+    temp_plates = [ctx.load_labware(
+                    'thermofast_96well_semiskirted_eppendorf_cooler',
+                    slot, f'Template Plate #{i}') for i, slot
+                   in enumerate([8, 5, 2], 1)]
+    dest_plates = [ctx.load_labware(
+                    'thermofast_96well_semiskirted_eppendorf_cooler',
+                    slot, f'Destination Plate #{i}') for i, slot
+                   in enumerate([9, 6, 3], 1)]
 
     # Load pipettes
-    m20 = ctx.load_instrument('p20_multi_gen2', 'left', tip_racks=tipracks_20ul)
-    m300 = ctx.load_instrument('p300_multi_gen2', 'right', tip_racks=tiprack_300ul)
+    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount,
+                              tip_racks=tipracks_20ul)
+    m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
+                               tip_racks=tiprack_300ul)
 
     # Transfer 40 uL of master mix
     m300.pick_up_tip()
