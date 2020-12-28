1b10deb53856371e3b7b015d771fd71366b5607e
Sakib Hossain <sakibh97@gmail.com>
2020-12-28 13:52:23 -0500

    Updated PCR plate definition with new base, added fields.json

--- protocols/6faa1e/6faa1e.ot2.apiv2.py
+++ protocols/6faa1e/6faa1e.ot2.apiv2.py
@@ -1,11 +1,15 @@
 metadata = {
-    'protocolName': 'PCR/qPCR prep',
+    'protocolName': 'PCR/qPCR prep (Master Mix Transfer)',
     'author': 'Sakib <sakib.hossain@opentrons.com>',
     'description': 'Custom Protocol Request',
     'apiLevel': '2.8'
 }
 
+
 def run(ctx):
+
+    [m20_mount] = get_values(  # noqa: F821
+    "m20_mount")
 
     # Load labware
     tiprack = ctx.load_labware('opentrons_96_tiprack_20ul', 10)
@@ -13,10 +17,10 @@ def run(ctx):
     
     for slot in range(1, 9):
         if int(slot) not in ctx.loaded_labwares:
-            ctx.load_labware('thermofast_pcr_plate_96well_semiskirted', slot, f'Destination Plate #{slot}')
+            ctx.load_labware('thermofast_96well_semiskirted_eppendorf_cooler', slot, f'Destination Plate #{slot}')
 
     # Load pipette
-    m20 = ctx.load_instrument('p20_multi_gen2', 'left', tip_racks=[tiprack])
+    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount, tip_racks=[tiprack])
 
     # Setup source and destinations
     mm_1 = source_plate_1['A1']
@@ -29,8 +33,3 @@ def run(ctx):
     for source, dest in zip([mm_1, mm_2], [sample_set_1, sample_set_2]):
         m20.transfer(16, source, dest, new_tip='never')
     m20.drop_tip()
-
-# Please define new labware:
-# 2. NEST 12-channel reservoir 15ml #360102 on top of a tray with ice (dimensions: L:127mm, W:85mm, H: 18.5mm) - named Source plate #1
-
-# Hours: 1.8
