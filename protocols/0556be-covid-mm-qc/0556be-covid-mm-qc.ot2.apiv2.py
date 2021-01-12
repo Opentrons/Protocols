ee660f8de3770a6597e40af75e4fbce1c1cfd73a
Sakib Hossain <sakibh97@gmail.com>
2021-01-12 11:59:23 -0500

    Updated categories and flake8 linting

--- protocols/0556be-covid-mm-qc/0556be-covid-mm-qc.ot2.apiv2.py
+++ protocols/0556be-covid-mm-qc/0556be-covid-mm-qc.ot2.apiv2.py
@@ -8,33 +8,44 @@ metadata = {
 
 def run(ctx):
 
-    [p300_mount, p20_mount, temperature, component_1_volume, component_2_volume] = get_values(  # noqa: F821
-    "p300_mount", "p20_mount", "temperature", "component_1_volume", "component_2_volume")
+    [p300_mount, p20_mount, temperature, component_1_volume,
+        component_2_volume] = get_values(  # noqa: F821
+        "p300_mount", "p20_mount", "temperature", "component_1_volume",
+        "component_2_volume")
 
     # Load Labware
-    tuberack = ctx.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 8)
+    tuberack = ctx.load_labware(
+        'opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 8)
     temp_mod = ctx.load_module('temperature module gen2', 9)
-    dest_tubes = temp_mod.load_labware('opentrons_24_aluminumblock_generic_2ml_screwcap')
-    component_3 = ctx.load_labware('opentrons_24_aluminumblock_generic_2ml_screwcap', 5)['A1']
-    pcr_plate = ctx.load_labware('opentrons_96_aluminumblock_generic_pcr_strip_200ul', 6)
+    dest_tubes = temp_mod.load_labware(
+        'opentrons_24_aluminumblock_generic_2ml_screwcap')
+    component_3 = ctx.load_labware(
+        'opentrons_24_aluminumblock_generic_2ml_screwcap', 5)['A1']
+    pcr_plate = ctx.load_labware(
+        'opentrons_96_aluminumblock_generic_pcr_strip_200ul', 6)
     tiprack_200ul = ctx.load_labware('opentrons_96_filtertiprack_200ul', 4)
     tiprack_20ul = ctx.load_labware('opentrons_96_filtertiprack_20ul', 1)
 
     # Load Instruments
-    p300 = ctx.load_instrument('p300_single_gen2', p300_mount, tip_racks=[tiprack_200ul])
-    p20 = ctx.load_instrument('p20_single_gen2', p20_mount, tip_racks=[tiprack_20ul])
+    p300 = ctx.load_instrument('p300_single_gen2', p300_mount,
+                               tip_racks=[tiprack_200ul])
+    p20 = ctx.load_instrument('p20_single_gen2', p20_mount,
+                              tip_racks=[tiprack_20ul])
 
     # Set Temperature to 8C
     temp_mod.set_temperature(temperature)
 
     # Transfer Component 1 to 24 Well Block Tubes
-    p300.transfer(float(component_1_volume), tuberack['A1'], dest_tubes.wells()[:24], new_tip='once')
+    p300.transfer(float(component_1_volume), tuberack['A1'],
+                  dest_tubes.wells()[:24], new_tip='once')
 
     # Transfer Component 2 to 24 Well Block Tubes
-    p300.transfer(float(component_2_volume), tuberack['B1'], dest_tubes.wells()[:24], new_tip='always')
+    p300.transfer(float(component_2_volume),
+                  tuberack['B1'], dest_tubes.wells()[:24], new_tip='always')
 
     # Get Select PCR Tube Wells
-    pcr_wells = [pcr_plate[well] for well in ['A1','A2','A3','A6','A7','A8','C1','C2','C3']]
+    pcr_wells = [pcr_plate[well] for well in ['A1', 'A2', 'A3', 'A6', 'A7',
+                                              'A8', 'C1', 'C2', 'C3']]
 
     # Transfer from Tube 1 to Select PCR Tubes
     p300.transfer(22, dest_tubes['A1'], pcr_wells, new_tip='once')
--- protocols/0556be-covid-ntc/0556be-covid-ntc.ot2.apiv2.py
+++ protocols/0556be-covid-ntc/0556be-covid-ntc.ot2.apiv2.py
@@ -9,16 +9,18 @@ metadata = {
 def run(ctx):
 
     [p1000_mount, component_1_volume] = get_values(  # noqa: F821
-    "p1000_mount", "component_1_volume")
+        "p1000_mount", "component_1_volume")
 
     # Load Labware
-    tuberack = ctx.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 10)['A1']
-    dest_tubes = ctx.load_labware('opentrons_24_aluminumblock_generic_2ml_screwcap', 11)
+    tuberack = ctx.load_labware(
+        'opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', 10)['A1']
+    dest_tubes = ctx.load_labware(
+        'opentrons_24_aluminumblock_generic_2ml_screwcap', 11)
     tiprack_1000ul = ctx.load_labware('opentrons_96_filtertiprack_1000ul', 1)
 
     # Load Instruments
-    p1000 = ctx.load_instrument('p1000_single_gen2', 'right', tip_racks=[tiprack_1000ul])
+    p1000 = ctx.load_instrument('p1000_single_gen2', 'right',
+                                tip_racks=[tiprack_1000ul])
 
     # Transfer Component 1 to Destination Tubes
     p1000.transfer(float(component_1_volume), tuberack, dest_tubes.wells())
-    
