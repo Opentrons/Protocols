c2f0227b3fc18ff57ed69507db812ef2f1ec8ab5
Sakib Hossain <sakibh97@gmail.com>
2021-01-12 09:31:56 -0500

    Added temperature module

--- protocols/0556be-bmda/0556be-bmda.ot2.apiv2.py
+++ protocols/0556be-bmda/0556be-bmda.ot2.apiv2.py
@@ -9,20 +9,22 @@ metadata = {
 def run(ctx):
 
     # Load Labware
-    reagents = ctx.load_labware('opentrons_24_aluminumblock_generic_2ml_screwcap', 7)
+    temp_mod = ctx.load_module('temperature module gen2', 7)
+    reagents = temp_mod.load_labware('opentrons_24_aluminumblock_generic_2ml_screwcap')
     pcr_plate = ctx.load_labware('opentrons_96_aluminumblock_generic_pcr_strip_200ul', 8)
     tiprack_200ul = ctx.load_labware('opentrons_96_filtertiprack_200ul', 1)
-    tiprack_20ul = ctx.load_labware('opentrons_96_filtertiprack_20ul', 2)
 
     # Load Instruments
     p300 = ctx.load_instrument('p300_single_gen2', 'right', tip_racks=[tiprack_200ul])
-    p20 = ctx.load_instrument('p20_single_gen2', 'left', tip_racks=[tiprack_20ul])
 
     # Get Sample Wells
     mm = reagents.wells()[0]
     components = reagents.wells()[12:]
     volumes = [51, 85, 51, 119, 136, 34, 34, 34, 34, 34, 34, 34]
 
+    # Set Temperature to 8C
+    temp_mod.set_temperature(8)
+    
     # Add Components to Master Mix
     p300.transfer(volumes, components, mm, new_tip='always')
     p300.pick_up_tip()
@@ -30,4 +32,7 @@ def run(ctx):
     p300.drop_tip()
 
     # Add Master Mix to 32 wells
-    p20.transfer(20, mm, pcr_plate.wells()[:32], new_tip='once')
+    p300.transfer(20, mm, pcr_plate.wells()[:32], new_tip='once')
+
+    # Deactivate Temp Mod
+    temp_mod.deactivate()
