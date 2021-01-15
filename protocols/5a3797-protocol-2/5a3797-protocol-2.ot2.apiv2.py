fd6bedd5b68b1a4f4b1c638b2ec55d0172d8ec49
Sakib Hossain <sakibh97@gmail.com>
2021-01-15 15:10:17 -0500

    Updated README, fields, and linting for 5a3797-protocol-2

--- protocols/5a3797-protocol-2/5a3797-protocol-2.ot2.apiv2.py
+++ protocols/5a3797-protocol-2/5a3797-protocol-2.ot2.apiv2.py
@@ -8,14 +8,16 @@ metadata = {
 
 def run(ctx):
 
-    # [m20_mount] = get_values(  # noqa: F821
-    #     "m20_mount")
+    [m20_mount] = get_values(  # noqa: F821
+        "m20_mount")
 
     # Load Labware
-    tipracks = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in range(1,3)]
+    tipracks = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
+                for slot in range(1, 3)]
     mastermix = ctx.load_labware('nest_12_reservoir_15ml', 7)
     sample_plate = ctx.load_labware('kingfisher_96_deepwell_plate_2ml', 4)
-    pcr_plate = ctx.load_labware('thermofishermicroampfast96well0.1_96_wellplate_100ul', 5)
+    pcr_plate = ctx.load_labware(
+                'thermofishermicroampfast96well0.1_96_wellplate_100ul', 5)
 
     # Load Pipette
     m20 = ctx.load_instrument('p20_multi_gen2', 'left', tip_racks=tipracks)
@@ -25,4 +27,3 @@ def run(ctx):
 
     # Aliquot 10 uL of Sample
     m20.transfer(10, sample_plate.wells(), pcr_plate.wells(), new_tip='always')
-    
