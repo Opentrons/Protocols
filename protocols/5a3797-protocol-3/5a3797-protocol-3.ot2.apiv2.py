098dda3d9e9132c60efabd46659561f40909cc61
Sakib Hossain <sakibh97@gmail.com>
2021-01-15 15:43:59 -0500

    Updated custom labware definitions, README, fields, and linting for 5a3797-protocol-3

--- protocols/5a3797-protocol-3/5a3797-protocol-3.ot2.apiv2.py
+++ protocols/5a3797-protocol-3/5a3797-protocol-3.ot2.apiv2.py
@@ -8,25 +8,39 @@ metadata = {
 
 def run(ctx):
 
-    # [m300_mount, p300_mount] = get_values(  # noqa: F821
-    #     "m300_mount", "p300_mount")
+    [m300_mount, p300_mount, tuberack_1, tuberack_2, tuberack_3, tuberack_4,
+        tuberack_5, tuberack_6, tuberack_7] = get_values(  # noqa: F821
+        "m300_mount", "p300_mount", "tuberack_1", "tuberack_2", "tuberack_3",
+        "tuberack_4", "tuberack_5", "tuberack_6", "tuberack_7")
 
     # Load Labware
-    tipracks = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot) for slot in range(1,3)]
-    tuberacks = [ctx.load_labware('opentrons_15_tuberack_falcon_15ml_conical', slot) for slot in range(5,12)]
+    tipracks = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
+                for slot in range(1, 3)]
+    tuberack_types = [tuberack_1, tuberack_2, tuberack_3, tuberack_4,
+                      tuberack_5, tuberack_6, tuberack_7]
+
+    tuberacks = []
+    for rack, slot in zip(tuberack_types, range(5, 12)):
+        tuberacks.append(ctx.load_labware(rack, slot))
+
     sample_plate = ctx.load_labware('kingfisher_96_deepwell_plate_2ml', 3)
     reservoir = ctx.load_labware('nest_12_reservoir_15ml', 4)
 
     # Load Pipette
-    p300 = ctx.load_instrument('p300_single_gen2', 'right', tip_racks=tipracks)
-    m300 = ctx.load_instrument('p300_multi_gen2', 'left', tip_racks=tipracks)
+    p300 = ctx.load_instrument('p300_single_gen2', p300_mount,
+                               tip_racks=tipracks)
+    m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
+                               tip_racks=tipracks)
 
     # Get list of tubes across 7 racks, get first 96
     tuberack_wells = [tuberacks[i].wells() for i in range(len(tuberacks))]
-    tuberack_samples = [well for wells in tuberack_wells for well in wells][:96]
+    tuberack_samples = [well for wells in tuberack_wells for well in
+                        wells][:96]
 
     # Aliquot 200 uL from ~7 Tube Racks
-    p300.transfer(200, tuberack_samples, sample_plate.wells(), new_tip='always')
+    p300.transfer(200, tuberack_samples, sample_plate.wells(),
+                  new_tip='always')
 
     # Aliquot 275 uL from Reservoir
-    m300.transfer(275, reservoir.wells(), sample_plate.wells(), new_tip='always')
+    m300.transfer(275, reservoir.wells(), sample_plate.wells(),
+                  new_tip='always')
