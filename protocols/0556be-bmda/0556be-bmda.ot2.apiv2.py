0692227d77ed8472a6e635eafe3eca8928fd880f
Sakib Hossain <sakibh97@gmail.com>
2021-02-01 14:05:56 -0500

    Updated labware, added field for custom tube numbers, changed temp mod slot position

--- protocols/0556be-bmda/0556be-bmda.ot2.apiv2.py
+++ protocols/0556be-bmda/0556be-bmda.ot2.apiv2.py
@@ -8,13 +8,13 @@ metadata = {
 
 def run(ctx):
 
-    [p300_mount, temperature] = get_values(  # noqa: F821
-        "p300_mount", "temperature")
+    [p300_mount, temperature, final_tubes] = get_values(  # noqa: F821
+        "p300_mount", "temperature", "final_tubes")
 
     # Load Labware
-    temp_mod = ctx.load_module('temperature module gen2', 7)
+    temp_mod = ctx.load_module('temperature module gen2', 10)
     reagents = temp_mod.load_labware(
-        'opentrons_24_aluminumblock_generic_2ml_screwcap')
+        'opentrons_24_aluminumblock_nest_1.5ml_snapcap')
     pcr_plate = ctx.load_labware(
         'opentrons_96_aluminumblock_generic_pcr_strip_200ul', 8)
     tiprack_200ul = ctx.load_labware('opentrons_96_filtertiprack_200ul', 1)
@@ -27,6 +27,7 @@ def run(ctx):
     mm = reagents.wells()[0]
     components = reagents.wells()[12:]
     volumes = [51, 85, 51, 119, 136, 34, 34, 34, 34, 34, 34, 34]
+    final_tubes = int(final_tubes)
 
     # Set Temperature to 8C
     temp_mod.set_temperature(temperature)
@@ -34,12 +35,12 @@ def run(ctx):
     # Add Components to Master Mix
     p300.transfer(volumes, components, mm, new_tip='always')
     p300.pick_up_tip()
-    p300.mix(20)
+    p300.mix(5, 200, mm)
     p300.drop_tip()
 
     # Get well distribution for PCR plate
     pcr_plate_wells = [pcr_plate.columns()[i] for i in [0, 3, 6, 9]]
-    pcr_plate_wells = [wells for well in pcr_plate_wells for wells in well]
+    pcr_plate_wells = [wells for well in pcr_plate_wells for wells in well][:final_tubes]
 
     # Add Master Mix to 32 wells
     p300.transfer(20, mm, pcr_plate_wells, new_tip='once')
