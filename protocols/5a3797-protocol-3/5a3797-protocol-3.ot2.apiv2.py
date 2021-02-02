eda11e5ea7ef6cd9f9681e386508cb33c8022612
Sakib Hossain <sakibh97@gmail.com>
2021-02-02 15:46:31 -0500

    Added custom sample number, adjust final aspiration speed

--- protocols/5a3797-protocol-3/5a3797-protocol-3.ot2.apiv2.py
+++ protocols/5a3797-protocol-3/5a3797-protocol-3.ot2.apiv2.py
@@ -1,3 +1,5 @@
+import math
+
 metadata = {
     'protocolName': 'Protocol 3 - Sample Plate setup',
     'author': 'Sakib <sakib.hossain@opentrons.com>',
@@ -8,10 +10,19 @@ metadata = {
 
 def run(ctx):
 
-    [m300_mount, p300_mount, tuberack_1, tuberack_2, tuberack_3, tuberack_4,
-        tuberack_5, tuberack_6, tuberack_7] = get_values(  # noqa: F821
-        "m300_mount", "p300_mount", "tuberack_1", "tuberack_2", "tuberack_3",
-        "tuberack_4", "tuberack_5", "tuberack_6", "tuberack_7")
+    [m300_mount, p300_mount, samples, tuberack_1, tuberack_2, tuberack_3, tuberack_4,
+        tuberack_5, tuberack_6, tuberack_7, final_asp_speed] = get_values(  # noqa: F821
+        "m300_mount", "p300_mount", "samples", "tuberack_1", "tuberack_2", "tuberack_3",
+        "tuberack_4", "tuberack_5", "tuberack_6", "tuberack_7", "final_asp_speed")
+    
+    final_asp_speed = float(final_asp_speed)
+    
+    # Get sample number
+    samples = int(samples)
+    if samples > 96:
+        raise Exception("You cannot have greater than 96 samples")
+
+    columns = math.ceil(samples/8)
 
     # Load Labware
     tipracks = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
@@ -32,15 +43,19 @@ def run(ctx):
     m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
                                tip_racks=tipracks)
 
-    # Get list of tubes across 7 racks, get first 96
+    # Get list of tubes across 7 racks based on sample number
     tuberack_wells = [tuberacks[i].wells() for i in range(len(tuberacks))]
     tuberack_samples = [well for wells in tuberack_wells for well in
-                        wells][:96]
+                        wells][:samples]
+
+    sample_plate_wells = sample_plate.rows()[0][:columns]
+    reservoir_columns = reservoir.wells()[:columns]
 
     # Aliquot 200 uL from ~7 Tube Racks
-    p300.transfer(200, tuberack_samples, sample_plate.wells(),
+    p300.transfer(200, tuberack_samples, sample_plate.wells()[:samples],
                   new_tip='always')
 
     # Aliquot 275 uL from Reservoir
-    m300.transfer(275, reservoir.wells(), sample_plate.wells(),
-                  new_tip='always')
+    m300.flow_rate.aspirate = final_asp_speed
+    m300.transfer(275, reservoir_columns, sample_plate_wells,
+                new_tip='always')
