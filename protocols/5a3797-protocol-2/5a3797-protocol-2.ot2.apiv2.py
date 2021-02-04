462e1a4011a189d9c0c1b31a8332d12a28e920da
Sakib Hossain <sakibh97@gmail.com>
2021-02-04 17:30:46 -0500

    Added parameters for pipette height, pipette takes tips from specific racks, mastermix is aspirated from channel 1

--- protocols/5a3797-protocol-2/5a3797-protocol-2.ot2.apiv2.py
+++ protocols/5a3797-protocol-2/5a3797-protocol-2.ot2.apiv2.py
@@ -8,22 +8,32 @@ metadata = {
 
 def run(ctx):
 
-    [m20_mount] = get_values(  # noqa: F821
-        "m20_mount")
+    [m20_mount, reservoir_height, pcr_plate_height, sample_plate_height] = get_values(  # noqa: F821
+        "m20_mount", "reservoir_height", "pcr_plate_height", "sample_plate_height")
+
+    reservoir_height = float(reservoir_height)
+    pcr_plate_height = float(pcr_plate_height)
+    sample_plate_height = float(sample_plate_height)
 
     # Load Labware
-    tipracks = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
-                for slot in range(1, 3)]
-    mastermix = ctx.load_labware('nest_12_reservoir_15ml', 7)
+    tiprack1 = ctx.load_labware('opentrons_96_filtertiprack_20ul', 1, 'Tip Box 1')
+    tiprack2 = ctx.load_labware('opentrons_96_filtertiprack_20ul', 2, 'Tip Box 2')
+    mastermix = ctx.load_labware('nest_12_reservoir_15ml', 7)['A1']
     sample_plate = ctx.load_labware('kingfisher_96_deepwell_plate_2ml', 4)
     pcr_plate = ctx.load_labware(
                 'thermofishermicroampfast96well0.1_96_wellplate_100ul', 5)
 
     # Load Pipette
-    m20 = ctx.load_instrument('p20_multi_gen2', 'left', tip_racks=tipracks)
+    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount, tip_racks=[tiprack1, tiprack2])
 
     # Aliquot 15uL of Mastermix
-    m20.transfer(15, mastermix.wells(), pcr_plate.wells())
+    m20.pick_up_tip(tiprack1['A1'])
+    for pcr_well in pcr_plate.rows()[0]:
+        m20.transfer(15, mastermix.bottom(reservoir_height), pcr_well.bottom(pcr_plate_height), new_tip='never')
+    m20.drop_tip()
 
     # Aliquot 10 uL of Sample
-    m20.transfer(10, sample_plate.wells(), pcr_plate.wells(), new_tip='always')
+    for source, dest, tip in zip(sample_plate.rows()[0], pcr_plate.rows()[0], tiprack2.rows()[0]):
+        m20.pick_up_tip(tip)
+        m20.transfer(10, source.bottom(sample_plate_height), dest.bottom(pcr_plate_height), new_tip='never')
+        m20.drop_tip()
