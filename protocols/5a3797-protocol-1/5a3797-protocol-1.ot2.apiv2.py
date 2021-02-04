511face211e1c730b89718ee4e6c7939ca2d6274
Sakib Hossain <sakibh97@gmail.com>
2021-02-04 16:09:42 -0500

    Updated protocol with max split transfers and pipette aspiration height parameters

--- protocols/5a3797-protocol-1/5a3797-protocol-1.ot2.apiv2.py
+++ protocols/5a3797-protocol-1/5a3797-protocol-1.ot2.apiv2.py
@@ -8,8 +8,11 @@ metadata = {
 
 def run(ctx):
 
-    [m300_mount] = get_values(  # noqa: F821
-        "m300_mount")
+    [m300_mount, reservoir_height, plate_height] = get_values(  # noqa: F821
+        "m300_mount", "reservoir_height", "plate_height")
+
+    reservoir_height = float(reservoir_height)
+    plate_height = float(plate_height)
 
     # Load Labware
     tipracks = ctx.load_labware('opentrons_96_filtertiprack_200ul', 1)
@@ -24,11 +27,30 @@ def run(ctx):
     m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
                                tip_racks=[tipracks])
 
+    # Split Transfer Volumes
+    max_vol = 200
+    def split_transfer(vol, source, dest):
+        while vol >= max_vol:
+            m300.transfer(max_vol, source, dest, new_tip='never')
+            vol -= max_vol
+        else:
+            if vol:
+                m300.transfer(vol, source, dest, new_tip='never')
+
     # Aliquot 500uL of Wash 1
-    m300.transfer(500, wash_1.wells(), dw_plate_wash_1.wells())
+    m300.pick_up_tip()
+    for wash_1_well, plate_1_well in zip(wash_1.wells(), dw_plate_wash_1.rows()[0]):
+        split_transfer(500, wash_1_well.bottom(reservoir_height), plate_1_well.bottom(plate_height))
+    m300.drop_tip()
 
     # Aliquot 1000uL of Wash 2
-    m300.transfer(1000, wash_2.wells(), dw_plate_wash_2.wells())
+    m300.pick_up_tip()
+    for wash_2_well, plate_2_well in zip(wash_2.wells(), dw_plate_wash_2.rows()[0]):
+        split_transfer(1000, wash_2_well.bottom(reservoir_height), plate_2_well.bottom(plate_height))
+    m300.drop_tip()
 
     # Aliquot 50uL of Elution
-    m300.transfer(50, elution.wells(), dw_plate_elution.wells())
+    m300.pick_up_tip()
+    for elution_well in dw_plate_elution.rows()[0]:
+        m300.transfer(50, elution.wells()[0].bottom(reservoir_height), elution_well.bottom(plate_height), new_tip="never")
+    m300.drop_tip()
