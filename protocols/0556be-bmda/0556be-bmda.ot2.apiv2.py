4bcb27d1f192c73141a97971bd4a9384a1e7c1bb
Sakib Hossain <sakibh97@gmail.com>
2021-02-09 15:37:44 -0500

    Added component aspiration and dispense flow rate parameters

--- protocols/0556be-bmda/0556be-bmda.ot2.apiv2.py
+++ protocols/0556be-bmda/0556be-bmda.ot2.apiv2.py
@@ -8,8 +8,8 @@ metadata = {
 
 def run(ctx):
 
-    [p300_mount, temperature, final_tubes] = get_values(  # noqa: F821
-        "p300_mount", "temperature", "final_tubes")
+    [p300_mount, temperature, final_tubes, comp_asp_speed, comp_disp_speed] = get_values(  # noqa: F821
+        "p300_mount", "temperature", "final_tubes", "comp_asp_speed", "comp_disp_speed")
 
     # Load Labware
     temp_mod = ctx.load_module('temperature module gen2', 10)
@@ -33,10 +33,16 @@ def run(ctx):
     temp_mod.set_temperature(temperature)
 
     # Add Components to Master Mix
+    p300.flow_rate.aspirate = comp_asp_speed
+    p300.flow_rate.dispense = comp_disp_speed
     p300.transfer(volumes, components, mm, new_tip='always')
     p300.pick_up_tip()
     p300.mix(5, 200, mm)
     p300.drop_tip()
+
+    # Reset Flow Rates
+    p300.flow_rate.aspirate = 92.86
+    p300.flow_rate.dispense = 92.86
 
     # Get well distribution for PCR plate
     pcr_plate_wells = [pcr_plate.columns()[i] for i in [0, 3, 6, 9]]
