b88bbb0cadf8220b61952ca84871aab32cb2644a
Sakib Hossain <sakibh97@gmail.com>
2021-02-10 11:39:46 -0500

    added parameter for sample number

--- protocols/5a3797-protocol-1/5a3797-protocol-1.ot2.apiv2.py
+++ protocols/5a3797-protocol-1/5a3797-protocol-1.ot2.apiv2.py
@@ -1,3 +1,5 @@
+import math
+
 metadata = {
     'protocolName': 'Protocol 1 - Washing Aliquot',
     'author': 'Sakib <sakib.hossain@opentrons.com>',
@@ -8,11 +10,14 @@ metadata = {
 
 def run(ctx):
 
-    [m300_mount, reservoir_height, plate_height] = get_values(  # noqa: F821
-        "m300_mount", "reservoir_height", "plate_height")
+    [m300_mount, samples, reservoir_height,
+        plate_height] = get_values(  # noqa: F821
+        "m300_mount", "samples", "reservoir_height", "plate_height")
 
     reservoir_height = float(reservoir_height)
     plate_height = float(plate_height)
+    samples = int(samples)
+    columns = math.ceil(samples/8)
 
     # Load Labware
     tipracks = ctx.load_labware('opentrons_96_filtertiprack_200ul', 1)
@@ -40,23 +45,23 @@ def run(ctx):
 
     # Aliquot 500uL of Wash 1
     m300.pick_up_tip()
-    for wash_1_well, plate_1_well in zip(wash_1.wells(),
-                                         dw_plate_wash_1.rows()[0]):
+    for wash_1_well, plate_1_well in zip(wash_1.wells()[:columns],
+                                         dw_plate_wash_1.rows()[0][:columns]):
         split_transfer(500, wash_1_well.bottom(reservoir_height),
                        plate_1_well.bottom(plate_height))
     m300.drop_tip()
 
     # Aliquot 1000uL of Wash 2
     m300.pick_up_tip()
-    for wash_2_well, plate_2_well in zip(wash_2.wells(),
-                                         dw_plate_wash_2.rows()[0]):
+    for wash_2_well, plate_2_well in zip(wash_2.wells()[:columns],
+                                         dw_plate_wash_2.rows()[0][:columns]):
         split_transfer(1000, wash_2_well.bottom(reservoir_height),
                        plate_2_well.bottom(plate_height))
     m300.drop_tip()
 
     # Aliquot 50uL of Elution
     m300.pick_up_tip()
-    for elution_well in dw_plate_elution.rows()[0]:
+    for elution_well in dw_plate_elution.rows()[0][:columns]:
         m300.transfer(50, elution.wells()[0].bottom(reservoir_height),
                       elution_well.bottom(plate_height), new_tip="never")
     m300.drop_tip()
