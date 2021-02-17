f92ffec05b5c369b5fcce7daef907c743ddc5b65
Sakib Hossain <sakibh97@gmail.com>
2021-02-16 20:13:24 -0500

    completed protocol logic for PCB board

--- protocols/74312c/74312c.ot2.apiv2.py
+++ protocols/74312c/74312c.ot2.apiv2.py
@@ -8,15 +8,24 @@ metadata = {
 
 def run(ctx):
 
-    p20_mount = 'left'
+    [p20_mount, samples, volume, trough_type] = get_values(  # noqa: F821
+        "p20_mount", "samples", "volume", "trough_type")
+
+    volume = float(volume)
+    trough_type = int(trough_type)
+
+    troughs = {1: "agilent_1_reservoir_290ml", 2: "axygen_1_reservoir_90ml",
+               3: "nest_12_reservoir_15ml", 4: "nest_1_reservoir_195ml",
+               5: "usascientific_12_reservoir_22ml"}
 
     # Load Labware
     pcb = ctx.load_labware('pcb_board_250_wells', 1)
-    trough = ctx.load_labware('nest_1_reservoir_195ml', 6, 'Tetrahydrofuran')['A1']
+    trough = ctx.load_labware(troughs[trough_type], 6, 'Tetrahydrofuran')['A1']
     tiprack = ctx.load_labware('opentrons_96_tiprack_20ul', 3)
-    
+
     # Load Pipette
-    p20 = ctx.load_instrument('p20_single_gen2', p20_mount, tip_racks=[tiprack])
+    p20 = ctx.load_instrument('p20_single_gen2', p20_mount,
+                              tip_racks=[tiprack])
 
     # Transfer 10 uL to all wells
-    p20.transfer(10, trough, pcb.wells(), new_tip='once')
+    p20.transfer(volume, trough, pcb.wells()[:samples], new_tip='once')
