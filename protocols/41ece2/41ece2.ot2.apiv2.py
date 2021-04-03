66efa4f56e552672d378a1e3c0530736bf24caf0
Chazman Childers <protocols@opentrons.com>
2021-04-02 17:19:56 -0700

    updated protocol file and fields

--- protocols/41ece2/41ece2.ot2.apiv2.py
+++ protocols/41ece2/41ece2.ot2.apiv2.py
@@ -7,36 +7,39 @@ metadata = {
 
 
 def run(protocol):
-    [mnt300, mnt10, cellsmedia] = get_values(  # noqa: F821
-     'mnt300', 'mnt10', 'cellsmedia')
+    [mnt300, mnt10, cellsmedia, np] = get_values(  # noqa: F821
+     'mnt300', 'mnt10', 'cellsmedia', 'np')
 
     # load labware
-    tips10 = [protocol.load_labware('opentrons_96_filtertiprack_10ul', '5')]
-    tips300 = [protocol.load_labware('opentrons_96_tiprack_300ul', '2')]
+    tips10 = [protocol.load_labware('opentrons_96_filtertiprack_10ul', '6')]
+    tips300 = [protocol.load_labware('opentrons_96_tiprack_300ul', '3')]
 
     m10 = protocol.load_instrument('p10_multi', mnt10, tip_racks=tips10)
     m300 = protocol.load_instrument('p300_multi', mnt300, tip_racks=tips300)
     m10.flow_rate.aspirate = 10
     m10.flow_rate.dispense = 10
 
-    dd01, dd1, dd10, ds200 = [
+    ds200 = protocol.load_labware(
+        'corning_96_wellplate_360ul_flat', '11', 'Drug Stock 200uM')
+    dplates = [
         protocol.load_labware(
             'corning_96_wellplate_360ul_flat',
             s,
-            n) for s, n in zip(range(1, 12, 3), [
-                'Drug Dilution 0.1uM',
-                'Drug Dilution 1uM',
+            n) for s, n in zip([10, 7, 4, 1], [
                 'Drug Dilution 10uM',
-                'Drug Stock 200uM'])]
-
+                'Drug Dilution 1uM',
+                'Drug Dilution 0.1uM',
+                'Drug Dilution 0.01uM'][:np])]
     media = protocol.load_labware(
-        'axygen_1_reservoir_90ml', '8', 'Media+IL-2+OKT3+CD28')
+        'axygen_1_reservoir_90ml', '2', 'Media+IL-2+OKT3+CD28')
     if cellsmedia:
         cells = protocol.load_labware(
-            'axygen_1_reservoir_90ml', '11', 'Cells')
+            'axygen_1_reservoir_90ml', '5', 'Cells')
 
     # Transfer 100µL (or 90µL) Media to wells in plates
-    allPlateWells = [dd10.rows()[0][1:]+dd1.rows()[0][1:], dd01.rows()[0][1:]]
+    lastplate = dplates[-1].rows()[0][1:]
+    otherplates = [r for plate in dplates[:-1] for r in plate.rows()[0][1:]]
+    allPlateWells = [otherplates, lastplate]
 
     protocol.comment('Transferring Media to columns 2-12 ...')
     tipctr = 0
@@ -63,23 +66,15 @@ def run(protocol):
 
     # Perform dilutions
     protocol.comment('Performing 10x dilutions...')
-    plates = [p.rows()[0][1:11] for p in [ds200, dd10, dd1, dd01]]
-    for s, p1, p2, p3 in zip(*plates):
+    for i in range(1, 11):
         m10.pick_up_tip()
-
-        m10.transfer(
-            10, s, p1, new_tip='never',
-            mix_before=(4, 10), mix_after=(4, 10))
-        m10.blow_out()
-        m10.transfer(
-            10, p1, p2, new_tip='never',
-            mix_before=(1, 7), mix_after=(4, 10))
-        m10.blow_out()
-        m10.transfer(
-            10, p2, p3, new_tip='never',
-            mix_before=(1, 7), mix_after=(4, 10))
-        m10.blow_out()
-
+        for j in range(len(dplates)):
+            dest = dplates[j].rows()[0][i]
+            src = ds200.rows()[0][i] if j == 0 else dplates[j-1].rows()[0][i]
+            m10.transfer(
+                10, src, dest, new_tip='never',
+                mix_before=(4, 10), mix_after=(4, 10))
+            m10.blow_out()
         m10.drop_tip()
 
     # Transfer 100µL Cells to wells in plates
@@ -87,12 +82,11 @@ def run(protocol):
         protocol.comment('Transferring 100µL of cells to columns 2-12...')
         tipctr = 0
         m300.flow_rate.dispense = 200
-        m300.starting_tip = tips300[0]['A2']
 
         for wells in allPlateWells:
             for well in wells:
                 if tipctr == 0:
-                    m300.pick_up_tip()
+                    m300.pick_up_tip(tips300[0]['A2'])
                 m300.mix(5, 300, cells['A1'])
                 m300.aspirate(20, cells['A1'].top())
                 m300.aspirate(60, cells['A1'])
@@ -103,10 +97,9 @@ def run(protocol):
                 m300.blow_out()
                 tipctr += 1
                 if tipctr == 4:
-                    m300.return_tip()
-                    m300.starting_tip = tips300[0]['A2']
+                    m300.drop_tip(tips300[0]['A2'])
                     tipctr = 0
         if m300.has_tip:
-            m300.return_tip()
+            m300.drop_tip(tips300[0]['A2'])
 
     protocol.comment('Protocol complete!')
