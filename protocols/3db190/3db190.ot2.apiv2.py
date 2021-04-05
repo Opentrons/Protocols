8135cc3cfabd3cd1e4f4eb47912238c3ab19f12e
Sakib Hossain <sakibh97@gmail.com>
2021-04-05 15:25:59 -0400

    updated protocol with multichannel support, added 12-well reservoir, and removed homing parameter

--- protocols/3db190/3db190.ot2.apiv2.py
+++ protocols/3db190/3db190.ot2.apiv2.py
@@ -1,23 +1,26 @@
+import math
+
 metadata = {
-    'protocolName': 'PCR/qPCR prep',
+    'protocolName': 'PCR/qPCR Prep',
     'author': 'Sakib <sakib.hossain@opentrons.com>',
     'description': 'Custom Protocol Request',
-    'apiLevel': '2.7'
+    'apiLevel': '2.9'
 }
 
 
 def run(protocol):
 
-    [total_samples, p20_mount, p1000_mount, home] = get_values(  # noqa: F821
-        "total_samples", "p20_mount", "p1000_mount", "home")
+    [total_samples, p20_mount, p300_mount] = get_values(  # noqa: F821
+        "total_samples", "p20_mount", "p300_mount")
 
     total_samples = int(total_samples)
+    cols = math.ceil(total_samples/8)
 
     # Load Tip Racks
     tipracks_20ul = [protocol.load_labware('opentrons_96_filtertiprack_20ul',
                                            slot) for slot in ['7', '8']]
-    tipracks_1000ul = [protocol.load_labware(
-        'opentrons_96_filtertiprack_1000ul', slot) for slot in ['4', '5']]
+    tipracks_200ul = [protocol.load_labware(
+        'opentrons_96_filtertiprack_200ul', slot) for slot in ['4', '5']]
 
     # Load Plates
     # 1 mL Deepwell Microtiter Plate
@@ -26,33 +29,36 @@ def run(protocol):
     pcr_plate = protocol.load_labware('enduraplate_96_wellplate_200ul', 3)
 
     # Load Instruments
-    p20 = protocol.load_instrument('p20_single_gen2', p20_mount,
+    p20 = protocol.load_instrument('p20_multi_gen2', p20_mount,
                                    tip_racks=tipracks_20ul)
-    p1000 = protocol.load_instrument('p1000_single_gen2', p1000_mount,
-                                     tip_racks=tipracks_1000ul)
+    p300 = protocol.load_instrument('p300_multi_gen2', p300_mount,
+                                    tip_racks=tipracks_200ul)
 
-    p1000.flow_rate.aspirate = 350
-    p1000.flow_rate.dispense = 350
+    p300.flow_rate.aspirate = 300
+    p300.flow_rate.dispense = 300
     p20.flow_rate.aspirate = 12
     p20.flow_rate.dispense = 12
 
     # Proccess Buffer (A1)
-    buffer = protocol.load_labware(
-        'opentrons_6_tuberack_falcon_50ml_conical', 1)['A1'].bottom(z=10)
+    # 10 mL per channel, enough for 3 columns of sample
+    reservoir = protocol.load_labware('nest_12_reservoir_15ml', 1)
+    buffer_wells = [well for well in reservoir.rows()[0][:cols]
+                    for i in range(3)]
 
-    deep_samples = deepwell_plate.wells()[:total_samples]
-    pcr_samples = pcr_plate.wells()[:total_samples]
+    # Sample Wells
+    deep_samples = deepwell_plate.rows()[0][:cols]
+    pcr_samples = pcr_plate.rows()[0][:cols]
 
     # Protocol Steps
 
     # Add 400 uL of Process Buffer to Required Wells in Deep Well Block
     protocol.comment(f'Adding 400 uL of process buffer \
                      sequentially to {total_samples} wells...')
-    p1000.pick_up_tip()
-    for well in deep_samples:
-        p1000.transfer(400, buffer, well,
-                       new_tip='never')
-    p1000.drop_tip(home_after=home)
+    p300.pick_up_tip()
+    for well, buffer in zip(deep_samples, buffer_wells):
+        p300.transfer(400, buffer, well,
+                      new_tip='never')
+    p300.drop_tip()
 
     # PAUSE PROTOCOL #
     protocol.pause('Pausing protocol for further specimen processing and \
@@ -60,12 +66,12 @@ def run(protocol):
                    Resume when ready...')
 
     # Mix and Add 5 uL from Deep Well Block to PCR Plate
-    # Mix 5x with P1000 set at 150 uL, (Uses new tip each time)
+    # Mix 5x with P300 set at 150 uL, (Uses new tip each time)
     # then transfer with P20 at 5 uL (Uses new tip each time)
     protocol.comment('Starting the mixing and transfer of specimen process...')
     for source, dest in zip(deep_samples, pcr_samples):
-        p1000.pick_up_tip()
-        p1000.mix(5, 150, source)
-        p1000.drop_tip(home_after=home)
+        p300.pick_up_tip()
+        p300.mix(5, 150, source)
+        p300.drop_tip()
         p20.transfer(5, source,
                      dest, new_tip='always')
