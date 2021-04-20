056b63abe420fb03cfe114db0c637269d77e5a14
ramifarawi <>
2021-04-20 08:12:45 -0400

    testing

--- protocols/4d9b8b/4d9b8b.ot2.apiv2.py
+++ protocols/4d9b8b/4d9b8b.ot2.apiv2.py
@@ -7,11 +7,6 @@ metadata = {
     'apiLevel': '2.7'
 }
 
-def get_values(*names):
-    import json
-    _all_values = json.loads("""{"vol_tube1": 36.746, "vol_tube2": 34, "vol_tube3": 34.21, "mix_vol": 20, "disp_rate": 7.56, "disp_height": -1, "mix_reps": 5, "num_samp":96,"p20_mount":"left", "csv": "Wafer well, Transfervol1, Transfervol2, Transfervol3, Transfervoltowafer \\n A1, 1, 2, 4, 12\\n B1, 2, 2, 2, 13\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\nC1, 3, 10, 10, 14\\n", "grid": "wafer_6x4_grid", "grid": "wafer_4x24_grid"}""")
-    return [_all_values[n] for n in names]
-
 
 def run(ctx):
 
@@ -77,7 +72,7 @@ def run(ctx):
             p20.return_tip()
         ctx.comment('\nAdding to Wafer Plate\n')
         p20.pick_up_tip(tips[start])
-        p20.mix(mix_reps, int(transfer[i][4]))
+        p20.mix(mix_reps, mix_vol))
         p20.aspirate(int(transfer[i][4]), mix_well)
         p20.dispense(int(transfer[i][4]), dest_well.top(z=disp_height))
         p20.drop_tip()
