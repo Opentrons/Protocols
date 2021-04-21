e9bebf1c58e96dc81c4affbc089c82395bc09795
Sakib Hossain <sakibh97@gmail.com>
2021-04-21 17:54:22 -0400

    updated protocol variables and parameters

--- protocols/2bdff3/2bdff3.ot2.apiv2.py
+++ protocols/2bdff3/2bdff3.ot2.apiv2.py
@@ -5,26 +5,32 @@ metadata = {
     'apiLevel': '2.9'
 }
 
-def get_values(*names):
-    import json
-    _all_values = json.loads("""{"step1":"True", "step2":"True", "quad1_cols":12, "quad2_cols":12, "quad3_cols":12, "quad4_cols":12, "trans_vol1":3}""")
-    return [_all_values[n] for n in names]
 
 def run(ctx):
 
-    [step1, step2, quad1_cols, quad2_cols, quad3_cols, quad4_cols, trans_vol1] = get_values(  # noqa: F821
-        "step1", "step2", "quad1_cols", "quad2_cols", "quad3_cols", "quad4_cols", "trans_vol1")
+    [step1, step2, quad1_cols, quad2_cols, quad3_cols, quad4_cols, m20_mount,
+        p300_mount, donor_384_type, trans_vol1,
+        step1_mix_vol] = get_values(  # noqa: F821
+        "step1", "step2", "quad1_cols", "quad2_cols", "quad3_cols",
+        "quad4_cols", "m20_mount", "p300_mount", "donor_384_type",
+        "trans_vol1", "step1_mix_vol")
 
     # Load Labware
-    donor_384_plate = ctx.load_labware('bioplastics_384_wellplate_50ul', 1)
-    recipient_96_plate = ctx.load_labware('biorad_96_wellplate_200ul_pcr', 2) # Swap with actual plate
-    tiprack_20ul = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in [4,5,6,7]]
-    tiprack_200ul = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot) for slot in [8]]
-    tuberack = ctx.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 3)['A1']
+    donor_384_plate = ctx.load_labware(donor_384_type, 1)
+    recipient_96_plate = ctx.load_labware(
+        'eppendorftwin.tec_96_wellplate_150ul', 2)
+    tiprack_20ul = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
+                    for slot in [4, 5, 6, 7]]
+    tiprack_200ul = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
+                     for slot in [8]]
+    tuberack = ctx.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 3)['A1']
 
     # Load Pipettes
-    m20 = ctx.load_instrument('p20_multi_gen2', 'left', tip_racks=tiprack_20ul)
-    p300 = ctx.load_instrument('p300_single_gen2', 'right', tip_racks=tiprack_200ul)
+    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount,
+                              tip_racks=tiprack_20ul)
+    p300 = ctx.load_instrument('p300_single_gen2', p300_mount,
+                               tip_racks=tiprack_200ul)
 
     # Get wells by quadrant
     quad1 = [col for col in donor_384_plate.rows()[0][::2]][:quad1_cols]
@@ -37,16 +43,19 @@ def run(ctx):
         for quad in [quad1, quad2, quad3, quad4]:
             for col in quad:
                 m20.pick_up_tip()
-                m20.transfer(trans_vol1, col, recipient_96_plate['A1'], new_tip='never', mix_before=(5, 8))
+                m20.transfer(trans_vol1, col, recipient_96_plate['A1'],
+                             new_tip='never', mix_before=(3, step1_mix_vol))
                 m20.drop_tip()
 
     # Step 2
     if step2 == "True":
         # Calculate max volume per well
-        max_well_vol = (quad1_cols + quad2_cols + quad3_cols + quad4_cols) * trans_vol1
+        max_well_vol = (quad1_cols + quad2_cols + quad3_cols +
+                        quad4_cols) * trans_vol1
         source_wells = recipient_96_plate.columns()[0]
 
         for source in source_wells:
             p300.pick_up_tip()
-            p300.transfer(max_well_vol, source, tuberack, new_tip='never', mix_before=(5, max_well_vol/2))
+            p300.transfer(max_well_vol, source, tuberack, new_tip='never',
+                          mix_before=(3, max_well_vol/2))
             p300.drop_tip()
