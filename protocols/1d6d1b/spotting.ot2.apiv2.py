d030b1257f7800c790dfccfd9e7a07e37d471844
Nick Diehl <ndiehl@opentrons.com>
2021-04-28 09:42:50 -0600

    update blot paper and wellplate labware

--- protocols/1d6d1b/spotting.ot2.apiv2.py
+++ protocols/1d6d1b/spotting.ot2.apiv2.py
@@ -19,16 +19,15 @@ def run(ctx):
         'sample_dwell_time', 'slide_dwell_time', 'm300_mount', 'tip_type')
 
     sample_plates = [
-        ctx.load_labware('eppendorf_96_wellplate_350ul', slot,
+        ctx.load_labware('abgene_96_wellplate_200ul', slot,
                          'sample plate ' + str(i+1))
         for i, slot in enumerate(['1', '3', '4', '6'][:num_plates])]
     slides_mount = ctx.load_labware(
-        'gracebiolabsflexwell_768_other_192x10ul_192x10ul_192x10ul_192x10ul',
-        '2', 'slides')
+        'gracebiolabsflexwell', '2', 'slides')
     pin_wash_res = ctx.load_labware('axygen_4_reservoir_73000ul', '7',
                                     'pin wash reservoir')
-    blot_res = ctx.load_labware('axygen_4_reservoir_73000ul', '8',
-                                'blot reservoir')
+    blot_res = ctx.load_labware('customblotpaper_1_reservoir_870000ul', '8',
+                                'blot paper reservoir')
     tiprack300 = [ctx.load_labware(tip_type, '10')]
     m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
                                tip_racks=tiprack300)
@@ -57,9 +56,12 @@ def run(ctx):
     sample_sets = [plate.rows()[0][:num_cols] for plate in sample_plates]
 
     # wash and blot
+    blot_locs = [
+        blot_res.wells()[0].bottom().move(Point(x=shift, z=-1))
+        for shift in [-40, -20, 20, 40]]
 
     def wash_blot():
-        for wash, blot in zip(pin_wash_res.wells(), blot_res.wells()):
+        for wash, blot in zip(pin_wash_res.wells(), blot_locs):
             movement_locs = [
                 wash.center().move(Point(x=side*5)) for side in [-1, 1]]
             m300.move_to(wash.center())
@@ -72,8 +74,8 @@ def run(ctx):
 
             ctx.max_speeds['A'] = slow_speed_down
             ctx.max_speeds['Z'] = slow_speed_down
-            m300.move_to(blot.top())
-            m300.move_to(blot.bottom())
+            m300.move_to(blot.move(Point(z=10)))
+            m300.move_to(blot)
             ctx.delay(seconds=blot_dwell_time)
 
             del ctx.max_speeds['A']
