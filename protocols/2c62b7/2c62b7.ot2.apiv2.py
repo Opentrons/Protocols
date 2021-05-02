f8c513b3fd717a9d4a9188e8abea4b32e53c04b3
Sakib Hossain <sakibh97@gmail.com>
2021-05-02 19:42:02 -0400

    Updated labware, titles, linting, misc...

--- protocols/2c62b7/2c62b7.ot2.apiv2.py
+++ protocols/2c62b7/2c62b7.ot2.apiv2.py
@@ -1,26 +1,21 @@
 from opentrons import types
-from datetime import datetime, timedelta
 from opentrons import protocol_api
-import threading
-from time import sleep
 import math
 
 metadata = {
-    'protocolName': 'Mag-Bind® Environmental DNA 96 Kit',
+    'protocolName': 'Omega Bio-tek Mag-Bind® Environmental DNA 96 Kit',
     'author': 'Sakib <sakib.hossain@opentrons.com>',
     'description': 'Custom Protocol Request',
     'apiLevel': '2.9'
 }
 
-def get_values(*names):
-    import json
-    _all_values = json.loads("""{"debug":"True","samples":96,"m300_mount":"left","m20_mount":"right","tip_type":"standard", "mm2_vol":300,"vhb_vol":300,"elution_buffer_vol":100,"settling_time":5}""")
-    return [_all_values[n] for n in names]
 
 def run(ctx):
 
-    [debug, samples, m300_mount, m20_mount, tip_type, mm2_vol, vhb_vol, elution_buffer_vol, settling_time] = get_values(  # noqa: F821
-        "debug", "samples", "m300_mount", "m20_mount", "tip_type", "mm2_vol", "vhb_vol", "elution_buffer_vol","settling_time")
+    [debug, samples, m300_mount, m20_mount, tip_type, mm2_vol, vhb_vol,
+        elution_buffer_vol, settling_time] = get_values(  # noqa: F821
+        "debug", "samples", "m300_mount", "m20_mount", "tip_type", "mm2_vol",
+        "vhb_vol", "elution_buffer_vol", "settling_time")
 
     cols = math.ceil(samples/8)
 
@@ -31,29 +26,30 @@ def run(ctx):
 
     # Load Labware/Modules
     temp_mod = ctx.load_module('temperature module gen2', 3)
-    temp_plate = temp_mod.load_labware('opentrons_96_aluminumblock_nest_wellplate_100ul')
+    temp_plate = temp_mod.load_labware(
+                    'opentrons_96_aluminumblock_nest_wellplate_100ul')
     mag_mod = ctx.load_module('magnetic module gen2', 1)
     mag_plate = mag_mod.load_labware('nest_96_wellplate_2ml_deep')
-    tipracks = [ctx.load_labware(tiprack_type[tip_type], slot) for slot in range(7,11)]
-    tiprack_20ul = ctx.load_labware('opentrons_96_tiprack_20ul', 11)
+    tipracks = [ctx.load_labware(tiprack_type[tip_type], slot) for slot in
+                range(7, 12)]
     tip_isolator = ctx.load_labware(tiprack_type[tip_type], 4, 'Tip Isolator')
     res1 = ctx.load_labware('nest_12_reservoir_15ml', 5)
     res2 = ctx.load_labware('nest_12_reservoir_15ml', 2)
-    dna_plate = ctx.load_labware('opentrons_96_aluminumblock_nest_wellplate_100ul', 6)
+    dna_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', 6)
     trash = ctx.loaded_labwares[12]['A1']
 
     # Load Pipettes
-    m300 = ctx.load_instrument('p300_multi_gen2', m300_mount, tip_racks=tipracks)
+    m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
+                               tip_racks=tipracks)
     max_tip_volume = tipracks[0]['A1'].geometry.max_volume
-    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount, tip_racks=[tiprack_20ul])
 
     # Reagents
-    # Splitting columns for an even 12 column transfer 
+    # Splitting columns for an even 12 column transfer
     # based on volume total
     mm2 = [well for well in res1.wells()[:2] for i in range(6)]
     vhb = [well for well in res1.wells()[2:6] for i in range(3)]
-    etoh1 = [well for well in res1.wells()[:4] for i in range(3)]
-    etoh2 = [well for well in res1.wells()[4:8] for i in range(3)]
+    etoh1 = [well for well in res2.wells()[:4] for i in range(3)]
+    etoh2 = [well for well in res2.wells()[4:8] for i in range(3)]
     elution = temp_plate.rows()[0][:cols]
 
     # Helper Functions
@@ -65,12 +61,14 @@ def run(ctx):
         m300.flow_rate.aspirate = 20
         while vol >= max_tip_volume:
             m300.aspirate(
-                max_tip_volume, src.bottom().move(types.Point(x=side, y=0, z=0.5)))
+                max_tip_volume, src.bottom().move(
+                    types.Point(x=side, y=0, z=0.5)))
             m300.dispense(max_tip_volume, dest)
             vol -= max_tip_volume
 
         if vol < max_tip_volume:
-            m300.aspirate(vol, src.bottom().move(types.Point(x=side, y=0, z=0.5)))
+            m300.aspirate(vol, src.bottom().move(
+                        types.Point(x=side, y=0, z=0.5)))
             m300.dispense(vol, dest)
         m300.flow_rate.aspirate = 50
 
@@ -93,7 +91,8 @@ def run(ctx):
             pip.reset_tipracks()
             pip.pick_up_tip()
 
-    def tip_mix(well, vol, reps, park_tip=False, tip_loc=None, tip_map=None, asp_speed=94, disp_speed=94):
+    def tip_mix(well, vol, reps, park_tip=False, tip_loc=None, tip_map=None,
+                asp_speed=94, disp_speed=94):
 
         m300.flow_rate.aspirate = asp_speed
         m300.flow_rate.dispense = disp_speed
@@ -119,7 +118,8 @@ def run(ctx):
     def bind(delay=settling_time):
         if mag_mod.status != 'engaged':
             mag_mod.engage()
-        ctx.delay(minutes=delay, msg=f'''Incubating on MagDeck for {delay} minute(s).''')
+        ctx.delay(minutes=delay, msg=f'''Incubating on MagDeck for
+                  {delay} minute(s).''')
 
     def remove(vol, src, dest=trash, use_park_tip=True):
         if use_park_tip:
@@ -139,25 +139,29 @@ def run(ctx):
     def etoh_wash(reservoir, park=True):
         # Steps 24-25
         # 70% Ethanol Wash
-        debug_mode(msg=f"Debug: Wash with 70% Ethanol")
+        debug_mode(msg="Debug: Wash with 70% Ethanol")
         for src, dest in zip(reservoir, mag_plate_wells):
             wash(500, src, dest)
         # Tip Mix (Vortex)
-        debug_mode(msg=f"Debug: Tip Mixing (Vortex)")
+        debug_mode(msg="Debug: Tip Mixing (Vortex)")
         for well in mag_plate_wells:
-            tip_mix(well, 300, 10, park_tip=park, tip_loc=tip_isolator.columns()[mag_plate_wells[well]][0], tip_map=mag_plate_wells, asp_speed=188, disp_speed=188)
+            tip_mix(well, 300, 10, park_tip=park,
+                    tip_loc=tip_isolator.columns()[mag_plate_wells[well]][0],
+                    tip_map=mag_plate_wells, asp_speed=188, disp_speed=188)
 
         # Steps 26-27
-        debug_mode(msg=f'''Debug: Engage Magnet for {settling_time} minutes and then remove supernatant''')
+        debug_mode(msg=f'''Debug: Engage Magnet for {settling_time} minutes
+                        and then remove supernatant''')
         bind()
         for well in mag_plate_wells:
-            remove(400, well, use_park_tip=False)
+            remove(500, well, use_park_tip=False)
         if mag_mod.status == 'engaged':
             mag_mod.disengage()
 
     # Wells
     # mag_plate_wells = mag_plate.rows()[0]
-    mag_plate_wells = {well:column for well, column in zip(mag_plate.rows()[0][:cols], range(cols))}
+    mag_plate_wells = {well: column for well, column in zip(
+                       mag_plate.rows()[0][:cols], range(cols))}
     dna_plate_wells = dna_plate.rows()[0][:cols]
 
     # Protocol Steps
@@ -183,15 +187,20 @@ def run(ctx):
 
     # Step 15
     # Incubate at Room Temp for 10 Minutes while mixing
-    debug_mode(msg="Debug: Incubate at Room Temperature while Mixing (Step 15)")
+    debug_mode(msg='''Debug: Incubate at Room Temperature while
+               Mixing (Step 15)''')
     for well in mag_plate_wells:
-        tip_mix(well, mm2_vol/2, 10, park_tip=True, tip_map=mag_plate_wells, asp_speed=94, disp_speed=94)
+        tip_mix(well, mm2_vol/2, 10, park_tip=True, tip_map=mag_plate_wells,
+                asp_speed=94, disp_speed=94)
 
     for well in mag_plate_wells:
-        tip_mix(well, mm2_vol/2, 5, park_tip=True, tip_loc=tip_isolator.columns()[mag_plate_wells[well]][0], tip_map=mag_plate_wells, asp_speed=94, disp_speed=94)
+        tip_mix(well, mm2_vol/2, 5, park_tip=True,
+                tip_loc=tip_isolator.columns()[mag_plate_wells[well]][0],
+                tip_map=mag_plate_wells, asp_speed=94, disp_speed=94)
 
     # Steps 16-18
-    debug_mode(msg=f'''Debug: Engage Magnet for {settling_time} minutes and then remove supernatant (Steps 16-18)''')
+    debug_mode(msg=f'''Debug: Engage Magnet for {settling_time} minutes
+                    and then remove supernatant (Steps 16-18)''')
     bind()
     for well in mag_plate_wells:
         remove(600, well, use_park_tip=True)
@@ -200,16 +209,18 @@ def run(ctx):
 
     # Steps 19-20
     # Add VHB Buffer
-    debug_mode(msg=f"Debug: Wash with VHB Buffer (Step 19)")
+    debug_mode(msg="Debug: Wash with VHB Buffer (Step 19)")
     for src, dest in zip(vhb, mag_plate_wells):
         wash(500, src, dest)
     # Tip Mix (Vortex)
-    debug_mode(msg=f"Debug: Tip Mixing (Vortex) (Step 20)")
+    debug_mode(msg="Debug: Tip Mixing (Vortex) (Step 20)")
     for well in mag_plate_wells:
-        tip_mix(well, 300, 10, park_tip=True, tip_map=mag_plate_wells, asp_speed=188, disp_speed=188)
+        tip_mix(well, 300, 10, park_tip=True, tip_map=mag_plate_wells,
+                asp_speed=188, disp_speed=188)
 
     # Steps 21-23
-    debug_mode(msg=f'''Debug: Engage Magnet for {settling_time} minutes and then remove supernatant (Steps 21-23)''')
+    debug_mode(msg=f'''Debug: Engage Magnet for {settling_time} minutes and
+               then remove supernatant (Steps 21-23)''')
     bind()
     for well in mag_plate_wells:
         remove(400, well, use_park_tip=False)
@@ -221,35 +232,42 @@ def run(ctx):
     etoh_wash(etoh2, park=False)
 
     # Step 29
-    debug_mode(msg=f'''Debug: Engaging Magnet for 1 minute and removing any supernatant (Step 29)''')
+    debug_mode(msg='''Debug: Engaging Magnet for 1 minute and removing any
+               supernatant (Step 29)''')
     bind(delay=1)
     for well in mag_plate_wells:
         remove(200, well, dest=trash, use_park_tip=False)
 
     # Step 30
-    debug_mode(msg=f'''Debug: Engaging Magnet for 10 minutes to allow beads to dry (Step 30)''')
+    debug_mode(msg='''Debug: Engaging Magnet for 10 minutes to allow beads to
+               dry (Step 30)''')
     bind(delay=10)
 
     # Step 31
-    debug_mode(msg=f'''Debug: Heating elution buffer on temperature module to 70C (Step 31)''')
+    debug_mode(msg='''Debug: Heating elution buffer on temperature module to
+               70C (Step 31)''')
     temp_mod.set_temperature(70)
-    debug_mode(msg=f'''Debug: Transferring elution buffer to sample wells (Step 31)''')
+    debug_mode(msg='''Debug: Transferring elution buffer to sample wells
+               (Step 31)''')
     for src, dest in zip(elution, mag_plate_wells):
         m300.transfer(elution_buffer_vol, src, dest)
 
     # Step 32
-    debug_mode(msg=f'''Debug: Tip Mixing (Vortex) (Step 32)''')
+    debug_mode(msg='''Debug: Tip Mixing (Vortex) (Step 32)''')
     for well in mag_plate_wells:
-        tip_mix(well, elution_buffer_vol/2, 10, park_tip=False, tip_loc=None, tip_map=None, asp_speed=94, disp_speed=94)
+        tip_mix(well, elution_buffer_vol/2, 10, park_tip=False, tip_loc=None,
+                tip_map=None, asp_speed=94, disp_speed=94)
 
     # Step 33
-    debug_mode(msg=f'''Debug: Engaging Magnetic Module for 2 minutes to allow beads to settle (Step 33)''')
+    debug_mode(msg='''Debug: Engaging Magnetic Module for 2 minutes to allow
+               beads to settle (Step 33)''')
     bind(delay=2)
 
     # Step 34
-    debug_mode(msg=f'''Debug: Transfer clear supernatant containing purified DNA to NEST 0.1 mL 96 Well PCR Plate (Step 34)''')
+    debug_mode(msg='''Debug: Transfer clear supernatant containing purified
+               DNA to NEST 0.1 mL 96 Well PCR Plate (Step 34)''')
     for src, dest in zip(elution, dna_plate_wells):
-        pick_up(m20)
-        m20.aspirate(5, src)
-        m20.dispense(5, dest.bottom(z=5))
-        m20.drop_tip()
+        pick_up(m300)
+        m300.aspirate(elution_buffer_vol, src)
+        m300.dispense(elution_buffer_vol, dest.bottom(z=5))
+        m300.drop_tip()
