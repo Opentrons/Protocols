99093933686b7ebd229afe556a780849537689ac
Sakib Hossain <sakibh97@gmail.com>
2021-05-26 15:53:11 -0400

    Various fixes and changes

--- protocols/516336/516336.ot2.apiv2.py
+++ protocols/516336/516336.ot2.apiv2.py
@@ -11,8 +11,9 @@ metadata = {
 
 def run(ctx):
 
-    [samples, m300_mount, mag_engage_height] = get_values(  # noqa: F821
-        "samples", "m300_mount", "mag_engage_height")
+    [samples, m300_mount, p300_mount,
+        mag_engage_height] = get_values(  # noqa: F821
+        "samples", "m300_mount", "p300_mount", "mag_engage_height")
 
     cols = math.ceil(samples/8)
 
@@ -22,32 +23,32 @@ def run(ctx):
     mag_mod = ctx.load_module('magnetic module', '1')
     mag_plate = mag_mod.load_labware(
         'thermofisher_96_midi_storage_plate_800ul')
-    midi_plate_2 = ctx.load_labware('thermofisher_96_midi_storage_plate_800ul',
-                                    2)
-    reservoir1 = ctx.load_labware('nest_12_reservoir_15ml', 4)
-    reservoir2 = ctx.load_labware('nest_1_reservoir_195ml', 5)
-    tipracks = [ctx.load_labware('opentrons_96_tiprack_300ul', slot) for
-                slot in [3, 6, 9]]
+    temp_mod = ctx.load_module('temperature module gen2', 3)
+    temp_rack = temp_mod.load_labware(
+                            'opentrons_24_aluminumblock_generic_2ml_screwcap')
+    final_plate = ctx.load_labware('biorad_96_wellplate_200ul_pcr',
+                                   2)
+    reservoir = ctx.load_labware('nest_12_reservoir_15ml', 4)
+    tipracks_multi = [ctx.load_labware('opentrons_96_tiprack_300ul', slot) for
+                      slot in [6, 9]]
+    tipsracks_single = ctx.load_labware('opentrons_96_tiprack_300ul', 5)
     trash = ctx.loaded_labwares[12]['A1']
 
     # Load Pipettes
     m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
-                               tip_racks=tipracks)
+                               tip_racks=tipracks_multi)
+    p300 = ctx.load_instrument('p300_single_gen2', p300_mount,
+                               tip_racks=[tipsracks_single])
 
     # Reagents
-    dna_repair_buff = reservoir1['A1']
-    dna_repair_mix = reservoir1['A2']
-    rxn_buffer = reservoir1['A3']
-    enzyme_mix = reservoir1['A4']
-    nfa = reservoir1['A5']
-    endprep_mm = reservoir1['A8']
-    ampure_beads = reservoir1['A12']
-    ethanol = reservoir2['A1']
+    ampure_beads = reservoir['A12']
+    nfa = reservoir['A5']
+    mm = temp_rack['A1']
 
     # Sample Wells
     tc_plate_wells = tc_plate.rows()[0][:cols]
     mag_plate_wells = mag_plate.rows()[0][:cols]
-    midi_plate_2_wells = midi_plate_2.rows()[0][:cols]
+    final_plate_wells = final_plate.rows()[0][:cols]
 
     # Helper Functions
     def pick_up(pip):
@@ -83,21 +84,68 @@ def run(ctx):
         mag_mod.engage(height_from_base=mag_engage_height)
         ctx.delay(minutes=delay_mins, msg='Allowing beads to settle.')
 
-    # PROTOCOL STEPS
+    # Volume Tracking
+    class VolTracker:
+        def __init__(self, labware, well_vol, pip_type='single',
+                     mode='reagent', start=0, end=12, msg='Reset Labware'):
+            try:
+                self.labware_wells = dict.fromkeys(
+                    labware.wells()[start:end], 0)
+            except Exception:
+                self.labware_wells = dict.fromkeys(
+                    labware, 0)
+            self.labware_wells_backup = self.labware_wells.copy()
+            self.well_vol = well_vol
+            self.pip_type = pip_type
+            self.mode = mode
+            self.start = start
+            self.end = end
+            self.msg = msg
 
-    # Create End Prep Master Mix
-    m300.transfer(27*samples, nfa, endprep_mm)
-    m300.transfer(3.5*samples, dna_repair_buff, endprep_mm)
-    m300.transfer(2*samples, dna_repair_mix, endprep_mm)
-    m300.transfer(3.5*samples, rxn_buffer, endprep_mm)
-    m300.transfer(3*samples, enzyme_mix, endprep_mm, mix_after=(5, 300))
+        def tracker(self, vol):
+            '''tracker() will track how much liquid
+            was used up per well. If the volume of
+            a given well is greater than self.well_vol
+            it will remove it from the dictionary and iterate
+            to the next well which will act as the reservoir.'''
+            well = next(iter(self.labware_wells))
+            if self.labware_wells[well] + vol >= 14800:
+                del self.labware_wells[well]
+                if len(self.labware_wells) < 1:
+                    ctx.pause(self.msg)
+                    self.labware_wells = self.labware_wells_backup.copy()
+                well = next(iter(self.labware_wells))
+            if self.pip_type == 'multi':
+                self.labware_wells[well] = self.labware_wells[well] + vol*8
+            elif self.pip_type == 'single':
+                self.labware_wells[well] = self.labware_wells[well] + vol
+            if self.mode == 'waste':
+                ctx.comment(f'''{well}: {int(self.labware_wells[well])} uL of
+                            total waste''')
+            else:
+                ctx.comment(f'''{int(self.labware_wells[well])} uL of liquid
+                            used from {well}''')
+            return well
+
+    ethanolTrack = VolTracker(reservoir, 14900, 'multi', start=0, end=4,
+                              msg='Replenish Ethanol')
+
+    # PROTOCOL STEPS
+    # Set Temperature Module to 6C
+    temp_mod.set_temperature(6)
 
     # Transfer End Prep Mix to Samples on PCR Plate (1)
-    m300.transfer(39, endprep_mm, tc_plate_wells, new_tip='always',
+    p300.transfer(12, mm, tc_plate_wells, new_tip='always',
                   mix_after=(3, 30))
 
     # Pause for Spin Down (2)
     ctx.pause('Spin down the plate and resume.')
+
+    # Pre-Heat Thermocycler to 20C
+    ctx.pause('Pre-Heating the thermocycler to 20C.')
+    tc_mod.set_block_temperature(20)
+    tc_mod.set_lid_temperature(70)
+    ctx.pause('Put the sample plate into the thermocycler')
 
     # Incubate on Thermocycler (3)
     tc_mod.close_lid()
@@ -105,10 +153,11 @@ def run(ctx):
                {'temperature': 65, 'hold_time_minutes': 5}]
     tc_mod.execute_profile(steps=profile, repetitions=1, block_max_volume=60)
     tc_mod.open_lid()
+    tc_mod.deactivate()
 
     # Resuspend AMPure Beads (4)
     m300.pick_up_tip()
-    m300.mix(5, 300, endprep_mm.bottom(z=3))
+    m300.mix(5, 300, ampure_beads.bottom(z=3))
     m300.drop_tip()
 
     # Transfer Samples from TC to Mag Mod (5)
@@ -140,7 +189,8 @@ def run(ctx):
     for _ in range(2):
         # Wash Beads with Ethanol (12)
         for well in mag_plate_wells:
-            m300.transfer(200, ethanol, well.top(-3), mix_after=(3, 200))
+            m300.transfer(200, ethanolTrack.tracker(200), well.top(-3),
+                          mix_after=(3, 200))
 
         # Remove Supernatant (11)
         for well, side in zip(mag_plate_wells, sides):
@@ -170,7 +220,10 @@ def run(ctx):
     magnet(5)
 
     # Transfer Eluate into MIDI plate for part 2 (19)
-    for well, dest, side in zip(mag_plate_wells, midi_plate_2_wells, sides):
+    for well, dest, side in zip(mag_plate_wells, final_plate_wells, sides):
         pick_up(m300)
         remove_supernatant(100, well, dest, side)
         m300.drop_tip()
+
+    temp_mod.deactivate()
+    mag_mod.disengage()
