a5eb890c9462b7096a33222eb8bd6baf00336105
ramifarawi <rami.farawi@opentrons.com>
2021-05-26 15:19:18 -0400

    removed get values

--- protocols/13ea1e-part1/13ea1e-part1.ot2.apiv2.py
+++ protocols/13ea1e-part1/13ea1e-part1.ot2.apiv2.py
@@ -8,12 +8,6 @@ metadata = {
 }
 
 
-def get_values(*names):
-    import json
-    _all_values = json.loads("""{"num_samp":96,"mix_reps":10,"p300_mount":"left","p1000_mount":"right"}""")
-    return [_all_values[n] for n in names]
-
-
 def run(ctx):
 
     [num_samp, mix_reps, p300_mount, p1000_mount] = get_values(  # noqa: F821
