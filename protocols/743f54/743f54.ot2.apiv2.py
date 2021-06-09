cb0fd65870e61e14d715accc03c70cdc6e5b5749
Chazman Childers <chaz@opentrons.com>
2021-06-09 10:24:26 -0700

    743f54

---
+++ protocols/743f54/743f54.ot2.apiv2.py
@@ -0,0 +1,14 @@
+metadata = {
+    'protocolName': 'Plate Prep KingFisher',
+    'author': 'Chaz <chaz@opentrons.com>',
+    'source': 'Custom Protocol Request',
+    'apiLevel': '2.9'
+}
+
+
+def run(protocol):
+    [] = get_values(  # noqa: F821
+    )
+
+    # load labware
+    
