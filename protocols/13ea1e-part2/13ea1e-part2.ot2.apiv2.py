6f2356e13083fca728c5b6235a018960d5e11fb3
ramifarawi <rami.farawi@opentrons.com>
2021-06-14 15:01:20 -0400

    fix

--- protocols/13ea1e-part2/13ea1e-part2.ot2.apiv2.py
+++ protocols/13ea1e-part2/13ea1e-part2.ot2.apiv2.py
@@ -12,10 +12,10 @@ metadata = {
 
 def run(ctx):
 
-    [num_samp, overage_percent, mix_reps, sample_height, tube_height,
+    [num_samp, overage_percent, mix_reps,
         p20_mount, p300_mount] = get_values(  # noqa: F821
         "num_samp", "overage_percent",
-        "mix_reps", "sample_height", "tube_height", "p20_mount", "p300_mount")
+        "mix_reps", "p20_mount", "p300_mount")
 
     if not 0 <= num_samp <= 384:
         raise Exception("Enter a sample number between 1-384")
