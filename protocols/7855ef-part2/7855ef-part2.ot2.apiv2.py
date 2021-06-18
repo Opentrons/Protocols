5164ced650c616d8eec08a4955925a9cf32d2a26
ramifarawi <rami.farawi@opentrons.com>
2021-06-18 08:25:36 -0400

    fix

--- protocols/7855ef-part2/7855ef-part2.ot2.apiv2.py
+++ protocols/7855ef-part2/7855ef-part2.ot2.apiv2.py
@@ -82,8 +82,8 @@ def run(protocol):
         pick_up()
         m20.aspirate(2, pre_ligation_mix)
         m20.air_gap(airgap)
-        m20.dispense(airgap+2, col.top())
-        m20.mix(2, 8)
+        m20.dispense(airgap+2, col.
+        m20.mix(2, 8, col)
         m20.blow_out()
         m20.return_tip()
         protocol.comment('\n')
