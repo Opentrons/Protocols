09c08faad2fe053fd3b802edf92d24249411ce8f
ramifarawi <rami.farawi@opentrons.com>
2021-08-31 09:35:56 -0400

    fixed protocol

--- protocols/581011-pt2/581011-pt2.ot2.apiv2.py
+++ protocols/581011-pt2/581011-pt2.ot2.apiv2.py
@@ -1,3 +1,4 @@
+"""Protocol."""
 import math
 
 metadata = {
@@ -7,15 +8,9 @@ metadata = {
     'apiLevel': '2.10'
 }
 
-def get_values(*names):
-    import json
-    _all_values = json.loads("""{"csv_samp":",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24\\nA,x,A1/A1,A1/A1,A1/A1,A2/A1,A2/A1,A2/A1,A3/A1,A3/A1,A3/A1,x,A4/A1,A4/A1,A4/A1,A5/A1,A5/A1,A5/A1,A6/A1,A6/A1,A6/A1,x,0/A1,0/A1,x\\nB,x,A1/A1,A1/A1,A1/A1,A2/A1,A2/A1,A2/A1,A3/A1,A3/A1,A3/A1,x,A4/A1,A4/A1,A4/A1,A5/A1,A5/A1,A5/A1,A6/A1,A6/A1,A6/A1,x,0/A1,0/A1,x\\nC,x,A1/B1,A1/B1,A1/B1,A2/B1,A2/B1,A2/B1,A3/B1,A3/B1,A3/B1,x,A4/B1,A4/B1,A4/B1,A5/B1,A5/B1,A5/B1,A6/B1,A6/B1,A6/B1,x,0/B1,0/B1,x\\nD,x,A1/B1,A1/B1,A1/B1,A2/B1,A2/B1,A2/B1,A3/B1,A3/B1,A3/B1,x,A4/B1,A4/B1,A4/B1,A5/B1,A5/B1,A5/B1,A6/B1,A6/B1,A6/B1,x,0/B1,0/B1,x\\nE,x,A1/C1,A1/C1,A1/C1,A2/C1,A2/C1,A2/C1,A3/C1,A3/C1,A3/C1,x,A4/C1,A4/C1,A4/C1,A5/C1,A5/C1,A5/C1,A6/C1,A6/C1,A6/C1,x,0/C1,0/C1,x\\nF,x,A1/C1,A1/C1,A1/C1,A2/C1,A2/C1,A2/C1,A3/C1,A3/C1,A3/C1,x,A4/C1,A4/C1,A4/C1,A5/C1,A5/C1,A5/C1,A6/C1,A6/C1,A6/C1,x,0/C1,0/C1,x\\nG,x,A1/D1,A1/D1,A1/D1,A2/D1,A2/D1,A2/D1,A3/D1,A3/D1,A3/D1,x,A4/D1,A4/D1,A4/D1,A5/D1,A5/D1,A5/D1,A6/D1,A6/D1,A6/D1,x,0/D1,0/D1,x\\nH,x,A1/D1,A1/D1,A1/D1,A2/D1,A2/D1,A2/D1,A3/D1,A3/D1,A3/D1,x,A4/D1,A4/D1,A4/D1,A5/D1,A5/D1,A5/D1,A6/D1,A6/D1,A6/D1,x,0/D1,0/D1,x\\nI,x,A1/E1,A1/E1,A1/E1,A2/E1,A2/E1,A2/E1,A3/E1,A3/E1,A3/E1,x,A4/E1,A4/E1,A4/E1,A5/E1,A5/E1,A5/E1,A6/E1,A6/E1,A6/E1,x,0/E1,0/E1,x\\nJ,x,A1/E1,A1/E1,A1/E1,A2/E1,A2/E1,A2/E1,A3/E1,A3/E1,A3/E1,x,A4/E1,A4/E1,A4/E1,A5/E1,A5/E1,A5/E1,A6/E1,A6/E1,A6/E1,x,0/E1,0/E1,x\\nK,x,A1/F1,A1/F1,A1/F1,A2/F1,A2/F1,A2/F1,A3/F1,A3/F1,A3/F1,x,A4/F1,A4/F1,A4/F1,A5/F1,A5/F1,A5/F1,A6/F1,A6/F1,A6/F1,x,0/F1,0/F1,x\\nL,x,A1/F1,A1/F1,A1/F1,A2/F1,A2/F1,A2/F1,A3/F1,A3/F1,A3/F1,x,A4/F1,A4/F1,A4/F1,A5/F1,A5/F1,A5/F1,A6/F1,A6/F1,A6/F1,x,0/F1,0/F1,x\\nM,x,A1/G1,A1/G1,A1/G1,A2/G1,A2/G1,A2/G1,A3/G1,A3/G1,A3/G1,x,A4/G1,A4/G1,A4/G1,A5/G1,A5/G1,A5/G1,A6/G1,A6/G1,A6/G1,x,0/G1,0/G1,x\\nN,x,A1/G1,A1/G1,A1/G1,A2/G1,A2/G1,A2/G1,A3/G1,A3/G1,A3/G1,x,A4/G1,A4/G1,A4/G1,A5/G1,A5/G1,A5/G1,A6/G1,A6/G1,A6/G1,x,0/G1,0/G1,x\\nO,x,A1/0,A1/0,A1/0,A2/0,A2/0,A2/0,A3/0,A3/0,A3/0,x,A4/0,A4/0,A4/0,A5/0,A5/0,A5/0,A6/0,A6/0,A6/0,x,x,x,x\\nP,x,A1/0,A1/0,A1/0,A2/0,A2/0,A2/0,A3/0,A3/0,A3/0,x,A4/0,A4/0,A4/0,A5/0,A5/0,A5/0,A6/0,A6/0,A6/0,x,x,x,x","vol_effector_cell":40,"pre_mix":true,"mix_asp_height":1,"mix_disp_height": 2,"premix_reps":3,"mix_vol":3,"mix_rate":1,"asp_height":1,"disp_height":1,"asp_rate":1,"disp_rate":1,"m300_mount":"left"}""")
-    return [_all_values[n] for n in names]
-
-
 
 def run(ctx):
-
+    """Protocol."""
     [csv_samp, vol_effector_cell, pre_mix, mix_asp_height, mix_disp_height,
      premix_reps, mix_vol, mix_rate,
      asp_height, disp_height, asp_rate,
