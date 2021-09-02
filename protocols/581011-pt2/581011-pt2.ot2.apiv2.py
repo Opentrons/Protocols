3aee4e1615b9d05c0315ff5fc3c1a67a54dcaa7a
ramifarawi <rami.farawi@opentrons.com>
2021-09-02 13:16:39 -0400

    fix

--- protocols/581011-pt2/581011-pt2.ot2.apiv2.py
+++ protocols/581011-pt2/581011-pt2.ot2.apiv2.py
@@ -1,5 +1,6 @@
 """Protocol."""
 import math
+from itertools import groupby
 
 metadata = {
     'protocolName': 'Cherrypicking with Multi-Channel Pipette and CSV-Part 2',
@@ -7,7 +8,10 @@ metadata = {
     'source': 'Custom Protocol Request',
     'apiLevel': '2.10'
 }
-
+def get_values(*names):
+    import json
+    _all_values = json.loads("""{"csv_samp":",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24\\nA,x,A1/A1,A1/A1,A1/A1,A2/A2,A2/A2,A2/A2,A3/A3,A3/A3,A3/A3,x,A4/A1,A4/A1,A4/A1,A5/A1,A5/A1,A5/A1,A6/A1,A6/A1,A6/A1,0/A1,0/A1,0/A2,0/A3\\nB,x,A1/A1,A1/A1,A1/A1,A2/A2,A2/A2,A2/A2,A3/A3,A3/A3,A3/A3,x,A4/A1,A4/A1,A4/A1,A5/A1,A5/A1,A5/A1,A6/A1,A6/A1,A6/A1,0/A1,0/A1,0/A2,0/A3\\nC,x,A1/B1,A1/B1,A1/B1,A2/B2,A2/B2,A2/B2,A3/B3,A3/B3,A3/B3,x,A4/B1,A4/B1,A4/B1,A5/B1,A5/B1,A5/B1,A6/B1,A6/B1,A6/B1,0/B1,0/B1,0/B2,0/B3\\nD,x,A1/B1,A1/B1,A1/B1,A2/B2,A2/B2,A2/B2,A3/B3,A3/B3,A3/B3,x,A4/B1,A4/B1,A4/B1,A5/B1,A5/B1,A5/B1,A6/B1,A6/B1,A6/B1,0/B1,0/B1,0/B2,0/B3\\nE,x,A1/C1,A1/C1,A1/C1,A2/C2,A2/C2,A2/C2,A3/C3,A3/C3,A3/C3,x,A4/C1,A4/C1,A4/C1,A5/C1,A5/C1,A5/C1,A6/C1,A6/C1,A6/C1,0/C1,0/C1,0/C2,0/C3\\nF,x,A1/C1,A1/C1,A1/C1,A2/C2,A2/C2,A2/C2,A3/C3,A3/C3,A3/C3,x,A4/C1,A4/C1,A4/C1,A5/C1,A5/C1,A5/C1,A6/C1,A6/C1,A6/C1,0/C1,0/C1,0/C2,0/C3\\nG,x,A1/D1,A1/D1,A1/D1,A2/D2,A2/D2,A2/D2,A3/D3,A3/D3,A3/D3,x,A4/D1,A4/D1,A4/D1,A5/D1,A5/D1,A5/D1,A6/D1,A6/D1,A6/D1,0/D1,0/D1,0/D2,0/D3\\nH,x,A1/D1,A1/D1,A1/D1,A2/D2,A2/D2,A2/D2,A3/D3,A3/D3,A3/D3,x,A4/D1,A4/D1,A4/D1,A5/D1,A5/D1,A5/D1,A6/D1,A6/D1,A6/D1,0/D1,0/D1,0/D2,0/D3\\nI,x,A1/E1,A1/E1,A1/E1,A2/E2,A2/E2,A2/E2,A3/E3,A3/E3,A3/E3,x,A4/E1,A4/E1,A4/E1,A5/E1,A5/E1,A5/E1,A6/E1,A6/E1,A6/E1,0/E1,0/E1,0/E2,0/E3\\nJ,x,A1/E1,A1/E1,A1/E1,A2/E2,A2/E2,A2/E2,A3/E3,A3/E3,A3/E3,x,A4/E1,A4/E1,A4/E1,A5/E1,A5/E1,A5/E1,A6/E1,A6/E1,A6/E1,0/E1,0/E1,0/E2,0/E3\\nK,x,A1/F1,A1/F1,A1/F1,A2/F2,A2/F2,A2/F2,A3/F3,A3/F3,A3/F3,x,A4/F1,A4/F1,A4/F1,A5/F1,A5/F1,A5/F1,A6/F1,A6/F1,A6/F1,0/F1,0/F1,0/F2,0/F3\\nL,x,A1/F1,A1/F1,A1/F1,A2/F2,A2/F2,A2/F2,A3/F3,A3/F3,A3/F3,x,A4/F1,A4/F1,A4/F1,A5/F1,A5/F1,A5/F1,A6/F1,A6/F1,A6/F1,0/F1,0/F1,0/F2,0/F3\\nM,x,A1/0,A1/0,A1/0,A2/0,A2/0,A2/0,A3/0,A3/0,A3/0,x,A4/0,A4/0,A4/0,A5/0,A5/0,A5/0,A6/0,A6/0,A6/0,x,x,x,x\\nN,x,A1/0,A1/0,A1/0,A2/0,A2/0,A2/0,A3/0,A3/0,A3/0,x,A4/0,A4/0,A4/0,A5/0,A5/0,A5/0,A6/0,A6/0,A6/0,x,x,x,x\\nO,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x\\nP,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x","vol_effector_cell":40,"pre_mix":false,"mix_asp_height":1,"mix_disp_height":1,"premix_reps":3,"mix_vol":3,"mix_rate":1,"asp_height":1,"disp_height":1,"asp_rate":1,"disp_rate":1,"m300_mount":"left"}""")
+    return [_all_values[n] for n in names]
 
 def run(ctx):
     """Protocol."""
@@ -73,11 +77,11 @@ def run(ctx):
         m300.pick_up_tip(tips_ordered[tip_count])
         tip_count += 1
 
-    def check_volume(counter, tot_vol, source_well):
+    def check_volume(counter, tot_vol, chunk, source_well):
         if m300.current_volume < vol_effector_cell:
             if m300.current_volume > 0:
                 m300.dispense(m300.current_volume, source_well)
-            if counter != len(plate_map[start_row])-1:
+            if counter != len(chunk)-1:
                 if pre_mix:
                     mix_diff_height(source_well)
                 m300.aspirate(tot_vol if tot_vol < 200 else
@@ -110,76 +114,61 @@ def run(ctx):
     start_row = math.floor(start_well/24)
 
     # find effecor cell column
-    for row in plate_map:
-        for well in row:
-            if '/' in well:
-                left_and_right = well.split('/')
-                right = left_and_right[1]
-                if 'A' in right:
-                    effector_column_name = right
-                    break
+    effector_columns = []
+
+    for well in plate_map[start_row]:
+        if '/' in well:
+            left_and_right = well.split('/')
+            right = left_and_right[1]
+            effector_columns.append(right)
+        else:
+            effector_columns.append(well)
+
+    dispense_wells = [list(j) for i, j in groupby(effector_columns)]
 
     # transfer effector cell
-    num_wells_to_dispense = 0
-    for col_ctr, well in enumerate(plate_map[start_row]):
-        if 'x' not in well or 'X' not in well:
-            num_wells_to_dispense += 1
-
-    if plate_map[start_row][-1] == 'x' or plate_map[start_row][-1] == 'X':
-        plate_map[start_row].pop()
-
     wells_by_row = [well for row in plate.rows() for well in row]
-    pickup()
-    tot_vol = vol_effector_cell*num_wells_to_dispense*2+20
-    source_well = effector_plate.wells_by_name()[effector_column_name]
-    if vol_effector_cell >= 100:
-        for j, well in enumerate(plate_map[start_row]):
-            if 'x' in well or 'X' in well:
-                if j > 0:
-                    start_well += 1
-                continue
-
-            if pre_mix:
-                mix_diff_height(source_well)
-
-            m300.aspirate(vol_effector_cell, source_well)
-            m300.dispense(vol_effector_cell, wells_by_row[start_well])
-
-            if pre_mix:
-                mix_diff_height(source_well)
-
-            m300.aspirate(vol_effector_cell,
-                          source_well)
-            m300.dispense(vol_effector_cell,
-                          wells_by_row[start_well+24])
-            start_well += 1
-
-    else:
-        for j, well in enumerate(plate_map[start_row]):
-            if 'x' in well or 'X' in well:
-                if j > 0:
-                    start_well += 1
-                continue
-
-            if m300.current_volume < vol_effector_cell:
+    for j, chunk in enumerate(dispense_wells):
+        if 'x' in chunk or 'X' in chunk or chunk[0][0] == '0':
+            if j > 0:
+                start_well += 1
+            continue
+        pickup()
+        tot_vol = vol_effector_cell*len(chunk)*2+20
+        for i, well in enumerate(chunk):
+            source_well = effector_plate.wells_by_name()[str(chunk[0][0:2])]
+            if vol_effector_cell >= 100:
                 if pre_mix:
                     mix_diff_height(source_well)
-                m300.aspirate(tot_vol if tot_vol < 200
-                              else 200-0.15*vol_effector_cell,
+                m300.aspirate(vol_effector_cell, source_well)
+                m300.dispense(vol_effector_cell, wells_by_row[start_well])
+                if pre_mix:
+                    mix_diff_height(source_well)
+                m300.aspirate(vol_effector_cell,
                               source_well)
+                m300.dispense(vol_effector_cell, wells_by_row[start_well+24])
+                start_well += 1
 
-            # first row dispense
-            m300.dispense(vol_effector_cell, wells_by_row[start_well])
-            tot_vol -= vol_effector_cell
-            check_volume(j, tot_vol, source_well)
+            else:
+                if m300.current_volume < vol_effector_cell:
+                    if pre_mix:
+                        mix_diff_height(source_well)
+                    m300.aspirate(tot_vol if tot_vol < 200
+                                  else 200-0.15*vol_effector_cell,
+                                  source_well)
 
-            # second row dispense
-            m300.dispense(vol_effector_cell,
-                          wells_by_row[start_well+24])
-            tot_vol -= vol_effector_cell
-            check_volume(j, tot_vol, source_well)
+                # first row dispense
+                m300.dispense(vol_effector_cell, wells_by_row[start_well])
+                tot_vol -= vol_effector_cell
+                check_volume(i, tot_vol, chunk, source_well)
 
-            start_well += 1
+                # second row dispense
+                m300.dispense(vol_effector_cell,
+                              wells_by_row[start_well+24])
+                tot_vol -= vol_effector_cell
+                check_volume(i, tot_vol, chunk, source_well)
+
+                start_well += 1
         if m300.current_volume > 0:
             m300.dispense(m300.current_volume,
                           source_well)
