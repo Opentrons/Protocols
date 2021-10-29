336d77e4ba024b166594c4d3bc4ae7f9b3969cba
ramifarawi <rami.farawi@opentrons.com>
2021-10-29 16:28:48 -0400

    fix

--- protocols/581011/581011.ot2.apiv2.py
+++ protocols/581011/581011.ot2.apiv2.py
@@ -125,12 +125,13 @@ def run(ctx):
     first_row_cut = []
     for item in plate_map[start_row]:
         if '/' in item:
-            first_row_cut.append(item[:2])
+            first_row_cut.append(item.split('/')[0])
         else:
             first_row_cut.append(item)
 
     # transfer target cell
     dispense_wells = [list(j) for i, j in groupby(first_row_cut)]
+    print(dispense_wells)
     wells_by_row = [well for row in plate.rows() for well in row]
 
     def mix_diff_height(well):
@@ -174,7 +175,7 @@ def run(ctx):
         pickup(num_tips_in_each_column[tip_col_ctr]+check_control[tip_col_ctr])
         tot_vol = vol_target_cell*len(chunk)*2+20
         for i, well in enumerate(chunk):
-            source_well = target_res.wells_by_name()[str(chunk[0][0:2])]
+            source_well = target_res.wells_by_name()[str(chunk[0])]
             if vol_target_cell >= 100:
                 if pre_mix:
                     mix_diff_height(source_well)
