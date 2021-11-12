fd129b8d7469381513e27fe6065005109a34ba79
ramifarawi <rami.farawi@opentrons.com>
2021-11-12 12:05:19 -0500

    fix

--- protocols/0045f1/0045f1.ot2.apiv2.py
+++ protocols/0045f1/0045f1.ot2.apiv2.py
@@ -1,5 +1,5 @@
 metadata = {
-    'protocolName': 'SuperScript III: qRT-PCR Prep with CSV File',
+    'protocolName': 'Variable Slide Dispensing',
     'author': 'Rami Farawi <rami.farawi@opentrons.com>',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.11'
@@ -8,22 +8,24 @@ metadata = {
 
 def run(ctx):
 
-    [num_slides, spot_labware,
+    [num_tubes, num_slides, spot_labware,
      spot_volume, asp_rate, disp_rate, p20_mount] = get_values(  # noqa: F821
-        "num_slides", "spot_labware",
+        "num_tubes", "num_slides", "spot_labware",
         "spot_volume", "asp_rate", "disp_rate", "p20_mount")
+    num_tubes = int(num_tubes)
 
     if not 1 <= num_slides <= 6:
         raise Exception("Enter a slide number 1-6")
     if not 1 <= spot_volume <= 6:
         raise Exception("Enter a volume between 1-20ul")
+    num_plates = 2 if num_slides > 3 else 1
 
     # load labware
     tuberack = ctx.load_labware(
                 'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '3')
     tiprack = ctx.load_labware('opentrons_96_filtertiprack_10ul', '4')
     slide_plates = [ctx.load_labware(spot_labware,
-                    slot) for slot in ['1', '2']]  # [:math.ceil(num_slides/6)]
+                    slot) for slot in ['1', '2'][:num_plates]]
 
     # load instrument
     p20 = ctx.load_instrument('p20_single_gen2',
@@ -33,18 +35,23 @@ def run(ctx):
     p20.flow_rate.aspirate = p20.flow_rate.aspirate*asp_rate
     p20.flow_rate.dispense = p20.flow_rate.dispense*disp_rate
 
-    bdf_wells = [
-                 well
-                 for j in range(-(len(slide_plates[0].columns())-1), 1, 1)
-                 for k in range(2)
-                 for i in range(2)
-                 for well in slide_plates[i].columns()[-j][2+k:14+k:2]
-                 ][:num_slides*48]
+    all_wells = []
 
-    bdf_wells_chunks = [bdf_wells[i:i+12]
-                        for i in range(0, len(bdf_wells), 12)]
+    for z in [2, 4]:
+        for k in range(4):
+            for i in range(2):
+                for j, plate in enumerate(slide_plates):
+                    if j > 0:
+                        rel_slides = num_slides - 3
+                    for column in plate.columns()[k:(rel_slides if j > 0
+                                                  else num_slides)*4:4]:
+                        for well in column[z+i:14+i:4]:
+                            all_wells.append(well)
+
+    all_wells_chunks = [all_wells[i:i+3*num_slides]
+                        for i in range(0, len(all_wells), 3*num_slides)]
 
     p20.pick_up_tip()
-    for tube, chunk in zip(tuberack.wells(), bdf_wells_chunks):
+    for tube, chunk in zip(tuberack.wells()[:num_tubes], all_wells_chunks):
         p20.distribute(spot_volume, tube, chunk, new_tip='never')
     p20.drop_tip()
