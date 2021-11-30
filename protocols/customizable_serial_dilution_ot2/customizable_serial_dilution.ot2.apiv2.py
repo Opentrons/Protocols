3bf41bd6200e9a1192ba7425c7b70104023a7ad3
Eskil Andersen <eskil.andersen@opentrons.com>
2021-11-30 11:33:36 -0500

    error checking on pipettes, updated method to discover if the pipette is single or multi to work with GEN2

--- protocols/customizable_serial_dilution_ot2/customizable_serial_dilution.ot2.apiv2.py
+++ protocols/customizable_serial_dilution_ot2/customizable_serial_dilution.ot2.apiv2.py
@@ -14,7 +14,8 @@ def get_values(*names):
                                   "total_mixing_volume":"200",
                                   "tip_use_strategy":"never",
                                   "plate_type":"corning_96_wellplate_360ul_flat",
-                                  "trough_type":"usascientific_12_reservoir_22ml"}
+                                  "trough_type":"usascientific_12_reservoir_22ml",
+                                  "tiprack_type":"opentrons_96_tiprack_300ul"}
                                   """)
     return [_all_values[n] for n in names]
 
@@ -22,36 +23,41 @@ def get_values(*names):
 def run(protocol_context):
     [pipette_type, dilution_factor, num_of_dilutions, total_mixing_volume,
         tip_use_strategy, plate_type, trough_type,
-        num_of_samples] = get_values(  # noqa: F821
+        tiprack_type] = get_values(  # noqa: F821
             'pipette_type', 'dilution_factor', 'num_of_dilutions',
             'total_mixing_volume', 'tip_use_strategy', 'plate_type',
-            'trough_type'
+            'trough_type', 'tiprack_type'
         )
+
+    total_mixing_volume = float(total_mixing_volume)
+    dilution_factor = float(dilution_factor)
+    num_of_dilutions = int(num_of_dilutions)
 
     transfer_volume = total_mixing_volume/dilution_factor
     diluent_volume = total_mixing_volume - transfer_volume
 
     # labware
     trough = protocol_context.load_labware(
-        'trough_type', '2')
-    liquid_trash = trough.wells()[0]  # Todo: I have to stick to multi-well?
+        trough_type, '2')
+    # Todo: I thought this well was were we got diluent? Figure out meaning
+    liquid_trash = trough.wells()[0]
     plate = protocol_context.load_labware(
-        'plate_type', '3')
+        plate_type, '3')
     tiprack = [
         protocol_context.load_labware('opentrons_96_tiprack_300ul', slot)
         for slot in ['1', '4']
     ]
-
-    pip_name = pipette_type.split('_')[-1]
+    # import pdb
+    # pdb.set_trace()
 
     pipette = protocol_context.load_instrument(
         pipette_type, mount='left', tip_racks=tiprack)
 
     # Error checking
-    pipette_min_vol_error_check(pipette, transfer_volume, "transfer")
-    pipette_min_vol_error_check(pipette, diluent_volume, "diluent")
+    pipette_vol_error_check(pipette, transfer_volume, "transfer")
+    pipette_vol_error_check(pipette, diluent_volume, "diluent")
 
-    if pip_name == 'multi':
+    if "multi" in pip_name:
 
         # Distribute diluent across the plate to the the number of samples
         # And add diluent to one column after the number of samples for a blank
@@ -122,10 +128,18 @@ def run(protocol_context):
                 pipette.drop_tip()
 
 
-def pipette_min_vol_error_check(pipette, volume, message):
-    """This function checks if the volumes to be pipetted are compatible with
-    the minimum volumes of the choosen pipette"""
+def pipette_vol_error_check(pipette, volume, message):
+    """
+    This function checks if the volumes to be pipetted are compatible with
+    the  volumes of the choosen pipette
+    """
     if volume < pipette.min_volume:
-        raise Exception(("The selected pipette cannot pipette "
+        raise Exception(("The mounted pipette cannot pipette "
                          "a {} volume of {} uL because the volume "
-                         "is too small for it.").format(message, volume))
+                         "is too small for it.").format(message, volume)
+                        )
+    if volume > pipette.max_volume:
+        raise Exception(("The mounted pipette cannot pipette "
+                         "a {} volume of {} uL because the volume "
+                         "is too large for it.").format(message, volume)
+                        )
