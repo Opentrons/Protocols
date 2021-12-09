ab6fdff0d1a7b6b7b7ebab4ef2772f7aa0bd3365
ncdiehl11 <ncdiehl11@gmail.com>
2021-12-09 15:42:24 -0500

    map SF user id to GH user id

--- data/protocols/labware_data.py
+++ data/protocols/labware_data.py
@@ -43,135 +43,142 @@ def transform_category(cat, subcat):
         return 'other'
 
 
-tuberack_vols = []
-for folder in os.listdir(protobuilds_path):
-    folder_path = protobuilds_path + folder
-    prtcl_data = {}
-    lw_data = {}
-    pip_data = {}
-    mod_data = {}
-    for file in os.listdir(folder_path):
-        if '.py.json' in file:
-            file_path = folder_path + '/' + file
-            with open(file_path) as json_file:
-                data = json.load(json_file)
-                lw_data['raw'] = data['labware']
-                pip_data['raw'] = data['instruments']
-                mod_data['raw'] = data['modules']
-        if 'README.json' in file:
-            rm_file_path = folder_path + '/' + file
-            with open(rm_file_path) as rm_file:
-                data = json.load(rm_file)
-                prtcl_data['category'] = [
-                    key for key in data['categories'].keys()][0].lower()
-                subcat = [val for val in data['categories'].values()][0]
-                if subcat:
-                    prtcl_data['subcategory'] = subcat[0].lower()
-                else:
-                    prtcl_data['subcategory'] = [
-                        key for key in data['categories'].keys()][1].lower()
-    lw_data['counts'] = {
-        'tuberack': 0,
-        'plate': 0,
-        'reservoir': 0,
-        'trash': 0,
-        'other': 0,
-        'opentrons_96_tiprack_10ul': 0,
-        'opentrons_96_tiprack_20ul': 0,
-        'opentrons_96_tiprack_300ul': 0,
-        'opentrons_96_tiprack_1000ul': 0,
-        'opentrons_96_filtertiprack_10ul': 0,
-        'opentrons_96_filtertiprack_20ul': 0,
-        'opentrons_96_filtertiprack_200ul': 0,
-        'opentrons_96_filtertiprack_1000ul': 0
+def get_protocol_data():
+    tuberack_vols = []
+    for folder in os.listdir(protobuilds_path):
+        folder_path = protobuilds_path + folder
+        prtcl_data = {}
+        lw_data = {}
+        pip_data = {}
+        mod_data = {}
+        for file in os.listdir(folder_path):
+            if '.py.json' in file:
+                file_path = folder_path + '/' + file
+                with open(file_path) as json_file:
+                    data = json.load(json_file)
+                    lw_data['raw'] = data['labware']
+                    pip_data['raw'] = data['instruments']
+                    mod_data['raw'] = data['modules']
+            if 'README.json' in file:
+                rm_file_path = folder_path + '/' + file
+                with open(rm_file_path) as rm_file:
+                    data = json.load(rm_file)
+                    prtcl_data['category'] = [
+                        key for key in data['categories'].keys()][0].lower()
+                    subcat = [val for val in data['categories'].values()][0]
+                    if subcat:
+                        prtcl_data['subcategory'] = subcat[0].lower()
+                    else:
+                        prtcl_data['subcategory'] = [
+                            key for key in data['categories'].keys()][1].lower()
+        lw_data['counts'] = {
+            'tuberack': 0,
+            'plate': 0,
+            'reservoir': 0,
+            'trash': 0,
+            'other': 0,
+            'opentrons_96_tiprack_10ul': 0,
+            'opentrons_96_tiprack_20ul': 0,
+            'opentrons_96_tiprack_300ul': 0,
+            'opentrons_96_tiprack_1000ul': 0,
+            'opentrons_96_filtertiprack_10ul': 0,
+            'opentrons_96_filtertiprack_20ul': 0,
+            'opentrons_96_filtertiprack_200ul': 0,
+            'opentrons_96_filtertiprack_1000ul': 0
+        }
+        for lw in lw_data['raw']:
+            lw_type = map_lw_to_type(lw['type'])
+            # if lw_type == 'tuberack':
+            #     print(lw)
+            if lw_type in lw_data['counts'].keys():
+                lw_data['counts'][lw_type] += 1
+            else:
+                lw_data['counts']['other'] += 1
+        pip_data['counts'] = {
+            'p10_single': 0,
+            'p10_multi': 0,
+            'p20_single_gen2': 0,
+            'p20_multi_gen2': 0,
+            'p50_single': 0,
+            'p50_multi': 0,
+            'p300_single': 0,
+            'p300_multi': 0,
+            'p300_single_gen2': 0,
+            'p300_multi_gen2': 0,
+            'p1000_single': 0,
+            'p1000_single_gen2': 0,
+        }
+        for pip in pip_data['raw']:
+            pip_data['counts'][pip['name']] += 1
+        prtcl_data['labware'] = lw_data
+        prtcl_data['pipettes'] = pip_data
+        # prtcl_data['modules'] = mod_data
+        prtcl_data['id'] = folder
+        protocol_data.append(prtcl_data)
+
+    fields = ['id', 'category', 'subcategory', 'transformed category'] + [
+        key for dict in [protocol_data[0]['labware']['counts'],
+                         protocol_data[0]['pipettes']['counts']]
+        for key in dict.keys()]
+
+    df_dict = {
+        field: []
+        for field in fields
     }
-    for lw in lw_data['raw']:
-        lw_type = map_lw_to_type(lw['type'])
-        # if lw_type == 'tuberack':
-        #     print(lw)
-        if lw_type in lw_data['counts'].keys():
-            lw_data['counts'][lw_type] += 1
-        else:
-            lw_data['counts']['other'] += 1
-    pip_data['counts'] = {
-        'p10_single': 0,
-        'p10_multi': 0,
-        'p20_single_gen2': 0,
-        'p20_multi_gen2': 0,
-        'p50_single': 0,
-        'p50_multi': 0,
-        'p300_single': 0,
-        'p300_multi': 0,
-        'p300_single_gen2': 0,
-        'p300_multi_gen2': 0,
-        'p1000_single': 0,
-        'p1000_single_gen2': 0,
-    }
-    for pip in pip_data['raw']:
-        pip_data['counts'][pip['name']] += 1
-    prtcl_data['labware'] = lw_data
-    prtcl_data['pipettes'] = pip_data
-    # prtcl_data['modules'] = mod_data
-    prtcl_data['id'] = folder
-    protocol_data.append(prtcl_data)
+    for p_d in protocol_data:
+        df_dict['id'].append(p_d['id'])
+        df_dict['category'].append(p_d['category'])
+        df_dict['subcategory'].append(p_d['subcategory'])
+        df_dict['transformed category'].append(
+            transform_category(p_d['category'], p_d['subcategory']))
+        for dict in [p_d['labware']['counts'],
+                     p_d['pipettes']['counts']]:
+            for field, count in dict.items():
+                df_dict[field].append(count)
 
-fields = ['id', 'category', 'subcategory', 'transformed category'] + [
-    key for dict in [protocol_data[0]['labware']['counts'],
-                     protocol_data[0]['pipettes']['counts']]
-    for key in dict.keys()]
+    df = pd.DataFrame(df_dict)
+    tiprack_types = [
+        'opentrons_96_tiprack_10ul', 'opentrons_96_tiprack_20ul',
+        'opentrons_96_tiprack_300ul', 'opentrons_96_tiprack_1000ul',
+        'opentrons_96_filtertiprack_10ul', 'opentrons_96_filtertiprack_20ul',
+        'opentrons_96_filtertiprack_200ul',
+        'opentrons_96_filtertiprack_1000ul']
+    df['total tipracks'] = df[tiprack_types].sum(axis=1)
+    df_by_category = df.groupby(['transformed category'])
+    averages_consumables = df.mean()[[
+        'tuberack', 'plate', 'reservoir', 'total tipracks']].round(2)
+    avg_outfile = averages_consumables.to_csv('data/csv/protocol_data_averages.csv')
+    averages_by_category_consumables = df_by_category.mean()[[
+        'tuberack', 'plate', 'reservoir', 'total tipracks']].round(2)
+        # 'opentrons_96_tiprack_10ul', 'opentrons_96_tiprack_20ul',
+        # 'opentrons_96_tiprack_300ul', 'opentrons_96_tiprack_1000ul',
+        # 'opentrons_96_filtertiprack_10ul', 'opentrons_96_filtertiprack_20ul',
+        # 'opentrons_96_filtertiprack_200ul',
+        # 'opentrons_96_filtertiprack_1000ul']].round(2)
+    avg_by_cat_outfile = averages_by_category_consumables.to_csv('data/csv/protocol_data_averages_by_cat.csv')
+    pip_types = [
+        'p10_single', 'p10_multi', 'p20_single_gen2', 'p20_multi_gen2',
+        'p50_single', 'p50_multi', 'p300_single', 'p300_multi', 'p300_single_gen2',
+        'p300_multi_gen2', 'p1000_single', 'p1000_single_gen2']
+    pipettes_avg = df.mean()[pip_types].round(2)
+    pip_outfile = pipettes_avg.to_csv('data/csv/pipettes.csv')
+    tipracks_avg = df.mean()[tiprack_types].round(3)
+    tipracks_outfile = tipracks_avg.to_csv('data/csv/tipracks.csv')
+    pipettes_by_category = df_by_category.mean()[pip_types].round(2)
+    pip_outfile_cat = pipettes_by_category.to_csv('data/csv/pipettes_by_cat.csv')
+    # print(dir(math))
+    # for pip_type in pip_types:
+    #     mean = np.mean(ngs_df[pip_type])
+    #     std = np.std(ngs_df[pip_type])
+    #     confidence_95 = [
+    #         mean - 1.958*std/np.sqrt(len(ngs_df[pip_type])),
+    #         mean + 1.958*std/np.sqrt(len(ngs_df[pip_type]))]
+    #     print(f'{pip_type}: {mean}\n')
+    outfile = df.to_csv('data/csv/protocol_data.csv')
+    # 7/3/2021: more granular on tipracks on tuberacks (volumes/formats)
 
-df_dict = {
-    field: []
-    for field in fields
-}
-for p_d in protocol_data:
-    df_dict['id'].append(p_d['id'])
-    df_dict['category'].append(p_d['category'])
-    df_dict['subcategory'].append(p_d['subcategory'])
-    df_dict['transformed category'].append(
-        transform_category(p_d['category'], p_d['subcategory']))
-    for dict in [p_d['labware']['counts'],
-                 p_d['pipettes']['counts']]:
-        for field, count in dict.items():
-            df_dict[field].append(count)
+    return df
 
-df = pd.DataFrame(df_dict)
-tiprack_types = [
-    'opentrons_96_tiprack_10ul', 'opentrons_96_tiprack_20ul',
-    'opentrons_96_tiprack_300ul', 'opentrons_96_tiprack_1000ul',
-    'opentrons_96_filtertiprack_10ul', 'opentrons_96_filtertiprack_20ul',
-    'opentrons_96_filtertiprack_200ul',
-    'opentrons_96_filtertiprack_1000ul']
-df['total tipracks'] = df[tiprack_types].sum(axis=1)
-df_by_category = df.groupby(['transformed category'])
-averages_consumables = df.mean()[[
-    'tuberack', 'plate', 'reservoir', 'total tipracks']].round(2)
-avg_outfile = averages_consumables.to_csv('data/csv/protocol_data_averages.csv')
-averages_by_category_consumables = df_by_category.mean()[[
-    'tuberack', 'plate', 'reservoir', 'total tipracks']].round(2)
-    # 'opentrons_96_tiprack_10ul', 'opentrons_96_tiprack_20ul',
-    # 'opentrons_96_tiprack_300ul', 'opentrons_96_tiprack_1000ul',
-    # 'opentrons_96_filtertiprack_10ul', 'opentrons_96_filtertiprack_20ul',
-    # 'opentrons_96_filtertiprack_200ul',
-    # 'opentrons_96_filtertiprack_1000ul']].round(2)
-avg_by_cat_outfile = averages_by_category_consumables.to_csv('data/csv/protocol_data_averages_by_cat.csv')
-pip_types = [
-    'p10_single', 'p10_multi', 'p20_single_gen2', 'p20_multi_gen2',
-    'p50_single', 'p50_multi', 'p300_single', 'p300_multi', 'p300_single_gen2',
-    'p300_multi_gen2', 'p1000_single', 'p1000_single_gen2']
-pipettes_avg = df.mean()[pip_types].round(2)
-pip_outfile = pipettes_avg.to_csv('data/csv/pipettes.csv')
-tipracks_avg = df.mean()[tiprack_types].round(3)
-tipracks_outfile = tipracks_avg.to_csv('data/csv/tipracks.csv')
-pipettes_by_category = df_by_category.mean()[pip_types].round(2)
-pip_outfile_cat = pipettes_by_category.to_csv('data/csv/pipettes_by_cat.csv')
-# print(dir(math))
-# for pip_type in pip_types:
-#     mean = np.mean(ngs_df[pip_type])
-#     std = np.std(ngs_df[pip_type])
-#     confidence_95 = [
-#         mean - 1.958*std/np.sqrt(len(ngs_df[pip_type])),
-#         mean + 1.958*std/np.sqrt(len(ngs_df[pip_type]))]
-#     print(f'{pip_type}: {mean}\n')
-outfile = df.to_csv('data/csv/protocol_data.csv')
-# 7/3/2021: more granular on tipracks on tuberacks (volumes/formats)
+
+if __name__ == '__main__':
+    get_protocol_data()
--- data/salesforce/salesforce.py
+++ data/salesforce/salesforce.py
@@ -4,60 +4,80 @@ from simple_salesforce import Salesforce, SalesforceLogin, SFType
 import pandas as pd
 
 
-# find or prompt for credentials
-creds_path = 'salesforce/credentials.json'
-if os.path.isfile(creds_path):
-    with open(creds_path, 'r') as creds_file:
-        creds = json.load(creds_file)
-        if 'username' not in creds:
-            username = input('Enter your Salesforce email address: ')
-            creds['username'] = username
-        else:
-            username = creds['username']
-        if 'password' not in creds:
-            password = input('Enter your Salesforce password: ')
-            creds['password'] = password
-        else:
-            password = creds['password']
-        if 'token' not in creds:
-            token = input('Enter your security token (see https://help.salesforce.com/s/articleView?id=sf.user_security_token.htm&type=5 for more info): ')
-            creds['token'] = token
-        else:
-            token = creds['token']
-    with open(creds_path, 'w') as creds_file:
-        json.dump(creds, creds_file)
-else:
-    print('Input Salesforce credentials. They will be saved for future attempts.')
-    username = input('Enter your Salesforce email address: ')
-    password = input('Enter your Salesforce password: ')
-    token = input('Enter your security token (see https://help.salesforce.com/s/articleView?id=sf.user_security_token.htm&type=5 for more info): ')
-    with open(creds_path, 'w') as creds_file:
-        creds = {
-            'username': username,
-            'password': password,
-            'token': token
-        }
-        json.dump(creds, creds_file)
-sf = Salesforce(username=username, password=password, security_token=token)
+def login():
+    # find or prompt for credentials
+    creds_path = 'data/salesforce/credentials.json'
+    if os.path.isfile(creds_path):
+        with open(creds_path, 'r') as creds_file:
+            creds = json.load(creds_file)
+            if 'username' not in creds:
+                username = input('Enter your Salesforce email address: ')
+                creds['username'] = username
+            else:
+                username = creds['username']
+            if 'password' not in creds:
+                password = input('Enter your Salesforce password: ')
+                creds['password'] = password
+            else:
+                password = creds['password']
+            if 'token' not in creds:
+                token = input('Enter your security token (see https://help.salesforce.com/s/articleView?id=sf.user_security_token.htm&type=5 for more info): ')
+                creds['token'] = token
+            else:
+                token = creds['token']
+        with open(creds_path, 'w') as creds_file:
+            json.dump(creds, creds_file)
+    else:
+        print('Input Salesforce credentials. They will be saved for future attempts.')
+        username = input('Enter your Salesforce email address: ')
+        password = input('Enter your Salesforce password: ')
+        token = input('Enter your security token (see https://help.salesforce.com/s/articleView?id=sf.user_security_token.htm&type=5 for more info): ')
+        with open(creds_path, 'w') as creds_file:
+            creds = {
+                'username': username,
+                'password': password,
+                'token': token
+            }
+            json.dump(creds, creds_file)
+    sf_client = Salesforce(username=username, password=password, security_token=token)
+    return sf_client
 
-metadata_org = sf.describe()
-df_sobjects = pd.DataFrame(metadata_org['sobjects'])
-# df_sobjects.to_csv('sf_metadata.csv', index=False)
 
-protocol = sf.Protocol__c
-protocol_metadata = protocol.describe()
-df_protocol_metadata = pd.DataFrame(protocol_metadata.get('fields'))
-df_protocol_metadata.to_csv('data/csv/protocol_metadata.csv', index=False)
+def extract():
 
-fields = ['Github_ID__c', 'CreatedDate', 'Delivery_Date__c', 'TAT_days__c', 'Category__c', 'Status__c', 'Assignee__c']
-fields_str = ','.join(fields)
-SOQL = f'SELECT {fields_str} FROM Protocol__c'
-data = sf.query(SOQL)
+    sf = login()
+    # metadata_org = sf.describe()
+    # df_sobjects = pd.DataFrame(metadata_org['sobjects'])
+    # df_sobjects.to_csv('sf_metadata.csv', index=False)
 
-df_dict = {field: [d[field] for d in data['records']] for field in fields}
-df = pd.DataFrame(df_dict)
-df_new = df.rename(
-    columns={'Github_ID__c': 'id', 'CreatedDate': 'created',
-             'Delivery_Date__c': 'delivered', 'TAT_days__c': 'tat',
-             'Category__c': 'category', 'Status__c': 'status',
-             'Assignee__c': 'assignee'})
+    # protocol = sf.Protocol__c
+    # protocol_metadata = protocol.describe()
+    # df_protocol_metadata = pd.DataFrame(protocol_metadata.get('fields'))
+    # df_protocol_metadata.to_csv('data/csv/protocol_metadata.csv',
+    #                             index=False)
+
+    fields = ['Github_ID__c', 'CreatedDate', 'Delivery_Date__c', 'TAT_days__c',
+              'Category__c', 'Status__c', 'Assignee__c']
+    fields_str = ','.join(fields)
+    SOQL = f'SELECT {fields_str} FROM Protocol__c'
+    data = sf.query(SOQL)
+
+    df_dict = {field: [d[field] for d in data['records']] for field in fields}
+    df = pd.DataFrame(df_dict)
+    df_new = df.rename(
+        columns={'Github_ID__c': 'id', 'CreatedDate': 'created',
+                 'Delivery_Date__c': 'delivered', 'TAT_days__c': 'tat',
+                 'Category__c': 'sf_category', 'Status__c': 'sf_status',
+                 'Assignee__c': 'sf_assignee'})
+
+    with open('data/salesforce/user_map.json') as user_map_file:
+        user_map = json.load(user_map_file)
+        df_new['sf_assignee'].replace(user_map, inplace=True)
+    # print(df_new['sf_assignee'].value_counts())
+
+    df_new.to_csv('data/csv/salesforce.csv')
+    return df_new
+
+
+if __name__ == '__main__':
+    extract()
