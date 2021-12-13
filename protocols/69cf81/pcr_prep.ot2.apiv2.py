ee9933d638168f37c1d6cade7374a4a52dc04160
ncdiehl11 <ncdiehl11@gmail.com>
2021-12-13 17:21:04 -0500

    1 set of tips for bigdye transfer

--- protocols/69cf81/pcr_prep.ot2.apiv2.py
+++ protocols/69cf81/pcr_prep.ot2.apiv2.py
@@ -59,10 +59,10 @@ with {num_primers} primers.')
 
     # transfer BigDye + water mix
     reagent_dests_multi = pcr_plate.rows()[0][:num_cols*num_primers]
+    m20.pick_up_tip()
     for d in reagent_dests_multi:
-        m20.pick_up_tip()
         m20.transfer(16, mm, d.bottom(0.5), new_tip='never')
-        m20.drop_tip()
+    m20.drop_tip()
 
     # transfer primers
     for primer, dest_set in zip(primer_sources, primer_dest_sets):
