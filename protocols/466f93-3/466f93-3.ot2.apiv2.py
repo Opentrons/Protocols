873677bf89b837edb46eee6debbc71dda100d368
Eskil Andersen <eskil.andersen@opentrons.com>
2022-01-18 15:25:52 -0500

    linting

--- protocols/466f93-3/466f93-3.ot2.apiv2.py
+++ protocols/466f93-3/466f93-3.ot2.apiv2.py
@@ -24,7 +24,7 @@ def run(ctx: protocol_api.ProtocolContext):
 
     if not 7 <= num_samples <= 36:
         raise Exception("The number of samples should be between 7 and 36")
-        
+
     # define all custom variables above here with descriptions:
     bead_volume = 25  # volume of bead solution for each sample
     bead_source_well_volume = 127
