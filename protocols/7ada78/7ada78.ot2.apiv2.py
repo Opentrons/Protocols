f357354deac29ee8c599d5b7b487d8770096c69b
ramifarawi <rami.farawi@opentrons.com>
2022-02-02 15:57:57 -0500

    protocol

--- protocols/7ada78/7ada78.ot2.apiv2.py
+++ protocols/7ada78/7ada78.ot2.apiv2.py
@@ -12,21 +12,27 @@ def run(ctx: protocol_api.ProtocolContext):
 
     [
      _csv,
-     _p20_mount
+     _source_type,
+     _p20_mount,
+     _m20_mount
     ] = get_values(  # noqa: F821 (<--- DO NOT REMOVE!)
         "_csv",
-        "_p20_mount")
+        "_source_type",
+        "_p20_mount",
+        "_m20_mount")
 
     # VARIABLES
     csv = _csv
+    source_type = _source_type
     p20_mount = _p20_mount
+    m20_mount = _m20_mount
 
     # MODULES
     thermocyc = ctx.load_module('thermocycler')
 
     # LABWARE
     tuberacks = [ctx.load_labware(
-                 'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+                 source_type,
                  slot, label='Sample Tuberack')
                  for slot in ['1', '2', '4', '5']]
     tc_plate = thermocyc.load_labware('nest_96_wellplate_100ul_pcr_full_skirt')
@@ -39,11 +45,15 @@ def run(ctx: protocol_api.ProtocolContext):
 
     # INSTRUMENTS
     p20 = ctx.load_instrument('p20_single_gen2', p20_mount, tip_racks=tipracks)
+    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount, tip_racks=tipracks)
+
+    pip = m20 if source_type == "kingfisher_96_wellplate_600ul" else p20
+    print(pip.type)
 
     def pre_wet(vol, well):
         ctx.comment('PRE-WET')
-        p20.aspirate(vol, well, rate=0.5)
-        p20.dispense(vol, well, rate=0.5)
+        pip.aspirate(vol, well, rate=0.5)
+        pip.dispense(vol, well, rate=0.5)
         ctx.comment('DONE PRE-WET')
 
     # MAPPING
@@ -73,8 +83,8 @@ def run(ctx: protocol_api.ProtocolContext):
                                         the[source_well]]
         dest = ctx.loaded_labwares[int(the[slot_dest])].wells_by_name()[
                                         the[dest_well]]
-        p20.pick_up_tip()
+        pip.pick_up_tip()
         pre_wet(vol, source)
-        p20.aspirate(vol, source)
-        p20.dispense(vol, dest)
-        p20.drop_tip()
+        pip.aspirate(vol, source)
+        pip.dispense(vol, dest)
+        pip.drop_tip()
