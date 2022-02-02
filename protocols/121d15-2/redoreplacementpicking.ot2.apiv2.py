1a8d69b8231d9abeebe658c77fd85780bde712cd
ncdiehl11 <ndiehl@opentrons.com>
2022-02-02 16:00:03 -0500

    implement custom tuberack

--- protocols/121d15-2/redoreplacementpicking.ot2.apiv2.py
+++ protocols/121d15-2/redoreplacementpicking.ot2.apiv2.py
@@ -1,6 +1,6 @@
 # metadata
 metadata = {
-    'protocolName': 'HPLC Picking',
+    'protocolName': 'Redo Replacement Picking',
     'author': 'Nick <protocols@opentrons.com>',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.11'
@@ -9,19 +9,17 @@ metadata = {
 
 def run(ctx):
 
-    [input_file, replacement_vol, plate_type,
+    [input_file, default_disposal_vol, default_transfer_vol, plate_type,
      p300_mount] = get_values(  # noqa: F821
-        'input_file', 'replacement_vol', 'plate_type', 'p300_mount')
+        'input_file', 'default_disposal_vol', 'default_transfer_vol',
+        'plate_type', 'p300_mount')
 
     # load labware
-    racks = [
-        ctx.load_labware('aluminumblock_48_tuberack_2000ul', f'{slot}',
-                         f'rack {i+1}')
-        for i, slot in enumerate(['1', '4', '2', '5'])]
-    plate = ctx.load_labware(plate_type, '3')
+    rack = ctx.load_labware('eurofins_96x2ml_tuberack', '2', 'tuberack')
+    plate = ctx.load_labware(plate_type, '1')
     tips300 = [
         ctx.load_labware('opentrons_96_tiprack_300ul', slot)
-        for slot in ['6', '9', '7', '8']]
+        for slot in ['4', '7']]
 
     # pipette
     p300 = ctx.load_instrument('p300_single_gen2', p300_mount,
@@ -34,25 +32,30 @@ def run(ctx):
         if line and line.split(',')[0].strip()]
 
     tubes_ordered = [
-        well for rack in racks for row in rack.rows()[::-1] for well in row]
+        well for i in range(2) for col in rack.columns()
+        for well in col[i*8:(i+1)*8]]
 
     for line in data:
         tube = tubes_ordered[int(line[0])-1]
         well = plate.wells()[int(line[1])-1]
-        if len(line) >= 3 and line[3]:
-            vol = float(line[3])
+        if len(line) >= 3 and line[2]:
+            disposal_vol = float(line[2])
         else:
-            vol = replacement_vol
+            disposal_vol = default_disposal_vol
+        if len(line) >= 4 and line[3]:
+            transfer_vol = float(line[3])
+        else:
+            transfer_vol = default_transfer_vol
 
         # remove contents of well
         p300.pick_up_tip()
         ctx.max_speeds['A'] = 100  # slow descent
         ctx.max_speeds['Z'] = 100  # slow descent
 
-        p300.aspirate(vol, well.bottom())
+        p300.aspirate(disposal_vol, well.bottom())
         del ctx.max_speeds['A']  # reset to default
         del ctx.max_speeds['Z']  # reset to default
         p300.drop_tip()
 
         # transfer tube to well
-        p300.transfer(replacement_vol, tube, well.top(-1))
+        p300.transfer(transfer_vol, tube, well.top(-1))
