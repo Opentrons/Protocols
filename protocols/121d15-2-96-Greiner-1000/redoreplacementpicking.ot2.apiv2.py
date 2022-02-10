033015b38d26653646534ecdc4516b5cc733a53f
ncdiehl11 <ndiehl@opentrons.com>
2022-02-10 18:06:59 -0500

    add tip tracking for picking protocols

--- protocols/121d15-2-96-Greiner-1000/redoreplacementpicking.ot2.apiv2.py
+++ protocols/121d15-2-96-Greiner-1000/redoreplacementpicking.ot2.apiv2.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 # metadata
 metadata = {
     'protocolName': 'Redo Replacement Picking (Greiner MASTERBLOCK 96 Well \
@@ -9,6 +12,8 @@ Plate 1000 µL)',
 
 
 def run(ctx):
+
+    tip_track = True
 
     [input_file, tuberack_scan, plate_scan, default_disposal_vol,
      default_transfer_vol, p300_mount] = get_values(  # noqa: F821
@@ -25,6 +30,47 @@ def run(ctx):
     # pipette
     p300 = ctx.load_instrument('p300_single_gen2', p300_mount,
                                tip_racks=tips300)
+
+    tip_log = {val: {} for val in ctx.loaded_instruments.values()}
+
+    folder_path = '/data/tip_track'
+    tip_file_path = folder_path + '/tip_log.json'
+    if tip_track and not ctx.is_simulating():
+        if os.path.isfile(tip_file_path):
+            with open(tip_file_path) as json_file:
+                data = json.load(json_file)
+                for pip in tip_log:
+                    if pip.name in data:
+                        tip_log[pip]['count'] = data[pip.name]
+                    else:
+                        tip_log[pip]['count'] = 0
+        else:
+            for pip in tip_log:
+                tip_log[pip]['count'] = 0
+    else:
+        for pip in tip_log:
+            tip_log[pip]['count'] = 0
+
+    for pip in tip_log:
+        if pip.type == 'multi':
+            tip_log[pip]['tips'] = [tip for rack in pip.tip_racks
+                                    for tip in rack.rows()[0]]
+        else:
+            tip_log[pip]['tips'] = [tip for rack in pip.tip_racks
+                                    for tip in rack.wells()]
+        tip_log[pip]['max'] = len(tip_log[pip]['tips'])
+
+    def _pick_up(pip, loc=None):
+        if tip_log[pip]['count'] == tip_log[pip]['max'] and not loc:
+            ctx.pause('Replace ' + str(pip.max_volume) + 'µl tipracks before \
+resuming.')
+            pip.reset_tipracks()
+            tip_log[pip]['count'] = 0
+        if loc:
+            pip.pick_up_tip(loc)
+        else:
+            pip.pick_up_tip(tip_log[pip]['tips'][tip_log[pip]['count']])
+            tip_log[pip]['count'] += 1
 
     # check barcode scans (tube, plate)
     tuberack_bar, plate_bar = input_file.splitlines()[3].split(',')[:2]
@@ -69,3 +115,11 @@ def run(ctx):
 
         # transfer tube to well
         p300.transfer(transfer_vol, tube.bottom(0.5), well.top(-1))
+
+    # track final used tip
+    if not ctx.is_simulating():
+        if not os.path.isdir(folder_path):
+            os.mkdir(folder_path)
+        data = {pip.name: tip_log[pip]['count'] for pip in tip_log}
+        with open(tip_file_path, 'w') as outfile:
+            json.dump(data, outfile)
