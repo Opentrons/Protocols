ed78e7133c58bca66e8c6bd7a688a18eb2b50225
Eskil Andersen <eskil.andersen@opentrons.com>
2022-02-11 09:36:13 -0500

    fields have choices of tube-racks and 12-well reservoirs, option to use filtered tips

--- protocols/pcr_prep_part_1/pcr_prep-part1.ot2.apiv2.py
+++ protocols/pcr_prep_part_1/pcr_prep-part1.ot2.apiv2.py
@@ -7,8 +7,20 @@ metadata = {
 
 
 def run(protocol_context):
-    [left_pipette, right_pipette, master_mix_csv] = get_values(  # noqa: F821
-        "left_pipette", "right_pipette", "master_mix_csv")
+    [right_pipette,
+     left_pipette,
+     is_filtered,
+     tuberack_1_lname,
+     tuberack_2_lname,
+     reservoir12_lname,
+     master_mix_csv] = get_values(  # noqa: F821
+     "right_pipette",
+     "left_pipette",
+     "is_filtered",
+     "tuberack_1_lname",
+     "tuberack_2_lname",
+     "reservoir12_lname",
+     "master_mix_csv")
 
     if not left_pipette and not right_pipette:
         raise Exception('You have to select at least 1 pipette.')
@@ -16,12 +28,29 @@ def run(protocol_context):
     pipette_l = None
     pipette_r = None
 
+    tiprack_pip_dict = {
+        "p10_single": ("opentrons_96_filtertiprack_20ul",
+                       "opentrons_96_tiprack_20ul"),
+        "p20_single_gen2": ("opentrons_96_filtertiprack_20ul",
+                            "opentrons_96_tiprack_20ul"),
+        "p50_single": ("opentrons_96_filtertiprack_200ul",
+                       "opentrons_96_tiprack_300ul"),
+        "p300_single": ("opentrons_96_filtertiprack_200ul",
+                        "opentrons_96_tiprack_300ul"),
+        "p300_single_gen2": ("opentrons_96_filtertiprack_200ul",
+                             "opentrons_96_tiprack_300ul"),
+        "p1000_single": ("opentrons_96_filtertiprack_1000ul",
+                         "opentrons_96_tiprack_1000ul"),
+        "p1000_single_gen2": ("opentrons_96_filtertiprack_1000ul",
+                              "opentrons_96_tiprack_1000ul")
+    }
+
     for pip, mount, slot in zip(
             [left_pipette, right_pipette], ['left', 'right'], ['5', '6']):
 
         if pip:
-            range = pip.split('_')[0][1:]
-            rack = 'opentrons_96_tiprack_' + range + 'ul'
+            rack = tiprack_pip_dict[pip][0] if is_filtered else \
+                tiprack_pip_dict[pip][1]
             tiprack = protocol_context.load_labware(rack, slot)
             if mount == 'left':
                 pipette_l = protocol_context.load_instrument(
@@ -31,21 +60,21 @@ def run(protocol_context):
                     pip, mount, tip_racks=[tiprack])
 
     # labware setup
-    snaprack = protocol_context.load_labware(
-        'opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap',
+    tuberack_1 = protocol_context.load_labware(
+        tuberack_1_lname,
         '1',
-        'snapcap 2ml tuberack'
+        'tuberack 1'
     )
-    screwrack = protocol_context.load_labware(
-        'opentrons_24_tuberack_generic_2ml_screwcap',
+    tuberack_2 = protocol_context.load_labware(
+        tuberack_2_lname,
         '2',
-        'screwcap 2ml tuberack'
+        'tuberack 2'
     )
     res12 = protocol_context.load_labware(
         'usascientific_12_reservoir_22ml', '3', '12-channel reservoir')
     reagents = {
-        '1': snaprack,
-        '2': screwrack,
+        '1': tuberack_1,
+        '2': tuberack_2,
         '3': res12
     }
 
