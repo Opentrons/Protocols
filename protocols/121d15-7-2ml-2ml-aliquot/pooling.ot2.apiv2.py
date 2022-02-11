f367a27051539e61bd4819e29cbd87609958a692
ncdiehl11 <ndiehl@opentrons.com>
2022-02-11 10:15:17 -0500

    121d15-7

---
+++ protocols/121d15-7-2ml-2ml-aliquot/pooling.ot2.apiv2.py
@@ -0,0 +1,123 @@
+import os
+import json
+
+# metadata
+metadata = {
+    'protocolName': 'Aliquoting - 2ml Tuberack to 2ml Tuberack',
+    'author': 'Nick <protocols@opentrons.com>',
+    'source': 'Custom Protocol Request',
+    'apiLevel': '2.11'
+}
+
+
+def run(ctx):
+
+    tip_track = True
+
+    [input_file, tuberack1_scan, tuberack2_scan, default_transfer_vol,
+     p300_mount] = get_values(  # noqa: F821
+        'input_file', 'tuberack1_scan', 'tuberack2_scan',
+        'default_transfer_vol', 'p300_mount')
+
+    # load labware
+    rack = ctx.load_labware('eurofins_96x2ml_tuberack', '2', 'tuberack')
+    tips300 = [
+        ctx.load_labware('opentrons_96_tiprack_300ul', slot)
+        for slot in ['11']]
+
+    # pipette
+    p300 = ctx.load_instrument('p300_single_gen2', p300_mount,
+                               tip_racks=tips300)
+
+    tip_log = {val: {} for val in ctx.loaded_instruments.values()}
+
+    folder_path = '/data/tip_track'
+    tip_file_path = folder_path + '/tip_log.json'
+    if tip_track and not ctx.is_simulating():
+        if os.path.isfile(tip_file_path):
+            with open(tip_file_path) as json_file:
+                data = json.load(json_file)
+                for pip in tip_log:
+                    if pip.name in data:
+                        tip_log[pip]['count'] = data[pip.name]
+                    else:
+                        tip_log[pip]['count'] = 0
+        else:
+            for pip in tip_log:
+                tip_log[pip]['count'] = 0
+    else:
+        for pip in tip_log:
+            tip_log[pip]['count'] = 0
+
+    for pip in tip_log:
+        if pip.type == 'multi':
+            tip_log[pip]['tips'] = [tip for rack in pip.tip_racks
+                                    for tip in rack.rows()[0]]
+        else:
+            tip_log[pip]['tips'] = [tip for rack in pip.tip_racks
+                                    for tip in rack.wells()]
+        tip_log[pip]['max'] = len(tip_log[pip]['tips'])
+
+    def _pick_up(pip, loc=None):
+        if tip_log[pip]['count'] == tip_log[pip]['max'] and not loc:
+            ctx.pause('Replace ' + str(pip.max_volume) + 'µl tipracks before \
+resuming.')
+            pip.reset_tipracks()
+            tip_log[pip]['count'] = 0
+        if loc:
+            pip.pick_up_tip(loc)
+        else:
+            pip.pick_up_tip(tip_log[pip]['tips'][tip_log[pip]['count']])
+            tip_log[pip]['count'] += 1
+
+    # check barcode scans (tube, plate)
+    tuberack1_bar, tuberack2_bar = input_file.splitlines()[3].split(',')[:2]
+    if not tuberack1_scan[:len(tuberack1_scan)-4] == tuberack1_bar.strip():
+        raise Exception(f'Tuberack 1 scans do not match ({tuberack1_bar}, \
+{tuberack1_scan})')
+    if not tuberack2_scan[:len(tuberack2_scan)-4] == tuberack2_bar.strip():
+        raise Exception(f'Tuberack 2 scans do not match ({tuberack2_bar}, \
+{tuberack2_bar})')
+
+    # parse
+    data = [
+        [val.strip() for val in line.split(',')]
+        for line in input_file.splitlines()[4:]
+        if line and line.split(',')[0].strip()]
+
+    tubes1_ordered = [
+        well for i in range(2) for col in rack.columns()
+        for well in col[i*8:(i+1)*8]]
+    tubes2_ordered = [
+        well for i in range(2) for col in rack.columns()
+        for well in col[i*8+8:(i+1)*8+8]]
+
+    prev_source = None
+    for line in data:
+        tube1 = tubes1_ordered[int(line[0])-1]
+        tube2 = tubes2_ordered[int(line[0])-1]
+        if len(line) >= 3 and line[2]:
+            transfer_vol = float(line[3])
+        else:
+            transfer_vol = default_transfer_vol
+
+        # transfer
+        if tube1 != prev_source:
+            if p300.has_tip:
+                p300.drop_tip()
+            _pick_up(p300)
+        p300.transfer(transfer_vol, tube1.bottom(0.5), tube2.top(-1),
+                      new_tip='never')
+        p300.blow_out(tube2)
+        p300.touch_tip(tube2)
+
+        prev_source = tube1
+    p300.drop_tip()
+
+    # track final used tip
+    if not ctx.is_simulating():
+        if not os.path.isdir(folder_path):
+            os.mkdir(folder_path)
+        data = {pip.name: tip_log[pip]['count'] for pip in tip_log}
+        with open(tip_file_path, 'w') as outfile:
+            json.dump(data, outfile)
