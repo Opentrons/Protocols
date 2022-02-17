2ac49c35beb2575add8cabe9bc7fe017091864f3
Steve Plonk <steveplonk@WS-00012.local>
2022-02-16 21:08:23 -0500

    changes to 121d15-2-96-Greiner-500 to adjust clearances for complete removal

--- protocols/121d15-2-96-Greiner-500/redoreplacementpicking.ot2.apiv2.py
+++ protocols/121d15-2-96-Greiner-500/redoreplacementpicking.ot2.apiv2.py
@@ -149,11 +149,10 @@ resuming.')
                 p300.move_to(well.top())
                 p300.air_gap(20)
                 p300.aspirate(vol, well.bottom(1))
-                # if rep < reps - 1:
                 p300.dispense(vol+20, ctx.fixed_trash.wells()[0].top(-5))
 
             # to improve completeness of removal
-            for clearance in [0.5, 0.3, 0.1, 0]:
+            for clearance in [0.7, 0.4, 0.2, 0]:
                 p300.aspirate(20, well.bottom(clearance))
 
             del ctx.max_speeds['A']  # reset to default
