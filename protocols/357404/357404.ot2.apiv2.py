01f6d904c6cb5bcefa1942e586a1b16f26f6b71b
Eskil Andersen <eskil.andersen@opentrons.com>
2022-02-25 16:27:30 -0500

    added slide holder labware, protocol skeleton

--- protocols/357404/357404.ot2.apiv2.py
+++ protocols/357404/357404.ot2.apiv2.py
@@ -1,35 +1,46 @@
 from opentrons import protocol_api
+import math
 
 metadata = {
-    'protocolName': 'Protocol Title',
-    'author': 'AUTHOR NAME <authoremail@company.com>',
+    'protocolName': '357404: Slide antibody assay',
+    'author': 'Eskil Andersen <protocols@opentrons.com>',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.11'   # CHECK IF YOUR API LEVEL HERE IS UP TO DATE
                          # IN SECTION 5.2 OF THE APIV2 "VERSIONING"
 }
 
 
+def get_values(*names):
+    import json
+    _all_values = json.loads("""{
+                                  "n_slots":6,
+                                  "vol_reagent":1500,
+                                  "n_ab_containers":3,
+                                  "do_step_x":true,
+                                  "tuberack_lname":"opentrons_24_aluminumblock_nest_2ml_screwcap"
+                                  }
+                                  """)
+    return [_all_values[n] for n in names]
+
+
 def run(ctx: protocol_api.ProtocolContext):
 
-    [
-     _custom_variable1,
-     _custom_variable2
-    ] = get_values(  # noqa: F821 (<--- DO NOT REMOVE!)
-        "_custom_variable1",
-        "_custom_variable2")
+    [n_slots,
+     vol_reagent,
+     n_ab_containers,
+     do_step_x,
+     tuberack_lname] = get_values(  # noqa: F821
+     "n_slots",
+     "vol_reagent",
+     "n_ab_containers",
+     "do_step_x",
+     "tuberack_lname")
 
-    if not 1 <= _custom_variable1 <= 12:
-        raise Exception("Enter a value between 1-12")
-
-    # define all custom variables above here with descriptions:
-
-    # number of samples
-    custom_variable1 = _custom_variable1
-
-    # "True" for park tips, "False" for discard tips
-    custom_variable2 = _custom_variable2
+    slide_holder_lname = 'customslideholder_8_wellplate_15000ul'
+    slide_slots = [1, 4, 5, 7, 8, 10, 11]
 
     # load modules
+    temp_mod = ctx.load_module('temperature module gen2', '3')
 
     '''
 
@@ -45,6 +56,13 @@ def run(ctx: protocol_api.ProtocolContext):
     '''
 
     # load labware
+    # Labware: 290 mL reservoir, Tuberack for reagents, slide holders
+    reservoir = ctx.load_labware('agilent_1_reservoir_290ml', '2')
+    tuberack = temp_mod.load_labware(tuberack_lname)
+    slide_holders = []
+    for slot in slide_slots[:n_slots]:
+        slide_holder = ctx.load_labware(slide_holder_lname, slot)
+        slide_holders.append(slide_holder)
 
     '''
 
@@ -58,7 +76,6 @@ def run(ctx: protocol_api.ProtocolContext):
     where module_name is defined above.
 
     '''
-
     # load tipracks
 
     '''
@@ -79,6 +96,9 @@ def run(ctx: protocol_api.ProtocolContext):
     e.g. tiprack10, tiprack20, tiprack200, tiprack300, tiprack1000
 
     '''
+    # Load 1000 uL tips on 9, 300 uL on 6
+    tiprack_300 = [ctx.load_labware('opentrons_96_tiprack_300ul', '6')]
+    tiprack_1000 = [ctx.load_labware('opentrons_96_tiprack_1000ul', '9')]
 
     # load instrument
 
@@ -99,6 +119,17 @@ def run(ctx: protocol_api.ProtocolContext):
                         tip_racks=tiprack
                         )
     '''
+    # Load p300, p1000 gen2
+    p300 = ctx.load_instrument(
+                              'p300_single_gen2',
+                              'left',
+                              tip_racks=tiprack_300
+                              )
+    p1000 = ctx.load_instrument(
+                              'p1000_single_gen2',
+                              'left',
+                              tip_racks=tiprack_1000
+                              )
 
     # pipette functions   # INCLUDE ANY BINDING TO CLASS
 
@@ -155,6 +186,43 @@ def run(ctx: protocol_api.ProtocolContext):
 
 
     '''
+    class VolTracker:
+        def __init__(self, labware, well_vol,
+                     start=1, end=8,
+                     mode='reagent',
+                     pip_type='single',
+                     msg='Reset labware volumes'):
+            """
+            Voltracker tracks the volume(s) used in a piece of labware
+
+            :param labware: The labware to track
+            :param well_vol: The volume of the liquid in the wells, if using a
+            multi-pipette with a well plate, treat the plate like a reservoir,
+            i.e. start=1, end=1, well_vol = 8 * vol of each individual well.
+            :param pip_type: The pipette type used 'single' or 'multi'
+            :param mode: 'reagent' or 'waste'
+            :param start: The starting well
+            :param end: The ending well
+            :param msg: Message to send to the user when all wells are empty
+            (or full when in waste mode)
+
+            """
+            self.labware_wells = dict.fromkeys(
+                labware.wells()[start-1:end], 0)
+            self.labware_wells_backup = self.labware_wells.copy()
+            self.well_vol = well_vol
+            self.pip_type = pip_type
+            self.mode = mode
+            self.start = start
+            self.end = end
+            self.msg = msg
+
+            # Parameter error checking
+            if not (pip_type == 'single' or pip_type == 'multi'):
+                raise Exception('Pipette type must be single or multi')
+
+            if not (mode == 'reagent' or mode == 'waste'):
+                raise Exception('mode must be reagent or waste')
 
     # reagents
 
@@ -169,8 +237,22 @@ def run(ctx: protocol_api.ProtocolContext):
     dnase = tuberack.wells_by_name()['A4']
 
     '''
+    total_reagent_vol = 100 * n_slots * 8
+    n_tubes = math.ceil(total_reagent_vol / vol_reagent)
+    block = VolTracker(tuberack, 1500, start=1, end=n_tubes)
+    antibody1 = VolTracker(tuberack, 1500, start=n_tubes+1, end=n_tubes*2+1)
+    antibody2 = VolTracker(tuberack, 1500, start=n_tubes*2+1, end=n_tubes*3+1)
+    # Nuclear counterstain
+    nuc_cstn = VolTracker(tuberack, 1500, start=n_tubes*3+1, end=n_tubes*4+1)
+    pbs = VolTracker(
+        reservoir, 290*10**3, start=1, end=2, mode='reagent',
+        pip_type='single', msg="Refill PBS reservoir")
 
     # plate, tube rack maps
+    target_wells = []
+    for slide_holder in slide_holders:
+        for well in slide_holder.wells():
+            target_wells.append(well)
 
     '''
     Define any plate or tube maps here.
@@ -205,3 +287,19 @@ def run(ctx: protocol_api.ProtocolContext):
 
 
     '''
+    # Set the temperature to 4 degrees on the temperature module.
+    temp_mod.set_temperature(4)
+    # Transfer 100 µL of block from the tuberack to each destination well
+    # (slide) (could be done as a multi-dispense with the P1000)
+    # Pause/Incubate for 1 hour
+    # Transfer 100 µL of primary antibody to dest. wells (slide)
+    # Pause 1 hour
+    # Transfer 4 mL of PBS to each slide target well (i.e. 4 round trips)
+    # with the P1000
+    # Transfer 100 µL of the second antibody
+    # Pause 1 hour
+    # Transfer 4 mL of PBS to slide target wells
+    # Transfer 100 µL nuclear counterstain to each well
+    # Incubate 5 minutes
+    # Transfer 4 mL PBS
+    # Pause until resume (stop the protocol)
