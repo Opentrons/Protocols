834400ac7876dd5ef9d559c1b428fa746c420fe6
Steve Plonk <steveplonk@WS-00012.attlocal.net>
2022-03-07 13:19:47 -0500

    labware change

--- protocols/78d33c/78d33c.ot2.apiv2.py
+++ protocols/78d33c/78d33c.ot2.apiv2.py
@@ -145,7 +145,7 @@ def run(ctx):
     mag = ctx.load_module('magnetic module gen2', '1')
     mag.disengage()
     mag_plate = mag.load_labware(
-     'corning_96_wellplate_600ul', 'Mag Plate')
+     'eppendorf_96_wellplate_200ul', 'Mag Plate')
 
     ctx.comment("STEP - KAPA Pure Beads")
 
