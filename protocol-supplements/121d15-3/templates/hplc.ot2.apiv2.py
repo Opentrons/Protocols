f9f0e5fa6d482f12629b0c8ecacd6fbf61668231
ncdiehl11 <ndiehl@opentrons.com>
2022-03-08 21:56:02 -0500

    merge latest HPLC template with flashing functionality

--- protocol-supplements/121d15-3/templates/hplc.ot2.apiv2.py
+++ protocol-supplements/121d15-3/templates/hplc.ot2.apiv2.py
@@ -1,4 +1,5 @@
 from opentrons import protocol_api
+import math
 import threading
 import os
 import json
@@ -84,15 +85,12 @@ def run(ctx):
 
     # load labware
     rack = ctx.load_labware('eurofins_96x2ml_tuberack', '2', 'tuberack')
-    plates = [
-        ctx.load_labware('irishlifesciences_96_wellplate_2200ul', slot,
-                         f'plate {i+1}')
-        for i, slot in enumerate(['10', '7', '4', '1'])]
     tips300 = [ctx.load_labware('opentrons_96_tiprack_300ul', '11')]
 
     # pipette
     p300 = ctx.load_instrument('p300_single_gen2', 'left',
                                tip_racks=tips300)
+    p300.flow_rate.dispense = 120
 
     tip_log = {val: {} for val in ctx.loaded_instruments.values()}
 
@@ -143,15 +141,11 @@ tipracks before resuming.')
         line.split(',') for line in INPUT_FILE.splitlines()
         if line and line.split(',')[0].strip()]
 
-    # order
-    wells_ordered = [
-        well for plate in plates for row in plate.rows() for well in row]
-
     # determine plates in use
     plates_used = {key: False for key in range(4)}
     for i, line in enumerate(data):
-        source = wells_ordered[int(line[0]) - 1]
-        plates_used[source//96] = True
+        source_ind = int(line[0]) - 1
+        plates_used[source_ind//96] = True
 
     if not plates_used[3]:
         plates = [
@@ -166,6 +160,10 @@ tipracks before resuming.')
             for i, slot in enumerate(['10', '7', '4', '1'])]
         water = plates[-1].wells_by_name()['D6']
 
+    # order
+    wells_ordered = [
+        well for plate in plates for row in plate.rows() for well in row]
+
     dest_vols = {}
     prev_dest = None
     for i, line in enumerate(data):
@@ -176,16 +174,25 @@ tipracks before resuming.')
         else:
             vol = DEFAULT_TRANSFER_VOL
 
-        # check for volumes slightly over 300ul from HPLC input files
-        vol = 300 if 300 < vol < 305 else vol
-
         if dest != prev_dest:
             if p300.has_tip:
                 p300.drop_tip()
             _pick_up(p300)
-        p300.transfer(vol, source.bottom(0.5), dest.top(-1), new_tip='never')
-        p300.blow_out(dest)
-        p300.touch_tip(dest)
+
+        # check for volumes slightly over 300ul from HPLC input files
+        vol = 300 if 300 < vol < 305 else vol
+        # effective tip capacity 280 with 20 uL air gap
+        reps = math.ceil(vol / 300)
+
+        v = vol / reps
+
+        for rep in range(reps):
+            p300.aspirate(v, source.bottom(0.5))
+            p300.dispense(
+             v+50, dest.top(-5), rate=2)
+            ctx.delay(seconds=1)
+            p300.blow_out()
+
         prev_dest = dest
 
         # track volumes for final adjustment
@@ -198,14 +205,22 @@ tipracks before resuming.')
     # final adjustment with water up to 1500ul
     ctx.pause('Replace plate 4 in slot 1 with water reservoir. Resume once \
 finished.')
-    water = plates[-1].wells_by_name()['D4'].bottom(1)
-    p300.pick_up_tip()
+    _pick_up(p300)
     for tube, vol in dest_vols.items():
         adjustment = 1500 - vol
         if adjustment > 0:
-            p300.transfer(adjustment, water, tube.top(-1), new_tip='never')
-            p300.blow_out(tube)
-            p300.touch_tip(tube)
+            # effective tip capacity 280 uL with 20 uL air gap
+            reps = math.ceil(adjustment / 300)
+
+            v = adjustment / reps
+
+            for rep in range(reps):
+                p300.aspirate(v, water.bottom(1))
+                p300.dispense(
+                 v+50, tube.top(-5), rate=2)
+                ctx.delay(seconds=1)
+                p300.blow_out()
+
     p300.drop_tip()
 
     # track final used tip
