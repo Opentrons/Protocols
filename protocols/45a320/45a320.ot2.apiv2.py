538ff5d63c161bf8898f63ede66475698793fff9
ramifarawi <rami.farawi@opentrons.com>
2022-03-15 16:56:01 -0400

    pb

--- protocols/45a320/45a320.ot2.apiv2.py
+++ protocols/45a320/45a320.ot2.apiv2.py
@@ -6,11 +6,6 @@ metadata = {
     'apiLevel': '2.10'
 }
 
-def get_values(*names):
-    import json
-    _all_values = json.loads("""{"num_samp":12,"p20_rate":1,"p1000_rate":1,"p20_mount":"left","p1000_mount":"right"}""")
-    return [_all_values[n] for n in names]
-
 
 def run(ctx):
     """Protocol."""
