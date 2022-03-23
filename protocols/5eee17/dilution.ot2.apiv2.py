2bcfa7164d2e44691653cacf9a43ce41357a5dd2
ncdiehl11 <ndiehl@opentrons.com>
2022-03-23 18:53:39 -0400

    initial commit

---
+++ protocols/5eee17/dilution.ot2.apiv2.py
@@ -0,0 +1,12 @@
+metadata = {
+    'protocolName': 'HPLC Dilution',
+    'author': 'Nick Diehl <ndiehl@opentrons.com>',
+    'source': 'Custom Protocol Request',
+    'apiLevel': '2.12'
+}
+
+
+def run(ctx):
+
+    num_samples = get_values(  # noqa: F821
+        )
