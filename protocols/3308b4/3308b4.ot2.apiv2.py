1747f672a821d4d8aca2f1adf157a59ad0519be8
John Lynch <johnlynch@WS-C02H60ZJQ05P.local>
2022-03-23 22:22:41 -0500

    setup almost done

---
+++ protocols/3308b4/3308b4.ot2.apiv2.py
@@ -0,0 +1,168 @@
+from opentrons import protocol_api
+
+metadata = {
+    'protocolName': 'Protocol Title',
+    'author': 'AUTHOR NAME <authoremail@company.com>',
+    'source': 'Custom Protocol Request',
+    'apiLevel': '2.11'   # CHECK IF YOUR API LEVEL HERE IS UP TO DATE
+                         # IN SECTION 5.2 OF THE APIV2 "VERSIONING"
+}
+
+
+def run(ctx: protocol_api.ProtocolContext):
+
+    [
+     tip_use_strategy
+    ] = get_values(  # noqa: F821 (<--- DO NOT REMOVE!)
+        "tip_use_strategy")
+
+    # define all custom variables above here with descriptions:
+
+    # number of samples
+
+    # "True" for park tips, "False" for discard tips
+
+    # load labware
+    spr_well = ctx.load_labware('nest_1_reservoir_195ml', '4')
+    vhb_well = ctx.load_labware('nest_1_reservoir_195ml', '5')
+    water_well = ctx.load_labware('nest_1_reservoir_195ml', '6')
+    spr_plate = ctx.load_labware('nest_96_wellplate_2ml_deep', '7')
+    vhb_plate = ctx.load_labware('nest_96_wellplate_2ml_deep', '8')
+    water_plate = ctx.load_labware('nest_96_wellplate_2ml_deep', '9')
+    '''
+
+    Add your labware here with:
+
+    labware_name = ctx.load_labware('{loadname}', '{slot number}')
+
+    If loading labware on a module, you can load with:
+
+    labware_name = module_name.load_labware('{loadname}')
+    where module_name is defined above.
+
+    '''
+
+    # load tipracks
+    tiprack = [
+        ctx.load_labware('opentrons_96_tiprack_300ul', slot)
+        for slot in ['1', '2', '3']
+        ]
+    '''
+
+    Add your tipracks here as a list:
+
+    For a single tip rack:
+
+    tiprack_name = [ctx.load_labware('{loadname}', '{slot number}')]
+
+    For multiple tip racks of the same type:
+
+    tiprack_name = [ctx.load_labware('{loadname}', 'slot')
+                     for slot in ['1', '2', '3']]
+
+    If two different tipracks are on the deck, use convention:
+    tiprack[number of microliters]
+    e.g. tiprack10, tiprack20, tiprack200, tiprack300, tiprack1000
+
+    '''
+
+    # load instrument
+    pipette = ctx.load_instrument(
+        'p300_multi_gen2', mount='left', tip_racks=tiprack)
+
+    # pipette functions   # INCLUDE ANY BINDING TO CLASS
+
+    '''
+
+    Define all pipette functions, and class extensions here.
+    These may include but are not limited to:
+
+    - Custom pickup functions
+    - Custom drop tip functions
+    - Custom Tip tracking functions
+    - Custom Trash tracking functions
+    - Slow tip withdrawal
+
+    For any functions in your protocol, describe the function as well as
+    describe the parameters which are to be passed in as a docstring below
+    the function (see below).
+
+    def pick_up(pipette):
+        """`pick_up()` will pause the protocol when all tip boxes are out of
+        tips, prompting the user to replace all tip racks. Once tipracks are
+        reset, the protocol will start picking up tips from the first tip
+        box as defined in the slot order when assigning the labware definition
+        for that tip box. `pick_up()` will track tips for both pipettes if
+        applicable.
+
+        :param pipette: The pipette desired to pick up tip
+        as definited earlier in the protocol (e.g. p300, m20).
+        """
+        try:
+            pipette.pick_up_tip()
+        except protocol_api.labware.OutOfTipsError:
+            ctx.pause("Replace empty tip racks")
+            pipette.reset_tipracks()
+            pipette.pick_up_tip()
+
+    '''
+
+    # helper functions
+    '''
+    Define any custom helper functions outside of the pipette scope here, using
+    the convention seen above.
+
+    e.g.
+
+    def remove_supernatant(vol, index):
+        """
+        function description
+
+        :param vol:
+
+        :param index:
+        """
+
+
+    '''
+
+    # reagents
+    spr = spr_well.wells()[0]
+    vhb = vhb_well.wells()[0]
+    water = water_well.wells()[0]
+
+    # plate, tube rack maps
+
+    '''
+    Define any plate or tube maps here.
+
+    e.g.
+
+    plate_wells_by_row = [well for row in plate.rows() for well in row]
+
+    '''
+
+    # protocol
+
+    '''
+
+    Include header sections as follows for each "section" of your protocol.
+
+    Section can be defined as a step in a bench protocol.
+
+    e.g.
+
+    ctx.comment('\n\nMOVING MASTERMIX TO SAMPLES IN COLUMNS 1-6\n')
+
+    for .... in ...:
+        ...
+        ...
+
+    ctx.comment('\n\nRUNNING THERMOCYCLER PROFILE\n')
+
+    ...
+    ...
+    ...
+
+
+    '''
