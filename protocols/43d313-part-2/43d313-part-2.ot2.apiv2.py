f81a1cb425f5b53a42cc9308a06a90b2acf91465
Steve Plonk <steveplonk@WS-00012.attlocal.net>
2022-04-22 12:13:31 -0400

    changes to 43d313-part-2 to split sample volume and use two 200 uL plates with magnetic module

--- protocols/43d313-part-2/43d313-part-2.ot2.apiv2.py
+++ protocols/43d313-part-2/43d313-part-2.ot2.apiv2.py
@@ -87,7 +87,8 @@ def run(ctx):
         "p300_multi_gen2", 'right', tip_racks=tips200)
 
     # reagents block for EDTA, RT Master Mix 1, RT Master Mix 2
-    reagents = ctx.load_labware(
+    # blockplate is twintec 200 uL plate on block by itself(use 3 mm clearance)
+    blockplate = reagents = ctx.load_labware(
      'opentrons_96_aluminumblock_generic_pcr_strip_200ul', '2', 'Reagents')
     [edta, rt_mm1, rt_mm2] = [reagents.columns()[index] for index in [0, 1, 2]]
 
@@ -102,14 +103,14 @@ def run(ctx):
     # reservoir for 80 percent ethanol
     etoh = ctx.load_labware(
      'nest_1_reservoir_195ml', '6', '80 percent etoh').wells()[0]
-    liq_volume([etoh], 144000)
+    liq_volume([etoh], 96000)
     waste = ctx.load_labware(
      'nest_1_reservoir_195ml', '4', '80 percent etoh').wells()[0]
 
     # input samples
     temp = ctx.load_module('temperature module gen2', '3')
     sampleplate200 = temp.load_labware(
-     'eppendorftwin.tec96_96_aluminumblock_200ul',
+     'eppendorf_twintec_on_opentrons_metal_block_033822',
      "Sample Plate at 4 Degrees")
     temp.set_temperature(4)
 
@@ -117,20 +118,25 @@ def run(ctx):
     mag = ctx.load_module('magnetic module gen2', '1')
     mag.disengage()
     mag_plate = mag.load_labware(
-     'corning_96_wellplate_600ul', 'Mag Plate')
+     'eppendorf_twintex_clickbio_adapter', 'Mag Plate')
 
     ctx.comment("STEP - EDTA")
     p20m.transfer(
-     0.8, edta,
+     0.8, edta[0],
      [column[0] for column in sampleplate200.columns()[:num_cols]],
      mix_after=(1, 10), new_tip='always')
 
-    ctx.comment("STEP - Transfer to larger plate")
+    ctx.comment("STEP - Transfer half volume to new 200 uL PCR plate")
+
+    ctx.pause("""Place fresh 200 uL twin tec plate on chilled aluminum block
+    in slot 2. Resume.""")
 
     for index, column in enumerate(sampleplate200.columns()[:num_cols]):
         p300m.pick_up_tip()
-        p300m.aspirate(80.8, column[0])
-        p300m.dispense(80.8, mag_plate.columns()[index][0])
+        # blockplate clearance 3 mm due to coded labware def (block+tubestrip)
+        p300m.transfer(
+         40.4, column[0], blockplate.columns()[index][0].bottom(3),
+         mix_before=(10, 65), new_tip='never')
         ctx.delay(seconds=0.5)
         p300m.blow_out()
         p300m.touch_tip(radius=0.75, v_offset=-2, speed=10)
@@ -144,30 +150,48 @@ def run(ctx):
 
     beadwell = beadwells()
     source = next(beadwell)
-    reps = math.ceil(240 / tips300.wells()[0].max_volume)
+    reps = math.ceil(120 / tips300.wells()[0].max_volume)
 
-    for column in mag_plate.columns()[:num_cols]:
+    # add beads to plate 1
+    for column in sampleplate200.columns()[:num_cols]:
         if source.liq_vol <= 1920:
             source = next(beadwell)
         for rep in range(reps):
             pick_up_or_refill(p300m, 300)
             p300m.aspirate(
-             240 / reps, source.bottom(clearance_reservoir), rate=0.5)
+             120 / reps, source.bottom(clearance_reservoir), rate=0.5)
             ctx.delay(seconds=1)
-            source.liq_vol -= (1920 / reps)
-            p300m.dispense(240 / reps, column[0])
-            if reps != 0:
-                p300m.mix(10, 200)
+            source.liq_vol -= (960 / reps)
+            p300m.dispense(120 / reps, column[0])
+            p300m.mix(10, 128)
             p300m.drop_tip()
 
+    ctx.delay(minutes=15)
+
     ctx.pause(
-        ''' Remove plate from magnetic module. Incubate 4 degree C at least
-        15 minutes. Return the plate to the magnetic module and resume.''')
+        '''Move plate1 (temperature module) to the magnetic module. Resume.''')
 
     mag.engage(height=height_engage)
-    ctx.delay(minutes=time_engage)
+    ctx.delay(minutes=time_engage,
+              msg='Move plate2 from slot 2 to the temperature module')
 
-    # remove supernatant
+    # add beads to plate 2
+    for column in sampleplate200.columns()[:num_cols]:
+        if source.liq_vol <= 1920:
+            source = next(beadwell)
+        for rep in range(reps):
+            pick_up_or_refill(p300m, 300)
+            p300m.aspirate(
+             120 / reps, source.bottom(clearance_reservoir), rate=0.5)
+            ctx.delay(seconds=1)
+            source.liq_vol -= (960 / reps)
+            p300m.dispense(120 / reps, column[0])
+            p300m.mix(10, 128)
+            p300m.drop_tip()
+
+    ctx.delay(minutes=15)  # this delay can probably be reduced or removed
+
+    # remove supernatant from plate 1
     for index, column in enumerate(mag_plate.columns()[:num_cols]):
         pick_up_or_refill(p300m)
         p300m.aspirate(200, column[0].bottom(4), rate=0.33)
@@ -187,11 +211,11 @@ def run(ctx):
         pick_up_or_refill(p300m, 300)
         for column in mag_plate.columns()[:num_cols]:
             for rep in range(2):
-                etoh.liq_vol -= 2000
+                etoh.liq_vol -= 1600
                 ht = liq_height(etoh) - 3 if liq_height(etoh) - 3 > 1 else 1
-                p300m.aspirate(250, etoh.bottom(ht))
+                p300m.aspirate(200, etoh.bottom(ht))
                 p300m.air_gap(20)
-                p300m.dispense(270, column[0].top())
+                p300m.dispense(220, column[0].top())
                 ctx.delay(seconds=0.5)
                 p300m.blow_out()
         p300m.drop_tip()
@@ -201,22 +225,21 @@ def run(ctx):
         # remove sup
         for index, column in enumerate(mag_plate.columns()[:num_cols]):
             pick_up_or_refill(p300m, 300)
-            for rep in range(2):
-                loc = column[0].bottom(4) if not rep == 1 else column[
-                 0].bottom(1).move(types.Point(x={True: 1}.get(
-                  not index % 2, -1)*offset_x, y=0, z=0))
-                p300m.aspirate(250, loc, rate=0.33)
-                p300m.air_gap(20)
-                p300m.dispense(270, waste.top())
-                ctx.delay(seconds=0.5)
-                p300m.blow_out()
-                p300m.air_gap(20)
+            p300m.aspirate(200, column[0].bottom(4), rate=0.33)
+            p300m.aspirate(75, column[
+             0].bottom(1).move(types.Point(x={True: 1}.get(
+              not index % 2, -1)*offset_x, y=0, z=0)), rate=0.33)
+            p300m.air_gap(20)
+            p300m.dispense(295, waste.top())
+            ctx.delay(seconds=0.5)
+            p300m.blow_out()
+            p300m.air_gap(20)
             p300m.drop_tip()
 
-    mag.disengage()
-
-    # air dry bead pellets
+    # air dry plate 1 bead pellets
     ctx.delay(minutes=time_dry)
+
+    mag.disengage()
 
     # resuspend bead pellet in Tris
     for index, column in enumerate(mag_plate.columns()[:num_cols]):
@@ -236,33 +259,142 @@ def run(ctx):
     ctx.delay(minutes=time_engage)
 
     ctx.pause(
-     '''Place a fresh 200 uL PCR plate on the temperature module. Resume.''')
+     '''Place a fresh 200 uL PCR plate and pre-chilled aluminum block
+     in slot 2. Resume.''')
+
+    # transfer plate 1 eluate to fresh PCR plate in slot 2
+    for index, column in enumerate(mag_plate.columns()[:num_cols]):
+        pick_up_or_refill(p20m)
+        p20m.aspirate(13, column[0].bottom().move(types.Point(
+          x={True: 1}.get(not index % 2, -1)*offset_x, y=0, z=0)))
+        # use 3 mm clearance for blockplate due to block+tubestrip labware def
+        p20m.dispense(13, blockplate.columns()[index][0].bottom(3))
+        p20m.move_to(
+         blockplate.columns()[index][0].top(-2).move(types.Point(
+          x=column[0].diameter / 2, y=0, z=0)))
+        p20m.blow_out()
+        p20m.move_to(blockplate.columns()[index][0].top(2))
+        p20m.drop_tip()
+
+    mag.disengage()
+
+    ctx.pause(
+     '''Remove plate1 (magnetic module) from the deck. Move plate2
+     (temperature module) to the magnetic module. Resume.''')
+
+    mag.engage(height=height_engage)
+    ctx.delay(minutes=time_engage)
+
+    # remove supernatant from plate 2
+    for index, column in enumerate(mag_plate.columns()[:num_cols]):
+        pick_up_or_refill(p300m)
+        p300m.aspirate(200, column[0].bottom(4), rate=0.33)
+        p300m.dispense(200, waste.top())
+        p300m.move_to(column[0].top())
+        p300m.air_gap(20)
+        p300m.aspirate(
+         50, column[0].bottom(1).move(types.Point(
+          x={True: 1}.get(not index % 2, -1)*offset_x, y=0, z=0)), rate=0.33)
+        p300m.dispense(70, waste.top(), rate=2)
+        ctx.delay(seconds=1)
+        p300m.blow_out()
+        p300m.drop_tip()
+
+    for repeat in range(2):
+        # add ethanol
+        pick_up_or_refill(p300m, 300)
+        for column in mag_plate.columns()[:num_cols]:
+            for rep in range(2):
+                etoh.liq_vol -= 1600
+                ht = liq_height(etoh) - 3 if liq_height(etoh) - 3 > 1 else 1
+                p300m.aspirate(200, etoh.bottom(ht))
+                p300m.air_gap(20)
+                p300m.dispense(220, column[0].top())
+                ctx.delay(seconds=0.5)
+                p300m.blow_out()
+        p300m.drop_tip()
+
+        ctx.delay(seconds=30)
+
+        # remove sup
+        for index, column in enumerate(mag_plate.columns()[:num_cols]):
+            pick_up_or_refill(p300m, 300)
+            p300m.aspirate(200, column[0].bottom(4), rate=0.33)
+            p300m.aspirate(75, column[
+             0].bottom(1).move(types.Point(x={True: 1}.get(
+              not index % 2, -1)*offset_x, y=0, z=0)), rate=0.33)
+            p300m.air_gap(20)
+            p300m.dispense(295, waste.top())
+            ctx.delay(seconds=0.5)
+            p300m.blow_out()
+            p300m.air_gap(20)
+            p300m.drop_tip()
+
+    # air dry plate 2 bead pellets
+    ctx.delay(minutes=time_dry)
+
+    mag.disengage()
+
+    # resuspend bead pellet in previous eluate
+    for index, column in enumerate(mag_plate.columns()[:num_cols]):
+        p20m.pick_up_tip()
+        # use 3 mm clearance for blockplate due to block+tubestrip labware def
+        p20m.aspirate(13, blockplate.columns()[index][0].bottom(3))
+        loc = column[0].bottom(clearance_bead_resuspension).move(types.Point(
+          x={True: -1}.get(not index % 2, 1)*offset_x_resuspension, y=0, z=0))
+        p20m.dispense(13, loc, rate=3)
+        for rep in range(10):
+            p20m.aspirate(13, column[0].bottom(1))
+            p20m.dispense(13, loc, rate=3)
+        p20m.drop_tip()
+
+    ctx.delay(
+     minutes=5, msg='''Place a fresh twin tec 200 uL PCR plate
+     on the temperature module''')
 
     # combine RT master mix 1 and eluted sample
     for column in sampleplate200.columns()[:num_cols]:
         pick_up_or_refill(p20m)
-        p20m.transfer(6, rt_mm1, column[0], new_tip='never')
+        p20m.transfer(6, rt_mm1[0], column[0], new_tip='never')
         p20m.drop_tip()
+
+    mag.engage(height=height_engage)
+    ctx.delay(minutes=time_engage)
 
     for index, column in enumerate(mag_plate.columns()[:num_cols]):
         pick_up_or_refill(p20m)
-        p20m.transfer(
-         11, column[0].bottom().move(types.Point(
-          x={True: 1}.get(not index % 2, -1)*offset_x, y=0, z=0)),
-         sampleplate200.columns()[index][0],
-         mix_after=(10, 15), new_tip='never')
+        p20m.aspirate(11, column[0].bottom().move(types.Point(
+          x={True: 1}.get(not index % 2, -1)*offset_x, y=0, z=0)))
+        p20m.dispense(11, sampleplate200.columns()[index][0])
+        for rep in range(10):
+            p20m.aspirate(13, sampleplate200.columns()[index][0])
+            p20m.dispense(13, sampleplate200.columns()[index][0])
+            if rep == 9:
+                p20m.move_to(sampleplate200.columns()[index][0].top(-2).move(
+                 types.Point(x=column[0].diameter / 2, y=0, z=0)))
+                p20m.blow_out()
+                p20m.move_to(sampleplate200.columns()[index][0].top())
         p20m.drop_tip()
 
     ctx.pause(
-     '''Remove the 200 uL PCR plate for off-deck cycler steps.
-     Return plate and resume.''')
+     '''Transfer the plate (temperature module) to the off-deck thermocycler
+     for fragmentation. Return plate to the temperature module. Resume.''')
 
     ctx.comment("STEP - cDNA Synthesis")
 
     # add RT master mix 2
     for column in sampleplate200.columns()[:num_cols]:
         pick_up_or_refill(p20m)
-        p20m.transfer(6, rt_mm2, column[0], new_tip='never')
+        p20m.aspirate(3, rt_mm2[0])
+        p20m.dispense(3, column[0])
+        for rep in range(10):
+            p20m.aspirate(16, column[0])
+            p20m.dispense(16, column[0])
+            if rep == 9:
+                p20m.move_to(column[0].top(-2).move(types.Point(
+                 x=column[0].diameter / 2, y=0, z=0)))
+                p20m.blow_out()
+                p20m.move_to(column[0].top())
         p20m.drop_tip()
 
     ctx.pause(
