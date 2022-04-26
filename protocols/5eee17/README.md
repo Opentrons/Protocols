10a5bc89c123598a025986d7a190637159172086
ncdiehl11 <ndiehl@opentrons.com>
2022-04-26 17:34:37 -0400

    5eee17

---
+++ protocols/5eee17/README.md
@@ -0,0 +1,60 @@
+# Custom Tuberack Dilutions
+
+### Author
+[Opentrons](https://opentrons.com/)
+
+## Categories
+* Sample Prep
+	* Dilution
+
+## Description
+
+This protocol performs a dilution on custom tuberacks with 2x P1000 pipettes in tandem. The deck layout is shown below:  
+![deck3d](https://opentrons-protocol-library-website.s3.amazonaws.com/custom-README-images/5eee17/deck3d.png)
+
+---
+
+### Labware
+* Custom 15ml, 50ml, and HPLC tuberacks
+* [Opentrons 1000µL Tips](https://shop.opentrons.com/opentrons-1000-l-tips/)
+
+### Pipettes
+* [2x Opentrons P1000 Single Channel Electronic Pipette (GEN2)](https://shop.opentrons.com/single-channel-electronic-pipette-p20/)
+
+---
+
+### Deck Setup
+![deck layout](https://opentrons-protocol-library-website.s3.amazonaws.com/custom-README-images/5eee17/deck2d.png)
+
+---
+
+### Protocol Steps
+1. Both pipettes pick up 1000µL tip
+2. Pipette 1 aspirates 700µL from tube at position 1 on rack 1 with air gap
+3. Pipette 2 aspirates 700µL from tube at position 2 on rack 1 with air gap
+4. Pipette 1 dispenses full volume into tube at position 1 on rack 3
+5. Pipette 1 mixes using 1000µL volume 5x
+6. Pipette 1 aspirates 800 µL from mixed dilution tube
+7. Pipette 1 dispenses full volume into vial at position 1 on HPLC rack at deck position 11
+8. Pipette 2 dispenses full volume into tube at position 2 on rack 3
+9. Pipette 2 mixes using 1000µL volume 5x
+10. Pipette 2 aspirates 800 µL from mixed dilution tube
+11. Pipette 2 dispenses full volume into vial at position 2 on HPLC rack at deck position 11
+12. Pipette 1&2 discard tips in waste tray
+13. Steps 1-12 are repeated for as many samples as specified, pausing for refills if more than 48 samples are input.
+
+
+### Process
+1. Input your protocol parameters above.
+2. Download your protocol and unzip if needed.
+3. Upload your custom labware to the [OT App](https://opentrons.com/ot-app) by navigating to `More` > `Custom Labware` > `Add Labware`, and selecting your labware files (.json extensions) if needed.
+4. Upload your protocol file (.py extension) to the [OT App](https://opentrons.com/ot-app) in the `Protocol` tab.
+5. Set up your deck according to the deck map.
+6. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support articles](https://support.opentrons.com/en/collections/1559720-guide-for-getting-started-with-the-ot-2).
+7. Hit 'Run'.
+
+### Additional Notes
+If you have any questions about this protocol, please contact the Protocol Development Team by filling out the [Troubleshooting Survey](https://protocol-troubleshooting.paperform.co/).
+
+###### Internal
+5eee17
--- protocols/5eee17/dilution.ot2.apiv2.py
+++ protocols/5eee17/dilution.ot2.apiv2.py
@@ -24,45 +24,61 @@ def run(ctx):
         for mount in ['left', 'right']]
     pips = [p1000l, p1000r]
 
-    # create proper order
-    num_sets = math.ceil(num_extracts/2)
-    sources = [
-        well for rack in tuberacks_50 for well in rack.wells()][:num_extracts]
-    source_sets = [
-        sources[i*2:(i+1)*2] if i < num_sets - 1 else sources[i*2:]
-        for i in range(num_sets)]
-    intermediates = [
-        well for col in tuberack_15.columns()
-        for well in col[:8]][:num_extracts]
-    intermediate_sets = [
-        intermediates[i*2:(i+1)*2]
-        if i < num_sets - 1 else intermediates[i*2:] for i in range(num_sets)]
-    finals = [
-        well for row in tuberack_hplc.rows()[::-1]
-        for well in row][:num_extracts]
-    final_sets = [
-        finals[i*2:(i+1)*2] if i < num_sets - 1 else finals[i*2:]
-        for i in range(num_sets)]
-
     def mix(pip, reps, vol, loc):
         for _ in range(reps):
             pip.aspirate(vol, loc.bottom(5))
             pip.dispense(vol, loc.center())
 
-    # dilute
-    for set_ind in range(num_sets):
-        [pip.pick_up_tip()
-         for pip in [p1000l, p1000r][:len(source_sets[set_ind])]]
+    # create proper order
+    max_extracts_per_refill = sum([len(rack.wells()) for rack in tuberacks_50])
+    num_refills = math.ceil(num_extracts/max_extracts_per_refill)
+    for refill_ind in range(num_refills):
+        if refill_ind < num_refills - 1:
+            num_extracts_refill = max_extracts_per_refill
+        else:
+            num_extracts_refill = num_extracts % 48 if num_extracts % 48 != 0 \
+                else 48
+        num_sets = math.ceil(num_extracts_refill/2)
+        sources = [
+            well for rack in tuberacks_50
+            for well in rack.wells()][:num_extracts_refill]
+        source_sets = [
+            sources[i*2:(i+1)*2] if i < num_sets - 1 else sources[i*2:]
+            for i in range(num_sets)]
+        intermediates = [
+            well for col in tuberack_15.columns()
+            for well in col[:8]][:num_extracts_refill]
+        intermediate_sets = [
+            intermediates[i*2:(i+1)*2]
+            if i < num_sets - 1 else intermediates[i*2:]
+            for i in range(num_sets)]
+        finals = [
+            well for row in tuberack_hplc.rows()[::-1]
+            for well in row][:num_extracts_refill]
+        final_sets = [
+            finals[i*2:(i+1)*2] if i < num_sets - 1 else finals[i*2:]
+            for i in range(num_sets)]
 
-        for pip, tube in zip(pips, source_sets[set_ind]):
-            pip.aspirate(700, tube.bottom(5))
-            pip.air_gap(100)
+        # dilute
+        for set_ind in range(num_sets):
+            [pip.pick_up_tip()
+             for pip in [p1000l, p1000r][:len(source_sets[set_ind])]]
 
-        for pip, i_tube, f_tube in zip(pips, intermediate_sets[set_ind],
-                                       final_sets[set_ind]):
-            pip.dispense(100, i_tube.top())
-            pip.dispense(pip.current_volume, i_tube.bottom(5))
-            mix(pip, 5, 1000, i_tube)
-            pip.transfer(800, i_tube.bottom(5), f_tube.bottom(5),
-                         new_tip='never')
-        [pip.drop_tip() for pip in [p1000l, p1000r] if pip.has_tip]
+            for pip, tube in zip(pips, source_sets[set_ind]):
+                pip.aspirate(700, tube.bottom(5))
+                pip.air_gap(100)
+
+            for pip, i_tube, f_tube in zip(pips, intermediate_sets[set_ind],
+                                           final_sets[set_ind]):
+                pip.dispense(100, i_tube.top())
+                pip.dispense(pip.current_volume, i_tube.bottom(5))
+                mix(pip, 5, 1000, i_tube)
+                pip.transfer(800, i_tube.bottom(5), f_tube.bottom(5),
+                             new_tip='never')
+            [pip.drop_tip() for pip in [p1000l, p1000r] if pip.has_tip]
+
+        ctx.comment(f'Extract set {refill_ind+1} out of {num_refills} \
+complete.')
+        if refill_ind < num_refills - 1:
+            ctx.pause(f'Please refill all tubes on the deck for the next \
+extract set ({refill_ind+2} out of {num_refills}).')
