6b3f14c07080d1b01524b31e9070b81a82ed310c
John Lynch <johnlynch@WS-C02H60ZJQ05P.local>
2022-05-06 13:18:46 -0500

    setup folder structure

---
+++ protocols/1464-natrix-separations-inc-milliporesigma-part2/ELISA_part2.ot2.apiv2.py
@@ -0,0 +1,207 @@
+from opentrons import protocol_api
+
+metadata = {
+    'protocolName': 'Protocol Title',
+    'author': 'AUTHOR NAME <authoremail@company.com>',
+    'source': 'Custom Protocol Request',
+    'apiLevel': '2.11'   # CHECK IF YOUR API LEVEL HERE IS UP TO DATE
+                         # IN SECTION 5.2 OF THE APIV2 "VERSIONING"
+}
+
+
+def run(ctx: protocol_api.ProtocolContext):
+
+    [
+     _custom_variable1,
+     _custom_variable2
+    ] = get_values(  # noqa: F821 (<--- DO NOT REMOVE!)
+        "_custom_variable1",
+        "_custom_variable2")
+
+    if not 1 <= _custom_variable1 <= 12:
+        raise Exception("Enter a value between 1-12")
+
+    # define all custom variables above here with descriptions:
+
+    # number of samples
+    custom_variable1 = _custom_variable1
+
+    # "True" for park tips, "False" for discard tips
+    custom_variable2 = _custom_variable2
+
+    # load modules
+
+    '''
+
+    Add your modules here with:
+
+    module_name = ctx.load_module('{module_loadname}', '{slot number}')
+
+    Note: if you are loading a thermocycler, you do not need to specify
+    a slot number - thermocyclers will always occupy slots 7, 8, 10, and 11.
+
+    For all other modules, you can load them on slots 1, 3, 4, 6, 7, 9, 10.
+
+    '''
+
+    # load labware
+
+    '''
+
+    Add your labware here with:
+
+    labware_name = ctx.load_labware('{loadname}', '{slot number}')
+
+    If loading labware on a module, you can load with:
+
+    labware_name = module_name.load_labware('{loadname}')
+    where module_name is defined above.
+
+    '''
+
+    # load tipracks
+
+    '''
+
+    Add your tipracks here as a list:
+
+    For a single tip rack:
+
+    tiprack_name = [ctx.load_labware('{loadname}', '{slot number}')]
+
+    For multiple tip racks of the same type:
+
+    tiprack_name = [ctx.load_labware('{loadname}', 'slot')
+                     for slot in ['1', '2', '3']]
+
+    If two different tipracks are on the deck, use convention:
+    tiprack[number of microliters]
+    e.g. tiprack10, tiprack20, tiprack200, tiprack300, tiprack1000
+
+    '''
+
+    # load instrument
+
+    '''
+    Nomenclature for pipette:
+
+    use 'p'  for single-channel, 'm' for multi-channel,
+    followed by number of microliters.
+
+    p20, p300, p1000 (single channel pipettes)
+    m20, m300 (multi-channel pipettes)
+
+    If loading pipette, load with:
+
+    ctx.load_instrument(
+                        '{pipette api load name}',
+                        pipette_mount ("left", or "right"),
+                        tip_racks=tiprack
+                        )
+    '''
+
+    # pipette functions   # INCLUDE ANY BINDING TO CLASS
+
+    '''
+
+    Define all pipette functions, and class extensions here.
+    These may include but are not limited to:
+
+    - Custom pickup functions
+    - Custom drop tip functions
+    - Custom Tip tracking functions
+    - Custom Trash tracking functions
+    - Slow tip withdrawal
+
+    For any functions in your protocol, describe the function as well as
+    describe the parameters which are to be passed in as a docstring below
+    the function (see below).
+
+    def pick_up(pipette):
+        """`pick_up()` will pause the protocol when all tip boxes are out of
+        tips, prompting the user to replace all tip racks. Once tipracks are
+        reset, the protocol will start picking up tips from the first tip
+        box as defined in the slot order when assigning the labware definition
+        for that tip box. `pick_up()` will track tips for both pipettes if
+        applicable.
+
+        :param pipette: The pipette desired to pick up tip
+        as definited earlier in the protocol (e.g. p300, m20).
+        """
+        try:
+            pipette.pick_up_tip()
+        except protocol_api.labware.OutOfTipsError:
+            ctx.pause("Replace empty tip racks")
+            pipette.reset_tipracks()
+            pipette.pick_up_tip()
+
+    '''
+
+    # helper functions
+    '''
+    Define any custom helper functions outside of the pipette scope here, using
+    the convention seen above.
+
+    e.g.
+
+    def remove_supernatant(vol, index):
+        """
+        function description
+
+        :param vol:
+
+        :param index:
+        """
+
+
+    '''
+
+    # reagents
+
+    '''
+    Define where all reagents are on the deck using the labware defined above.
+
+    e.g.
+
+    water = reservoir12.wells()[-1]
+    waste = reservoir.wells()[0]
+    samples = plate.rows()[0][0]
+    dnase = tuberack.wells_by_name()['A4']
+
+    '''
+
+    # plate, tube rack maps
+
+    '''
+    Define any plate or tube maps here.
+
+    e.g.
+
+    plate_wells_by_row = [well for row in plate.rows() for well in row]
+
+    '''
+
+    # protocol
+
+    '''
+
+    Include header sections as follows for each "section" of your protocol.
+
+    Section can be defined as a step in a bench protocol.
+
+    e.g.
+
+    ctx.comment('\n\nMOVING MASTERMIX TO SAMPLES IN COLUMNS 1-6\n')
+
+    for .... in ...:
+        ...
+        ...
+
+    ctx.comment('\n\nRUNNING THERMOCYCLER PROFILE\n')
+
+    ...
+    ...
+    ...
+
+
+    '''
