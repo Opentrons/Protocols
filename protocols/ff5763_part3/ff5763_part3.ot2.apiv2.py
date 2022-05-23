24e7fdf67a7fefeb638e217473b69390b6ca5018
John Lynch <johnlynch@WS-C02H60ZJQ05P.local>
2022-05-23 00:19:17 -0500

    part 3 skeleton good. Further polish onsite

--- protocols/ff5763_part3/ff5763_part3.ot2.apiv2.py
+++ protocols/ff5763_part3/ff5763_part3.ot2.apiv2.py
@@ -1,9 +1,10 @@
 """OPENTRONS."""
 import math
+from opentrons import types
 
 metadata = {
-    'protocolName': 'Protocol Title',
-    'author': 'AUTHOR NAME <authoremail@company.com>',
+    'protocolName': 'Illumina DNA Prep, Amplify Tagmented DNA',
+    'author': 'John C. Lynch <john.lynch@opentrons.com>',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.11'   # CHECK IF YOUR API LEVEL HERE IS UP TO DATE
                          # IN SECTION 5.2 OF THE APIV2 "VERSIONING"
@@ -22,23 +23,22 @@ def run(ctx):
     # number of samples
     num_cols = math.ceil(num_samples/8)
 
-    # "True" for park tips, "False" for discard tips
-
     # load modules/labware
+    """SAMPLE PLATE ON MAG MODULE"""
     temp_1 = ctx.load_module('tempdeck', '1')
     thermo_tubes = temp_1.load_labware('opentrons_96_aluminumblock_generic_pcr'
                                        '_strip_200ul')
-    sample_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt',
-                                    '2')
     mag_module = ctx.load_module('magnetic module gen2', '4')
+    sample_plate = mag_module.load_labware('nest_96_wellplate_'
+                                           '100ul_pcr_full_skirt')
     reagent_resv = ctx.load_labware('nest_12_reservoir_15ml', '5')
     liquid_trash = ctx.load_labware('nest_1_reservoir_195ml', '9')
 
     # load tipracks
     tiprack20 = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
-                 for slot in ['3']]
+                 for slot in ['3', '10']]
     tiprack200 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
-                  for slot in ['6']]
+                  for slot in ['6', '11']]
 
     # load instrument
     m20 = ctx.load_instrument('p20_multi_gen2', 'right', tip_racks=tiprack20)
@@ -50,20 +50,65 @@ def run(ctx):
     nf_water = thermo_tubes.rows()[0][1]
     tsb = thermo_tubes.rows()[0][2]
     sample_dest = sample_plate.rows()[0][:num_cols]
+    pcr_mix = reagent_resv.wells()[0]
+    index_adapters = reagent_resv.wells()[0]
+    # Constants
 
     # hard code variables
-
-    # protocol
+    vol_supernatant = 50
+    z_mod_value = 5
+    a_mod_value = 5
+    # Functions
 
     # Discard supernatant
+    mag_module.engage(height_from_base=10)
+    ctx.max_speeds['Z'] = 50
+    ctx.max_speeds['A'] = 50
+    num_times = 1
+    for source in sample_dest:
+        side = 1 if num_times % 2 == 0 else -1
+        m300.flow_rate.aspirate /= 5
+        ctx.max_speeds['Z'] /= z_mod_value
+        ctx.max_speeds['A'] /= a_mod_value
+        m300.pick_up_tip()
+        m300.aspirate(
+            vol_supernatant, source.bottom().move(types.Point(x=side,
+                                                              y=0, z=0.5)))
+        m300.dispense(vol_supernatant, liquid_trash.wells()[0])
+        m300.drop_tip()
+        m300.flow_rate.aspirate *= 5
+        ctx.max_speeds['Z'] *= z_mod_value
+        ctx.max_speeds['A'] *= a_mod_value
+        num_times += 1
+        print(side)
+    mag_module.disengage()
 
-    # Disengage mag
-
-    # Add 40ul Master mix (made off-deck w/10% overage)
+    # Add 40ul Master mix (EFW and NFW made off-deck w/10% overage), mix 10x
+    for dest in sample_dest:
+        m300.pick_up_tip()
+        m300.aspirate(40, pcr_mix)
+        m300.dispense(40, dest)
+        m300.mix(5, 40)
+        m300.drop_tip()
 
     # centrifuge off deck, 280 x g for 3 seconds
+    ctx.pause('Please centrifuge plate at 280 x g for 3 seconds, return to'
+              'magnetic module when done')
 
     # Add 10ul index adapters (made offdeck or supplied in 96 well plate)
+    for dest in sample_dest:
+        m20.pick_up_tip()
+        m20.aspirate(10, index_adapters)
+        m20.dispense(10, dest)
+        m20.drop_tip()
     # mix 10x at 40ul
+    for dest in sample_dest:
+        m300.pick_up_tip()
+        m300.mix(10, 40)
+        m300.drop_tip()
 
     # Move to off-deck thermo cycler
+    ctx.pause('Run complete, please move sample plate to off-deck thermocycler'
+              )
+    for c in ctx.commands():
+        print(c)
