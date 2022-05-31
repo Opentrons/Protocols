819edb9b1069a56897501c7355bc9988e491263a
John Lynch <johnlynch@WS-C02H60ZJQ05P.local>
2022-05-30 20:40:42 -0500

    onsite updates and labware changes

--- protocols/ff5763_part4/ff5763_part4.ot2.apiv2.py
+++ protocols/ff5763_part4/ff5763_part4.ot2.apiv2.py
@@ -28,12 +28,11 @@ def run(ctx):
     # load modules/labware
     temp_1 = ctx.load_module('tempdeck', '1')
     # will be custom_from_maurice
-    midi_plate_1 = ctx.load_labware('nest_96_wellplate_2ml_deep', '2')
+    midi_plate_1 = ctx.load_labware('customabnest_96_wellplate_200ul', '2')
     thermo_tubes = temp_1.load_labware('opentrons_96_aluminumblock_generic_pcr'
                                        '_strip_200ul')
     mag_module = ctx.load_module('magnetic module gen2', '4')
-    sample_plate = mag_module.load_labware('nest_96_wellplate_100ul_pcr_'
-                                           'full_skirt')
+    sample_plate = mag_module.load_labware('customabnest_96_wellplate_200ul')
     reagent_resv = ctx.load_labware('nest_12_reservoir_15ml', '5')
     liquid_trash = ctx.load_labware('nest_1_reservoir_195ml', '9')
 
@@ -57,33 +56,34 @@ def run(ctx):
     sample_dest_MIDI_1 = midi_plate_1.rows()[0][:num_cols]
 
     # hard code variables
-    vol_supernatant = 45
+    vol_supernatant = 50
     z_mod_value = 5
     a_mod_value = 5
+    supernatant_headspeed_modulator = 5
     # protocol
 
     # move supernatant to MIDI plate
 
-    mag_module.engage(height_from_base=10)
-    # is this delay needed here? Probs on part 3
+    mag_module.engage(height=10)
     ctx.delay(minutes=5)
     ctx.max_speeds['Z'] = 50
     ctx.max_speeds['A'] = 50
     num_times = 1
     for source, dest in zip(sample_dest_nest, sample_dest_MIDI_1):
         side = 1 if num_times % 2 == 0 else -1
-        m300.flow_rate.aspirate /= 5
-        ctx.max_speeds['Z'] /= z_mod_value
-        ctx.max_speeds['A'] /= a_mod_value
         m300.pick_up_tip()
+        m300.flow_rate.aspirate /= 5
+        ctx.max_speeds['Z'] /= supernatant_headspeed_modulator
+        ctx.max_speeds['A'] /= supernatant_headspeed_modulator
         m300.aspirate(
             vol_supernatant, source.bottom().move(types.Point(x=side,
                                                               y=0, z=0.5)))
-        m300.dispense(vol_supernatant, dest)
-        m300.drop_tip()
+        m300.move_to(source.top())
         m300.flow_rate.aspirate *= 5
-        ctx.max_speeds['Z'] *= z_mod_value
-        ctx.max_speeds['A'] *= a_mod_value
+        ctx.max_speeds['Z'] *= supernatant_headspeed_modulator
+        ctx.max_speeds['A'] *= supernatant_headspeed_modulator
+        m300.dispense(vol_supernatant, dest)
+        m300.return_tip()
         num_times += 1
         print(side)
     mag_module.disengage()
@@ -93,20 +93,19 @@ def run(ctx):
         m300.pick_up_tip()
         m300.aspirate(40, nf_water)
         m300.dispense(40, dest)
-        m300.drop_tip()
+        m300.return_tip()
     # add 45ul IPB to MIDI plate 1 and mix 10x
     for dest in sample_dest_MIDI_1:
         m300.pick_up_tip()
         m300.aspirate(45, ipb)
         m300.dispense(45, dest)
         m300.mix(10, 100)
-        m300.drop_tip()
+        m300.return_tip()
     # Incubate 5 minutes
-    ctx.delay(minutes=5)
-
-    ctx.comment('''First half of library cleanup completed. Please move MIDI'''
-                ''' plate in slot 2 to magnetic module and replace with'''
-                ''' fresh, final MIDI sample plate in slot 2''')
+        ctx.delay(minutes=5)
+    ctx.comment('''First half of library cleanup completed. Please move deep'''
+                ''' well plate in slot 2 to magnetic module and replace with'''
+                ''' fresh, final deep well sample plate in slot 2''')
 
     for c in ctx.commands():
         print(c)
