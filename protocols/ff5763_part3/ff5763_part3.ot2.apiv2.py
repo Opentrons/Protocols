e596ab3c20299adba05667d09c0fed6895cd8210
John Lynch <johnlynch@WS-C02H60ZJQ05P.local>
2022-06-01 21:51:49 -0500

    moving labware

--- protocols/ff5763_part3/ff5763_part3.ot2.apiv2.py
+++ protocols/ff5763_part3/ff5763_part3.ot2.apiv2.py
@@ -32,13 +32,13 @@ def run(ctx):
     mag_module = ctx.load_module('magnetic module gen2', '4')
     sample_plate = mag_module.load_labware('nest_96_wellplate_2ml_deep')
     reagent_resv = ctx.load_labware('nest_12_reservoir_15ml', '5')
-    liquid_trash = ctx.load_labware('nest_1_reservoir_195ml', '9')
+    liquid_trash = ctx.load_labware('nest_1_reservoir_195ml', '6')
 
     # load tipracks
     tiprack20 = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
-                 for slot in ['3', '10']]
+                 for slot in ['7', '8']]
     tiprack200 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
-                  for slot in ['6', '11']]
+                  for slot in ['9', '10']]
 
     # load instrument
     m20 = ctx.load_instrument('p20_multi_gen2', 'right', tip_racks=tiprack20)
