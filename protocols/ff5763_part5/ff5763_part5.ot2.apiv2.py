5ca2082443ba174395addf73173e26bed39e96df
John Lynch <johnlynch@WS-C02H60ZJQ05P.local>
2022-06-01 19:25:44 -0500

    WIP, part4 needs work changing this inevitably

--- protocols/ff5763_part5/ff5763_part5.ot2.apiv2.py
+++ protocols/ff5763_part5/ff5763_part5.ot2.apiv2.py
@@ -25,24 +25,21 @@ def run(ctx):
     # "True" for park tips, "False" for discard tips
 
     # load modules/labware
-    """DWP from Part4 is on magnetic module to start, moves to trash at"""
-    """ specified step then replaced with DWP plate in slot 2"""
     temp_1 = ctx.load_module('tempdeck', '1')
     thermo_tubes = temp_1.load_labware('opentrons_96_aluminumblock_generic_pcr'
                                        '_strip_200ul')
+    # can change to whatever plate works better for next off-deck step
+    final_plate = ctx.load_labware('customabnest_96_wellplate_200ul',
+                                   '2')
     mag_module = ctx.load_module('magnetic module gen2', '4')
-    # will be custom labware from Maurice
+    # may be custom labware from Maurice
     midi_plate_1 = mag_module.load_labware('nest_96_wellplate_2ml_deep')
-    midi_plate_2 = ctx.load_labware('nest_96_wellplate_2ml_deep', '2')
     reagent_resv = ctx.load_labware('nest_12_reservoir_15ml', '5')
     liquid_trash = ctx.load_labware('nest_1_reservoir_195ml', '9')
-    # can change to whatever plate works better for next off-deck step
-    final_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt',
-                                   '8')
 
     # load tipracks
     tiprack20 = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
-                 for slot in ['3']]
+                 for slot in ['3', '8']]
     tiprack200 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
                   for slot in ['6', '7', '10', '11']]
 
@@ -58,9 +55,8 @@ def run(ctx):
     ipb = thermo_tubes.rows()[0][6]
     rsb = thermo_tubes.rows()[0][8]
     etoh = reagent_resv.wells()[11]
-    sample_dest_MIDI_1_pos_1 = midi_plate_1.rows()[0][:num_cols]
-    sample_dest_MIDI_2_pos_1 = midi_plate_2.rows()[0][:num_cols]
-    sample_dest_MIDI_2_pos_2 = midi_plate_1.rows()[0][:num_cols]
+    source_midi_plate = midi_plate_1.rows()[0][:num_cols]
+    dest_midi_plate = midi_plate_1.rows()[0][6:6+num_cols]
     final_plate_dest = final_plate.rows()[0][:num_cols]
     # hard code variables
     vol_supernatant = 45
@@ -70,44 +66,57 @@ def run(ctx):
     # protocol
 
 
-# transfer 15ul IPB to each empty well in MIDI plate 2
+# transfer 15ul IPB to each empty well second half of MIDI plate
     m20.pick_up_tip()
-    for dest in sample_dest_MIDI_2_pos_1:
+    for dest in dest_midi_plate:
+        m20.flow_rate.aspirate /= 4
+        m20.flow_rate.dispense /= 4
+        m20.flow_rate.blow_out /= 4
         m20.aspirate(15, ipb)
+        m20.move_to(ipb.top(-2))
+        ctx.delay(seconds=2)
+        m20.touch_tip(v_offset=-2)
+        m20.move_to(ipb.top(-2))
+        m20.aspirate(3, ipb.top(-2))
+        m20.dispense(3, dest.top())
         m20.dispense(15, dest)
-    m20.return_tip()
-# Transfer 125ul supernatant from MIDI plate 1 to midi plate 2 with a 10x mix
+        m20.flow_rate.aspirate *= 4
+        m20.flow_rate.dispense *= 4
+        m20.flow_rate.blow_out *= 4
+    m20.drop_tip()
+# Transfer 125ul supernatant from left side to right side with a 10x mix
 
     mag_module.engage(height=MIDI_plate_mag_height)
     ctx.max_speeds['Z'] = 50
     ctx.max_speeds['A'] = 50
     num_times = 1
-    for source, dest in zip(sample_dest_MIDI_1_pos_1,
-                            sample_dest_MIDI_2_pos_1):
+    for source, dest in zip(source_midi_plate, dest_midi_plate):
         side = 1 if num_times % 2 == 0 else -1
         m300.flow_rate.aspirate /= 5
         m300.pick_up_tip()
+        m300.move_to(source.top())
         ctx.max_speeds['Z'] /= z_mod_value
         ctx.max_speeds['A'] /= a_mod_value
         m300.aspirate(
             125, source.bottom().move(types.Point(x=side,
                                                   y=0, z=0.5)))
+        m300.move_to(source.top(-2))
         ctx.max_speeds['Z'] *= z_mod_value
         ctx.max_speeds['A'] *= a_mod_value
+        ctx.delay(seconds=2)
+        m300.touch_tip(v_offset=-2)
+        m300.move_to(source.top(-2))
+        m300.aspirate(10, source.top(-2))
+        m300.dispense(10, dest.top(-2))
         m300.dispense(125, dest)
         m300.flow_rate.aspirate *= 5
         m300.mix(10, 120)
-        m300.return_tip()
+        m300.drop_tip()
         num_times += 1
         print(side)
     mag_module.disengage()
     # Incubate 5 minutes
     ctx.delay(minutes=5)
-# Move MIDI 2 to mag stand, toss MIDI plate 1
-    ctx.pause('''Please remove MIDI plate on the magnetic module from the'''
-              ''' robot and discard. Move the MIDI plate in slot 2, now full'''
-              ''' of liquid, to the magnetic module. Click resume'''
-              ''' when complete''')
     # Mag stand engage for 5 minutes
     # discard supernatant
     mag_module.engage(height=MIDI_plate_mag_height)
@@ -115,7 +124,7 @@ def run(ctx):
     ctx.max_speeds['Z'] = 50
     ctx.max_speeds['A'] = 50
     num_times = 1
-    for source in sample_dest_MIDI_2_pos_2:
+    for source in dest_midi_plate:
         side = 1 if num_times % 2 == 0 else -1
         m300.flow_rate.aspirate /= 5
         m300.pick_up_tip()
@@ -127,18 +136,18 @@ def run(ctx):
         ctx.max_speeds['Z'] *= z_mod_value
         ctx.max_speeds['A'] *= a_mod_value
         m300.dispense(45, liquid_trash.wells()[0])
-        m300.return_tip()
+        m300.drop_tip()
         m300.flow_rate.aspirate *= 5
         num_times += 1
         print(side)
     mag_module.disengage()
 # wash twice: Add 200ul EtOH (let's try 100), wait 30 secs, discard supernatant
     for _ in range(2):
-        for dest in sample_dest_MIDI_2_pos_2:
+        for dest in dest_midi_plate:
             m300.pick_up_tip()
             m300.aspirate(100, etoh)
             m300.dispense(100, dest)
-            m300.return_tip()
+            m300.drop_tip()
         ctx.delay(seconds=30)
         # discard supernatant
         mag_module.engage(height=MIDI_plate_mag_height)
@@ -146,7 +155,7 @@ def run(ctx):
         ctx.max_speeds['Z'] = 50
         ctx.max_speeds['A'] = 50
         num_times = 1
-        for source in sample_dest_MIDI_2_pos_2:
+        for source in dest_midi_plate:
             side = 1 if num_times % 2 == 0 else -1
             m300.flow_rate.aspirate /= 5
             m300.pick_up_tip()
@@ -158,7 +167,7 @@ def run(ctx):
             ctx.max_speeds['Z'] *= z_mod_value
             ctx.max_speeds['A'] *= a_mod_value
             m300.dispense(100, liquid_trash.wells()[0])
-            m300.return_tip()
+            m300.drop_tip()
             m300.flow_rate.aspirate *= 5
             num_times += 1
             print(side)
@@ -167,7 +176,7 @@ def run(ctx):
     ctx.max_speeds['Z'] = 50
     ctx.max_speeds['A'] = 50
     num_times = 1
-    for source in sample_dest_MIDI_2_pos_2:
+    for source in dest_midi_plate:
         side = 1 if num_times % 2 == 0 else -1
         m20.flow_rate.aspirate /= 5
         m20.pick_up_tip()
@@ -179,7 +188,7 @@ def run(ctx):
         ctx.max_speeds['Z'] *= z_mod_value
         ctx.max_speeds['A'] *= a_mod_value
         m20.dispense(100, liquid_trash.wells()[0])
-        m20.return_tip()
+        m20.drop_tip()
         m20.flow_rate.aspirate *= 5
         num_times += 1
         print(side)
@@ -188,12 +197,12 @@ def run(ctx):
 # disengage mag stand
     mag_module.disengage()
 # add 32ul RSB, mix to resuspend
-    for dest in sample_dest_MIDI_2_pos_2:
+    for dest in dest_midi_plate:
         m300.pick_up_tip()
         m300.aspirate(32, rsb)
         m300.dispense(32, dest)
         m300.mix(10, 40)
-        m300.return_tip()
+        m300.drop_tip()
 # Wait 2 minutes
     ctx.delay(minutes=2)
 # engage mag stand 2 minutes
@@ -202,7 +211,7 @@ def run(ctx):
     ctx.max_speeds['Z'] = 50
     ctx.max_speeds['A'] = 50
     num_times = 1
-    for source, dest in zip(sample_dest_MIDI_2_pos_2, final_plate_dest):
+    for source, dest in zip(dest_midi_plate, final_plate_dest):
         side = 1 if num_times % 2 == 0 else -1
         m300.flow_rate.aspirate /= 5
         m300.pick_up_tip()
@@ -214,7 +223,7 @@ def run(ctx):
         ctx.max_speeds['Z'] *= z_mod_value
         ctx.max_speeds['A'] *= a_mod_value
         m300.dispense(30, dest)
-        m300.return_tip()
+        m300.drop_tip()
         m300.flow_rate.aspirate *= 5
         num_times += 1
         print(side)
