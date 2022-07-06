0caf18f021bf764e7314620a5ab791ea97ea0439
John Lynch <johnlynch@WS-C02H60ZJQ05P.local>
2022-07-06 11:34:56 -0500

    readme updates, part 3 barcoding redone

--- protocols/onsite-ganda-3/pcr_prep_2.ot2.apiv2.py
+++ protocols/onsite-ganda-3/pcr_prep_2.ot2.apiv2.py
@@ -4,7 +4,7 @@ import threading
 from time import sleep
 
 metadata = {
-    'protocolName': 'rhAmpSeq Library Prep Part 1 - PCR Prep 1',
+    'protocolName': 'rhAmpSeq Library Prep Part 3 - PCR Prep 3',
     'author': 'Opentrons <protocols@opentrons.com>',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.11'   # CHECK IF YOUR API LEVEL HERE IS UP TO DATE
@@ -65,6 +65,8 @@ def run(ctx):
                                            '_100ul_pcr_full_skirt')
     reagent_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt',
                                      '2')
+    barcode_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt',
+                                     '4')
     # load tipracks
     tiprack20 = [ctx.load_labware('opentrons_96_filtertiprack_20ul',
                                   str(slot))
@@ -79,8 +81,9 @@ def run(ctx):
 
     # reagents
     library_mix = reagent_plate.rows()[0][0]
-    pcr_forward = reagent_plate.rows()[0][1]
-    pcr_reverse = reagent_plate.rows()[0][2]
+    # pcr_forward = reagent_plate.rows()[0][1]
+    # pcr_reverse = reagent_plate.rows()[0][2]
+    pcr_barcode = barcode_plate.rows()[0][:num_cols]
     # plate, tube rack maps
     sample_dest = sample_plate.rows()[0][:num_cols]
     # protocol
@@ -106,28 +109,50 @@ def run(ctx):
         del ctx.max_speeds['Z']
         m20.flow_rate.aspirate *= m20_speed_mod
         m20.flow_rate.dispense *= m20_speed_mod
+    # add barcode primers previously made from slot 4 plate
+    for s, d in zip(pcr_barcode, sample_dest):
+        m20.flow_rate.aspirate /= m20_speed_mod
+        m20.flow_rate.dispense /= m20_speed_mod
+        m20.pick_up_tip()
+        m20.aspirate(4, s)
+        m20.move_to(s.top(-2))
+        ctx.delay(seconds=2)
+        # m20.touch_tip(v_offset=-2)
+        # m20.move_to(reagent_source.top(-2))
+        # m20.aspirate(airgap_library, reagent_source.top())
+        # m20.dispense(airgap_library, dest.top())
+        m20.dispense(2, d)
+        m20.mix(1, 5, d)
+        ctx.max_speeds['A'] = 100
+        ctx.max_speeds['Z'] = 100
+        m20.drop_tip()
+        del ctx.max_speeds['A']
+        del ctx.max_speeds['Z']
+        m20.flow_rate.aspirate *= m20_speed_mod
+        m20.flow_rate.dispense *= m20_speed_mod
+
     # add forward, reverse primers, 2 uL each
-    for reagent_source in [pcr_forward, pcr_reverse]:
-        for dest in sample_dest:
-            m20.flow_rate.aspirate /= m20_speed_mod
-            m20.flow_rate.dispense /= m20_speed_mod
-            m20.pick_up_tip()
-            m20.aspirate(2, reagent_source)
-            m20.move_to(reagent_source.top(-2))
-            ctx.delay(seconds=2)
-            # m20.touch_tip(v_offset=-2)
-            # m20.move_to(reagent_source.top(-2))
-            # m20.aspirate(airgap_library, reagent_source.top())
-            # m20.dispense(airgap_library, dest.top())
-            m20.dispense(2, dest)
-            m20.mix(1, 5, dest)
-            ctx.max_speeds['A'] = 100
-            ctx.max_speeds['Z'] = 100
-            m20.drop_tip()
-            del ctx.max_speeds['A']
-            del ctx.max_speeds['Z']
-            m20.flow_rate.aspirate *= m20_speed_mod
-            m20.flow_rate.dispense *= m20_speed_mod
+    # for reagent_source in [pcr_forward, pcr_reverse]:
+        # for dest in sample_dest:
+        #     m20.flow_rate.aspirate /= m20_speed_mod
+        #     m20.flow_rate.dispense /= m20_speed_mod
+        #     m20.pick_up_tip()
+        #     m20.aspirate(2, reagent_source)
+        #     m20.move_to(reagent_source.top(-2))
+        #     ctx.delay(seconds=2)
+        #     # m20.touch_tip(v_offset=-2)
+        #     # m20.move_to(reagent_source.top(-2))
+        #     # m20.aspirate(airgap_library, reagent_source.top())
+        #     # m20.dispense(airgap_library, dest.top())
+        #     m20.dispense(2, dest)
+        #     m20.mix(1, 5, dest)
+        #     ctx.max_speeds['A'] = 100
+        #     ctx.max_speeds['Z'] = 100
+        #     m20.drop_tip()
+        #     del ctx.max_speeds['A']
+        #     del ctx.max_speeds['Z']
+        #     m20.flow_rate.aspirate *= m20_speed_mod
+        #     m20.flow_rate.dispense *= m20_speed_mod
 
     if flash:
         if not ctx._hw_manager.hardware.is_simulator:
