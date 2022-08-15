edb75a0132a8a58408d5222429116a68740024e0
ramifarawi <rami.farawi@opentrons.com>
2022-08-15 13:10:44 -0400

    fix

--- protocols/49b828/49b828.ot2.apiv2.py
+++ protocols/49b828/49b828.ot2.apiv2.py
@@ -16,7 +16,7 @@ def run(ctx: protocol_api.ProtocolContext):
      plate_96,
      plate_384,
      use_temp,
-     m20_mount,
+     m20_mount
      ] = get_values(  # noqa: F821
          "num_samp",
          "plate_96",
