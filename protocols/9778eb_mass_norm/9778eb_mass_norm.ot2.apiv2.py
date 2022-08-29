aa3722758a9e6f58cf62eb7cd25b250149b50e85
John C Lynch <john.lynch@opentrons.com>
2022-08-29 13:22:49 -0500

    readme needs work

--- protocols/9778eb_mass_norm/9778eb_mass_norm.ot2.apiv2.py
+++ protocols/9778eb_mass_norm/9778eb_mass_norm.ot2.apiv2.py
@@ -1,6 +1,5 @@
 """OPENTRONS."""
 import math
-import csv
 
 metadata = {
     'protocolName': 'Normalization',
@@ -11,48 +10,55 @@ metadata = {
 
 def run(ctx):
     """PROTOCOL."""
-    [num_samples, vol_water, conc_target,
+    [vol_water, src_plate, dst_plate,
      p20_mount, file_input] = get_values(  # noqa: F821
-        'num_samples', 'vol_water', 'conc_target', 'p20_mount', 'file_input')
+        'vol_water', 'src_plate', 'dst_plate', 'p20_mount', 'file_input')
 
-    source_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt',
-                                    '1')
-    dest_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt',
-                                  '2')
-    reagent_tubes = ctx.load_labware('opentrons_10_tuberack_falcon_4x50ml_'
-                                     '6x15ml_conical', '3')
+    source_plate = ctx.load_labware(src_plate, '1')
+    dest_plate = ctx.load_labware(dst_plate, '2')
+    reagent_tubes = ctx.load_labware('opentrons_6_tuberack_'
+                                     'falcon_50ml_conical', '4')
+
+    # parse
+    csv_rows = [val.strip() for val in file_input.split(',')]
+    header_removed = csv_rows[6:]
+    well_list = header_removed[::5]
+    sample_mass = [eval(i) for i in header_removed[1::5]]
+    sample_vol = [eval(i) for i in header_removed[2::5]]
+    final_mass = [eval(i) for i in header_removed[3::5]]
+    final_vol = [eval(i) for i in header_removed[4::5]]
+    start_conc = []
+    final_conc = []
+    bad_wells = []
+
+    for mass, vol in zip(sample_mass, sample_vol):
+        start_conc.append(mass/vol)
+
+    for mass, vol in zip(final_mass, final_vol):
+        final_conc.append(mass/vol)
+
+    transfer_vol = []
+    nfw_vol = []
+    for final, start, vol in zip(final_conc, start_conc, final_vol):
+        transfer_vol.append(vol*round(final/start, 1))
+    for s_vol, f_vol in zip(transfer_vol, final_vol):
+        nfw_vol.append(f_vol-s_vol)
+    lists = [well_list, sample_mass, sample_vol, final_mass, final_vol,
+             start_conc, final_conc, transfer_vol, nfw_vol]
+    # clean up bad wells from lists
+    for i, (start, final) in enumerate(zip(start_conc, final_conc)):
+        if start < final:
+            bad_wells.append(well_list[i])
+            for list in lists:
+                del list[i]
 
     # Reagents and Well Lists
 
     nfw_source = reagent_tubes.wells()[0]
-    source_wells = source_plate.wells()[:num_samples]
-    dest_wells = dest_plate.wells()[:num_samples]
-    # parse
-    data = [
-        [val.strip() for val in line.split(',')]
-        for line in file_input.splitlines()[1:]
-        if line and line.split(',')[0].strip()]
 
     tips20 = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
-              for slot in ['7', '10'][:math.ceil(len(data)/48)]]
+              for slot in ['3', '6'][:math.ceil(len(well_list)/48)]]
     p20 = ctx.load_instrument('p20_single_gen2', p20_mount, tip_racks=tips20)
-
-    sample_mass = []
-    sample_vol = []
-    start_conc = []
-    # doing it by nmols adds one extra calc and field if starting w/weight
-    for mass, vol in zip(sample_mass, sample_vol):
-        start_conc.append(mass/vol)
-    # final conc can be calculated, set by variable, or imported via CSV
-    # depending on what client says
-    final_conc = 1  # ngrams/uL
-    tot_vol = 30
-    transfer_vol = []
-    nfw_vol = []
-    for final, start in zip(final_conc, start_conc):
-        transfer_vol.append(round(final/start, 1))
-    for vol in transfer_vol:
-        nfw_vol.append(tot_vol-vol)
 
     # liquid height tracking
     v_naught_dil = vol_water*1000
@@ -69,18 +75,22 @@ def run(ctx):
 
     # do NFW addition first to save tips, mix after sample addition
     p20.pick_up_tip()
-    for nfw, d in zip(nfw_vol, dest_wells):
-        p20.transfer(nfw, nfw_source.bottom(h), d)
+    for nfw, d in zip(nfw_vol, well_list):
+        p20.transfer(nfw, nfw_source.bottom(h), dest_plate[d],
+                     new_tip='never')
         adjust_height(nfw)
     p20.drop_tip()
 
-    for t_vol, s, d in (transfer_vol, source_wells, dest_wells):
+    for t_vol, well in zip(transfer_vol, well_list):
         p20.pick_up_tip()
-        p20.transfer(t_vol, s, d)
-        p20.mix(tot_vol/2, d)
+        p20.transfer(t_vol, source_plate.wells_by_name()[well],
+                     dest_plate.wells_by_name()[well], new_tip='never')
+        p20.mix(4, f_vol/2, dest_plate.wells_by_name()[well])
         p20.drop_tip()
 
     # bad_list = [well.display_name.split(' ')[0] for well in bad_wells]
-    # if len(bad_list) > 0:
-    #     bad_msg = '\n\n'.join(bad_list)
-    #     ctx.comment(f'The following sample wells failed: \n\n{bad_msg}')
+    # print(lists)
+
+    if len(bad_wells) > 0:
+        bad_msg = '\n\n'.join(bad_wells)
+        ctx.comment(f'The following sample wells failed: \n\n{bad_msg}')
