870df57c95f43cf7b02109a14f4605d9f2f53413
Chaz <chaz@opentrons.com>
2022-10-20 11:56:22 -0400

    noqa

--- protocols/7855ef/7855ef.ot2.apiv2.py
+++ protocols/7855ef/7855ef.ot2.apiv2.py
@@ -16,10 +16,7 @@ def run(protocol):
     if not 1 <= num_samp <= 384:
         raise Exception("Enter a sample number between 1-384")
 
-    metadata = {
-        'protocolName': f'[{num_samp}/384] Agriseq Library Prep (1/4)\
-         DNA transfer'
-    }
+    metadata = {'protocolName': f'[{num_samp}/384] Agriseq Library Prep (1/4) DNA transfer'}  # flake8: noqa
 
     num_col = math.ceil(num_samp/8)
     num_plate = math.ceil(num_col/12)
