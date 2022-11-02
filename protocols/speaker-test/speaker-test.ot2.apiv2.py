3c8b2a5969df68e242735404b82b34c2c15cad98
Chaz <chaz@opentrons.com>
2022-11-02 17:44:00 -0400

    fix: linter errors

--- protocols/speaker-test/speaker-test.ot2.apiv2.py
+++ protocols/speaker-test/speaker-test.ot2.apiv2.py
@@ -1,15 +1,18 @@
 import subprocess
-from opentrons import protocol_api, types
+from opentrons import protocol_api
+
 metadata = {
-    'protocolName': 'Speaker Test',
+    'protocolName': 'OT-2 Speaker Test',
     'author': 'Parrish Payne <protocols@opentrons.com>',
     'description': 'Tests out the speaker system on the OT-2',
     'apiLevel': '2.11'
 }
 
 AUDIO_FILE_PATH = '/etc/audio/speaker-test.mp3'
+
 def run_quiet_process(command):
     subprocess.check_output('{} &> /dev/null'.format(command), shell=True)
+
 def test_speaker(protocol):
     print('Speaker')
     print('Next\t--> CTRL-C')
@@ -26,7 +29,7 @@ def test_speaker(protocol):
 def run(protocol: protocol_api.ProtocolContext):
     [pip, mnt, tips] = get_values(  # noqa: F821
       'pip', 'mnt', 'tips')
-    
+
     tr2 = protocol.load_labware(tips, '1')
     pipette = protocol.load_instrument(pip, mnt, tip_racks=[tr2])
     pipette.pick_up_tip()
