110875af0017c60d93fcb2cc04cb07081f08dcec
ncdiehl11 <ndiehl@opentrons.com>
2023-01-10 12:09:21 -0500

    overhaul to fix per troubleshooting form

--- protocols/059126/059126.ot2.apiv2.py
+++ protocols/059126/059126.ot2.apiv2.py
@@ -1,5 +1,6 @@
 """OPENTRONS."""
 from opentrons import protocol_api
+from opentrons.types import Point
 
 metadata = {
     'protocolName': 'Reagent Addition',
@@ -16,7 +17,7 @@ def run(ctx: protocol_api.ProtocolContext):
      starting_tip_col,
      m20_mount,
      file_input
-    ] = get_values(  # noqa: F821 (<--- DO NOT REMOVE!)
+    ] = get_values(  # noqa: F821
         "num_384_cols",
         "starting_tip_col",
         "m20_mount",
@@ -29,74 +30,79 @@ def run(ctx: protocol_api.ProtocolContext):
         m300_mount = 'right'
 
     # load labware
-
     plate_96 = ctx.load_labware('greiner_96_wellplate_340ul', '1')
     plate_384 = ctx.load_labware('greiner_384_wellplate_50ul', '2')
     vials = ctx.load_labware('cytiva_24_wellplate_2000ul', '7')
-
-    # load tipracks
     tiprack_300 = [ctx.load_labware('opentrons_96_tiprack_300ul', slot)
-                   for slot in '8']
+                   for slot in ['8']]
     tiprack_20 = [ctx.load_labware('opentrons_96_tiprack_20ul', slot)
-                  for slot in '9']
+                  for slot in ['9']]
 
-    # List Creation
-    csv_1 = file_input.split('\\n')
-    csv_2 = [val.split(',') for val in csv_1]
-    header_removed = csv_2[1:]
-    flattened_list = [item for sublist in header_removed
-                      for item in sublist]
-    well_list = flattened_list[::3]
-    vol_trans_96 = [eval(i) for i in flattened_list[1::3]]
-    vol_trans_384 = [eval(i) for i in flattened_list[2::3]]
-
-    # Tip Logic
-    starting_tip = 8*starting_tip_col
-    final_tip = starting_tip-len(well_list)
-    single_tip_loc = tiprack_300[0].wells()[final_tip:starting_tip][::-1]
-    if final_tip < 0:
-        raise Exception('Invalid starting tip column for specified number of'
-                        ' vials. Not enough tips.')
-
-    # load instrument
-    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount,
-                              tip_racks=tiprack_20)
+    # load pipettes
     m300 = ctx.load_instrument('p300_multi_gen2', m300_mount,
                                tip_racks=tiprack_300)
+    m20 = ctx.load_instrument('p20_multi_gen2', m20_mount,
+                              tip_racks=tiprack_20)
 
-    # reagents
-    row_a_384 = plate_384.rows()[0][:num_384_cols]
-    row_b_384 = plate_384.rows()[1][:num_384_cols]
-    vial_list = []
-    vial_list.append(well_list)
-    plate_96_cols = plate_96.rows()[0][:len(well_list)]
-    rows_384 = [row_a_384, row_b_384]
+    # parse .csv data
+    data = [
+        [val.strip() for val in line.split(',')]
+        for line in file_input.splitlines()[1:]]
 
-    pre_air_gap = 5
-    # BEGIN PROTOCOL
+    tips_single_300 = [
+        tip
+        for rack in tiprack_300
+        for col in rack.columns()[::-1]
+        for tip in col[::-1]]
+    starting_tip_300 = tiprack_300[0].columns()[starting_tip_col-1][-1]
+    tips_single_300 = tips_single_300[tips_single_300.index(starting_tip_300):]
 
-    # Transfer to 96 Plate
-    ctx.comment('\n\n~~~~~~~~~~~~~~ADDING TO 96 WELL PLATE~~~~~~~~~~~~~~\n\n')
-    for i, (tip, well, vol) in enumerate(zip(single_tip_loc,
-                                         vial_list[0], vol_trans_96)):
-        m300.pick_up_tip(tip)
-        for dest in plate_96.columns()[i][:8]:
-            m300.aspirate(vol, vials.wells_by_name()[well], rate=0.5)
-            m300.dispense(vol, dest, rate=0.5)
+    counter_300 = 0
+
+    def pick_up_300(mode='single'):
+        nonlocal counter_300
+        if mode == 'single':
+            m300.pick_up_tip(tips_single_300[counter_300])
+            counter_300 += 1
+        else:
+            m300.pick_up_tip()
+
+    def wick(pip, well, side=1):
+        pip.move_to(well.bottom().move(Point(x=side*well.diameter/2*0.8, z=3)))
+
+    def slow_withdraw(well, pip=m300):
+        ctx.max_speeds['A'] = 25
+        ctx.max_speeds['Z'] = 25
+        pip.move_to(well.top())
+        del ctx.max_speeds['A']
+        del ctx.max_speeds['Z']
+
+    # transfer vial to 96-well plate
+    for i, line in enumerate(data):
+        source_vial = vials.wells_by_name()[line[0]]
+        vol96 = float(line[1])
+        pick_up_300('single')
+        dest_col = plate_96.columns()[i]
+        for well in dest_col:
+            m300.aspirate(vol96, source_vial)
+            slow_withdraw(source_vial, m300)
+            m300.dispense(vol96, well.bottom(1.5))
+            slow_withdraw(well, m300)
         m300.drop_tip()
 
-    # Transfer to 384 Well Plate
-    ctx.comment('\n\n~~~~~~~~~~~~~~ADDING TO 384 WELL PLATE~~~~~~~~~~~~~~\n\n')
-    for src, vol in zip(plate_96_cols, vol_trans_384):
+    # transfer 96-well plate to 384-well plate
+    dests_384 = [
+        well for col in plate_384.columns()[:num_384_cols]
+        for well in col[:2]]
+
+    for i, line in enumerate(data):
+        source_column = plate_96.rows()[0][i]
+        vol384 = float(line[2])
         m20.pick_up_tip()
-        for plate_384 in rows_384:
-            for i, dest in enumerate(plate_384):
-                if i == 0:
-                    m20.aspirate(pre_air_gap, src.top())
-                m20.aspirate(vol, src, rate=0.5)
-                m20.dispense(vol+pre_air_gap, dest.top(), rate=2)
-                m20.aspirate(pre_air_gap, src.top())
+        for d in dests_384:
+            m20.aspirate(vol384, source_column)
+            slow_withdraw(source_column, m20)
+            m20.dispense(vol384, d.bottom(0.5))
+            wick(pip=m20, well=d)
         m20.drop_tip()
 
-    for c in ctx.commands():
-        print(c)
