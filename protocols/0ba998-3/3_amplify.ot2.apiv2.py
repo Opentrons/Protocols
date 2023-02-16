3f8c2e8ecb80cd7e1810d53665d690cc7ab96f8b
ncdiehl11 <ndiehl@opentrons.com>
2023-02-16 13:39:15 -0500

    slow initial supernatant removal 4x

--- protocols/0ba998-3/3_amplify.ot2.apiv2.py
+++ protocols/0ba998-3/3_amplify.ot2.apiv2.py
@@ -104,7 +104,7 @@ resuming.\n\n\n\n")
         nonlocal parked_tips
         if not pip:
             pip = m300 if vol >= 20 else m20
-        pip.flow_rate.aspirate /= 5
+        pip.flow_rate.aspirate /= 20
         for i, s in enumerate(mag_samples):
             if not pip.has_tip:
                 if park:
@@ -127,7 +127,7 @@ resuming.\n\n\n\n")
             else:
                 pip.drop_tip()
         parked_tips = []
-        pip.flow_rate.aspirate *= 5
+        pip.flow_rate.aspirate *= 20
 
     def resuspend(location, reps=reps_mix, vol=vol_mix,
                   samples=mag_samples, x_mix_fraction=radial_offset_fraction,
