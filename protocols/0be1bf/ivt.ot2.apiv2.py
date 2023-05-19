c135c9e714f223e10f0f8c7ffd3c830f87ad3337
ncdiehl11 <ndiehl@opentrons.com>
2023-05-19 13:56:32 -0400

    allow for separate template concentrations

--- protocols/0be1bf/ivt.ot2.apiv2.py
+++ protocols/0be1bf/ivt.ot2.apiv2.py
@@ -1,3 +1,4 @@
+from opentrons.types import Point
 import math
 
 metadata = {
@@ -13,13 +14,17 @@ concentration_target = 0.1
 
 def run(ctx):
 
-    [num_rxns, concentration_template] = get_values(  # noqa: F821
-        'num_rxns', 'concentration_template')
+    [num_rxns, num_templates,
+     concentration_template] = get_values(  # noqa: F821
+        'num_rxns', 'num_templates', 'concentration_template')
 
     # labware
-    mix_rack = ctx.load_labware(
+    rack1 = ctx.load_labware(
         'opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap',
-        '4', 'component rack')
+        '4', 'component rack 1')
+    rack2 = ctx.load_labware(
+        'opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap',
+        '7', 'component rack 2')
     rack15 = ctx.load_labware(
         'opentrons_15_tuberack_5000ul', '8', 'mix tube')
     aliquot_rack = ctx.load_labware(
@@ -29,19 +34,35 @@ def run(ctx):
     tiprack_20 = [ctx.load_labware('opentrons_96_tiprack_20ul', '1')]
     tiprack_300 = [ctx.load_labware('opentrons_96_tiprack_300ul', '2')]
 
-    template_vol = round(rxn_vol*concentration_target/concentration_template,
-                         2)
+    template_concs = [
+        float(conc)
+        for conc in concentration_template.split(',') if conc.strip()]
+    template_volumes = [
+        round(rxn_vol*concentration_target/conc, 2)
+        for conc in template_concs]
+
+    if not len(template_concs) == num_templates:
+        ctx.pause(f'Number of templates entered ({num_templates}) \
+does not match number of concentrations entered ({len(template_concs)}). \
+Continue?')
+
     mix_volumes = [
-        20.0, 1.0, 35.0, 35.0, 35.0, 35.0, 20.0, 16.0, 2.5, 5.0, template_vol]
+        20.0, 1.0, 35.0, 35.0, 35.0, 35.0, 20.0, 16.0, 2.5, 5.0]
     mix_map = {
         well: vol
-        for well, vol in zip(mix_rack.wells()[:len(mix_volumes)], mix_volumes)
+        for well, vol in zip(rack1.wells()[:len(mix_volumes)], mix_volumes)
     }
-    enzyme_volumes = [25, 0.1]
+    template_map = {
+        well: vol
+        for well, vol in zip(rack2.wells()[:len(template_volumes)],
+                             template_volumes)
+    }
+    enzyme_volumes = [0.1, 25]
     enzyme_map = {
         well: vol
         for well, vol in zip(
-            mix_rack.wells()[len(mix_volumes):len(mix_volumes)+2],
+            rack2.wells()[len(template_volumes):
+                          len(template_volumes)+len(enzyme_volumes)],
             enzyme_volumes)
     }
 
@@ -67,7 +88,7 @@ def run(ctx):
         '#ee06d2',
         '#9ffd59']
 
-    for letter, (well, vol) in zip('ABCDEFGHIJK', mix_map.items()):
+    for letter, (well, vol) in zip('ABCDEFGHIJ', mix_map.items()):
 
         temp = ctx.define_liquid(
             name=letter,
@@ -76,7 +97,17 @@ def run(ctx):
         )
         well.load_liquid(temp, vol*num_rxns*1.1)
 
-    for letter, (well, vol) in zip(['TP', 'P'], enzyme_map.items()):
+    template_color = colors.pop()
+    for i, (well, vol) in enumerate(template_map.items()):
+
+        temp = ctx.define_liquid(
+            name=f'DNA template {i+1}',
+            description='',
+            display_color=template_color
+        )
+        well.load_liquid(temp, vol*1.1)
+
+    for letter, (well, vol) in zip(['P', 'TP'], enzyme_map.items()):
 
         temp = ctx.define_liquid(
             name=letter,
@@ -86,9 +117,9 @@ def run(ctx):
         well.load_liquid(temp, vol*num_rxns*1.1)
 
     mix_tube = rack15.wells()[0]
-    enzyme_mix_tube = mix_rack.rows()[0][-1]
-    dn = mix_rack.wells()[len(mix_volumes)+2]
-    cac = mix_rack.wells()[len(mix_volumes)+3]
+    enzyme_mix_tube = rack2.rows()[0][-1]
+    dn = rack2.wells()[len(template_volumes)+len(enzyme_volumes)]
+    cac = rack2.wells()[len(template_volumes)+len(enzyme_volumes)+1]
     rxns = aliquot_rack.wells()[:num_rxns]
     water = reservoir.wells()[0]
     licl_h2o = reservoir.wells()[1]
@@ -133,10 +164,12 @@ def run(ctx):
     )
     cac.load_liquid(cac_liq, vol_cac)
 
-    vol_water = rxn_vol - (sum(mix_volumes) + sum(enzyme_volumes))
+    vols_water = [
+        rxn_vol - (sum(mix_volumes) + sum(enzyme_volumes) + template_vol)
+        for template_vol in template_volumes]
     vol_licl_h2o = (rxn_vol + sum([vol_dn, vol_cac]))*2
 
-    water.load_liquid(water_liq, vol_water)
+    water.load_liquid(water_liq, sum(vols_water))
     licl_h2o.load_liquid(licl_h2o_liq, vol_licl_h2o)
 
     # pipettes
@@ -144,6 +177,12 @@ def run(ctx):
         'p300_single_gen2', 'left', tip_racks=tiprack_300)
     p20 = ctx.load_instrument(
         'p20_single_gen2', 'right', tip_racks=tiprack_20)
+
+    def wick(pip, well, side=1):
+        pip.default_speed /= 5
+        radius = well.diameter/2 if well.diameter else well.length/2
+        pip.move_to(well.bottom().move(Point(x=side*radius*0.7, z=3)))
+        pip.default_speed *= 5
 
     def slow_withdraw(pip, well, delay_seconds=1.0):
         pip.default_speed /= 10
@@ -174,36 +213,22 @@ def run(ctx):
             slow_withdraw(pip, mix_tube)
         pip.drop_tip()
 
-    # enzyme mix
-    for i, (well, vol) in enumerate(enzyme_map.items()):
-        transfer_vol = vol*num_rxns*factor_overage
-        pip = p300 if transfer_vol > 20 else p20
-        num_trans = math.ceil(
-            transfer_vol/pip.tip_racks[0].wells()[0].max_volume)
-        vol_per_trans = round(transfer_vol/num_trans, 2)
-        pip.pick_up_tip()
-        for _ in range(num_trans):
-            pip.aspirate(vol_per_trans, well.bottom(1.0))
-            slow_withdraw(pip, well)
-            pip.dispense(pip.current_volume, enzyme_mix_tube.bottom(3))
-            slow_withdraw(pip, enzyme_mix_tube)
-        if i == len(enzyme_map.items()) - 1:
-            pip.mix(5, pip.max_volume*0.8, enzyme_mix_tube)
-            slow_withdraw(pip, enzyme_mix_tube)
-        pip.drop_tip()
-
     # water addition
-    pip = p300 if vol_water >= 20 else p20
-    num_trans = math.ceil(vol_water/pip.tip_racks[0].wells()[0].max_volume)
-    vol_per_trans = round(vol_water/num_trans, 2)
-    pip.pick_up_tip()
-    for d in rxns:
+    for vol_water, d in zip(vols_water,
+                            aliquot_rack.wells()[:len(template_volumes)]):
+        pip = p300 if vol_water >= 20 else p20
+        num_trans = math.ceil(vol_water/pip.tip_racks[0].wells()[0].max_volume)
+        vol_per_trans = round(vol_water/num_trans, 2)
+        if not pip.has_tip:
+            pip.pick_up_tip()
         for _ in range(num_trans):
             pip.aspirate(vol_per_trans, water)
             slow_withdraw(pip, water)
             pip.dispense(pip.current_volume, d.bottom(2))
             slow_withdraw(pip, d)
-    pip.drop_tip()
+    for pip in [p20, p300]:
+        if pip.has_tip:
+            pip.drop_tip()
 
     # mix addition
     vol_mix = sum(mix_volumes)
@@ -219,7 +244,42 @@ def run(ctx):
             slow_withdraw(pip, d)
         pip.drop_tip()
 
+    # DNA template addition
+    for (template, vol), d in zip(
+            template_map.items(), 
+            aliquot_rack.wells()[:len(template_volumes)]):
+        pip = p300 if vol >= 20 else p20
+        num_trans = math.ceil(vol/pip.tip_racks[0].wells()[0].max_volume)
+        vol_per_trans = round(vol/num_trans, 2)
+        pip.pick_up_tip()
+        for _ in range(num_trans):
+            pip.aspirate(vol_per_trans, template)
+            slow_withdraw(pip, template)
+            pip.dispense(pip.current_volume, d.bottom(2))
+            slow_withdraw(pip, d)
+        pip.drop_tip()
+
     ctx.pause('Place enzymes in tuberack.')
+
+    # enzyme mix
+    for i, (well, vol) in enumerate(enzyme_map.items()):
+        transfer_vol = vol*num_rxns*factor_overage
+        pip = p300 if transfer_vol > 20 else p20
+        num_trans = math.ceil(
+            transfer_vol/pip.tip_racks[0].wells()[0].max_volume)
+        vol_per_trans = round(transfer_vol/num_trans, 2)
+        pip.pick_up_tip()
+        for _ in range(num_trans):
+            pip.aspirate(vol_per_trans, well.bottom(1.0))
+            slow_withdraw(pip, well)
+            pip.dispense(pip.current_volume, enzyme_mix_tube.bottom(1.5))
+            if i == 0:
+                wick(pip, enzyme_mix_tube)
+            slow_withdraw(pip, enzyme_mix_tube)
+        if i == len(enzyme_map.items()) - 1:
+            pip.mix(5, pip.max_volume*0.8, enzyme_mix_tube)
+            slow_withdraw(pip, enzyme_mix_tube)
+        pip.drop_tip()
 
     # enzyme mix addition
     vol_enzyme_mix = sum(enzyme_volumes)
