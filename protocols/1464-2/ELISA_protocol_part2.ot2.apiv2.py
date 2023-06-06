f17e04e58bc2b23c479a55c67ac01afc92472a64
ncdiehl11 <ndiehl@opentrons.com>
2023-06-06 12:33:57 -0400

    1464

---
+++ protocols/1464-2/ELISA_protocol_part2.ot2.apiv2.py
@@ -0,0 +1,99 @@
+import math
+
+metadata = {
+    'protocolName': 'ELISA',
+    'author': 'Alise <protocols@opentrons.com>',
+    'source': 'Custom Protocol Request',
+    'apiLevel': '2.13'
+}
+
+
+def run(ctx):
+    [number_of_standards, concentration_csv] = get_values(  # noqa: F821
+     'number_of_standards', 'concentration_csv')
+
+    # labware setup
+    tuberack_4 = ctx.load_labware(
+        'opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap', '4')
+    deep_plates = [ctx.load_labware('plateone_96_wellplate_2000ul', slot)
+                   for slot in ['5', '6']]
+    trough = ctx.load_labware('nest_12_reservoir_15ml', '8')
+    plate = ctx.load_labware(
+        'corning_96_wellplate_360ul_flat', '9')
+
+    tipracks_300 = [ctx.load_labware('opentrons_96_tiprack_300ul', slot)
+                    for slot in ['3', '10']]
+    tiprack_m300 = ctx.load_labware('opentrons_96_tiprack_300ul', '7')
+
+    # instrument setup
+    m300 = ctx.load_instrument(
+        'p300_multi',
+        mount='left',
+        tip_racks=[tiprack_m300])
+    p300 = ctx.load_instrument(
+        'p300_single',
+        mount='right',
+        tip_racks=tipracks_300)
+
+    # reagent setup
+    tubes = [well for row in tuberack_4.rows() for well in row]
+    water = tubes[0]
+    diluent = tubes[1]
+    standards = tubes[2:2 + number_of_standards]
+    samples = tubes[2 + number_of_standards:]
+    antibody = trough.wells('A1')
+
+    # define elution pool
+    dil_dests = [row for deep_plate in deep_plates
+                 for row in deep_plate.rows()]
+    conc_lists = [[int(cell) for cell in line.split(',') if cell]
+                  for line in concentration_csv.splitlines() if line]
+    concs = [5, 10, 25, 50, 100, 500, 1000, 5000, 10000, 25000, 50000, 100000]
+
+    samples = [
+        row[concs.index(conc)]
+        for concentrations, row in zip(conc_lists, dil_dests)
+        for conc in concentrations
+        ]
+
+    dests = [
+        [plate.columns()[index][num], plate.columns()[index+1][num]]
+        for index in range(0, 12, 2) for num in range(8)
+        ]
+
+    """
+    Adding Enzyme Conjugate Reagent
+    """
+    num_cols = math.ceil((2 + number_of_standards + len(samples)) / 8) * 2
+    m300.distribute(100, antibody,
+                    plate.columns()[:num_cols], blow_out=antibody)
+
+    """
+    Adding Water
+    """
+    p300.distribute(50, water, dests[0])
+    dests.pop(0)
+
+    """
+    Adding Dilution Buffer
+    """
+    p300.distribute(50, diluent, dests[0])
+    dests.pop(0)
+
+    """
+    Adding HCP Standards
+    """
+    for standard in standards:
+        p300.distribute(50, standard, dests[0])
+        dests.pop(0)
+
+    """
+    Adding Samples
+    """
+    for sample in samples:
+        p300.pick_up_tip()
+        p300.aspirate(130, sample)
+        for dest in dests[0]:
+            p300.dispense(50, dest)
+        p300.drop_tip()
+        dests.pop(0)
---
+++ protocols/1464-3/README.md
@@ -0,0 +1,50 @@
+# ELISA Protocol 3/3: Substrate and Stop Solution Addition
+
+### Author
+[Opentrons](http://www.opentrons.com/)
+
+## Categories
+* Proteins & Proteomics
+    * ELISA
+
+## Description
+Links:
+* [Part 1: Dilution](./1464-natrix-separations-inc-milliporesigma-part1)
+* [Part 2-1: Antibody Addition](./1464-natrix-separations-inc-milliporesigma-part2)
+* [Part 2-2: Substrate and Stop Solution Addtion](1464-natrix-separations-inc-milliporesigma-part3)
+
+This is the third part of a ELISA protocol: Substrate and Stop Solution Addition. This should be run immediately after the incubation and wash steps.
+
+Download full protocol details [here](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/1464-natrix-separations-inc-milliporesigma/NEW_ELISA_protocol.xlsx).
+
+---
+
+You will need:
+* P300 Multi-channel Pipette
+* P300 Single-channel Pipette
+* [Opentrons Tube Racks (2 mL Eppendorf)](https://shop.opentrons.com/collections/opentrons-tips/products/tube-rack-set-1)
+* [Opentrons Tube Rack (15 + 50 mL)](https://shop.opentrons.com/collections/opentrons-tips/products/tube-rack-set-1)
+* 96-well Plate
+* 12-row Trough
+* 300 uL Tip Racks
+
+### Robot
+* [OT-2](https://opentrons.com/ot-2)
+
+## Process
+1. Input the number of columns that are currently occupied in the 96-well plate.
+2. Download your protocol.
+3. Upload your protocol into the [OT App](https://opentrons.com/ot-app).
+4. Set up your deck according to the deck map.
+5. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support article](https://support.opentrons.com/ot-2/getting-started-software-setup/deck-calibration).
+6. Hit "Run".
+
+
+### Additional Notes
+![plate](https://s3.amazonaws.com/opentrons-protocol-library-website/custom-README-images/1464-natrix-separations-inc-milliporesigma/plate.png)
+
+If you have any questions about this protocol, please contact protocols@opentrons.com.
+
+###### Internal
+AwOUI09G
+1464
