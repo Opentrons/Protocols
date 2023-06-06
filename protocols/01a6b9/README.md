2be74725405271a8c8db2c9368f21b035a3c3c13
ncdiehl11 <ndiehl@opentrons.com>
2023-06-06 17:52:10 -0400

    stash

---
+++ protocols/01a6b9/README.md
@@ -0,0 +1,55 @@
+# Media Refilling
+
+
+### Author
+[Opentrons](https://opentrons.com/)
+
+
+## Categories
+* Broad Category
+	* Specific Category
+
+
+## Description
+This protocol does stuff!
+
+
+### Labware
+* PhenoPlate 96 Well Plate 425 µL #6055302
+* PhenoPlate 384 Well Plate 145 µL #6057302
+* Opentrons 96 Filter Tip Rack 1000 µL
+* [NEST 1 Well Reservoir 195 mL #360103](http://www.cell-nest.com/page94?_l=en&product_id=102)
+
+
+### Pipettes
+* [Opentrons P1000 Single Channel Electronic Pipette (GEN2)](https://shop.opentrons.com/single-channel-electronic-pipette-p20/)
+
+
+### Deck Setup
+![deck](https://opentrons-protocol-library-website.s3.amazonaws.com/custom-README-images/01a6b9/deck.png)
+
+
+### Reagent Setup
+![reagents](https://opentrons-protocol-library-website.s3.amazonaws.com/custom-README-images/01a6b9/reagents.png)
+
+
+### Protocol Steps
+1. Step 1...
+
+
+### Process
+1. Input your protocol parameters above.
+2. Download your protocol and unzip if needed.
+3. Upload your custom labware to the [OT App](https://opentrons.com/ot-app) by navigating to `More` > `Custom Labware` > `Add Labware`, and selecting your labware files (.json extensions) if needed.
+4. Upload your protocol file (.py extension) to the [OT App](https://opentrons.com/ot-app) in the `Protocol` tab.
+5. Set up your deck according to the deck map.
+6. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support articles](https://support.opentrons.com/en/collections/1559720-guide-for-getting-started-with-the-ot-2).
+7. Hit "Run".
+
+
+### Additional Notes
+If you have any questions about this protocol, please contact the Protocol Development Team by filling out the [Troubleshooting Survey](https://protocol-troubleshooting.paperform.co/).
+
+
+###### Internal
+01a6b9
---
+++ protocols/01a6b9/media_filling.ot2.apiv2.py
@@ -0,0 +1,96 @@
+from opentrons import protocol_api
+
+metadata = {
+    'protocolName': 'Media Refilling',
+    'author': 'Nick Diehl <ndiehl@opentrons.com>',
+    'apiLevel': '2.13'
+}
+
+offset_aspiration_from_bottom = 0.5
+offset_dispense_from_top = -1.0
+
+
+def run(ctx):
+
+    [vol_refill, num_lw, lw_refill, lw_media, type_pip,
+     mount_pip] = get_values(  # noqa:F821
+     'vol_refill', 'num_lw', 'lw_refill', 'lw_media', 'type_pip',
+     'mount_pip')
+
+    tip_vol_map = {
+        20: '20',
+        300: '300',
+        1000: '1000'
+    }
+
+    pipette = ctx.load_instrument(type_pip, mount_pip)
+    tiprack_type = f'opentrons_96_filtertiprack_\
+{tip_vol_map[int(pipette.max_volume)]}ul'
+
+    plates = [
+        ctx.load_labware(lw_refill, slot)
+        for slot in range(1, 1+num_lw)]
+    media = ctx.load_labware(lw_media, '10', 'media reservoir (A1)').wells()[0]
+    waste = ctx.load_labware(
+        lw_media, '11', 'waste reservoir').wells()[0].top()
+
+    grid_size = len(plates[0].rows()) * len(plates[0].columns())
+    if grid_size < 96 and pipette.channels == 8:
+        raise Exception('Incompatible labware with 8-channel pipette.')
+
+    tipracks = [
+        ctx.load_labware(tiprack_type, slot)
+        for slot in range(num_lw+1, 10)]
+    pipette.tip_racks = tipracks
+    if pipette.channels == 8:
+        pipette.starting_tip = pipette.tip_racks[0].rows()[0][1]
+    else:
+        pipette.starting_tip = pipette.tip_racks[0].wells()[1]
+    media_tip = pipette.tip_racks[0].wells()[0]
+
+    def slow_withdraw(well, delay_seconds=2.0, pip=pipette):
+        pip.default_speed /= 16
+        if delay_seconds > 0:
+            ctx.delay(seconds=delay_seconds)
+        pip.move_to(well.top())
+        pip.default_speed *= 16
+
+    def pick_up(pip=pipette):
+        try:
+            pip.pick_up_tip()
+        except protocol_api.labware.OutOfTipsError:
+            ctx.pause("Replace all tipracks")
+            pip.reset_tipracks()
+            if pip.channels == 8:
+                pipette.starting_tip = pip.tip_racks[0].rows()[0][1]
+            else:
+                pipette.starting_tip = pip.tip_racks[0].wells()[1]
+            pip.pick_up_tip()
+
+    if pipette.channels == 8:
+        if grid_size == 96:
+            plate_locs = [well for plate in plates for well in plate.rows()[0]]
+        else:
+            plate_locs = [
+                well for plate in plates
+                for row in plate.rows()[:2]
+                for well in row]
+    else:
+        plate_locs = [well for plate in plates for well in plate.wells()]
+
+    for i, well in enumerate(plate_locs):
+        # remove old volume
+        pick_up()
+        pipette.aspirate(
+            vol_refill, well.bottom(offset_aspiration_from_bottom))
+        slow_withdraw(well)
+        pipette.dispense(vol_refill, waste)
+        pipette.drop_tip()
+
+        # add media
+        pipette.pick_up_tip(media_tip)
+        pipette.aspirate(vol_refill, media.bottom(2))
+        slow_withdraw(media)
+        pipette.dispense(vol_refill, well.top(offset_dispense_from_top))
+        slow_withdraw(well)
+        pipette.return_tip()
