5ce8f0c1a56f5e75df98a2d77e4c61fda4a44792
Parrish Payne <parrish.payne@opentrons.com>
2023-07-06 16:49:30 -0500

    Update protocol steps

--- protocols/9038af/LA County.py
+++ protocols/9038af/LA County.py
@@ -19,47 +19,36 @@ genomic DNA using the Nanopore Rapid Barcoding Kit 96.''',
 
 def run(ctx):
 
-    [input_csv, p20_type, p20_mount, p300_type, p300_mount, source_type,
-     using_tempdeck, dest_type, reservoir_type] = get_values(  # noqa: F821
-        'input_csv', 'p20_type', 'p20_mount', 'p300_type', 'p300_mount',
-        'source_type', 'using_tempdeck', 'dest_type', 'reservoir_type')
+    [input_csv, p20_mount, p300_mount, source_type, 
+        dest_type] = get_values(  # noqa: F821
+        'input_csv', 'p20_mount', 'p300_mount',
+        'source_type', 'dest_type')
 
     # labware
 
-    if using_tempdeck:
-        tempdeck = ctx.load_module('temperature module gen2', '3')
-        tempdeck.set_temperature(4)
-        source_plate = tempdeck.load_labware(source_type, 'source plate')
-
-    else:
-        source_plate = ctx.load_labware(source_type, 2, 'source plate')
-
-    destination_plate = ctx.load_labware(dest_type, '3', 'destination plate')
     
-    water = ctx.load_labware(
-        'opentrons_24_aluminumblock_nest_1.5ml_screwcap', '5',
-        (position A1)').wells()[0].bottom(1)
-
-    barcodes = ctx.load_labware(
-        'opentrons_24_aluminumblock_nest_1.5ml_screwcap', '5',
-        (position B1)').wells()[0].bottom(1)
-
-    beads = ctx.load_labware(
-        'opentrons_24_aluminumblock_nest_1.5ml_screwcap', '5',
-        (position C1)').wells()[0].bottom(1)
+    tempdeck = ctx.load_module('temperature module gen2', '3')
+    destination_plate = tempdeck.load_labware(dest_type, 'normalization plate')
+    sample_rack = ctx.load_labware(source_type, '3', 'genomic dna')
+    
+    tube_rack = ctx.load_labware(
+        'opentrons_24_aluminumblock_nest_1.5ml_screwcap', '5')
+    water = tube_rack.wells()[0]
+    barcodes_plate = ctx.load_labware('opentrons_96_aluminumblock_biorad_wellplate_200ul', '6')
+    beads = tube_rack.wells()[1]
 
     tiprack20 = [
         ctx.load_labware('opentrons_96_tiprack_20ul', slot, '20ul tiprack')
         for slot in ['1']
     ]
     tiprack300 = [
-        ctx.load_labware('opentrons_96_tiprack_300ul', slot, '300ul tiprack')
+        ctx.load_labware('opentrons_96_filtertiprack_200ul', slot, '200ul tiprack')
         for slot in ['4']
     ]
 
     # pipettes
-    p20 = ctx.load_instrument(p20_type, p20_mount, tip_racks=tiprack20)
-    p300 = ctx.load_instrument(p300_type, p300_mount, tip_racks=tiprack300)
+    p20 = ctx.load_instrument('p20_single_gen2', p20_mount, tip_racks=tiprack20)
+    p300 = ctx.load_instrument('p300_single_gen2', p300_mount, tip_racks=tiprack300)
 
     # Helper Functions
     def pick_up(pip):
@@ -79,72 +68,70 @@ def run(ctx):
         [val.strip().upper() for val in line.split(',')]
         for line in input_csv.splitlines()[1:]
         if line and line.split(',')[0]]
+    barcodes = barcodes_plate.wells()[:len(data)]
+
+    target_mass = 50    # ng
+    target_vol = 9      # uL
 
     # perform normalization
-    for line in data:
-        s, d, vol_s, vol_w = line[:4]
-
-        if not vol_w:
-            vol_w = 0
-        else:
-            vol_w = float(vol_w)
-
-        d = destination_plate.wells_by_name()[d]
+    for i, line in enumerate(data):
+        conc = float(line[0])
+        transfer_vol = target_mass/conc
+        s = sample_rack.wells()[i]
+        d = destination_plate.wells()[i]
 
         # pre-transfer diluent
-        pip = p300 if vol_w > 20 else p20
-        if not pip.has_tip:
-            pick_up(pip)
+        pick_up(p20)
+        water_vol = target_vol - transfer_vol
+        pip.apsirate(water_vol, water)
+        pip.aspirate(transfer_vol, s)
+        pip.dispense(p20.current_volume, d)
+        pip.drop_tip()
+    
+    # Add 1 uL of Rapid Barcodes (RB01-96, one for each sample)
+    num_mix = 5
+    mix_vol = 7
+    reactions = destination_plate.wells()[:len(data)]
+    for b, r in zip(barcodes, reactions)
+        pick_up(p20)
+        p20.apirate(1, b.bottom(1), r, new_tip='always')
+        p20.dispense (1, r.bottom(1))
+        p20.mix(num_mix, mix_vol, r.bottom(2))
+        p20.blow_out(r.top(-2))
+        p20.drop_tip()
 
-        pip.transfer(vol_w, water, d.bottom(2), new_tip='never')
+    # Temperature Control
+    temp_mod.set_temperature(celsius=30)
+    temp_mod.status  # 'holding at target'
+    ctx.delay(minutes=2)             # delay for 2 minutes
+    temp_mod.set_temperature(celsius=80)
+    temp_mod.status  # 'holding at target'
+    ctx.delay(minutes=2)             # delay for 2 minutes
+    temp_mod.set_temperature(celsius=4)
+    temp_mod.status  # 'holding at target'
+    ctx.delay(minutes=2)             # delay for 2 minutes
+    temp_mod.deactivate()
+    temp_mod.status  # 'idle'
+
+    # Pool all barcoded samplpes in 1.5 mL tube, note total vol
+    pool = tube_rack.wells()[2]
+    for r in reactions
+        pick_up(p300)
+        pip.apirate(12, b.bottom(1), new_tip='always')
+     
         pip.blow_out(d.top(-2))
+        pip.drop_tip()
 
-    for pip in [p20, p300]:
-        if pip.has_tip:
-            pip.drop_tip()
-
-    # perform normalization
-    for line in data:
-        s, d, vol_s, vol_w = line[:4]
-        if not vol_s:
-            vol_s = 0
-        else:
-            vol_s = float(vol_s)
-
-        s = source_plate.wells_by_name()[s]
-        d = destination_plate.wells_by_name()[d]
-
-        # transfer sample
-        pip = p300 if vol_s > 20 else p20
-        if vol_s != 0:
-            pick_up(pip)
-            pip.transfer(vol_s, s.bottom(3), d, new_tip='never')
-            pip.blow_out(d.top(-2))
-            pip.drop_tip()
-
-        # Add 1 uL of Rapid Barcodes (RB01-96, one for each sample)
-        pip = p20
-            pick_up(pip)
-                pip.transfer(barcodes, s.bottom(3), d, new_tip='always')
-                pip.mix(5, 7, d.bottom(2))
-                pip.blow_out(d.top(-2))
-                pip.drop_tip()
-
-        # Temperature Control
-        temp_mod.set_temperature(celsius=30)
-        temp_mod.status  # 'holding at target'
-        ctx.delay(minutes=2)             # delay for 2 minutes
-        temp_mod.set_temperature(celsius=80)
-        temp_mod.status  # 'holding at target'
-        ctx.delay(minutes=2)             # delay for 2 minutes
-        temp_mod.set_temperature(celsius=4)
-        temp_mod.status  # 'holding at target'
-        ctx.delay(minutes=2)             # delay for 2 minutes
-        temp_mod.deactivate()
-        temp_mod.status  # 'idle'
-
-        # Pool all barcoded samplpes in 1.5 mL tube, note total vol
-
-        # Resuspend beads, add equal vol to pooled barcoded
-
-        # Mix for 5 min at room temp
+    # Resuspend beads, add equal vol to pooled barcoded samples
+    bead_transfer_vol = len(data)*10
+    bead_mix_vol = 150
+    pick_up(p300)
+    pip.mix(10, bead_mix_vol, beads)
+    pip.aspirate(bead_transfer_vol, beads)
+    pip.dispense(bead_transfer_vol, pool)
+ 
+    # Mix for 5 min at room temp
+    num_mix = 10
+    mix_vol = 150
+    pip.mix(10, mix_vol, beads)
+    pip.drop_tip()
