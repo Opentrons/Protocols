6979c5d9c9b09a3a4cb6d75a9db483c5d93b32e9
ncdiehl11 <ndiehl@opentrons.com>
2023-07-18 20:18:44 -0700

    all features

--- protocols/052d03/tc.ot2.apiv2.py
+++ protocols/052d03/tc.ot2.apiv2.py
@@ -8,19 +8,19 @@ metadata = {
     'author': 'Nick Diehl <ndiehl@opentrons.com>'
 }
 
-DO_THERMOCYCLER = False
-DO_FIRST_DILUTION = True
+DO_THERMOCYCLER = True
 
 # 101 - 1 enzymatic reaction (no second rxn mix)
 # 401 - exactly like this
 # pDNA - only second enzymatic rxn
-# (10ul sample neat into column 3 with 90ul rxn mix)
+#    (10ul sample neat into column 3 with 90ul rxn mix)
 
 
 def run(ctx):
 
-    [num_samples, cp_list, type_molecule] = get_values(  # noqa: F821
-        'num_samples', 'cp_list', 'type_molecule')
+    [num_samples, cp_list, type_molecule,
+     type_sample_rack] = get_values(  # noqa: F821
+        'num_samples', 'cp_list', 'type_molecule', 'type_sample_rack')
 
     # [num_samples, cp_list, type_molecule] = [24, cp_list_ex, 'pDNA']
 
@@ -31,7 +31,7 @@ def run(ctx):
         if line and line.split(',')[0].strip()]
 
     num_cols = math.ceil((num_samples+1)/8)
-    tc = ctx.load_module('thermocycler')
+    tc = ctx.load_module('thermocyclerModuleV2')
     tc.open_lid()
     tc_plate = tc.load_labware('biorad_96_wellplate_200ul_pcr')
     tipracks300 = [
@@ -43,8 +43,7 @@ def run(ctx):
     dil_plate = ctx.load_labware('biorad_96_wellplate_200ul_pcr',
                                  '5', 'dilution plate')
     res = ctx.load_labware('nest_12_reservoir_15ml', '2')
-    tuberack = ctx.load_labware(
-        'opentrons_24_aluminumblock_nest_1.5ml_snapcap', '4')
+    tuberack = ctx.load_labware(type_sample_rack, '4')
     mm_plate = ctx.load_labware('biorad_96_aluminumblock_250ul', '1',
                                 'mastermix plate')
 
@@ -60,53 +59,58 @@ def run(ctx):
     diluent = res.wells()[2:4]
     mm = res.wells()[4]
 
-    # # define liquids
-    # rxn_mix_1_liq = ctx.define_liquid(
-    #     name="DNAse",
-    #     description="DNAse",
-    #     display_color="#00FF00",
-    # )
-    # rxn_mix_2_liq = ctx.define_liquid(
-    #     name="PK",
-    #     description="PK",
-    #     display_color="#0000FF",
-    # )
-    # diluent_liq = ctx.define_liquid(
-    #     name="dilution buffer",
-    #     description="dilution buffer",
-    #     display_color="#FF0000",
-    # )
-    # mastermix_liq = ctx.define_liquid(
-    #     name="mastermix",
-    #     description="mastermix",
-    #     display_color="#FBFF00",
-    # )
-    # sample_liq = ctx.define_liquid(
-    #     name="sample",
-    #     description="sample, NTC, and DAC",
-    #     display_color="#F300FF",
-    # )
-    # pc_liq = ctx.define_liquid(
-    #     name="positive control",
-    #     description="positive control",
-    #     display_color="#050F5D",
-    # )
+    if type_molecule == '401':
+        vol_rxn_mix_2 = 50.0
+    elif type_molecule == 'pDNA':
+        vol_rxn_mix_2 = 90.0
 
-    # # load liquids
-    # [s.load_liquid(sample_liq, volume=200) for s in samples]
-    # pc.load_liquid(pc_liq, volume=200)
-    # if not type_molecule == 'pDNA':
-    #     rxn_mix_1.load_liquid(rxn_mix_1_liq, volume=30*num_cols*8)
-    # if not type_molecule == '101':
-    #     rxn_mix_2.load_liquid(rxn_mix_2_liq, volume=50*num_cols*8)
-    # mm.load_liquid(mastermix_liq, volume=16.5*len(data)*4)
-    # [d.load_liquid(diluent_liq, volume=13500) for d in diluent]
+    # define liquids
+    rxn_mix_1_liq = ctx.define_liquid(
+        name='DNAse',
+        description='DNAse',
+        display_color='#00FF00',
+    )
+    rxn_mix_2_liq = ctx.define_liquid(
+        name='PK',
+        description='PK',
+        display_color='#0000FF',
+    )
+    diluent_liq = ctx.define_liquid(
+        name='dilution buffer',
+        description='dilution buffer',
+        display_color='#FF0000',
+    )
+    mastermix_liq = ctx.define_liquid(
+        name='mastermix',
+        description='mastermix',
+        display_color='#FBFF00',
+    )
+    sample_liq = ctx.define_liquid(
+        name='sample',
+        description='sample, NTC, and DAC',
+        display_color='#F300FF',
+    )
+    pc_liq = ctx.define_liquid(
+        name='positive control',
+        description='positive control',
+        display_color='#050F5D',
+    )
+
+    # load liquids
+    [s.load_liquid(sample_liq, volume=200) for s in samples]
+    pc.load_liquid(pc_liq, volume=200)
+    if not type_molecule == 'pDNA':
+        rxn_mix_1.load_liquid(rxn_mix_1_liq, volume=30*num_cols*8)
+    if not type_molecule == '101':
+        rxn_mix_2.load_liquid(rxn_mix_2_liq, volume=vol_rxn_mix_2*num_cols*8)
+    mm.load_liquid(mastermix_liq, volume=16.5*len(data)*4)
+    [d.load_liquid(diluent_liq, volume=13500) for d in diluent]
 
     def pick_up(pip):
         try:
             pip.pick_up_tip()
         except protocol_api.labware.OutOfTipsError:
-            ctx.pause("Replace the tips")
+            ctx.pause('Replace the tips')
             pip.reset_tipracks()
             pip.pick_up_tip()
 
@@ -137,72 +141,99 @@ def run(ctx):
         pip.move_to(well.top())
         pip.default_speed *= 16
 
-    # first dilution
-    first_dil_dests_m = [tc_plate.rows()[0][i*4] for i in range(num_cols)]
-    pick_up(m300)
-    vol_dil = 180
-    for d in first_dil_dests_m:
-        source = track_dilution(vol_dil)
-        m300.aspirate(vol_dil, source)
-        slow_withdraw(m300, source)
-        m300.dispense(vol_dil, d.bottom(5))
-        slow_withdraw(m300, d)
-    m300.drop_tip()
-
     rxn_mix_1_dests = [tc_plate.rows()[0][i*4+1] for i in range(num_cols)]
-    pick_up(m300)
-    for d in rxn_mix_1_dests:
-        m300.aspirate(30, rxn_mix_1)
-        slow_withdraw(m300, rxn_mix_1)
-        m300.dispense(30, d.bottom(2))
-        slow_withdraw(m300, d)
-    m300.drop_tip()
-
-    # transfer sample
-    first_dil_cols = [tc_plate.columns()[i*4] for i in range(num_cols)]
-    first_dil_dests_s = [
-        well for col in first_dil_cols for well in col][:num_samples]
-    for s, d in zip(samples, first_dil_dests_s):
-        pick_up(p20)
-        p20.aspirate(20, s.bottom(0.5))
-        slow_withdraw(p20, s)
-        p20.dispense(20, d.top(-5))
-        # p20.mix(5, 20, d.bottom(d.depth/2))
-        slow_withdraw(p20, d)
-        p20.drop_tip()
-
-    # add to mix
-    for s, d in zip(first_dil_dests_m, rxn_mix_1_dests):
+    if not type_molecule == 'pDNA':
+        # first dilution
+        first_dil_dests_m = [tc_plate.rows()[0][i*4] for i in range(num_cols)]
         pick_up(m300)
-        m300.mix(5, 100, s.bottom(s.depth/2))
-        m300.aspirate(20, s.bottom(5))
-        slow_withdraw(m300, s)
-        m300.dispense(20, d.bottom(2))
-        m300.mix(5, 20, d.bottom(d.depth/2))
-        slow_withdraw(m300, d)
+        vol_dil = 180
+        for d in first_dil_dests_m:
+            source = track_dilution(vol_dil)
+            m300.aspirate(vol_dil, source)
+            slow_withdraw(m300, source)
+            m300.dispense(vol_dil, d.bottom(5))
+            slow_withdraw(m300, d)
         m300.drop_tip()
 
-    tc.close_lid()
-    if DO_THERMOCYCLER:
-        tc.set_block_temperature(37, hold_time_minutes=30)
-    tc.open_lid()
-
-    # rxn mix 2
-    for d in rxn_mix_1_dests:
         pick_up(m300)
-        m300.aspirate(50, rxn_mix_2)
-        slow_withdraw(m300, rxn_mix_2)
-        m300.dispense(50, d.bottom(2))
-        m300.mix(5, 20, d.bottom(d.depth/2))
-        slow_withdraw(m300, d)
+        for d in rxn_mix_1_dests:
+            m300.aspirate(30, rxn_mix_1)
+            slow_withdraw(m300, rxn_mix_1)
+            m300.dispense(30, d.bottom(2))
+            slow_withdraw(m300, d)
         m300.drop_tip()
 
-    tc.close_lid()
-    if DO_THERMOCYCLER:
-        tc.set_block_temperature(55, hold_time_minutes=30)
-        tc.set_block_temperature(95, hold_time_minutes=15)
-        tc.set_block_temperature(4)
-    tc.open_lid()
+        # transfer sample
+        first_dil_cols = [tc_plate.columns()[i*4] for i in range(num_cols)]
+        first_dil_dests_s = [
+            well for col in first_dil_cols for well in col][:num_samples]
+        for s, d in zip(samples, first_dil_dests_s):
+            pick_up(p20)
+            p20.aspirate(20, s.bottom(0.5))
+            slow_withdraw(p20, s)
+            p20.dispense(20, d.top(-5))
+            # p20.mix(5, 20, d.bottom(d.depth/2))
+            slow_withdraw(p20, d)
+            p20.drop_tip()
+
+        # add to mix
+        for s, d in zip(first_dil_dests_m, rxn_mix_1_dests):
+            pick_up(m300)
+            m300.mix(5, 100, s.bottom(s.depth/2))
+            m300.aspirate(20, s.bottom(5))
+            slow_withdraw(m300, s)
+            m300.dispense(20, d.bottom(2))
+            m300.mix(5, 20, d.bottom(d.depth/2))
+            slow_withdraw(m300, d)
+            m300.drop_tip()
+
+        tc.close_lid()
+        if DO_THERMOCYCLER:
+            tc.set_block_temperature(37, hold_time_minutes=30)
+        tc.open_lid()
+
+    else:
+        # rxn mix 2
+        for d in rxn_mix_1_dests:
+
+            pick_up(m300)
+            m300.aspirate(vol_rxn_mix_2, rxn_mix_2)
+            slow_withdraw(m300, rxn_mix_2)
+            m300.dispense(vol_rxn_mix_2, d.bottom(2))
+            # m300.mix(5, 20, d.bottom(d.depth/2))
+            slow_withdraw(m300, d)
+            m300.drop_tip()
+        rxn_mix_1_columns = [
+            tc_plate.columns()[tc_plate.rows()[0].index(col)]
+            for col in rxn_mix_1_dests]
+        rxn_mix_1_s = [
+            well for col in rxn_mix_1_columns for well in col][:num_samples]
+        for s, d in zip(samples, rxn_mix_1_s):
+            pick_up(p20)
+            p20.aspirate(10, s.bottom(0.5))
+            slow_withdraw(p20, s)
+            p20.dispense(10, d.top(-5))
+            p20.mix(5, 20, d.bottom(d.depth/2))
+            slow_withdraw(p20, d)
+            p20.drop_tip()
+
+    if not type_molecule == '101':
+        if type_molecule == '401':  # rxn mix 2
+            for d in rxn_mix_1_dests:
+                pick_up(m300)
+                m300.aspirate(50, rxn_mix_2)
+                slow_withdraw(m300, rxn_mix_2)
+                m300.dispense(50, d.bottom(2))
+                m300.mix(5, 20, d.bottom(d.depth/2))
+                slow_withdraw(m300, d)
+                m300.drop_tip()
+
+        tc.close_lid()
+        if DO_THERMOCYCLER:
+            tc.set_block_temperature(55, hold_time_minutes=30)
+            tc.set_block_temperature(95, hold_time_minutes=15)
+            tc.set_block_temperature(4)
+        tc.open_lid()
 
     dil_sets_tc = [
         tc_plate.rows()[0][i*4+2:i*4+4] for i in range(num_cols)
