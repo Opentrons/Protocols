a7863b26b41d795318a1748e02b5964a938df5fa
“ramifarawi” <rami.farawi@opentrons.com>
2023-08-03 14:10:27 -0400

    fix

---
+++ protocols/6af807-amendment/6af807.ot2.apiv2.py
@@ -0,0 +1,123 @@
+"""Protocol."""
+import os
+import csv
+
+
+metadata = {
+    'protocolName': '384 Well Plate PCR Plate with Triplicates',
+    'author': 'Rami Farawi <rami.farawi@opentrons.com>',
+    'source': 'Custom Protocol Request',
+    'apiLevel': '2.11'
+}
+
+
+def run(ctx):
+    """Protocol."""
+    [num_gene, num_mastermix,
+        cdna_vol,
+        reset_tipracks, p20_mount] = get_values(  # noqa: F821
+        "num_gene", "num_mastermix",
+        "cdna_vol",
+            "reset_tipracks", "p20_mount")
+
+    mmx_vol = 20-cdna_vol
+
+    if not 1 <= num_mastermix <= 16:
+        raise Exception("Enter a number of mastermixes between 1-16")
+    if not 1 <= num_gene <= 8:
+        raise Exception("Enter a number of cDNA 1-8")
+
+    # Tip tracking between runs
+    if not ctx.is_simulating():
+        file_path = '/data/csv/tiptracking.csv'
+        file_dir = os.path.dirname(file_path)
+        # check for file directory
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        # check for file; if not there, create initial tip count tracking
+        if not os.path.isfile(file_path):
+            with open(file_path, 'w') as outfile:
+                outfile.write("0, 0\n")
+
+    tip_count_list = []
+    if ctx.is_simulating():
+        tip_count_list = [0]
+    elif reset_tipracks:
+        tip_count_list = [0]
+    else:
+        with open(file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            tip_count_list = next(csv_reader)
+
+    tip_counter = int(tip_count_list[0])
+
+    # load labware
+    plate = ctx.load_labware('100ul_384_wellplate_100ul', '1')
+    mastermix = ctx.load_labware(
+                'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '3')
+    cDNA = ctx.load_labware(
+                'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '2')
+    tiprack = [ctx.load_labware('opentrons_96_tiprack_20ul', slot)
+               for slot in [4, 5, 6, 7]]
+
+    # load instrument
+    p20 = ctx.load_instrument('p20_single_gen2',
+                              p20_mount, tip_racks=tiprack)
+
+    # ctx
+    cDNA_tubes = cDNA.wells()[:num_gene]
+
+    # remove P-row
+    chopped_plate = [plate.columns()[i][:num_mastermix]
+                     for i in range(0, len(plate.columns()))]
+
+    chunked_columns = [chopped_plate[i:i+3] for i in range(
+                      0, len(chopped_plate), 3)]
+
+    # print(chunked_columns)
+
+    # chunk down the column by triplicates
+    dispense_wells = [[] for _ in range(24)]
+    for i, chunked_column in enumerate(chunked_columns):
+        for j in range(0, 24, num_mastermix):
+            for col in chunked_column:
+                dispense_wells[i].append(col[j:j+num_mastermix])
+
+    # concatanate sublists for each tube
+    final_dispense_wells = []
+    ctr = 0
+    for list in dispense_wells:
+        x = []
+        for i, sublist in enumerate(list):
+            x += sublist
+            ctr += 1
+            if ctr == 3:
+                final_dispense_wells.append([x])
+                x = []
+                ctr = 0
+
+    # remove empty brackets
+    final_dispense_wells = [x for x in final_dispense_wells if x != [[]]]
+
+    airgap = 2
+    for tube, chunk in zip(cDNA_tubes, final_dispense_wells):
+        p20.pick_up_tip()
+        for small_chunk in chunk:
+            for well in small_chunk:
+                p20.aspirate(cdna_vol, tube)
+                # p20.air_gap(airgap)
+                p20.dispense(cdna_vol+airgap, well)
+                # p20.blow_out()
+        p20.drop_tip()
+        ctx.comment('\n\n')
+    ctx.comment('\n\n\n\n\n\n\n')
+
+    for row, tube in zip(plate.rows()[:num_mastermix], mastermix.wells()):
+        for well in row[:num_gene*3]:
+            p20.pick_up_tip()
+            p20.aspirate(mmx_vol, tube, rate=0.5)
+            p20.dispense(mmx_vol, well, rate=0.5)
+            p20.blow_out(well.top())
+            p20.touch_tip()
+            p20.drop_tip()
+        ctx.comment('\n')
---
+++ protocols/6af807-amendment/README.md
@@ -0,0 +1,61 @@
+# 384 Well Plate PCR Plate with Triplicates
+
+### Author
+[Opentrons](https://opentrons.com/)
+
+## Categories
+* PCR
+	* PCR Prep
+
+## Description
+This protocol preps a 384 well plate for up to 8 cDNA samples in triplicates. The protocol can be broken down into 2 main parts:
+
+* cDNA is added in triplicates down the column onto the plate.
+* Mastermix is added to the cDNA
+
+Explanation of complex parameters below:
+* `Number of cDNA`: Specify the number of cDNA tubes that will be loaded onto the tube rack.
+* `Number of mastermix tubes`: Specify the number of mastermix tubes that will be added to the cDNA.
+* `cDNA/mastermix volume`: Specify the volume of both the cDNA and mastermix. Note, the mastermix volume will be calculated from this volume (total 20ul).
+* `Reset Tipracks?`: The OT-2 will track tips from run to run of the protocol (e.g. tip leaves off in H11 at the end of protocol 1; first tip pick up will be from H12 in Part 2). When tips run out for any particular run, the user will be prompted to replace all tip racks. The user will also have the option to "Reset Tip Racks" in the customized parameters section. Reset tip racks to pick up from A1 of the first tip rack of any one protocol.
+* `P20 Mount`: Specify the mount (left or right) for the P20 single channel pipette.
+
+---
+
+### Labware
+* [Thermofisher 384 well plate](https://www.thermofisher.com/order/catalog/product/4309849#/4309849)
+* [Opentrons 24 tube rack with 1.5mL Eppendorf tubes](opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap)
+* [Opentrons 20ul Tips](opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap)
+
+### Pipettes
+* [P20 Single Channel Pipette](https://opentrons.com/pipettes/)
+
+---
+
+### Deck Setup
+* Note: Mastermixes and cDNA tubes should be placed on their respective tube racks by column. Please look at the deck layout below for a full run (8 genes and 16 mastermixes).
+
+
+![deck layout](https://opentrons-protocol-library-website.s3.amazonaws.com/custom-README-images/6af807/Screen+Shot+2023-08-03+at+2.05.09+PM.png)
+
+
+---
+
+### Protocol Steps
+1. cDNA is transferred in triplicates down a column up to the number of columns as specified in number of mastermixes.
+2. Mastermixes are added to each column up to each populated row.
+
+### Process
+1. Input your protocol parameters above.
+2. Download your protocol and unzip if needed.
+3. Upload your custom labware to the [OT App](https://opentrons.com/ot-app) by navigating to `More` > `Custom Labware` > `Add Labware`, and selecting your labware files (.json extensions) if needed.
+4. Upload your protocol file (.py extension) to the [OT App](https://opentrons.com/ot-app) in the `Protocol` tab.
+5. Set up your deck according to the deck map.
+6. Calibrate your labware, tiprack and pipette using the OT App. For calibration tips, check out our [support articles](https://support.opentrons.com/en/collections/1559720-guide-for-getting-started-with-the-ot-2).
+7. Hit 'Run'.
+
+### Additional Notes
+If you have any questions about this protocol, please contact the Protocol Development Team by filling out the [Troubleshooting Survey](https://protocol-troubleshooting.paperform.co/).
+
+###### Internal
+6af807
