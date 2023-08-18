b60cc474ef1ecba22373c8e4b29b5758f77c0869
“ramifarawi” <rami.farawi@opentrons.com>
2023-08-18 10:37:36 -0400

    fix

--- protocols/onsite-circlelabs/onsite-circlelabs.ot2.apiv2.py
+++ protocols/onsite-circlelabs/onsite-circlelabs.ot2.apiv2.py
@@ -2,19 +2,22 @@ import math
 from opentrons import types
 
 metadata = {
-    'protocolName': 'Tagmentation and Bead Cleanup',
-    'author': 'Rami Farawi <rami.farawi@opentrons.com>',
-    'source': 'Custom Protocol Request',
-    'apiLevel': '2.13'
+    "protocolName": "Tagmentation and Bead Cleanup",
+    "author": "Rami Farawi <rami.farawi@opentrons.com>",
+    "source": "Custom Protocol Request",
+    "apiLevel": "2.13"
 }
 
 
 def run(ctx):
 
     [num_samp, barcode_sample_plate, water_vol,
-        barcode_vol, temp_mod_temp] = get_values(  # noqa: F821
+        barcode_vol, p300_mount, m20_mount] = get_values(  # noqa: F821
         "num_samp", "barcode_sample_plate",
-        "water_vol", "barcode_vol", "temp_mod_temp")
+        "water_vol", "barcode_vol", "p300_mount",
+            "m20_mount")
+
+    num_samp = int(num_samp)
 
     if barcode_sample_plate == "fisher_96_wellplate_200ul":
         temp_mod_plate = "fisher_96_aluminumblock_200ul"
@@ -26,25 +29,24 @@ def run(ctx):
         temp_mod_plate = "opentrons_96_aluminumblock_biorad_wellplate_200ul"
 
     # labware
-    mag_mod = ctx.load_module('magnetic module gen2', 4)
-    mag_plate = mag_mod.load_labware('nest_96_wellplate_2ml_deep')
-    temp_mod = ctx.load_module('temperature module gen2', 6)
-    res = ctx.load_labware('nest_12_reservoir_15ml', 2)
+    mag_mod = ctx.load_module("magnetic module gen2", 4)
+    mag_plate = mag_mod.load_labware("nest_96_wellplate_2ml_deep")
+    temp_mod = ctx.load_module("temperature module gen2", 6)
+    res = ctx.load_labware("nest_12_reservoir_15ml", 2)
     sample_plate = ctx.load_labware(barcode_sample_plate, 3)
     dest_plate = temp_mod.load_labware(temp_mod_plate, 6)
     barcode_plate = ctx.load_labware(barcode_sample_plate, 1)
-    reag_rack = ctx.load_labware('opentrons_24_aluminumblock_nest_2ml_snapcap', 5)  # noqa: E501
+    reag_rack = ctx.load_labware("opentrons_24_aluminumblock_nest_2ml_snapcap", 5)  # noqa: E501
 
-    tips300 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
+    tips300 = [ctx.load_labware("opentrons_96_filtertiprack_200ul", slot)
                for slot in [8]]
-    tips20 = [ctx.load_labware('opentrons_96_tiprack_20ul', slot)
+    tips20 = [ctx.load_labware("opentrons_96_tiprack_20ul", slot)
               for slot in [9, 10, 11]]
 
-    temp_mod.set_temperature(temp_mod_temp)
-
     # pipettes
-    m20 = ctx.load_instrument('p20_multi_gen2', 'right', tip_racks=tips20)
-    p300 = ctx.load_instrument('p300_single_gen2', 'left', tip_racks=tips300)
+    m20 = ctx.load_instrument("p20_multi_gen2", m20_mount, tip_racks=tips20)
+    p300 = ctx.load_instrument("p300_single_gen2", p300_mount,
+                               tip_racks=tips300)
 
     p300.flow_rate.blow_out = 0.33*p300.flow_rate.blow_out
     m20.flow_rate.blow_out = 0.33*m20.flow_rate.blow_out
@@ -52,10 +54,10 @@ def run(ctx):
     # functions
     # apply speed limit to departing tip
     def slow_tip_withdrawal(pipette, well_location, to_center=False):
-        if pipette.mount == 'right':
-            axis = 'A'
+        if pipette.mount == "right":
+            axis = "A"
         else:
-            axis = 'Z'
+            axis = "Z"
         ctx.max_speeds[axis] = 10
         if to_center is False:
             pipette.move_to(well_location.top())
@@ -65,7 +67,7 @@ def run(ctx):
 
     def resuspend_pellet(well, pip, mvol, reps=5):
         """
-        'resuspend_pellet' will forcefully dispense liquid over the pellet
+        "resuspend_pellet" will forcefully dispense liquid over the pellet
         after the magdeck engage in order to more thoroughly resuspend the
         pellet. param well: The current well that the resuspension will occur
         in. param pip: The pipet that is currently attached/ being used.
@@ -76,12 +78,12 @@ def run(ctx):
         dispensing at the bottom (5 mixes total)
         """
 
-        rightLeft = int(str(well).split(' ')[0][1:]) % 2
+        rightLeft = int(str(well).split(" ")[0][1:]) % 2
         """
-        'rightLeft' will determine which value to use in the list of 'top' and
-        'bottom' (below), based on the column of the 'well' used.
-        In the case that an Even column is used, the first value of 'top' and
-        'bottom' will be used, otherwise, the second value of each will be
+        "rightLeft" will determine which value to use in the list of "top" and
+        "bottom" (below), based on the column of the "well" used.
+        In the case that an Even column is used, the first value of "top" and
+        "bottom" will be used, otherwise, the second value of each will be
         used.
         """
         center = well.bottom().move(types.Point(x=0, y=0, z=0.8))
@@ -123,14 +125,14 @@ def run(ctx):
     ethanol = res.wells()[1]
 
     # protocol
-    ctx.comment('\n---------------ADDING WATER----------------\n\n')
+    ctx.comment("\n---------------ADDING WATER----------------\n\n")
     m20.pick_up_tip()
     for col in dest_cols:
         m20.aspirate(water_vol, water)
         m20.dispense(water_vol, col)
     m20.return_tip()
 
-    ctx.comment('\n---------------ADDING BARCODE----------------\n\n')
+    ctx.comment("\n---------------ADDING BARCODE----------------\n\n")
     # CHANGE MIX ########################################################
 
     for s, d in zip(barcode_wells, dest_cols):
@@ -146,7 +148,7 @@ def run(ctx):
 
         m20.return_tip()
     #
-    ctx.comment('\n---------------ADDING DNA----------------\n\n')
+    ctx.comment("\n---------------ADDING DNA----------------\n\n")
 
     for s, d in zip(dna_samples, dest_cols):
         m20.pick_up_tip()
@@ -158,9 +160,9 @@ def run(ctx):
         m20.blow_out(d.top())
         m20.return_tip()
 
-    ctx.pause('Move plate to thermocycler then bring back on slot 6.')
+    ctx.pause("Move plate to thermocycler then bring back on slot 6.")
 
-    ctx.comment('\n---------------POOLING INTO COL----------------\n\n')
+    ctx.comment("\n---------------POOLING INTO COL----------------\n\n")
     pool_col = mag_plate.rows()[0][0]
     m20.pick_up_tip()  # do we change tip here or keep tip
     for col in dest_cols:
@@ -168,7 +170,7 @@ def run(ctx):
         m20.dispense(11, pool_col)
     m20.return_tip()
 
-    ctx.comment('\n---------------POOLING INTO WELLS----------------\n\n')
+    ctx.comment("\n---------------POOLING INTO WELLS----------------\n\n")
     p300.pick_up_tip()
     for well in mag_plate.columns()[0][2:5]:
 
@@ -185,10 +187,10 @@ def run(ctx):
         p300.blow_out(pool_wells[1].bottom(7))
     p300.return_tip()
 
-    ctx.comment('\n---------------ADDING BEADS----------------\n\n')
+    ctx.comment("\n---------------ADDING BEADS----------------\n\n")
     p300.pick_up_tip()
     p300.mix(15, 200, beads, rate=1.5)
-    ctx.comment('\n')
+    ctx.comment("\n")
     for i, pool_well in enumerate(pool_wells):
         if not p300.has_tip:
             p300.pick_up_tip()
@@ -203,7 +205,7 @@ def run(ctx):
         p300.dispense(leftover_vol, pool_well)
         resuspend_pellet(pool_well, p300, 200, reps=8)
         p300.return_tip()
-        ctx.comment('\n\n')
+        ctx.comment("\n\n")
 
     ctx.delay(minutes=5)
 
@@ -247,7 +249,7 @@ def run(ctx):
     if not p300.has_tip:
         p300.pick_up_tip()
 
-    ctx.comment('\n---------------TWO WASHES----------------\n\n')
+    ctx.comment("\n---------------TWO WASHES----------------\n\n")
     for i in range(2):
         if not p300.has_tip:
             p300.pick_up_tip()
@@ -258,13 +260,13 @@ def run(ctx):
                 p300.move_to(pool_well.top())
                 ctx.delay(seconds=4)
                 p300.blow_out()
-        ctx.comment('\n')
+        ctx.comment("\n")
 
         for pool_well in pool_wells:
             resuspend_pellet(pool_well, p300, 200, reps=8)
             p300.mix(15, 200,
                      pool_well.bottom(z=1), rate=1.5)
-        ctx.comment('\n')
+        ctx.comment("\n")
 
         p300.move_to(pool_well.top())
 
@@ -291,7 +293,7 @@ def run(ctx):
     p300.move_to(pool_wells[0].top())
     mag_mod.engage()
     ctx.delay(seconds=7*60)
-    ctx.pause('Samples are all consolidated with ethanol in A1')
+    ctx.pause("Samples are all consolidated with ethanol in A1")
 
     for _ in range(2):
         p300.aspirate(200, pool_wells[0].bottom(z=0.6), rate=0.1)
